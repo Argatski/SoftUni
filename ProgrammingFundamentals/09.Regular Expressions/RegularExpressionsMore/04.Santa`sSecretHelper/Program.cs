9d2bd3ab016cfbabed6fc4428ae145ba7d389688
Denis Argatski <denis.argatski@gmail.com>
2021-01-31 21:56:44 +0200

    "Regular More Expressions "

---
+++ ProgrammingFundamentals/09.Regular Expressions/RegularExpressionsMore/04.Santa`sSecretHelper/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _04.Santa_sSecretHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int key = int.Parse(Console.ReadLine());
+
+            string messages = string.Empty;
+
+
+            while ((messages = Console.ReadLine())!="end")
+            {
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i < messages.Length; i++)
+                {
+                    char current = (char)(messages[i] - key);
+                    sb.Append(current);
+                }
+
+                string decodeText= sb.ToString();
+
+                string pattern = @"(@)(?<name>[A-Za-z]*)(?<sep>[^@\-!:>]*)(?<mark>[!])(?<type>[GN])(\k<mark>)";
+
+                Regex rgx = new Regex(pattern);
+
+                Match match = rgx.Match(decodeText);
+
+                string name = match.Groups["name"].Value;
+                string type = match.Groups["type"].Value;
+
+                if (type=="G")
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
