88693baa541c8856b88ecb40c9f1923d109809c2
Denis Argatski <denis.argatski@gmail.com>
2021-02-11 22:54:09 +0200

    "FinalExam/01.FinalExam/01"

---
+++ ProgrammingFundamentals/FinalExam/01.FinalExam/01.TheImitationGame/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace _01.TheImitationGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input message
+            string message = Console.ReadLine();
+
+            //Processing
+            message = Processing(message);
+
+            Console.WriteLine($"The decrypted message is: {message}");
+
+        }
+
+        static string Processing(string message)
+        {
+            string arg = string.Empty;
+
+            while ((arg = Console.ReadLine()) != "Decode")
+            {
+                string[] instructions = arg.Split('|');
+
+                string command = instructions[0];
+
+                switch (command)
+                {
+                    case "Move":
+                        message = MoveWord(message, instructions);
+                        break;
+                    case "Insert":
+                        message = InsertWord(message, instructions);
+                        break;
+                    case "ChangeAll":
+                        message = ChangeAllWord(message, instructions);
+                        break;
+                }
+            }
+            return message;
+        }
+
+        static string ChangeAllWord(string message, string[] instructions)
+        {
+            string substring = instructions[1];
+            string replacement = instructions[2];
+            message = message.Replace(substring, replacement);
+
+            return message;
+        }
+
+        static string InsertWord(string message, string[] instructions)
+        {
+            int index = int.Parse(instructions[1]);
+            string word = instructions[2];
+
+            message = message.Insert(index, word);
+
+            return message;
+        }
+
+        static string MoveWord(string message, string[] instructions)
+        {
+            int lenght = int.Parse(instructions[1]);
+            string word = message.Substring(0, lenght);
+
+            message = message.Remove(0, lenght);
+            message += word;
+
+            return message;
+        }
+    }
+}
---
+++ ProgrammingFundamentals/FinalExam/01.FinalExam/02.AdAstra/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02.AdAstra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input text
+            string text = Console.ReadLine();
+
+            //Procesing
+            Proceessing(text);
+        }
+
+        static void Proceessing(string text)
+        {
+            //Pattern
+            string pattern = @"([|]|[#]{1,2})(?<food>[A-Za-z ]+)(\1)(?<data>[\d]{2}[\/][\d]{2}[\/][\d]{2})\1(?<calories>[\d]+)\1";
+
+            Regex rgx = new Regex(pattern);
+
+            int dailyCalories = 2000;
+
+            //Matches all word in text with pattern
+            MatchCollection matches = rgx.Matches(text);
+
+            int totalCalories = 0;
+
+            foreach (Match product in matches)
+            {
+                totalCalories += int.Parse(product.Groups["calories"].Value);
+            }
+
+            int days = totalCalories / dailyCalories;
+
+            Console.WriteLine($"You have food to last you for: {days} days!");
+
+            foreach (Match product in matches)
+            {
+                Console.WriteLine($"Item: {product.Groups["food"].Value}, Best before: {product.Groups["data"].Value}, Nutrition: {product.Groups["calories"].Value}");
+            }
+        }
+    }
+}
