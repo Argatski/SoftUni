e95eb25c853ef66bb95a72ea8b7cf44c3f9c5d2d
Denis Argatski <denis.argatski@gmail.com>
2021-02-11 22:46:06 +0200

    "Mid Exam/01.MidExam-12.08.2020/01"

---
+++ ProgrammingFundamentals/Mid Exam/01.MidExam-12.08.2020/01.ComputerStore/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace _01.ComputerStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            string price = string.Empty;
+            double priceTaxes = 0;
+            double taxes = 0;
+            double sum = 0;
+            double totalPrices = 0;
+            double additional = 0;
+            int count = 0;
+            
+
+            while (true)
+            {
+                price = Console.ReadLine();
+                
+                if (price == "special")
+                {
+                    taxes = priceTaxes * 0.2;
+                    sum = priceTaxes + taxes;
+                    additional = sum * 0.1;
+                    totalPrices = sum - additional;
+
+                    if (totalPrices == 0)
+                    {
+                        Console.WriteLine("Invalid order!");
+                        return;
+                    }
+
+                    break;
+                }
+                if (price == "regular")
+                {
+                    taxes = priceTaxes * 0.2;
+                    sum = priceTaxes + taxes;
+                    totalPrices = sum;
+
+                    if (totalPrices == 0)
+                    {
+                        Console.WriteLine("Invalid order!");
+                        return;
+                    }
+
+                    break;
+                }
+
+
+                double num = double.Parse(price);
+               
+
+                if (num < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                    continue;
+                }
+                
+
+                priceTaxes += num;
+            }
+
+            //Output
+            Console.WriteLine("Congratulations you've just bought a new computer!");
+            Console.WriteLine($"Price without taxes: {priceTaxes:f2}$");
+            Console.WriteLine($"Taxes: {taxes:f2}$");
+            Console.WriteLine("-----------");
+            Console.WriteLine($"Total price: {totalPrices:f2}$");
+        }
+    }
+}
---
+++ ProgrammingFundamentals/Mid Exam/01.MidExam-12.08.2020/03.MemoryGame/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace _03.MemoryGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            List<string> elements = Console.ReadLine().Split(" ").ToList();
+
+            //Solution 
+            MatchesElement(elements);
+        }
+
+        static void MatchesElement(List<string> e)
+        {
+            int count = 0;
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split().ToArray();
+
+                if (command[0] == "end" && e.Count > 1)
+                {
+                    Console.WriteLine("Sorry you lose :(");
+                    Console.WriteLine(string.Join(" ", e));
+                    break;
+                }
+                int firstIndex = int.Parse(command[0]);
+                int secondIndex = int.Parse(command[1]);
+                bool sameIndex = firstIndex == secondIndex;
+
+                if (firstIndex >= e.Count || firstIndex < 0 || sameIndex)
+                {
+                    count++;
+                    string added = "-" + count + "a";
+                    int index = e.Count / 2;
+                    e.Insert(index, added);
+                    e.Insert(index, added);
+                    Console.WriteLine("Invalid input! Adding additional elements to the board");
+                }
+                else if (secondIndex >= e.Count || secondIndex < 0)
+                {
+                    count++;
+                    string current = "-"+count + "a";
+                    int index = e.Count / 2;
+                    e.Insert(index, current);
+                    e.Insert(index, current);
+
+                    Console.WriteLine("Invalid input! Adding additional elements to the board");
+                }
+                else
+                {
+
+                    if (e[firstIndex] == e[secondIndex])
+                    {
+                        string currentNum = e[firstIndex];
+
+                        int max = Math.Max(firstIndex, secondIndex);
+                        int min = Math.Min(firstIndex, secondIndex);
+
+                        e.RemoveAt(max);
+                        e.RemoveAt(min);
+
+                        count++;
+                        Console.WriteLine($"Congrats! You have found matching elements - {currentNum}!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Try again!");
+                    }
+                }
+
+
+
+                if (e.Count == 0 || e.Count==1)
+                {
+
+                    Console.WriteLine($"You have won in {count} turns!");
+                    break;
+                }
+
+            }
+        }
+    }
+}
