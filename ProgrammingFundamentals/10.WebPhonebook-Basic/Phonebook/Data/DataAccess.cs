c4d454ba4207c1d7b88350c0234452bfa8ebc1d0
Denis Argatski <denis.argatski@gmail.com>
2021-02-11 22:42:20 +0200

    "Simple Web Phonebook "

---
+++ ProgrammingFundamentals/10.WebPhonebook-Basic/Phonebook/Data/DataAccess.cs
@@ -0,0 +1,10 @@
+﻿using Phonebook.Data.Models;
+using System.Collections.Generic;
+
+namespace Phonebook.Data
+{
+    public class DataAccess
+    {
+        public static new List<Contact> Contacts { get; set; } = new List<Contact>();
+    }
+}
---
+++ ProgrammingFundamentals/10.WebPhonebook-Basic/Phonebook/Data/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Phonebook.Data.Models
+{
+    public class Contact
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string  Number { get; set; }
+    }
+}
