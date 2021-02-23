6c920c6e7f1116bef104b614efb9da5515c9a495
Denis Argatski <denis.argatski@gmail.com>
2021-02-23 22:07:00 +0200

    ""

---
+++ 04.C# Advanced/Exercise/01.StacksAndQueus/StacksAndQueuS-Labs/07.HotPotato/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            Queue<string> array = new Queue<string>(Console.ReadLine()
+                .Split());
+
+            int n = int.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            while (array.Count>1)
+            {
+                count++;
+
+                if (n==count)
+                {
+                    Console.WriteLine($"Removed {array.Dequeue()}");
+                    count = 0;
+                }
+                else
+                {
+                    string name = array.Dequeue();
+                    array.Enqueue(name);
+                }
+            }
+            Console.WriteLine($"Last is {array.Dequeue()}");
+        }
+    }
+}
