47b903b1fcbec3128b2d8cba9c1346b7d02c7174
Denis Argatski <denis.argatski@gmail.com>
2021-02-23 22:06:13 +0200

    ""

---
+++ 04.C# Advanced/Exercise/01.StacksAndQueus/StacksAndQueuS-Labs/06.Supermarket/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            string name = string.Empty;
+
+            Queue<string> people = new Queue<string>();
+
+            while ((name = Console.ReadLine())!="End")
+            {
+                
+                if (name=="Paid")
+                {
+                    while (people.Count>0)
+                    {
+                        Console.WriteLine(people.Dequeue());
+                    }
+                }
+                else
+                {
+                    people.Enqueue(name);
+                }
+            }
+            Console.WriteLine($"{people.Count} people remaining.");
+
+        }
+    }
+}
