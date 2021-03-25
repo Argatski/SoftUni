119fd324792a6403076167ce0c9bc105eccd17f9
Denis Argatski <denis.argatski@gmail.com>
2021-03-25 22:47:08 +0200

    "Decimal to binary converter Program.cs"

---
+++ C#/04.C# Advanced/Exercise/01.StacksAndQueus/StacksAndQueuS-Labs/02.01.DecimalToBinaryConverter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._01.DecimalToBinaryConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int decimalNumber = int.Parse(Console.ReadLine());
+
+            Stack<int> binaryNumber = new Stack<int>();
+
+            if (decimalNumber==0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                while (decimalNumber>0)
+                {
+                    int divide =  decimalNumber % 2;
+
+                    decimalNumber /= 2;
+
+                    binaryNumber.Push(divide);
+                }
+
+            }
+
+            while (binaryNumber.Count>0)
+            {
+                Console.Write(binaryNumber.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
