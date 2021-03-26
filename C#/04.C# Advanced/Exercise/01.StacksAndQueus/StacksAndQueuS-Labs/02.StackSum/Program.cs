ab4188285fc45128076df5e64aa95fbae3781bf9
Denis Argatski <denis.argatski@gmail.com>
2021-03-26 21:23:11 +0200

    "02.Stack sum Program.cs"

---
+++ C#/04.C# Advanced/Exercise/01.StacksAndQueus/StacksAndQueuS-Labs/02.StackSum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.StackSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int[] num = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            string arg = string.Empty;
+
+            Stack<int> numbers = new Stack<int>(num);
+
+            while ((arg=Console.ReadLine().ToLower())!="end")
+            {
+                string[] command = arg.Split();
+
+                switch (command[0])
+                {
+                    case "add":
+                        numbers.Push(int.Parse(command[1]));
+                        numbers.Push(int.Parse(command[2]));
+                        break;
+                    case "remove":
+                        int count = int.Parse(command[1]);
+                        if (count<= numbers.Count)
+                        {
+                            for (int i = 0; i < count; i++)
+                            {
+                                numbers.Pop();
+                            }
+                        }
+                        
+                        break;
+                }
+            }
+            Console.WriteLine("Sum: " + numbers.Sum());
+        }
+    }
+}
