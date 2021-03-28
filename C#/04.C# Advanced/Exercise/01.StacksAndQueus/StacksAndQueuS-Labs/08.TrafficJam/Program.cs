de18a404b8716af1274d7c5746c7629569ea9942
Denis Argatski <denis.argatski@gmail.com>
2021-03-28 23:25:59 +0300

    ""

---
+++ C#/04.C# Advanced/Exercise/01.StacksAndQueus/StacksAndQueuS-Labs/08.TrafficJam/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int numberCars = int.Parse(Console.ReadLine());
+
+            string arg = string.Empty;
+
+            Queue<string> cars = new Queue<string>();
+
+            int count = 0;
+
+            while ((arg = Console.ReadLine()) != "end")
+            {
+
+                if (arg == "green")
+                {
+                    if (cars.Count < numberCars)
+                    {
+                        numberCars = cars.Count;
+                    }
+                    for (int i = 0; i < numberCars; i++)
+                    {
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                        count++;
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(arg);
+                }
+            }
+
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}
