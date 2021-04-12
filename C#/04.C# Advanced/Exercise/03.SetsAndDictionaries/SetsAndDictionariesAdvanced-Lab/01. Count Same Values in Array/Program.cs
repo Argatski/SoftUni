1d0c84206abfd9a3b026abfd7ef49ae5b76bb183
Denis Argatski <denis.argatski@gmail.com>
2021-04-12 23:40:13 +0300

    "Program.cs"

---
+++ C#/04.C# Advanced/Exercise/03.SetsAndDictionaries/SetsAndDictionariesAdvanced-Lab/01. Count Same Values in Array/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace _01._Count_Same_Values_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                    //Input
+            double[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, int> occurrences = new Dictionary<double, int>();
+
+            //Processing
+            CountSameValues(numbers,occurrences);
+
+            //Print result
+            Print(occurrences);
+        }
+
+        static void Print(Dictionary<double, int> occurrences)
+        {
+            foreach (var item in occurrences)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+
+        static void CountSameValues(double[] numbers, Dictionary<double, int> occurrences)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (!occurrences.ContainsKey(numbers[i]))
+                {
+                    occurrences.Add(numbers[i], 0);
+                }
+                occurrences[numbers[i]]++;
+            }
+        }
+    }
+}
