e5e8e771b2b0f181d2abe3e86c340bc22b920962
Denis Argatski <denis.argatski@gmail.com>
2021-04-15 19:46:50 +0300

    "Program.cs"

---
+++ C#/04.C# Advanced/Exercise/03.SetsAndDictionaries/SetsAndDictionariesAdvanced-Lab/07.SoftUniParty/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> vipList = new HashSet<string>();
+            HashSet<string> regularList = new HashSet<string>();
+
+            //Fill list guests
+            Fill(vipList, regularList);
+
+            //Cheked guests
+            CheckedAllGuests(vipList, regularList);
+
+            //Print result
+            Print(vipList, regularList);
+        }
+
+        private static void Print(HashSet<string> vipList, HashSet<string> regularList)
+        {
+            int count = vipList.Count + regularList.Count;
+            Console.WriteLine(count);
+
+            foreach (var guest in vipList)
+            {
+                Console.WriteLine($"{guest}");
+            }
+
+            foreach (var guest in regularList)
+            {
+                Console.WriteLine($"{guest}");
+            }
+        }
+
+        private static void CheckedAllGuests(HashSet<string> vipList, HashSet<string> regularList)
+        {
+            string guest = string.Empty;
+
+            while ((guest=Console.ReadLine())!="END")
+            {
+                if (vipList.Contains(guest))
+                {
+                    vipList.Remove(guest);
+                }
+                if (regularList.Contains(guest))
+                {
+                    regularList.Remove(guest);
+                }
+            }
+        }
+
+        private static void Fill(HashSet<string> vipList, HashSet<string> regularList)
+        {
+            string currentGuest = string.Empty;
+
+            while ((currentGuest = Console.ReadLine()) != "PARTY")
+            {
+                if (char.IsDigit(currentGuest[0]))
+                {
+                    vipList.Add(currentGuest);
+                }
+                else
+                {
+                    regularList.Add(currentGuest);
+                }
+            }
+        }
+    }
+}
