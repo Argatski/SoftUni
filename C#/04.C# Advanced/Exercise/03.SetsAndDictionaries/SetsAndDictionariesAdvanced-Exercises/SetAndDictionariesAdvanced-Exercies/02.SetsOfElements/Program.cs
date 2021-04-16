e4669abe6eb93294e713f8631f31e13471d782b5
Denis Argatski <denis.argatski@gmail.com>
2021-04-16 20:57:18 +0300

    "Program.cs"

---
+++ C#/04.C# Advanced/Exercise/03.SetsAndDictionaries/SetsAndDictionariesAdvanced-Exercises/SetAndDictionariesAdvanced-Exercies/02.SetsOfElements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+
+            //Add "n" set in dicitionary
+            int n = numbers[0];
+            HashSet<string> nSet = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                string num = Console.ReadLine();
+                nSet.Add(num);
+            }
+
+
+            //Add "n" set in dicitionary
+            int m = numbers[1];
+            HashSet<string> mSet = new HashSet<string>();
+            for (int i = 0; i < m; i++)
+            {
+                string num = Console.ReadLine();
+                mSet.Add(num);
+            }
+
+            nSet.IntersectWith(mSet);
+
+            Console.WriteLine($"{string.Join(" ",nSet)}");
+        }
+    }
+}
