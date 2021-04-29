d181fb8de5ddfed536bc550bd279ab5e44c0ec58
Denis Argatski <denis.argatski@gmail.com>
2021-04-29 19:52:18 +0300

    Program.cs

---
+++ C#/04.C# Advanced/Exercise/04.StreamsFilesAndDirectiories/StreamsFilesAndDirectiories-Labs/01.OddLines/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace _01.OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new StreamReader("../../../input.txt");
+
+            using (reader)
+            {
+                int count = 0;
+                string line = reader.ReadLine();
+
+                using (var writer = new StreamWriter("../../../Output.txt"))
+                {
+                    while (line != null)
+                    {
+                        if (count % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        count++;
+                        line = reader.ReadLine();
+                    }
+                    
+                }
+            }
+        }
+    }
+}
