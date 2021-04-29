971b5e76323400329e2e821acbf114f858fe65bb
Denis Argatski <denis.argatski@gmail.com>
2021-04-29 19:54:34 +0300

    Program.cs

---
+++ C#/04.C# Advanced/Exercise/04.StreamsFilesAndDirectiories/StreamsFilesAndDirectiories-Labs/02.LineNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace _02.LineNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../input.txt"))
+            {
+
+                using (StreamWriter writer = new StreamWriter("../../../Output.txt"))
+                {
+                    string line = reader.ReadLine();
+                    int count = 0;
+
+                    while (line != null)
+                    {
+                        count++;
+                        writer.WriteLine($"{count}. {line}");
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
