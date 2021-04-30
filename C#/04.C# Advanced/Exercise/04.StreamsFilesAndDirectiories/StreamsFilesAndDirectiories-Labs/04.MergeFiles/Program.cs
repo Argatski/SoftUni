245a3b568189ef20d8b483777bcbdc1a0048d23d
Denis Argatski <denis.argatski@gmail.com>
2021-04-30 19:36:03 +0300

    Program.cs

---
+++ C#/04.C# Advanced/Exercise/04.StreamsFilesAndDirectiories/StreamsFilesAndDirectiories-Labs/04.MergeFiles/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _04.MergeFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> dict = new List<string>();
+            using (StreamReader rd = new StreamReader("../../../Input1.txt"))
+            {
+                string text1 = rd.ReadLine();
+                while (text1 != null)
+                {
+                    dict.Add(text1);
+                    text1 = rd.ReadLine();
+                }
+
+
+            }
+            using (StreamReader rd2 = new StreamReader("../../../Input2.txt"))
+            {
+                string text2 = rd2.ReadLine();
+                while (text2 != null)
+                {
+                    dict.Add(text2);
+                    text2 = rd2.ReadLine();
+                }
+            }
+
+            using (StreamWriter writer = new StreamWriter("../../../Output.txt"))
+            {
+                dict.Sort();
+                foreach (var item in dict)
+                {
+                    writer.WriteLine($"{item}");
+                }
+            }
+        }
+    }
+}
+
+
