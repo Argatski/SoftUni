881c6203e881b14e4cf0dedccfa15de95be002b1
Denis Argatski <denis.argatski@gmail.com>
2021-04-30 19:31:51 +0300

    Program.cs

---
+++ C#/04.C# Advanced/Exercise/04.StreamsFilesAndDirectiories/StreamsFilesAndDirectiories-Labs/03.WordCount/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _03.WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, int> wordAndFrequency = new Dictionary<string, int>();
+            string[] words = File.ReadAllText("../../../words.txt").Split();
+
+            using (StreamReader reader = new StreamReader("../../../Input.txt"))
+            {
+                string currentLine = reader.ReadLine();
+
+                while (currentLine != null)
+                {
+                    string[] wordsInCurrentLine = currentLine.ToLower()
+                    .Split(new[] { ' ', '.', ',', '-', '?', '!', ':', ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (var word in words)
+                    {
+                        foreach (var item in wordsInCurrentLine)
+                        {
+                            if (word == item)
+                            {
+                                if (wordAndFrequency.ContainsKey(item) == false)
+                                {
+                                    wordAndFrequency.Add(item, 0);
+                                }
+                                wordAndFrequency[item]++;
+                            }
+                        }
+                    }
+                    currentLine = reader.ReadLine();
+                }
+            }
+            using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+            {
+                foreach (var item in wordAndFrequency.OrderByDescending(x => x.Value))
+                {
+                    writer.WriteLine($"{item.Key} - {item.Value}");
+                }
+            }
+        }
+    }
+}
