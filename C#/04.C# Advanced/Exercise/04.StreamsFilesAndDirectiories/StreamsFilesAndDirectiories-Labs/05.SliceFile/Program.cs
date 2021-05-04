c47ed5512b6df6c66cf6fb062038525f80886e54
Denis Argatski <denis.argatski@gmail.com>
2021-05-04 19:29:45 +0300

    Program.cs

---
+++ C#/04.C# Advanced/Exercise/04.StreamsFilesAndDirectiories/StreamsFilesAndDirectiories-Labs/05.SliceFile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace _05.SliceFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream reader = new FileStream("../../../sliceMe.txt", FileMode.Open))
+            {
+                byte[] buffer = new byte[reader.Length/4];
+
+                for (int i = 0; i < reader.Length/buffer.Length; i++)
+                {
+                    reader.Read(buffer, 0, buffer.Length);
+
+                    using (FileStream writeStream = new FileStream($"../../../{i+1}.SliceMe.txt",FileMode.Create,FileAccess.Write))
+                    {
+                        writeStream.Write(buffer);
+                    }
+                }
+            }
+        }
+    }
+}
