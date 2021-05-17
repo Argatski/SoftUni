d1da5d67eb3913bd49e336d5a2e954d77a693e50
Denis Argatski <denis.argatski@gmail.com>
2021-05-17 22:50:15 +0300

    Program.cs

---
+++ C#/04.C# Advanced/Exercise/04.StreamsFilesAndDirectiories/StreamsFilesAndDirectories-Exercise/02.LineNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textPath = "Text.txt";
+            string outputPath = "Output.txt";
+
+            var textLines = File.ReadAllLines(textPath);
+
+            int lineCounter = 1;
+
+            foreach (var line in textLines)
+            {
+                int lettersCount = line.Count(char.IsLetter);
+                int punctuationCount = line.Count(char.IsPunctuation);
+
+                File.AppendAllText(outputPath, $"Line {lineCounter}: {line} ({lettersCount})({punctuationCount}){Environment.NewLine}");
+
+                lineCounter++;
+            }
+        }
+    }
+}
