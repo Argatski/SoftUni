1d06e515681b7cc77e5ba7440b40f150fab2b7c8
Denis Argatski <denis.argatski@gmail.com>
2021-05-17 22:47:48 +0300

    Program.cs

---
+++ C#/04.C# Advanced/Exercise/04.StreamsFilesAndDirectiories/StreamsFilesAndDirectories-Exercise/01.EvenLines/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01.EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            using (StreamReader reader = new StreamReader("../../../text.txt"))
+            {
+                string line = reader.ReadLine();
+                int count = 0;
+                while (line != null)
+                {
+                    if (count % 2 == 0)
+                    {
+                        /*Regex rx = new Regex(@"(?:[-, \.!?])");
+
+                        MatchCollection mathces = rx.Matches(line);*/
+                        string replacedLine = ReplacedSymbol(line);
+
+                        string[] reversedLine = replacedLine.Split().Reverse().ToArray();
+
+                        Console.WriteLine(string.Join(" ", reversedLine));
+                    }
+
+                    line = reader.ReadLine();
+                    count++;
+                }
+            }
+        }
+
+        private static string ReplacedSymbol(string line)
+        {
+            return line
+                 .Replace("-", "@")
+                 .Replace(",", "@")
+                 .Replace(", ", "@")
+                 .Replace(".", "@")
+                 .Replace("!", "@")
+                 .Replace("?", "@");
+        }
+    }
+}
