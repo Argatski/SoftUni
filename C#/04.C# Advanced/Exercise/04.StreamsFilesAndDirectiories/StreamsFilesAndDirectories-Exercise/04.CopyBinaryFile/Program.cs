0c2a4d10432c0590918ee18556d3df247a6677e9
Denis Argatski <denis.argatski@gmail.com>
2021-05-18 23:37:21 +0300

    Program.cs

---
+++ C#/04.C# Advanced/Exercise/04.StreamsFilesAndDirectiories/StreamsFilesAndDirectories-Exercise/04.CopyBinaryFile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace _04.CopyBinaryFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string picPath = "../../../copyMe.png";
+            string picCopyPath = "../../../copyMe-copy.png";
+
+            using (FileStream file = new FileStream(picPath, FileMode.Open))
+            {
+                using (FileStream wr = new FileStream(picCopyPath, FileMode.Create))
+                {
+                    while (true)
+                    {
+                        byte[] buffer = new byte[4096];
+                        var readSize = file.Read(buffer, 0, buffer.Length);
+
+                        if (readSize==0)
+                        {
+                            break;
+                        }
+                        file.Write(buffer, 0, readSize);
+
+                    }
+                }
+            }
+        }
+    }
+}
