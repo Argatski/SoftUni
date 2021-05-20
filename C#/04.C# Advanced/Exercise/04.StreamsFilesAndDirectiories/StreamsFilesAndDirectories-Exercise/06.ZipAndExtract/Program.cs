e30e97317c9a20c36acc820fec5f9f1597867713
Denis Argatski <denis.argatski@gmail.com>
2021-05-20 20:51:34 +0300

    Program.cs

---
+++ C#/04.C# Advanced/Exercise/04.StreamsFilesAndDirectiories/StreamsFilesAndDirectories-Exercise/06.ZipAndExtract/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace _06.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string zipFile = @"..\..\..\result.zip";
+            string picPath = "copyMe.png";
+
+            using (var archive = ZipFile.Open(zipFile, ZipArchiveMode.Create))
+            {
+                archive.CreateEntryFromFile(picPath, Path.GetFileName(picPath));
+            }
+
+            ZipFile.ExtractToDirectory("../../../rezult.zip", "../../../");
+
+            //using (var extract = ZipFile.Open("../../../rezult.zip", ZipArchiveMode.Read))
+            //{
+            //    extract.ExtractToDirectory("../../../");
+            //}
+
+        }
+    }
+}
