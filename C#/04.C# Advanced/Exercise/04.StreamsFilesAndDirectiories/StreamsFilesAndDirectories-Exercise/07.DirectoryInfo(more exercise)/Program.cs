5102b98edd99390d6946961d4654ad0fb2dc1749
Denis Argatski <denis.argatski@gmail.com>
2021-05-22 10:46:58 +0300

    Program.cs

---
+++ C#/04.C# Advanced/Exercise/04.StreamsFilesAndDirectiories/StreamsFilesAndDirectories-Exercise/07.DirectoryInfo(more exercise)/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace _07.DirectoryInfo_more_exercise_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input path folder
+            string folderPath = Console.ReadLine();
+
+            //Print solution
+            Console.WriteLine(ScanFolderRecursively(folderPath, 0));
+        }
+
+        static double ScanFolderRecursively(string folderPath, int indentation)
+        {
+            double fileSize = 0;
+
+            try
+            {
+                var files = Directory.GetFiles(folderPath);
+
+                var directories = Directory.GetDirectories(folderPath);
+
+                foreach (var directory in directories)
+                {
+                    fileSize += ScanFolderRecursively(directory, indentation);
+                }
+
+                foreach (var filePath in files)
+                {
+                    FileInfo file = new FileInfo(filePath);
+
+                    Console.WriteLine($"{new string(' ', indentation)} {file.FullName}");
+
+                    fileSize += file.Length;
+                }
+            }
+
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return fileSize / 1024 / 1024;
+        }
+    }
+}
