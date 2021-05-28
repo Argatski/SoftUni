b3f68473868b012ca5239ab730d643715036ca00
Denis Argatski <denis.argatski@gmail.com>
2021-05-28 23:00:08 +0300

    03.CountUppercaseWords.csproj

---
+++ C#/04.C# Advanced/Exercise/05.FunctionalProgramming/Functional Programming - Lab/03.CountUppercaseWords/03.CountUppercaseWords.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input 
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(w => char.IsUpper(w[0]))
+                .ToList()
+                .ForEach(w=>Console.WriteLine(w));
+
+        }
+    }
+}
