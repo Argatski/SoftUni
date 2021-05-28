743b27d9a0d643eb00f284dc6d284cd733e983e1
Denis Argatski <denis.argatski@gmail.com>
2021-05-28 22:58:24 +0300

    02.SumNumbers.cs

---
+++ C#/04.C# Advanced/Exercise/05.FunctionalProgramming/Functional Programming - Lab/02.SumNumbers/02.SumNumbers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int length = numbers.Length;
+            //int sum = numbers.Sum();
+
+            Console.WriteLine(Operation(numbers,LengthArray));
+            Console.WriteLine(Operation(numbers,Sum));
+        }
+
+        public static int Operation(int[] num, Func<int[], int> opr) 
+        {
+            return opr(num);
+        }
+
+        public static int Sum(int[] n) 
+        {
+            int sum = 0;
+            for (int i = 0; i < n.Length; i++)
+            {
+                sum += n[i];
+            }
+            return sum;
+        }
+
+        public static int LengthArray(int[] arr)
+        {
+            int length = arr.Count();
+
+            return length;
+        }
+
+    }
+}
