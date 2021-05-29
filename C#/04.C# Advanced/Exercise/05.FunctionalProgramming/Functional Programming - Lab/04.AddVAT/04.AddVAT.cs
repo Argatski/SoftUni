0b1e4cc6e467eacbee1bbbdf4bea09da9dff7336
Denis Argatski <denis.argatski@gmail.com>
2021-05-29 15:03:23 +0300

    04.AddVAT.cs

---
+++ C#/04.C# Advanced/Exercise/05.FunctionalProgramming/Functional Programming - Lab/04.AddVAT/04.AddVAT.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            Console.ReadLine()
+                 .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                 //.Select(double.Parse)
+                 //.Select(p => p * 1.20)
+                 .Select(Parser)
+                 .Select(AddVAT)
+                 .ToList()
+                 .ForEach(p => Console.WriteLine($"{p:f2}"));
+
+        }
+        public static Func<string, double> Parser = p => double.Parse(p);
+        public static Func<double, double> AddVAT = p =>
+        {
+            return p *= 1.20;
+        };
+    }
+}
