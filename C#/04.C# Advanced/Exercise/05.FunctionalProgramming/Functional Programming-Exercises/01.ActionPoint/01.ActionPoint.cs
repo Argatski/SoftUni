ba8be5cdc0ff3ffdb1ef41495465777bac527fd7
Denis Argatski <denis.argatski@gmail.com>
2021-05-30 22:50:25 +0300

    01.ActionPoint.cs

---
+++ C#/04.C# Advanced/Exercise/05.FunctionalProgramming/Functional Programming-Exercises/01.ActionPoint/01.ActionPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            printResult(text);
+        }
+        public static Action<string[]> printResult = w =>
+        {
+            foreach (var item in w)
+            {
+                Console.WriteLine(item);
+            }
+        };
+    }
+}
