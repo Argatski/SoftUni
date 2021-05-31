e31104864248ebbcf118d8e9c8445931cab81e3b
Denis Argatski <denis.argatski@gmail.com>
2021-05-31 23:33:25 +0300

    06.ReverseAndExclude.cs

---
+++ C#/04.C# Advanced/Exercise/05.FunctionalProgramming/Functional Programming-Exercises/06.ReverseAndExclude/06.ReverseAndExclude.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int divisibleNum = int.Parse(Console.ReadLine());
+
+
+            Predicate<int> filter = x => x % divisibleNum != 0;
+            Func<int, bool> filterFunc = x => filter(x);
+
+            numbers = numbers
+                .Where(filterFunc)
+                .ToArray();
+
+            numbers = Reverse(numbers);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        static Func<int[], int[]> Reverse = r =>
+        {
+            int[] result = new int[r.Length];
+
+            for (int i = 0; i < r.Length; i++)
+            {
+                result[i] = r[r.Length - i - 1];
+            }
+            return result;
+        };
+
+        //Another solution use lambda.
+        /*static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int divisibleNum = int.Parse(Console.ReadLine());
+
+            numbers = numbers.Reverse()
+                .Where(n => n % divisibleNum != 0)
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }*/
+    }
+}
