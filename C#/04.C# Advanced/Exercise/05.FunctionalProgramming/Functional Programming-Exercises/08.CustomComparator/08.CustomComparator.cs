316b5d2f51a13d9312d1d4ae7aa1ca6ff4e71b71
Denis Argatski <denis.argatski@gmail.com>
2021-06-01 22:26:10 +0300

    08.CustomComparator.cs

---
+++ C#/04.C# Advanced/Exercise/05.FunctionalProgramming/Functional Programming-Exercises/08.CustomComparator/08.CustomComparator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int, int, int> filtterEven = (x, y) => Math.Abs(x % 2) == Math.Abs(y % 2) ? (x == y ? 0 : (x < y ? -1 : 1)) : (Math.Abs(x % 2) == 0 ? -1 : 1);
+
+            Array.Sort(numbers,(x,y)=>filtterEven(x,y));
+
+            Console.WriteLine(string.Join(" ", numbers));
+
+        }
+    }
+}
