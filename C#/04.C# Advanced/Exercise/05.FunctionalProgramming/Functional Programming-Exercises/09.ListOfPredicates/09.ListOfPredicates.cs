fdbd4c7782e164ad33ee14cca3d9fd52f4dab1a2
Denis Argatski <denis.argatski@gmail.com>
2021-06-01 22:27:59 +0300

    09.ListOfPredicates.csproj

---
+++ C#/04.C# Advanced/Exercise/05.FunctionalProgramming/Functional Programming-Exercises/09.ListOfPredicates/09.ListOfPredicates.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int endRange = int.Parse(Console.ReadLine());
+
+            List<int> dividers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Distinct()
+                .Select(int.Parse)
+                .ToList();
+
+            //Predicate
+
+            List<int> numbers = new List<int>();
+
+            for (int i = 1; i <=endRange; i++)
+            {
+                numbers.Add(i);
+            }
+
+            foreach (var item in dividers)
+            {
+                Func<int, bool> filter = x => x % item == 0;
+
+                numbers = numbers.Where(filter).ToList();
+            }
+
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+
+        //Another solution//
+        /*
+         * static void Main(string[] args)
+        {
+            //Input
+            int endRange = int.Parse(Console.ReadLine());
+
+            List<int> array = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Distinct()
+                .Select(int.Parse)
+                .ToList();
+
+            //Predicate
+            List<Predicate<int>> predicates = new List<Predicate<int>>();
+
+            array.ForEach(d => predicates.Add(x => x % d == 0));
+
+            List<int> result = new List<int>();
+
+            for (int i = 1; i <= endRange; i++)
+            {
+                bool isDivisible = true;
+
+                foreach (var predicate in predicates)
+                {
+                    if (!predicate(i))
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+
+                if (isDivisible)
+                {
+                    result.Add(i);
+                }
+            }
+            Console.WriteLine(" ", result);
+        }
+         */
+    }
+}
