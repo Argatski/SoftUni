c42b1e7bbbf6bb071d503cfc327e3d00aa9d76a2
Denis Argatski <denis.argatski@gmail.com>
2021-06-13 23:18:44 +0300

    Car.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DefiningClasses-Lab/01.Car/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        //Properties
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+    }
+}
