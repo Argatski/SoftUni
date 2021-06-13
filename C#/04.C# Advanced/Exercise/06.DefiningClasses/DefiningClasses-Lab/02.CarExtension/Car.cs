3b7b2d8a22e46fac34a450442e99b307897b6220
Denis Argatski <denis.argatski@gmail.com>
2021-06-13 23:23:24 +0300

    Car.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DefiningClasses-Lab/02.CarExtension/Car.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        //Fields
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        //Properties
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        //Method(funcitonal)
+        public void Drive(double distance)
+        {
+            double result = fuelQuantity - (distance * fuelConsumption);
+            if (result >= 0)
+            {
+                fuelQuantity = result;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI(Car car)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {Make}");
+            sb.AppendLine($"Model {Model}");
+            sb.AppendLine($"Year: {Year}");
+            sb.AppendLine($"Fuel: {FuelQuantity:F2}L");
+
+
+            return sb.ToString();
+        }
+
+    }
+}
