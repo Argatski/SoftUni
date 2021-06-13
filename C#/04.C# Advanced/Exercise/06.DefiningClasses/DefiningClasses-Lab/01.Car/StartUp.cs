17fc1ac9dd4f6e78c7cce0df879323dd4256573b
Denis Argatski <denis.argatski@gmail.com>
2021-06-13 23:20:57 +0300

    StartUp.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DefiningClasses-Lab/01.Car/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car()
+            {
+                Make = "Vw",
+                Model = "MK3",
+                Year = 1992
+            };
+
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+}
