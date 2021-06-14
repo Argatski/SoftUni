66292a2f1364630a56fdb0fba75f2bb6d365afe5
Denis Argatski <denis.argatski@gmail.com>
2021-06-14 23:32:44 +0300

    StartUp.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DefiningClasses-Lab/03.CarConstructors/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+
+            Car firstCar = new Car();
+            Car secondCar = new Car(name, model, year);
+            Car thirdCar = new Car(name, model, year, fuelQuantity, fuelConsumption);
+
+            firstCar.Drive(1);
+            secondCar.Drive(2);
+            thirdCar.Drive(3);
+
+            Console.WriteLine($"{firstCar.WhoIam(firstCar)}");
+            Console.WriteLine("............");
+            Console.WriteLine($"{secondCar.WhoIam(secondCar)}");
+            Console.WriteLine("............");
+            Console.WriteLine($"{thirdCar.WhoIam(thirdCar)}");
+        }
+    }
+}
