a3268e46aec1c8182e49a502f4a51535c39a9abc
Denis Argatski <denis.argatski@gmail.com>
2021-06-21 21:30:45 +0300

    Car.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DefiningClasses-Lab/05.SpecialCars/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+
+        //Properties
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+
+        //Constructors 
+        //This is defualt constructor if we need.
+        public Car()
+        {
+
+        }
+
+        public Car(string make, string model, int year)
+            : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelCunsumption)
+            : this(make, model, year)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelCunsumption;
+        }
+
+
+
+        public Tire[] Tires { get; set; }
+        public Engine Engine { get; set; }
+
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelCunsumption, Engine engine, Tire[] tires)
+            : this(make, model, year, fuelQuantity, fuelCunsumption)
+        {
+            Engine = engine;
+            Tires = tires;
+        }
+
+
+        //All car drive 20 kilometers
+        public void Drive(double distance)
+        {
+            double result = FuelQuantity - (distance * FuelConsumption)/100;
+
+            if (result >= 0)
+            {
+                FuelQuantity = result;
+            }
+        }
+
+
+    }
+}
