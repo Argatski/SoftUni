e44e27d52b02cfacbe933ea02ea5834da20d9c10
Denis Argatski <denis.argatski@gmail.com>
2021-06-21 21:32:30 +0300

    Engine.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DefiningClasses-Lab/05.SpecialCars/Engine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Engine
+    {
+
+        //Properties
+        public int HorsePower { get; set; }
+        public double CubicCapacity { get; set; }
+
+        //Constructor
+
+        public Engine(int horsePower,double cubicCapacity)
+        {
+            HorsePower = horsePower;
+            CubicCapacity = cubicCapacity;
+        }
+    }
+}
