80b68807cf17d37402f8cce2263ed845fb43826d
Denis Argatski <denis.argatski@gmail.com>
2021-06-21 21:33:28 +0300

    Tire.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DefiningClasses-Lab/05.SpecialCars/Tire.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Tire
+    {
+        //Properties
+        public int Year { get; set; }
+        public double Pressure { get; set; }
+
+        //Constructor
+        public Tire(int year, double pressure)
+        {
+            Year = year;
+            Pressure = pressure;
+        }
+    }
+}
