23a3f6cc36b26b6e75bce0e1b330fb0a9bde2dab
Denis Argatski <denis.argatski@gmail.com>
2021-06-23 23:32:53 +0300

    BankAccount.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DefiningClasses-Lab/01.BankAccount/BankAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount
+{
+    public class BankAccount
+    {
+        //Prive fields
+        private int id;
+        private decimal balance;
+
+        //Properties
+        public int Id { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
