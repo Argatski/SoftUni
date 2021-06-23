fa14717489f7f75ae0d78245da06a37e4ef477d4
Denis Argatski <denis.argatski@gmail.com>
2021-06-23 23:33:33 +0300

    StartUp.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DefiningClasses-Lab/01.BankAccount/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BankAccount
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            BankAccount acc = new BankAccount();
+
+            //Instance
+            acc.Id = 1;
+            acc.Balance = 15;
+
+            Console.WriteLine($"Account {acc.Id}, balance{acc.Balance}");
+        }
+    }
+}
