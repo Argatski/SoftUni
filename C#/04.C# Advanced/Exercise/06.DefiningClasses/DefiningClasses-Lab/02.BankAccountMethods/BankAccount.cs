5976292d130a89bb0df31a5f27e9ccace58dd235
Denis Argatski <denis.argatski@gmail.com>
2021-06-24 23:30:13 +0300

    BankAccount.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DefiningClasses-Lab/02.BankAccountMethods/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount
+{
+    public class BankAccount
+    {
+
+        //Properties
+        public int Id { get; set; }
+        public decimal Balance { get; set; }
+
+        //Methonds
+        public  void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            return $"Account {Id}, balance {Balance}";
+        }
+
+    }
+}
