10528aa890f0183083ca795c0c8548415ddf32bd
Denis Argatski <denis.argatski@gmail.com>
2021-06-24 23:31:17 +0300

    Program.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DefiningClasses-Lab/02.BankAccountMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BankAccount
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            BankAccount acc = new BankAccount();
+
+            //Instant
+            acc.Id = 1;
+            acc.Deposit(15);
+            acc.Withdraw(10);
+
+            Console.WriteLine(acc);
+        }
+    }
+}
