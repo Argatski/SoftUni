60396ce2a3f98e9278633a645dca7f5e12b8b82d
Denis Argatski <denis.argatski@gmail.com>
2021-06-27 23:05:00 +0300

    Person.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/06.DeffingClasses-Exercises/01.DefineClassPerson/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+
+        //Properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        //Constructor
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
