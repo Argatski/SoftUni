f17305ad87e8632dec2170965e2bcbd72c60c34b
Denis Argatski <denis.argatski@gmail.com>
2021-06-27 23:05:35 +0300

    StartUp.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/06.DeffingClasses-Exercises/01.DefineClassPerson/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Pesho",25);
+            Person person1 = new Person("Gosho",18);
+            Person person2 = new Person("Stamat",43);
+
+        }
+    }
+}
