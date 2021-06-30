dae6676abdf3169ca1d59d3cb02ea67d612e4c78
Denis Argatski <denis.argatski@gmail.com>
2021-06-30 22:44:24 +0300

    Person.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/06.DeffingClasses-Exercises/03.OldestFamilyMember/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        //Properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        //Constructor
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+        public Person(int age):this()
+        {
+            Age = age;
+        }
+        public Person(string name,int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
