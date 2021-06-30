5934d6cbff53e7ba1fcb3fe6d7d68e2bf14a2ae4
Denis Argatski <denis.argatski@gmail.com>
2021-06-30 22:45:23 +0300

    StartUp.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/06.DeffingClasses-Exercises/03.OldestFamilyMember/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Input 
+            int numberOfPeople = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                string[] arguments = Console.ReadLine()
+                    .Split(" ");
+
+                string name = arguments[0];
+                int age = int.Parse(arguments[1]);
+
+                var person = new Person(name, age);
+
+                family.AddMember(person);
+            }
+
+            var oldPerson = family.GetOldestMember();
+
+            Console.WriteLine($"{oldPerson.Name} {oldPerson.Age}");
+        }
+    }
+}
