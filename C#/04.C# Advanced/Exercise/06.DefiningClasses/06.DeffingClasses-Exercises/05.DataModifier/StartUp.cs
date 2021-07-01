0c14d470940f9b11835138d29bf6b7d5e6e67a39
Denis Argatski <denis.argatski@gmail.com>
2021-07-01 23:47:27 +0300

    StartUp.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/06.DeffingClasses-Exercises/05.DataModifier/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            string firstData = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            //Instancing
+
+            Console.WriteLine(DataModifier.Calculates(firstData,secondDate));
+        }
+    }
+}
