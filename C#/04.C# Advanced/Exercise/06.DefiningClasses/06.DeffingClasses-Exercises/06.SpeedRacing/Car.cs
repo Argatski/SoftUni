35b7855e09859be77101ac290c54a529587d0295
Denis Argatski <denis.argatski@gmail.com>
2021-07-05 23:14:23 +0300

    Car.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/06.DeffingClasses-Exercises/06.SpeedRacing/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        //Proprties
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumptionPerKilometer { get; set; }
+        public double TravelledDistance { get; set; }
+
+        //Constructors
+        public Car()
+        {
+
+        }
+        public Car(string model,double fuelAmount,double fuelConsumptionPerKilometer,double distance)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            TravelledDistance = distance; 
+        }
+
+        //Methods
+        public void Drive(double amountOfKm) 
+        {
+            bool canMove = FuelAmount >= amountOfKm * FuelConsumptionPerKilometer;
+
+            if (canMove)
+            {
+                FuelAmount -= (amountOfKm * FuelConsumptionPerKilometer);
+
+                TravelledDistance += amountOfKm; 
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Model} {FuelAmount:f2} {TravelledDistance}";
+        }
+    }
+}
