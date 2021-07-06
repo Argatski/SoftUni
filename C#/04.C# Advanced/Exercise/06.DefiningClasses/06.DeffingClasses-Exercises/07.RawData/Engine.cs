b5ecb7898b55ff8037b7280db85e7d261dd9d9a0
Denis Argatski <denis.argatski@gmail.com>
2021-07-06 23:54:27 +0300

    Engine.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/06.DeffingClasses-Exercises/07.RawData/Engine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Engine
+    {
+        //Properties
+        public int Speed { get; set; }
+        public int Power { get; set; }
+
+        //Constructor
+        public Engine(int speed,int power)
+        {
+            Speed = speed;
+            Power = power;
+        }
+        
+    }
+}
