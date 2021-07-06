d5272a801b6ad5c5b7b61c666a30c4f90b3ab821
Denis Argatski <denis.argatski@gmail.com>
2021-07-06 23:52:48 +0300

    Car.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/06.DeffingClasses-Exercises/07.RawData/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        //Properties
+        public string Model { get; set; }
+
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+        public Tire[] Tires { get; set; }
+
+        //Constructor
+        public Car(string model, Engine engine, Cargo cargo, Tire[] tires)
+        {
+            Model = model;
+            Engine = engine;
+            Cargo = cargo;
+            Tires = tires;
+        }
+
+        //Print result
+        public override string ToString()
+        {
+            return $"{Model}";
+        }
+    }
+}
