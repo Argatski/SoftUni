169ba24ce6172f8680b9e90187460683455be3ab
Denis Argatski <denis.argatski@gmail.com>
2021-07-06 23:53:35 +0300

    Cargo.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/06.DeffingClasses-Exercises/07.RawData/Cargo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Cargo
+    {
+        //Properties
+        public int Weight { get; set; }
+        public string Type { get; set; }
+
+        //Constructors
+        public Cargo(int weight,string type)
+        {
+            Weight = weight;
+            Type = type;
+        }
+    }
+}
