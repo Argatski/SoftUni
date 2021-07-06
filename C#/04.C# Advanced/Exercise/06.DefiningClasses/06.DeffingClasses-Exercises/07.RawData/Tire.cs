cb3b2db672df08bc69613ea430d802589eefae9f
Denis Argatski <denis.argatski@gmail.com>
2021-07-06 23:55:12 +0300

    Tire.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/06.DeffingClasses-Exercises/07.RawData/Tire.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Tire
+    {
+        //Properties
+        public double Pressure { get; set; }
+        public int Age { get; set; }
+
+        //Constructor
+        public Tire(double pressure, int age)
+        {
+            Pressure = pressure;
+            Age = age;
+        }
+    }
+}
