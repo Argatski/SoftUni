b9ccbfa8cc39a39d2f817b2f68f031653dbda008
Denis Argatski <denis.argatski@gmail.com>
2021-07-11 15:14:56 +0300

    StartUp.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/09.PokemonTrainer/StartUp.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            string command;
+
+            var trainers = new Dictionary<string,Trainer>();
+
+            while ((command = Console.ReadLine()) != "Tournament")
+            {
+                string[] arguments = command
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                //Trainer information
+                string trainerName = arguments[0];
+
+                //Pokemon information
+                string pokemonName = arguments[1];
+                string pokemonElemnt = arguments[2];
+                int pokemonHealth = int.Parse(arguments[3]);
+
+                //Initialising pokemon
+                Pokemon pokemon = new Pokemon
+                    (
+                    pokemonName, pokemonElemnt, pokemonHealth
+                    );
+
+                //Is name unique 
+                if (!trainers.ContainsKey(trainerName))
+                {
+                    trainers.Add(trainerName, new Trainer(trainerName));
+                }
+
+                //Add another pockemon
+                trainers[trainerName].AddPokemons(pokemon);
+            }
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                switch (command)
+                {
+                    case "Fire":
+                        foreach (var trainer in trainers.Values)
+                        {
+                            trainer.CheckPokemons(command);
+                        }
+                        break;
+                    case "Water":
+                        foreach (var trainer in trainers.Values)
+                        {
+                            trainer.CheckPokemons(command);
+                        }
+                        break;
+                    case "Electricity":
+                        foreach (var trainer in trainers.Values)
+                        {
+                            trainer.CheckPokemons(command);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            var result = trainers.Values.OrderByDescending(x => x.NumberOfBadges).ToList();
+
+            foreach (var trainer in result)
+            {
+                Console.WriteLine(trainer);
+            }
+
+        }
+
+    }
+}
