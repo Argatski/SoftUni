815c96a2f645eb0cfdb39198902b5843b88dc03f
Denis Argatski <denis.argatski@gmail.com>
2021-07-11 15:15:25 +0300

    Trainer.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/09.PokemonTrainer/Trainer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+
+{
+    public class Trainer
+    {
+        //Fields
+        private List<Pokemon> pokemos;
+
+        //Properties
+        public string Name { get; set; }
+
+        public int NumberOfBadges { get; set; } 
+
+
+        //Constructor
+        public Trainer(string name)
+        {
+            Name = name;
+            NumberOfBadges = 0;
+            pokemos = new List<Pokemon>();
+        }
+
+        //Mehtonds
+        public void AddPokemons(Pokemon pokemon)
+        {
+            pokemos.Add(pokemon);
+        }
+
+        public void CheckPokemons(string element) 
+        {
+            foreach (var pokemon in pokemos)
+            {
+                if (pokemon.Element==element)
+                {
+                    NumberOfBadges++;
+                    break;
+                }
+                else
+                {
+                    pokemon.Health -= 10;
+                }
+            }
+
+            for (int i = 0; i < pokemos.Count; i++)
+            {
+                Pokemon pokemon = pokemos[i];
+
+                if (pokemon.Health<=0)
+                {
+                    pokemos.Remove(pokemon);
+                    i--;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {NumberOfBadges} {pokemos.Count}"; 
+        }
+    }
+}
