ace3f7d7efe621556bc0a53a5d1457a6cf417cd3
Denis Argatski <denis.argatski@gmail.com>
2021-08-05 19:25:52 +0300

    "Employee.cs"

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/06.01.CompanyRoster/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    class Employee
+    {
+        //This properties are mandatory
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+
+
+        //This properties are optional
+        public string Email { get; set; }
+        public int Age { get; set; }
+
+
+        public Employee(string name, double salary, string position, string department,string email,int age)
+        {
+            Name = name;
+            Salary = salary;
+            Position = position;
+            Department = department;
+            Email = email;
+            Age = age;
+        }
+
+    }
+}
