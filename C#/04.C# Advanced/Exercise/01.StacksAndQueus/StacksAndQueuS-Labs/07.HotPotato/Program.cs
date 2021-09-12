3fd2c5dc4d2febb48c36c3ec58b87ed90f853800
Denis Argatski <denis.argatski@gmail.com>
2021-09-12 22:32:03 +0300

    Program.cs

--- C#/04.C# Advanced/Exercise/01.StacksAndQueus/StacksAndQueuS-Labs/07.HotPotato/Program.cs
+++ C#/04.C# Advanced/Exercise/01.StacksAndQueus/StacksAndQueuS-Labs/07.HotPotato/Program.cs
@@ -1,13 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace _07.HotPotato
+namespace _07.HotPatato
 {
     class Program
     {
         static void Main(string[] args)
         {
             //Input
+            string[] names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int n = int.Parse(Console.ReadLine());
+
+            //Solution
+            Queue<string> children = new Queue<string>(names);
+
+            while (children.Count > 1)
+            {
+
+                for (int i = 1; i < n; i++)
+                {
+                    string first = children.Dequeue();
+                    children.Enqueue(first);
+                }
+                //Print loss
+                Console.WriteLine("Removed " + children.Dequeue()); 
+            }
+
+            //Print winner
+            Console.WriteLine("Last is " + children.Dequeue());
+
+        }
+
+        /*Another solution
+             //Input
             Queue<string> array = new Queue<string>(Console.ReadLine()
                 .Split());
 
@@ -31,6 +58,6 @@ namespace _07.HotPotato
                 }
             }
             Console.WriteLine($"Last is {array.Dequeue()}");
-        }
+            */
     }
 }
