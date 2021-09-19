e31834e8368a3be53481075f072400fc1337c1f4
Denis Argatski <denis.argatski@gmail.com>
2021-09-19 18:28:40 +0300

    TruckTour.cs

---
+++ C#/04.C# Advanced/Exercise/01.StacksAndQueus/StacksAndQueues-Exercise/08.BalancedParenthesis/BalanceParenthesis.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.BalancedParenthesis
+{
+    class BalanceParenthesis
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            string text = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            bool isBalance = true;
+
+            foreach (var ch in text)
+            {
+                switch (ch)
+                {
+                    case '(':
+                    case '{':
+                    case '[':
+                        stack.Push(ch);
+                        break;
+                    case ')':
+                        if (stack.Any() == false || stack.Pop() != '(')
+                        {
+                            isBalance = false;
+                        }
+                        break;
+                    case '}':
+                        if (stack.Any() == false || stack.Pop() != '{')
+                        {
+                            isBalance = false;
+                        }
+                        break;
+                    case ']':
+                        if (stack.Any() == false || stack.Pop() != '[')
+                        {
+                            isBalance = false;
+                        }
+                        break;
+                }
+
+            }
+            string result = isBalance ? "YES" : "NO";
+            Console.WriteLine(result);
+        }
+    }
+}
