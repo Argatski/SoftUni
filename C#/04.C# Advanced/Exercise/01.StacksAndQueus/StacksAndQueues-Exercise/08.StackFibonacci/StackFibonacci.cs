90a3e3a86564e5afe773dbb77ad30c980712d358
Denis Argatski <denis.argatski@gmail.com>
2021-09-20 23:15:33 +0300

    StackFibonacci.cs

---
+++ C#/04.C# Advanced/Exercise/01.StacksAndQueus/StacksAndQueues-Exercise/08.StackFibonacci/StackFibonacci.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09.StackFibonacci
+{
+    class StackFibonacci
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            long numberFibonacci = long.Parse(Console.ReadLine());
+
+            Stack<long> stack = new Stack<long>();
+
+            stack.Push(0);
+            stack.Push(1);
+
+            //Processing
+            StackFibonnacci(numberFibonacci, stack);
+
+            //Print result
+            Console.WriteLine(stack.Pop());
+        }
+
+        private static void StackFibonnacci(long numberFibonacci, Stack<long> stack)
+        {
+            for (int i = 0; i < numberFibonacci - 1; i++)
+            {
+                long currentNum = stack.Pop();
+                long previous = stack.Peek();
+
+                stack.Push(currentNum);
+
+                long result = currentNum + previous;
+                stack.Push(result);
+            }
+        }
+    }
+}
