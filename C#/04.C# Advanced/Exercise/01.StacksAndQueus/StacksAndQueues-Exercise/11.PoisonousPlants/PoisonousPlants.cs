f5aaafe520527675b6b12d420f007fc2a4e713e9
Denis Argatski <denis.argatski@gmail.com>
2021-09-25 21:25:40 +0300

    PoisonousPlants.cs

---
+++ C#/04.C# Advanced/Exercise/01.StacksAndQueus/StacksAndQueues-Exercise/11.PoisonousPlants/PoisonousPlants.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.PoisonousPlants
+{
+    class PoisonousPlants
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int numberOfPlants = int.Parse(Console.ReadLine());
+
+            int[] plants = Console.ReadLine()
+                .Trim()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] daysToDie = new int[plants.Count()]; // days each plant dies(0=>plant never die)
+
+            Stack<int> plantsLeftSeq = new Stack<int>(); // plants (indices) to the left of given plant
+
+            plantsLeftSeq.Push(0);
+
+            for (int i = 1; i < plants.Length; i++)
+            {
+                int maxDaysToDie = 0;
+                //Pop plants bigger than current plant and left to it.
+
+                while (plantsLeftSeq.Count != 0 && plants[plantsLeftSeq.Peek()] >= plants[i])
+                {
+                    maxDaysToDie = Math.Max(maxDaysToDie, daysToDie[plantsLeftSeq.Pop()]);
+                }
+                // empty plants stack => min plant [i] found (smaller than all plants to the left of it)
+                // min plants never die => daysToDie = 0
+                if (plantsLeftSeq.Count != 0)
+                {
+                    daysToDie[i] = maxDaysToDie + 1;
+                }
+                plantsLeftSeq.Push(i);
+            }
+            Console.WriteLine(daysToDie.Max());
+        }
+    }
