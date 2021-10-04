8cb00f3cc73d1356aeda9cdd868ec0e692bab4b5
Denis Argatski <denis.argatski@gmail.com>
2021-10-04 22:27:24 +0300

    PascalTriangle.cs

---
+++ C#/04.C# Advanced/Exercise/02.MultidimensionalArrays/MultidimensionalArrays-Labs/08.PascalTriangle/PascalTriangle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _08.PascalTriangle
+{
+    class PascalTriangle
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int number = int.Parse(Console.ReadLine());
+
+            long[][] triangle = new long[number][];
+
+            //Create pascal triangle
+            CreateTriangle(triangle);
+
+            //Print pascla triangle
+            PrintPascalTriangle(triangle);
+        }
+
+        static void PrintPascalTriangle(long[][] triangle)
+        {
+            foreach (long[] item in triangle)
+            {
+                Console.WriteLine(string.Join(" ", item));
+            }
+        }
+
+        static void CreateTriangle(long[][] triangle)
+        {
+            for (int row = 0; row < triangle.Length; row++)
+            {
+                triangle[row] = new long[row + 1];
+
+                for (int col = 0; col < row + 1; col++)
+                {
+                    long sum = 0;
+                    if (row - 1 >= 0 && col < triangle[row - 1].Length)
+                    {
+                        sum += triangle[row - 1][col];
+                    }
+                    if (row - 1 >= 0 && col - 1 >= 0)
+                    {
+                        sum += triangle[row - 1][col - 1];
+                    }
+                    if (sum==0)
+                    {
+                        sum = 1;
+                    }
+                    triangle[row][col] = sum;
+                }
+            }
+        }
+    }
+}
