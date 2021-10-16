8f234299432059dd606d8e02fd71f41a3e87646d
Denis Argatski <denis.argatski@gmail.com>
2021-10-16 21:59:30 +0300

    SpiralMatrix.cs

---
+++ C#/04.C# Advanced/Exercise/02.MultidimensionalArrays/MultidimensionalArrays-Exercises/07.01.SpiralMatrix/SpiralMatrix.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._01.SpiralMatrix
+{
+    class SpiralMatrix
+    {
+        static void Main(string[] args)
+        {
+            //input
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] spiralMatrix = new int[n, n];
+
+            //Create spiral matrix
+            CreateSpiralMatrix(spiralMatrix, n);
+
+            //Print spiral matrix
+            PrintSpiralMatrix(spiralMatrix);
+        }
+
+        /// <summary>
+        /// Print spiral matrix
+        /// </summary>
+        /// <param name="spiralMatrix"></param>
+        static void PrintSpiralMatrix(int[,] spiralMatrix)
+        {
+            for (int r = 0; r < spiralMatrix.GetLength(0); r++)
+            {
+                for (int c = 0; c < spiralMatrix.GetLength(1); c++)
+                {
+                    if (spiralMatrix[r,c]<10)
+                    {
+                        Console.Write(" "+spiralMatrix[r, c] + " ");
+                    }
+                    else 
+                    {
+                        Console.Write(spiralMatrix[r, c] + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Checked coordinats
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <param name="r"></param>
+        /// <param name="c"></param>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        static bool isRange(int[,]matrix,int r,int c,int s)
+        {
+            return r >= 0 && c >= 0 && r < s && c < s && matrix[r, c] != 0;
+        }
+
+        /// <summary>
+        /// Create spiral matrix.
+        /// </summary>
+        /// <param name="spiralMatrix"></param>
+        /// <param name="size"></param>
+        static void CreateSpiralMatrix(int[,] spiralMatrix, int size)
+        {
+            string direction = "right";
+            int row = 0;
+            int col = 0;
+            for (int i = 0; i < size * size; i++)
+            {
+                spiralMatrix[row, col] = i+1;
+                if (direction == "right")
+                {
+                    col++;
+                }
+                if (direction == "left")
+                {
+                    col--;
+                }
+                if (direction == "down")
+                {
+                    row++;
+                }
+                if (direction == "up")
+                {
+                    row--;
+                }
+
+
+                if ((col == size || isRange(spiralMatrix,row,col,size)) && direction == "right")
+                {
+                    row++;
+                    col--;
+                    direction = "down";
+                }
+                if ((row == size || isRange(spiralMatrix,row,col,size)) && direction == "down")
+                {
+                    row--;
+                    col--;
+                    direction = "left";
+                }
+                if ((col == -1 || isRange(spiralMatrix, row, col, size)) && direction == "left")
+                {
+                    row--;
+                    col++;
+                    direction = "up";
+                }
+                if ((row == -1 || isRange(spiralMatrix, row, col, size)) && direction == "up")
+                {
+                    row++;
+                    col++;
+                    direction = "right";
+                }
+            }
+        }
+    }
+}
