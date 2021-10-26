a2007dab8c8fc4a7b643d54d409b813c476efc8f
Denis Argatski <denis.argatski@gmail.com>
2021-10-26 22:57:25 +0300

    RecordUniqueNames.cs

---
+++ C#/04.C# Advanced/Exercise/03.SetsAndDictionaries/SetsAndDictionariesAdvanced-Lab/05.RecordUniqueName/RecordUniqueNames.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.RecordUniqueName
+{
+    class RecordUniqueNames
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int number = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new HashSet<string>();
+
+            //Processing
+            Processing(number, names);
+
+            //Print unique names
+            Print(names);
+        }
+
+        /// <summary>
+        /// Print unique names
+        /// </summary>
+        /// <param name="names"></param>
+        private static void Print(HashSet<string> names)
+        {
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        /// <summary>
+        /// Create list with unique names
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="names"></param>
+        static void Processing(int n, HashSet<string> names)
+        {
+            //Create list with unique names
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+
+                names.Add(name);
+            }
+        }
+    }
+}
+
