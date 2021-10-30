5fc1de4f909c60479b3c597bf1af81ea6e341ce0
Denis Argatski <denis.argatski@gmail.com>
2021-10-30 22:18:40 +0300

    ""

---
+++ C#/04.C# Advanced/Exercise/03.SetsAndDictionaries/SetAndDictionariesAdvanced-Exercies/01. Unique Usernames/UniqueUserNames.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Unique_Usernames
+{
+    class UniqueUserNames
+    {
+        static void Main(string[] args)
+        {
+            //Input 
+            int numberUserNames = int.Parse(Console.ReadLine());
+
+            HashSet<string> uniqueNames = new HashSet<string>();
+
+            //Processing
+            Processing(numberUserNames, uniqueNames);
+
+            //Print result
+            PrintUniqueNames(uniqueNames);
+
+        }
+
+        /// <summary>
+        /// Print unique names
+        /// </summary>
+        /// <param name="uniqueNames"></param>
+        static void PrintUniqueNames(HashSet<string> uniqueNames)
+        {
+            foreach (var name in uniqueNames)
+            {
+                Console.WriteLine($"{name}");
+            }
+        }
+
+        /// <summary>
+        /// Create list unique user
+        /// </summary>
+        /// <param name="numberUserNames"></param>
+        /// <param name="uniqueNames"></param>
+        static void Processing(int numberUserNames, HashSet<string> uniqueNames)
+        {
+            for (int i = 0; i < numberUserNames; i++)
+            {
+                string name = Console.ReadLine();
+
+                uniqueNames.Add(name);
+            }
+        }
+    }
+}
+
+
