48040c709b9f9c9cbf4406a474357f88923e3cdd
Denis Argatski <denis.argatski@gmail.com>
2021-10-30 22:22:18 +0300

    ""

---
+++ C#/04.C# Advanced/Exercise/03.SetsAndDictionaries/SetAndDictionariesAdvanced-Exercies/04.EvenTimes/EvenTimes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.EvenTimes
+{
+    class EvenTimes
+    {
+        static void Main(string[] args)
+        {
+            //Input
+
+        }
+    }
+}
+
+/*
+ *    static void Main(string[] args)
+        {
+            int rotations = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            for (int i = 0; i < rotations; i++)
+            {
+                int currentDigit = int.Parse(Console.ReadLine());
+
+                if (!numbers.ContainsKey(currentDigit))
+                {
+                    numbers.Add(currentDigit, 0);
+                }
+
+                numbers[currentDigit]++;
+            }
+
+
+            foreach (var digit in numbers)
+            {
+                if (digit.Value % 2 == 0)
+                {
+                    Console.WriteLine(digit.Key);
+                }
+            }
+        }
+    }
+ */
