567b06b4fdeae1e7b8aed7e1a69de6561b764f09
Denis Argatski <denis.argatski@gmail.com>
2021-11-01 22:26:09 +0200

    CountSymbols.cs

---
+++ C#/04.C# Advanced/Exercise/03.SetsAndDictionaries/SetAndDictionariesAdvanced-Exercies/05.CountSymbols/CountSymbols.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.CountSymbols
+{
+    class CountSymbols
+    {
+        static void Main(string[] args)
+        {
+            //Input 
+            string text = Console.ReadLine();
+
+            SortedDictionary<char/*symbol*/, int/*times*/> dict = new SortedDictionary<char, int>();
+
+            //Count symbols
+            CountSymbol(text, dict);
+
+            //Print result count symbols
+            Print(dict);
+        }
+        /// <summary>
+        /// Print counts the occurrences symbol.
+        /// </summary>
+        /// <param name="dict"></param>
+        private static void Print(SortedDictionary<char, int> dict)
+        {
+            foreach (var symbol in dict)
+            {
+                Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+            }
+        }
+
+        /// <summary>
+        /// Count symbols occurrences
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="dict"></param>
+        static void CountSymbol(string text, SortedDictionary<char, int> dict)
+        {
+            foreach (var symbol in text)
+            {
+                if (dict.ContainsKey(symbol) == false)
+                {
+                    dict.Add(symbol, 0);
+                }
+                dict[symbol]++;
+            }
+        }
+    }
+}
+
