78f86246ed40398e32804f22263974574621536e
Denis Argatski <denis.argatski@gmail.com>
2021-11-01 22:24:41 +0200

    EvenTimes.cs

--- C#/04.C# Advanced/Exercise/03.SetsAndDictionaries/SetAndDictionariesAdvanced-Exercies/04.EvenTimes/EvenTimes.cs
+++ C#/04.C# Advanced/Exercise/03.SetsAndDictionaries/SetAndDictionariesAdvanced-Exercies/04.EvenTimes/EvenTimes.cs
@@ -8,38 +8,50 @@ namespace _04.EvenTimes
         static void Main(string[] args)
         {
             //Input
+            int n = int.Parse(Console.ReadLine());
 
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            //Processing
+            Processing(n, numbers);
+
+            //Print
+            Print(numbers);
+        }
+
+        /// <summary>
+        /// Print even times number.
+        /// </summary>
+        /// <param name="numbers"></param>
+        static void Print(Dictionary<int, int> numbers)
+        {
+            foreach (var num in numbers)
+            {
+                if (num.Value % 2 == 0)
+                {
+                    Console.WriteLine(num.Key);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Create dicitionary with numbers.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="numbers"></param>
+        private static void Processing(int n, Dictionary<int, int> numbers)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                if (numbers.ContainsKey(currentNumber) == false)
+                {
+                    numbers.Add(currentNumber, 0);
+                }
+
+                numbers[currentNumber]++;
+            }
         }
     }
 }
 
-/*
- *    static void Main(string[] args)
-        {
-            int rotations = int.Parse(Console.ReadLine());
-
-            Dictionary<int, int> numbers = new Dictionary<int, int>();
-
-            for (int i = 0; i < rotations; i++)
-            {
-                int currentDigit = int.Parse(Console.ReadLine());
-
-                if (!numbers.ContainsKey(currentDigit))
-                {
-                    numbers.Add(currentDigit, 0);
-                }
-
-                numbers[currentDigit]++;
-            }
-
-
-            foreach (var digit in numbers)
-            {
-                if (digit.Value % 2 == 0)
-                {
-                    Console.WriteLine(digit.Key);
-                }
-            }
-        }
-    }
- */
