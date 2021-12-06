3b7c1d055ece1bbdaffd5db1e398d01dc4bf885e
Denis Argatski <denis.argatski@gmail.com>
2021-12-06 22:48:51 +0200

    SortEvenNums.cs

---
+++ C#/04.C# Advanced/Exercise/05.FunctionalProgramming/Functional Programming - Lab/01. Sort Even Numbers/SortEvenNums.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SortEvenNumbers
+{
+    class SortEvenNums
+    {
+        static void Main(string[] args)
+        {
+            //Input 
+            int[] evenNumbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                //.Select(IntParser)
+                .Where(n => n % 2 == 0)
+                //.Where(EvenNumbers)
+                //.Where(EvenNumbersSecond)
+                .OrderBy(n => n)
+                //.OrderBy(OrderBy);
+                .ToArray();
+
+            //Print result collection
+            Console.WriteLine(string.Join(", ", evenNumbers));
+
+        }
+
+        /// <summary>
+        /// This function select every string to integer.
+        /// </summary>
+        public static Func<string, int> Parser = n => int.Parse(n);
+
+        /// <summary>
+        /// This function select every strung to integer.
+        /// </summary>
+        public static Func<string, int> IntParser = n =>
+         {
+             int result = 0;
+
+             if (int.TryParse(n, out  result))
+             {
+                 return result;
+             }
+             return 0;
+         };
+
+        /// <summary>
+        /// This function finds which number is even.
+        /// </summary>
+        public static Func<int, bool> EvenNumbers = n => n % 2 == 0;
+    
+        /// <summary>
+        /// This function finds which number is even.
+        /// </summary>
+        public static Func<int, bool> EvenNumbersSecond = n =>
+         {
+             return n % 2 == 0;
+         };
+
+         /// <summary>
+         /// The function sorted elements in ascending order
+         /// </summary>
+        public static Func<int, int> OrderBy = o => o;
+        
+    }
+}
+
+
+
+
+
+
+
+
