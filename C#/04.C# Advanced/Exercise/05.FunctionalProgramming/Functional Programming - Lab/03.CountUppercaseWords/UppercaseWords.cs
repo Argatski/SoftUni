d60c6f794e75ec2c1072e4c416e3ad939c1d17ab
Denis Argatski <denis.argatski@gmail.com>
2021-12-07 21:56:19 +0200

    UppercaseWords.cs

---
+++ C#/04.C# Advanced/Exercise/05.FunctionalProgramming/Functional Programming - Lab/03.CountUppercaseWords/UppercaseWords.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class UppercaseWords
+    {
+        static void Main(string[] args)
+        {
+            //Input 
+            List<string> text =
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                //.Where(UppercaseWords)
+                //.Where(Uppercase)
+                .Where(w => char.IsUpper(w[0]))
+                .ToList();
+                //.ForEach(w => Console.WriteLine(w)); Another way to print result.
+
+            //Print uppercase words
+            foreach (var word in text)
+            {
+                Console.WriteLine(word);
+            }
+
+        }
+        /// <summary>
+        /// The function presents all the words that start with an uppercase.
+        /// </summary>
+        /*public static Func<string, bool> UppercaseWords = u => char.IsUpper(u[0]);*/
+
+        /// <summary>
+        /// The function presents all the words that start with an uppercase.
+        /// </summary>
+        public static Func<string, bool> Uppercase = w =>
+        {
+            if (char.IsUpper(w[0]))
+            {
+                return true;
+            }
+            return false;
+        };
+
+    }
+}
+
+//Another solution.
+/*
+  Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(w => char.IsUpper(w[0]))
+                .ToList()
+                .ForEach(w=>Console.WriteLine(w));
+ */
