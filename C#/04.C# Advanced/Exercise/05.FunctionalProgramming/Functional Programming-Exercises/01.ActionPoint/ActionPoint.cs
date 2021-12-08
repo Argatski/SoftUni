a9c60996c9758566836ee5a2c1b98f6596946ac4
Denis Argatski <denis.argatski@gmail.com>
2021-12-08 21:56:31 +0200

    ActionPoint.cs

---
+++ C#/04.C# Advanced/Exercise/05.FunctionalProgramming/Functional Programming-Exercises/01.ActionPoint/ActionPoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01.ActionPoint
+{
+    class ActionPoint
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            string[] text = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            //Print result
+            PrintResult(text);
+
+        }
+        /// <summary>
+        /// The action prints each word on a new line.
+        /// </summary>
+        public static Action<string[]> PrintResult = text => 
+        {
+            foreach (var word in text)
+            {
+                Console.WriteLine(word);
+            }
+        };
+       
+    }
+}
+
