9848a6c195491f0dd255175336197d665ef1c366
Denis Argatski <denis.argatski@gmail.com>
2021-12-08 21:59:14 +0200

    ""

---
+++ C#/04.C# Advanced/Exercise/05.FunctionalProgramming/Functional Programming-Exercises/02.KnightsOfHonor/PrintSirName.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.KnightsOfHonor
+{
+    class PrintSirName
+    {
+        static void Main(string[] args)
+        {
+            //Input 
+            List<string> names = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .ToList();
+
+            //Print result
+            PrintSir(names);
+
+        }
+
+        /// <summary>
+        /// The action appends the word "Sir" and prints a new word.
+        /// </summary>
+        public static Action<List<string>> PrintSir = n =>
+            {
+                n.ForEach(n => Console.WriteLine($"Sir {n}"));
+            };
+        
+    }
+}
+
