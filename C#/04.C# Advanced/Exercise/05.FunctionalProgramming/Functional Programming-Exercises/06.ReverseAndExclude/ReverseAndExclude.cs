a6759a001e5bdad1eaa512a6eb5e012d24ab703a
Denis Argatski <denis.argatski@gmail.com>
2021-12-09 22:44:19 +0200

    ReverseAndExclude.cs

---
+++ C#/04.C# Advanced/Exercise/05.FunctionalProgramming/Functional Programming-Exercises/06.ReverseAndExclude/ReverseAndExclude.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.ReverseAndExclude
+{
+    class ReverseAndExclude
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int[] number = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+
+            int divisibleNum = int.Parse(Console.ReadLine());
+
+            //Removes elements that are divisible 
+            Predicate<int> fillter = n => n % 2 != 0;
+            Func<int, bool> filterFunc = x => fillter(x);
+
+            //Reverse
+            
+
+            RevereseArray(number);
+
+            number = number.Where(filterFunc).ToArray();
+
+            Console.WriteLine(string.Join(" ", number));
+        }
+
+        public static Func<int[], int[]> RevereseArray = n =>
+        {
+            for (int i = 0; i < n.Length; i++)
+            {
+                int endNumber = n[n.Length - i];
+                int startNumber = n[i];
+
+                n[i] = endNumber;
+                n[n.Length - i] = startNumber;
+            }
+            return n;
+
+            /*Another reverse method
+              int[] result = new int[r.Length];
+
+               for (int i = 0; i < r.Length; i++)
+               {
+                     result[i] = r[r.Length - i - 1];
+               }
+               return result;
+             */
+        };
+
+        //Another solution use lambda.
+        /*static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int divisibleNum = int.Parse(Console.ReadLine());
+
+            numbers = numbers.Reverse()
+                .Where(n => n % divisibleNum != 0)
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }*/
+    }
+}
