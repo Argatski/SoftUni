03fc17d7cec06a254f5752899c278c792c2e2b1c
Denis Argatski <denis.argatski@gmail.com>
2021-12-14 21:41:22 +0200

    TriFunction.cs

---
+++ C#/04.C# Advanced/Exercise/05.FunctionalProgramming/Functional Programming-Exercises/13.TriFunction/TriFunction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _13.TriFunction
+{
+    class TriFunction
+    {
+        static void Main(string[] args)
+        {
+            int numberLenght = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Func<string, int, bool> isGreat = (a, b) => a.Sum(c=>c) >= b;
+
+            Func<Func<string, int, bool>, List<string>, string> returnFirst = (a, b) => b.FirstOrDefault(s => a(s, numberLenght));
+
+            string result = returnFirst(isGreat, names);
+
+            if (result!=null)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine(string.Empty);
+            }
+        }
+    }
+}
