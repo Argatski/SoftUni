f31ee9fbd74387f1a43d9149fbc7a52eed38cb0f
Denis Argatski <denis.argatski@gmail.com>
2022-01-04 22:55:51 +0200

    StartUp.cs

--- C#/04.C# Advanced/Exercise/06.DefiningClasses/DefiningClasses-Lab/03.CarConstructors/StartUp.cs
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DefiningClasses-Lab/03.CarConstructors/StartUp.cs
@@ -6,25 +6,30 @@ namespace CarManufacturer
     {
         public static void Main(string[] args)
         {
+            //Input
             string name = Console.ReadLine();
             string model = Console.ReadLine();
             int year = int.Parse(Console.ReadLine());
             double fuelQuantity = double.Parse(Console.ReadLine());
             double fuelConsumption = double.Parse(Console.ReadLine());
 
+            //Instant
             Car firstCar = new Car();
             Car secondCar = new Car(name, model, year);
             Car thirdCar = new Car(name, model, year, fuelQuantity, fuelConsumption);
-
+           
+            //Get distance
             firstCar.Drive(1);
             secondCar.Drive(2);
             thirdCar.Drive(3);
 
-            Console.WriteLine($"{firstCar.WhoIam(firstCar)}");
+            //Print result
+            Console.WriteLine($"{firstCar.ToString()}");
             Console.WriteLine("............");
-            Console.WriteLine($"{secondCar.WhoIam(secondCar)}");
+            Console.WriteLine($"{secondCar.ToString()}");
             Console.WriteLine("............");
-            Console.WriteLine($"{thirdCar.WhoIam(thirdCar)}");
+            Console.WriteLine($"{thirdCar.ToString()}");
+
         }
     }
 }
