dd7580065f0a55070dad1a843eb9887bccc4554b
Denis Argatski <denis.argatski@gmail.com>
2022-01-06 22:13:59 +0200

    Tire.cs

--- C#/04.C# Advanced/Exercise/06.DefiningClasses/DefiningClasses-Lab/04.CarEngineAndTires/Tire.cs
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DefiningClasses-Lab/04.CarEngineAndTires/Tire.cs
@@ -14,7 +14,8 @@ namespace CarManufacturer
         public int Year { get; set; }
         public double Pressure { get; set; }
 
-        public Tire(int year,double pressure)
+        //Constructor
+        public Tire(int year, double pressure)
         {
             Year = year;
             Pressure = pressure;
