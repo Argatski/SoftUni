947344453ebc72f452e8cc5d1051742a33796341
Denis Argatski <denis.argatski@gmail.com>
2022-01-06 22:14:21 +0200

    StartUp.cs

--- C#/04.C# Advanced/Exercise/06.DefiningClasses/DefiningClasses-Lab/04.CarEngineAndTires/StartUp.cs
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DefiningClasses-Lab/04.CarEngineAndTires/StartUp.cs
@@ -6,19 +6,20 @@ namespace CarManufacturer
     {
         public static void Main(string[] args)
         {
+            //Instance
             Tire[] tires = new Tire[4]
             {
                 new Tire(1,2.5),
                 new Tire(1,2.1),
                 new Tire(2,2.3),
-                new Tire(3,1.8)
+                new Tire(3,1.8),
             };
 
             Engine engine = new Engine(680, 6300);
 
             Car car = new Car($"Lamborgini", "Urus", 2010, 250, 9, engine, tires);
 
-            /*Console.WriteLine($"Mark{car.Make}\nModel{car.Model}\nYear{car.Year}\nTires{string.Join("\n",car.Tires)}");*/
+            Console.WriteLine(car.WhoIAm(car));
         }
     }
 }
