52da57cfb5efcadc62f403e29b17a6bc042851c1
Denis Argatski <denis.argatski@gmail.com>
2022-01-06 22:08:29 +0200

    StartUp.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DefiningClasses-Lab/04.BankAccountPersonClass/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankAccount
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
