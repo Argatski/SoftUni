d379fdc7f4b289406a285ba1811070f2796e2bc3
Denis Argatski <denis.argatski@gmail.com>
2022-01-10 22:22:01 +0200

    StartUp.cs

--- C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/02.CreatingConstructors/StartUp.cs
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/02.CreatingConstructors/StartUp.cs
@@ -6,9 +6,17 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
-           /* Person firstPerson = new Person();
+            /*
+             //Instance
+            Person firstPerson = new Person();
             Person secondPerson = new Person(25);
-            Person thirdPerson = new Person("Stamat", 45);*/
+            Person thirdPerson = new Person("Stamat", 45);
+            
+            //Print result
+            Console.WriteLine(firstPerson);
+            Console.WriteLine(secondPerson);
+            Console.WriteLine(thirdPerson);
+            */
         }
     }
 }
