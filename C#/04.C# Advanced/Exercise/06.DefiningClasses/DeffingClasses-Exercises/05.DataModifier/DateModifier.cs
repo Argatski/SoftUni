8f81af9c0fcb899a5dadb556fd8802e456af3af4
Denis Argatski <denis.argatski@gmail.com>
2022-01-11 22:25:21 +0200

    DateModifier.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/05.DataModifier/DateModifier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class DateModifier
+    {
+        //Methods
+        public static double Calculates(string first, string second)
+        {
+            var firstDate = DateTime.ParseExact(first, "yyyy MM dd", CultureInfo.InvariantCulture);
+
+            var secondDate = DateTime.ParseExact(second, "yyyy MM dd",
+                CultureInfo.InvariantCulture);
+
+            if (firstDate > secondDate)
+            {
+                return Calculates(second, first);
+            }
+            return (secondDate - firstDate).Days;
+        }
+    }
+}
