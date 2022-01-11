70623360840786fa120edf6705e4dc77cd05e4ab
Denis Argatski <denis.argatski@gmail.com>
2022-01-11 22:18:30 +0200

    Person.cs

--- C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/03.OldestFamilyMember/Person.cs
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/03.OldestFamilyMember/Person.cs
@@ -10,20 +10,40 @@ namespace DefiningClasses
         public string Name { get; set; }
         public int Age { get; set; }
 
-        //Constructor
+        //Constructors
+        /// <summary>
+        ///DefautThe first should take no arguments and produce a person with name "No name" and age = 1. 
+        /// </summary>
         public Person()
         {
             Name = "No name";
             Age = 1;
         }
-        public Person(int age):this()
+        /// <summary>
+        /// The second should accept only an integer number for the age and produce a person with name "No name" and age equal to the passed parameter.
+        /// </summary>
+        /// <param name="age"></param>
+        public Person(int age)
+            : this()
         {
             Age = age;
         }
-        public Person(string name,int age)
+        /// <summary>
+        ///The third one should accept a string for the name and an integer for the age and should produce a person with the given name and age.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="age"></param>
+        public Person(string name, int age)
+            : this(age)
         {
             Name = name;
             Age = age;
+        }
+
+        //Method
+        public override string ToString()
+        {
+            return $"{Name} {Age}";
         }
     }
 }
