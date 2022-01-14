5b287d7fa4905df5d18a1a06270e8dc2eb41bafd
Denis Argatski <denis.argatski@gmail.com>
2022-01-14 22:55:53 +0200

    Employee.cs

--- C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/06.01.CompanyRoster/Employee.cs
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/06.01.CompanyRoster/Employee.cs
@@ -4,6 +4,93 @@ using System.Text;
 
 namespace DefiningClasses
 {
+    public class Employee
+    {
+        //Properties
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+
+
+        //Constructor
+        /// <summary>
+        /// Default
+        /// </summary>
+        public Employee()
+        {
+        }
+        /// <summary>
+        /// The name, salary, position and department are mandatory.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="salary"></param>
+        /// <param name="position"></param>
+        /// <param name="department"></param>
+        public Employee(string name, double salary, string position, string department) : this()
+        {
+            Name = name;
+            Salary = salary;
+            Position = position;
+            Department = department;
+            Email = @"n/a";
+            Age = -1;
+        }
+        /// <summary>
+        /// If an employee doesn’t have an email – in place of that field you should print “n/a” instead.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="salary"></param>
+        /// <param name="position"></param>
+        /// <param name="department"></param>
+        /// <param name="email"></param>
+        /// <param name="age"></param>
+        public Employee(string name, double salary, string position, string department, int age)
+        : this(name, salary, position, department)
+        {
+            Email = @"n/a";
+            Age = age;
+        }
+        /// <summary>
+        /// "If he doesn’t have an age – print “-1” instead."
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="salary"></param>
+        /// <param name="position"></param>
+        /// <param name="departemnet"></param>
+        /// <param name="email"></param>
+        public Employee(string name, double salary, string position, string departemnet, string email) : this(name, salary, position, departemnet)
+        {
+            Email = email;
+            Age = -1;
+        }
+        /// <summary>
+        /// If all parameters are valid.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="salary"></param>
+        /// <param name="position"></param>
+        /// <param name="departemnet"></param>
+        /// <param name="email"></param>
+        public Employee(string name, double salary, string position, string departemnet, string email, int age) : this(name, salary, position, departemnet)
+        {
+            Email = email;
+            Age = age;
+        }
+
+
+    }
+}
+
+/*Another solution
+ using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
     class Employee
     {
         //This properties are mandatory
@@ -30,3 +117,5 @@ namespace DefiningClasses
 
     }
 }
+
+ */
