0dbe2a9242f28fca8e9f2e5d1008810bc3200c0e
Denis Argatski <denis.argatski@gmail.com>
2022-01-15 21:24:35 +0200

    StartUp.cs

--- C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/07.RawData/StartUp.cs
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/07.RawData/StartUp.cs
@@ -8,63 +8,80 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
+            //Input
             int number = int.Parse(Console.ReadLine());
 
+            //Create cars list
             List<Car> cars = new List<Car>();
 
             for (int i = 0; i < number; i++)
             {
                 string[] input = Console.ReadLine()
-                    .Split();
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
-                string model = input[0];
+                string name = input[0];
 
                 //Engine
                 int speed = int.Parse(input[1]);
                 int power = int.Parse(input[2]);
-                Engine engine = new Engine(speed,power);
+                Engine engine = new Engine(speed, power);
 
                 //Cargo
                 int weight = int.Parse(input[3]);
                 string type = input[4];
                 Cargo cargo = new Cargo(weight, type);
 
-                Tire[] tires = new Tire[]
+                //Tire
+                Tire[] tires = new Tire[4]
                 {
-                new Tire (double.Parse(input[5]),int.Parse(input[6])),
-                new Tire(  double.Parse(input[7]),int.Parse(input[8])),
-                new Tire( double.Parse(input[9]),int.Parse(input[10])),
-                new Tire( double.Parse(input[11]),int.Parse(input[12])),
+                    new Tire(double.Parse(input[5]),int.Parse(input[6])),
+                    new Tire(double.Parse(input[7]),int.Parse(input[8])),
+                    new Tire(double.Parse(input[9]),int.Parse(input[10])),
+                    new Tire(double.Parse(input[11]),int.Parse(input[12]))
                 };
 
-                //Check the car is unique.
-                Car car = new Car(model,engine,cargo,tires);
+                //Instance car
+                Car car = new Car(name, engine, cargo, tires);
+
+                //Add car in list cars
                 cars.Add(car);
             }
 
+            //Processing
+            Print(cars);
+
+        }
+
+
+        /// <summary>
+        /// Print type cargo "fragile" or "flammable".
+        /// </summary>
+        /// <param name="cars"></param>
+        /// <returns></returns>
+        public static void Print(List<Car> cars)
+        {
             string command = Console.ReadLine();
 
-            if (command=="fragile")
+            if (command == "fragile")
             {
-                List<Car> result = cars
-                    .Where(c => c.Cargo.Type == "fragile")
-                    .Where(c => c.Tires.Any(p => p.Pressure < 1)).ToList();
+                cars = cars
+                    .Where(t => t.Cargo.Type == "fragile")
+                    .Where(p => p.Tires.Any(p => p.Pressure < 1))
+                    .ToList();
 
-
-                foreach (var car in result)
+                foreach (var car in cars)
                 {
                     Console.WriteLine(car);
                 }
-
             }
             else
             {
-                List<Car> result = cars
-                    .Where(c => c.Cargo.Type == "flamable")
-                    .Where(c => c.Engine.Power > 250)
+                cars = cars
+                    .Where(t => t.Cargo.Type == "flammable")
+                    .Where(p => p.Engine.Power > 250)
                     .ToList();
 
-                foreach (var car in result)
+                foreach (var car in cars)
                 {
                     Console.WriteLine(car);
                 }
@@ -72,3 +89,5 @@ namespace DefiningClasses
         }
     }
 }
+
+
