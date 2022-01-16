2e1a5050cb19aeb7865779b8f689c5390c17f301
Denis Argatski <denis.argatski@gmail.com>
2022-01-16 21:57:29 +0200

    StartUp.cs

--- C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/08.CarSalesman/StartUp.cs
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/08.CarSalesman/StartUp.cs
@@ -8,7 +8,136 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
-            //Input engines
+            //Numbers about engines
+            int numberOfEngines = int.Parse(Console.ReadLine());
+
+            //List of engines
+            List<Engine> engines = new List<Engine>();
+
+            //Create engines
+            CreateEnginesList(numberOfEngines, engines);
+
+
+            //Numbers about a Car
+            int numbersOfCars = int.Parse(Console.ReadLine());
+
+            //List of cars
+            List<Car> cars = new List<Car>();
+
+            //Create of cars
+            CreateCarsList(numbersOfCars, cars, engines);
+
+            //Print output
+            PrintResult(cars);
+        }
+        /// <summary>
+        /// Your task is to print all the cars in the order they were received and their information in the format defined bellow. If any of the optional fields is missing, print "n/a".
+        /// </summary>
+        /// <param name="cars"></param>
+        private static void PrintResult(List<Car> cars)
+        {
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+
+        /// <summary>
+        /// Method about creating cars list.
+        /// </summary>
+        /// <param name="numbersOfCars"></param>
+        /// <param name="cars"></param>
+        private static void CreateCarsList(int numbersOfCars, List<Car> cars, List<Engine> engines)
+        {
+            for (int i = 0; i < numbersOfCars; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string model = input[0];
+                string engine = input[1];
+
+                Engine currentEngine = engines
+                    .Where(e => e.Model == engine)
+                    .FirstOrDefault();
+
+                if (input.Length == 2)
+                {
+                    cars.Add(new Car(model, currentEngine));
+                }
+                else if (input.Length == 3)
+                {
+                    if (int.TryParse(input[2], out int weight))
+                    {
+                        cars.Add(new Car(model, currentEngine, weight));
+                    }
+                    else
+                    {
+                        string color = input[2];
+                        cars.Add(new Car(model, currentEngine, color));
+                    }
+                }
+                else if (input.Length == 4)
+                {
+                    int weight = int.Parse(input[2]);
+                    string color = input[3];
+
+                    cars.Add(new Car(model, currentEngine, weight, color));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method about creating engines list.
+        /// </summary>
+        /// <param name="numberOfEngines"></param>
+        /// <param name="engines"></param>
+        private static void CreateEnginesList(int numberOfEngines, List<Engine> engines)
+        {
+            for (int i = 0; i < numberOfEngines; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string model = input[0];
+                int power = int.Parse(input[1]);
+
+                //Instance
+                if (input.Length == 2)
+                {
+                    Engine engine = new Engine(model, power);
+                    engines.Add(engine);
+                }
+                else if (input.Length == 3)
+                {
+                    if (int.TryParse(input[2], out int displacement))
+                    {
+                        Engine engine = new Engine(model, power, displacement);
+                        engines.Add(engine);
+                    }
+                    else
+                    {
+                        string efficiency = input[2];
+                        Engine engine = new Engine(model, power, efficiency);
+                        engines.Add(engine);
+                    }
+                }
+                else if (input.Length == 4)
+                {
+                    int displacement = int.Parse(input[2]);
+                    string efficiency = input[3];
+
+                    Engine engine = new Engine(model, power, displacement, efficiency);
+                    engines.Add(engine);
+                }
+
+            }
+        }
+    }
+}
+
+/*
+ *  //Input engines
             int numberOfEngines = int.Parse(Console.ReadLine());
 
             List<Engine> engines = new List<Engine>();
@@ -103,6 +232,4 @@ namespace DefiningClasses
                 Console.WriteLine(car);
             }
             
-        }
-    }
-}
+ */
