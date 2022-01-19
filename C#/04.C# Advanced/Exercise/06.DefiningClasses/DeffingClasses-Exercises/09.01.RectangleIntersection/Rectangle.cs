0129dd21019aa287f76e4c3d485fdaa951673f26
Denis Argatski <denis.argatski@gmail.com>
2022-01-19 22:29:56 +0200

    Rectangle.cs

--- C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/09.01.RectangleIntersection/Rectangle.cs
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/09.01.RectangleIntersection/Rectangle.cs
@@ -2,42 +2,49 @@
 using System.Collections.Generic;
 using System.Text;
 
-
-public class Rectangle
+namespace DefiningClasses
 {
-    private string id;
-    private double width;
-    private double height;
-    private double x;
-    private double y;
-
-    public string Id
+    public class Rectangle
     {
-        get
+        //Fields
+        private string id;
+        private double width;
+        private double height;
+        private double x;
+        private double y;
+
+        //Properties
+        public string Id
         {
-            return this.id;
-        }
-    }
-
-    public Rectangle(string id, double width, double height, double x, double y)
-    {
-        this.id = id;
-        this.width = width;
-        this.height = height;
-        this.x = x;
-        this.y = y;
-    }
-
-    public bool Intersects(Rectangle other)
-    {
-        if ((other.y >= this.y && other.y - other.height <= this.y && other.x <= this.x && other.x + other.width >= this.x) ||
-            (other.y >= this.y && other.y - other.height <= this.y && other.x >= this.x && other.x <= this.x + this.width) ||
-            (other.y <= this.y && other.y >= this.y - this.height && other.x <= this.x && other.x + other.width >= this.x) ||
-            (other.y <= this.y && other.y >= this.y - this.height && other.x >= this.x && other.x <= this.x + this.width))
-        {
-            return true;
+            get
+            {
+                return this.id;
+            }
         }
 
-        return false;
+        public Rectangle(string id, double width, double height, double x, double y)
+        {
+            this.id = id;
+            this.width = width;
+            this.height = height;
+            this.x = x;
+            this.y = y;
+        }
+        /// <summary>
+        /// Mehtod which receives as a parameter another Rectangle, checks if the two rectangles intersect and returns true or false.
+        /// </summary>
+        /// <returns></returns>
+        public bool Intersects(Rectangle other)
+        {
+            if ((other.y >= this.y && other.y - other.height <= this.y && other.x <= this.x && other.x + other.width >= this.x) ||
+                (other.y >= this.y && other.y - other.height <= this.y && other.x >= this.x && other.x <= this.x + this.width) ||
+                (other.y <= this.y && other.y >= this.y - this.height && other.x <= this.x && other.x + other.width >= this.x) ||
+                (other.y <= this.y && other.y >= this.y - this.height && other.x >= this.x && other.x <= this.x + this.width))
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
