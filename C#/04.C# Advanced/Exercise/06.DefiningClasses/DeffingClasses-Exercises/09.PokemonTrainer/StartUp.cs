6da8668aef283e4cc0dee04b826dc8831005af2d
Denis Argatski <denis.argatski@gmail.com>
2022-01-20 22:20:32 +0200

    StartUp.cs

--- C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/09.PokemonTrainer/StartUp.cs
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/09.PokemonTrainer/StartUp.cs
@@ -9,10 +9,93 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
-            //Input
+            /*First etap.
+             * You will be receiving lines until you receive the command "Tournament". Each line will carry information about a pokemon and the trainer who caught it in the format:
+                "{trainerName} {pokemonName} {pokemonElement} {pokemonHealth}"
+             */
+
+            string arguments;
+            var trainers = new Dictionary<string, Trainer>();
+
+            while ((arguments = Console.ReadLine()) != "Tournament")
+            {
+                string[] input = arguments
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                //Triner information
+                string trainerName = input[0];
+
+                //Pokemon information
+                string pokemonName = input[1];
+                string element = input[2];
+                int health = int.Parse(input[3]);
+
+                //Instance Pokemon
+                Pokemon pokemon = new Pokemon(pokemonName, element, health);
+
+                //Instance Trainer
+                //Is name unique
+                if (!trainers.ContainsKey(trainerName))
+                {
+                    trainers.Add(trainerName, new Trainer(trainerName));
+                }
+
+                trainers[trainerName].AddPokemons(pokemon);
+            }
+
+            //Second etap
+            /*After receiving the command "Tournament", you will start receiving commands until the "End" command is received. They can contain one of the following:
+             * •	"Fire"
+               •	"Water"
+               •	"Electricity"
+             */
+
+            while ((arguments = Console.ReadLine()) != "End")
+            {
+                switch (arguments)
+                {
+                    case "Fire":
+                        foreach (var trainer in trainers.Values)
+                        {
+                            trainer.CheckPokemons(arguments);
+                        }
+                        break;
+                    case "Water":
+                        foreach (var trainer in trainers.Values)
+                        {
+                            trainer.CheckPokemons(arguments);
+                        }
+                        break;
+                    case "Electricity":
+                        foreach (var trainer in trainers.Values)
+                        {
+                            trainer.CheckPokemons(arguments);
+                        }
+                        break;
+                }
+            }
+
+            //Print output.
+            Print(trainers);
+        }
+
+        private static void Print(Dictionary<string, Trainer> trainers)
+        {
+            var result = trainers.Values.OrderByDescending(b => b.NumberOfBadges).ToList();
+
+            foreach (var trainer in result)
+            {
+                Console.WriteLine(trainer);
+            }
+        }
+    }
+}
+
+/*
+ *   //Input
             string command;
 
-            var trainers = new Dictionary<string,Trainer>();
+            var trainers = new Dictionary<string,TrainerHelper>();
 
             while ((command = Console.ReadLine()) != "Tournament")
             {
@@ -28,7 +111,7 @@ namespace DefiningClasses
                 int pokemonHealth = int.Parse(arguments[3]);
 
                 //Initialising pokemon
-                Pokemon pokemon = new Pokemon
+                PokemonHelper pokemon = new PokemonHelper
                     (
                     pokemonName, pokemonElemnt, pokemonHealth
                     );
@@ -36,7 +119,7 @@ namespace DefiningClasses
                 //Is name unique 
                 if (!trainers.ContainsKey(trainerName))
                 {
-                    trainers.Add(trainerName, new Trainer(trainerName));
+                    trainers.Add(trainerName, new TrainerHelper(trainerName));
                 }
 
                 //Add another pockemon
@@ -76,8 +159,4 @@ namespace DefiningClasses
             {
                 Console.WriteLine(trainer);
             }
-
-        }
-
-    }
-}
+ */
