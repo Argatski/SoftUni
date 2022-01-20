6af3b354beace854192af13c0ed1962f5e3bafe6
Denis Argatski <denis.argatski@gmail.com>
2022-01-20 22:20:45 +0200

    Trainer.cs

--- C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/09.PokemonTrainer/Trainer.cs
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/09.PokemonTrainer/Trainer.cs
@@ -3,63 +3,76 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace DefiningClasses
-
 {
     public class Trainer
     {
         //Fields
-        private List<Pokemon> pokemos;
+        private List<Pokemon> pokemons;
 
         //Properties
         public string Name { get; set; }
-
-        public int NumberOfBadges { get; set; } 
-
+        public int NumberOfBadges { get; set; }
 
         //Constructor
         public Trainer(string name)
         {
             Name = name;
             NumberOfBadges = 0;
-            pokemos = new List<Pokemon>();
+            pokemons = new List<Pokemon>();
         }
 
-        //Mehtonds
+        //Methods
+        /// <summary>
+        /// Add pokemons for every trainer. 
+        /// </summary>
+        /// <param name="pokemon"></param>
         public void AddPokemons(Pokemon pokemon)
         {
-            pokemos.Add(pokemon);
+            pokemons.Add(pokemon);
         }
 
-        public void CheckPokemons(string element) 
+        /// <summary>
+        /// For every command you must check if a trainer has at least 1 pokemon with the given element. 
+        /// </summary>
+        /// <param name="element"></param>
+        public void CheckPokemons(string element)
         {
-            foreach (var pokemon in pokemos)
+            foreach (var pokemon in pokemons)
             {
-                if (pokemon.Element==element)
+                //If he does, he receives 1 badge. 
+                if (pokemon.Element == element)
                 {
                     NumberOfBadges++;
                     break;
                 }
+                //Otherwise, all of his pokemon lose 10 health. 
                 else
                 {
                     pokemon.Health -= 10;
                 }
             }
 
-            for (int i = 0; i < pokemos.Count; i++)
+            //
+            for (int i = 0; i < pokemons.Count; i++)
             {
-                Pokemon pokemon = pokemos[i];
+                Pokemon pokemon = pokemons[i];
 
-                if (pokemon.Health<=0)
+                //If a pokemon falls to 0 or less health, he dies and must be deleted from the trainer’s collection. 
+                if (pokemon.Health <= 0)
                 {
-                    pokemos.Remove(pokemon);
+                    pokemons.Remove(pokemon);
                     i--;
                 }
             }
         }
 
+        /// <summary>
+        /// Pritn format
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
-            return $"{Name} {NumberOfBadges} {pokemos.Count}"; 
+            return $"{Name} {NumberOfBadges} {pokemons.Count}";
         }
     }
 }
