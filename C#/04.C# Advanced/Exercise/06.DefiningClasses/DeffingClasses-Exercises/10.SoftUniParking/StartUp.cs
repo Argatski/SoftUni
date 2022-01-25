cd481d1899dd4145f2ced2daaf73cd8b99c83ffe
Denis Argatski <denis.argatski@gmail.com>
2022-01-25 21:54:28 +0200

    StartUp.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/10.SoftUniParking/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SoftUniParking
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //Instance cars
+            var car = new Car("Skoda", "Fabia", 65, "CC1856BG");
+            var car2 = new Car("Audi", "A3", 110, "EB8787MN");
+
+            Console.WriteLine(car.ToString());
+
+            //Instace parking
+            var parking = new Parking(5);
+            Console.WriteLine(parking.AddCar(car));
+
+            Console.WriteLine(parking.AddCar(car));
+
+            Console.WriteLine(parking.AddCar(car2));
+
+            Console.WriteLine(parking.GetCar("EB8787MN"));
+
+            Console.WriteLine(parking.RemoveCar("EB8787MN"));
+
+            Console.WriteLine(parking.Count);
+        }
+    }
+}
