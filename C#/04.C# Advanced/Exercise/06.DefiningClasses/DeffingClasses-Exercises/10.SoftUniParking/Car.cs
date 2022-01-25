107d5aa92784493b4cf7fe262449ee69f808463f
Denis Argatski <denis.argatski@gmail.com>
2022-01-25 21:53:56 +0200

    Car.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/10.SoftUniParking/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Car
+    {
+        //Properties
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int HorsePower { get; set; }
+        public string RegistrationNumber { get; set; }
+
+        //Constructor
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            Make = make;
+            Model = model;
+            HorsePower = horsePower;
+            RegistrationNumber = registrationNumber;
+        }
+
+        //Method
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+            result.AppendLine($"Make: {Make}");
+            result.AppendLine($"Model: {Model}");
+            result.AppendLine($"HorsePower: {HorsePower}");
+            result.Append($"RegistrationNumber: {RegistrationNumber}");
+
+            return result.ToString();
+        }
+    }
+}
