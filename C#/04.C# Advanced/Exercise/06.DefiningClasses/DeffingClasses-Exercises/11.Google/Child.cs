4c8875f4795e96ef0cebb7c5d97e0101ac660c10
Denis Argatski <denis.argatski@gmail.com>
2022-01-26 21:08:54 +0200

    Child.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/11.Google/Child.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _11.Google
+{
+    public class Child
+    {
+        //Field
+        public string name;
+        public string birthday;
+
+        //Constructor
+        public Child(string name, string birthday)
+        {
+            this.name = name;
+            this.birthday = birthday;
+        }
+
+        //Print format
+        public override string ToString()
+        {
+            return this.name + " " + this.birthday;
+        }
+    }
+}
