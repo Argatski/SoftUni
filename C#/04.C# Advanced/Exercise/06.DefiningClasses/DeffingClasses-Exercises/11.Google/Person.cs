d9b557c1636fefb4c3981fd678b0a9a206925722
Denis Argatski <denis.argatski@gmail.com>
2022-01-26 21:09:53 +0200

    Person.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/11.Google/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _11.Google
+{
+    public class Person
+    {
+        public string name;
+        public Company company;
+        public Car car;
+        public List<Pokemon> pokemons;
+        public List<Parent> parents;
+        public List<Child> children;
+
+        //Consturctor
+        public Person(string name)
+        {
+            this.name = name;
+            this.car = null;
+            this.pokemons = new List<Pokemon>();
+            this.parents = new List<Parent>();
+            this.children = new List<Child>();
+        }
+    }
+}
