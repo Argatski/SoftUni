6333e177a1dc93d1a461b9e1e052f4220ab2ea96
Denis Argatski <denis.argatski@gmail.com>
2022-01-26 21:09:17 +0200

    Company.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/11.Google/Company.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _11.Google
+{
+    public class Company
+    {
+        //Fields
+        public string name;
+        public string department;
+        public decimal salary;
+
+        //Construnctor
+        public Company(string name, string department, decimal salary)
+        {
+            this.name = name;
+            this.department = department;
+            this.salary = salary;
+        }
+
+        //Print format
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2:F2}", this.name, this.department, this.salary);
+        }
+
+    }
+}
