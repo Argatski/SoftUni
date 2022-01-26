93d834db6ffd0ac608e677e42aa713b6af5bf677
Denis Argatski <denis.argatski@gmail.com>
2022-01-26 21:10:10 +0200

    Pokemon.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/11.Google/Pokemon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _11.Google
+{
+    public class Pokemon
+    {
+        //Field
+        public string name;
+        public string type;
+
+        //Constructor
+        public Pokemon(string name, string type)
+        {
+            this.name = name;
+            this.type = type;
+        }
+
+        //Print format
+        public override string ToString()
+        {
+            return this.name + " " + this.type;
+        }
+    }
+}
