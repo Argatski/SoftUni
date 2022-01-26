f3a89f7119703c4518be0c3f7221b406d95486d9
Denis Argatski <denis.argatski@gmail.com>
2022-01-26 21:08:37 +0200

    Car.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/11.Google/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _11.Google
+{
+    public class Car
+    {
+        //Fields
+        public string model;
+        public int speed;
+
+        //Constructr
+        public Car(string model, int speed)
+        {
+            this.model = model;
+            this.speed = speed;
+        }
+
+        //Print format
+        public override string ToString()
+        {
+            return this.model + " " + this.speed;
+        }
+    }
+}
