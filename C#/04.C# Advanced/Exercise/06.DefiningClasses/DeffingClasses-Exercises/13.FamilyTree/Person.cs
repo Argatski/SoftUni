537a734fd5435ab3b1323e6405644d2fcb1f9a9c
Denis Argatski <denis.argatski@gmail.com>
2022-01-27 22:46:44 +0200

    Person.cs

---
+++ C#/04.C# Advanced/Exercise/06.DefiningClasses/DeffingClasses-Exercises/13.FamilyTree/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _13.FamilyTree
+{
+    class Person
+    {
+        //Properteis
+        public string Name { get; set; }
+        public string BirthDate { get; set; }
+        public List<Person> Parents { get; set; }
+        public List<Person> Children { get; set; }
+
+        //Constructor   
+        public Person()
+        {
+            Parents = new List<Person>();
+            Children = new List<Person>();
+        }
+        public Person(string name, string birthDate) : this()
+        {
+            Name = name;
+            BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            //Parents output
+            sb.AppendLine(Name + " " + BirthDate);
+            sb.AppendLine("Parents:");
+            foreach (var parent in Parents)
+            {
+                sb.AppendLine(parent.Name + " " + parent.BirthDate);
+            }
+
+            //Children output
+            sb.AppendLine("Children:");
+
+            foreach (var child in Children)
+            {
+                sb.AppendLine(child.Name + " " + child.BirthDate);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
