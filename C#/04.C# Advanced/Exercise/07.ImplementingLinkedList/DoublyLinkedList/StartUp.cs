f3cf1c50d43d8c7c8f0223ea78c3d0f888d8758c
Denis Argatski <denis.argatski@gmail.com>
2022-01-30 22:57:22 +0200

    StartUp.cs

---
+++ C#/04.C# Advanced/Exercise/07.ImplementingLinkedList/DoublyLinkedList/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomDoublyLinkedList
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Input  (Instance)
+            DoublyLinkedList list = new DoublyLinkedList();
+
+            //Add numbers in head
+            for (int i = 0; i < 10; i++)
+            {
+                list.AddFirst(i);
+            }
+
+
+
+            //Add numbers in tail
+            for (int j = 0; j < 10; j++)
+            {
+                list.AddLast(j);
+            }
+
+            ////Remove first 3 numbers
+            for (int k = 0; k < 3; k++)
+            {
+                Console.WriteLine($"Remove: {list.RemoveFirst()}");
+            }
+
+            //////remove last 3 numbers
+            for (int z = 0; z < 3; z++)
+            {
+                Console.WriteLine($"remove: {list.RemoveLast()}");
+            }
+
+            //Print resutl
+            list.ForEach(n => Console.WriteLine(n));
+
+            Console.WriteLine("Print LinkedList convert to Array:");
+            //Print array result
+            var result = list.ToArray();
+            Console.WriteLine($"{string.Join(", ",result)}");
+        }
+    }
+}
