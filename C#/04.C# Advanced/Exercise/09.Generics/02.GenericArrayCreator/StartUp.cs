eebd540be5aec30c0a0a241ca68793e3f99ca6b6
Denis Argatski <denis.argatski@gmail.com>
2022-02-09 23:09:33 +0200

    StartUp.cs

---
+++ C#/04.C# Advanced/Exercise/09.Generics/02.GenericArrayCreator/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace GenericArrayCreator
+{
+    using System;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+            int[] integers = ArrayCreator.Create(10, 33);
+
+            Console.WriteLine(string.Join(",", strings));
+            Console.WriteLine(string.Join(",", integers));
+        }
+    }
+}
