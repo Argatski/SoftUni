43db5a57eb60ff0d1461792f6020f4572aff281d
Denis Argatski <denis.argatski@gmail.com>
2022-02-09 23:19:57 +0200

    "Update"

---
+++ C#/04.C# Advanced/Exercise/09.Generics/GenericLab/01.BoxOfT/BoxOfT.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T> //If we have<> that mean is generics
+    {
+        //Field
+        private Stack<T> list; // We can use List<T> but is slow if elements are more.
+
+        //Propertie
+        public int Count { get; private set; }
+
+        public Box()
+        {
+            list = new Stack<T>();
+        }
+
+        //Mehtods
+        /// <summary>
+        /// Adds an element on the top of list
+        /// </summary>
+        /// <param name="element"></param>
+        public void Add(T element)
+        {
+            list.Push(element);
+            Count++;
+        }
+
+        //Removes the topmost element.
+        public T Remove()
+        {
+            return list.Pop();
+        }
+    }
+}
+
---
+++ C#/04.C# Advanced/Exercise/09.Generics/GenericLab/02.GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,23 @@
+﻿namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+
+        /// <summary>
+        /// The method should return an array with the given length and every element should be set to the given default item.
+        /// </summary>
+        /// <param name="lenght"></param>
+        /// <param name=""></param>
+        /// <returns></returns>
+        public static T[] Create<T>(int lenght, T item)
+        {
+            T[] array = new T[lenght];
+            for (int i = 0; i < lenght; i++)
+            {
+                array[i] = item;
+            }
+
+            return array;
+        }
+    }
+}
