66f3aca72bb187cae877eb1758c6be09c5e59400
Denis Argatski <denis.argatski@gmail.com>
2022-02-10 20:38:58 +0200

    EqualityScale.cs

--- C#/04.C# Advanced/Exercise/09.Generics/GenericLab/03.GenericScale/EqualityScale.cs
+++ C#/04.C# Advanced/Exercise/09.Generics/GenericLab/03.GenericScale/EqualityScale.cs
@@ -4,7 +4,28 @@ using System.Text;
 
 namespace GenericScale
 {
-    class EqualityScale
+    
+    public class EqualityScale<T>
     {
+        //Fieald
+        private T firstElement;
+        private T secondElement;
+
+        //Constructor
+        public EqualityScale(T first, T second)
+        {
+            this.firstElement = first;
+            this.secondElement = second;
+        }
+
+        /// <summary>
+        /// The method should return true if the elements are eauql.
+        /// </summary>
+        /// <returns></returns>
+        public bool AreEqual()
+        {
+            return this.firstElement.Equals(this.secondElement);
+        }
     }
+    
 }
