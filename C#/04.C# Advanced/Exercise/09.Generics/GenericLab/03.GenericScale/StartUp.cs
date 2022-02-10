56ebd3522cc080d8bd61da03c877863c55c7d1e6
Denis Argatski <denis.argatski@gmail.com>
2022-02-10 20:39:20 +0200

    StartUp.cs

--- C#/04.C# Advanced/Exercise/09.Generics/GenericLab/03.GenericScale/StartUp.cs
+++ C#/04.C# Advanced/Exercise/09.Generics/GenericLab/03.GenericScale/StartUp.cs
@@ -6,7 +6,19 @@ namespace GenericScale
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int first = 5;
+            int second = 5;
+
+            EqualityScale<int> equalityScale = new EqualityScale<int>(first, second);
+
+            Console.WriteLine(equalityScale.AreEqual());
+
+            string text1 = "Denis";
+            string text2 = "DeniS";
+
+            EqualityScale<string> equalityScale1 = new EqualityScale<string>(text1, text2);
+
+            Console.WriteLine(equalityScale1.AreEqual());
         }
     }
 }
