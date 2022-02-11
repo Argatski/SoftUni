99758b55ccf90d7fe89a15e7c7947bf31a5b2645
Denis Argatski <denis.argatski@gmail.com>
2022-02-11 22:48:54 +0200

    Box.cs

---
+++ C#/04.C# Advanced/Exercise/09.Generics/GenericsExercise/01.GenericBoxOfString/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        //Field
+        private T text;
+
+        //Constructors
+        public Box(T text)
+        {
+            this.text = text;
+        }
+
+        //Print
+        public override string ToString()
+        {
+            return $"{this.text.GetType().FullName}: {this.text}";
+        }
+    }
+}
