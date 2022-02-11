be14de7ee8d5ed16ada0918b1d0b2c02f52e76d8
Denis Argatski <denis.argatski@gmail.com>
2022-02-11 22:52:04 +0200

    Box.cs

---
+++ C#/04.C# Advanced/Exercise/09.Generics/GenericsExercise/02.GenericBoxOfInteger/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfInteger
+{
+    class Box<T>
+    {
+        //Field
+        private T value;
+
+        //Constructor
+        public Box(T value)
+        {
+            this.value = value;
+        }
+
+        //Print
+        public override string ToString()
+        {
+            return $"{this.value.GetType().FullName}: {this.value}"; 
+        }
+    }
+}
