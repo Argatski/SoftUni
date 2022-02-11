d2cdfa4a9b394e31abc53ecb74cc20e873c33a95
Denis Argatski <denis.argatski@gmail.com>
2022-02-11 22:49:28 +0200

    StartUp.cs

---
+++ C#/04.C# Advanced/Exercise/09.Generics/GenericsExercise/01.GenericBoxOfString/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int numberOfString = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfString; i++)
+            {
+                var text = Console.ReadLine();
+
+                Box<string> box = new Box<string>(text);
+
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
