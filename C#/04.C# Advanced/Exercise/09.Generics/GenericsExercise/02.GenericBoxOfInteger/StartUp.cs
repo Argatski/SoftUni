cf33f160fc06cf444ead793b3b7c8c312e9a1ea9
Denis Argatski <denis.argatski@gmail.com>
2022-02-11 22:52:24 +0200

    StartUp.cs

---
+++ C#/04.C# Advanced/Exercise/09.Generics/GenericsExercise/02.GenericBoxOfInteger/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericBoxOfInteger
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Input
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                Box<int> box = new Box<int>(input);
+
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
