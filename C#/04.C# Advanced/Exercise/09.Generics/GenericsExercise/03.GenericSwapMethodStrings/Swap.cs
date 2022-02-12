d1398cc8c4a651662abf9e5c3b36832f76730316
Denis Argatski <denis.argatski@gmail.com>
2022-02-12 22:01:23 +0200

    Swap.cs

---
+++ C#/04.C# Advanced/Exercise/09.Generics/GenericsExercise/03.GenericSwapMethodStrings/Swap.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodStrings
+{
+    class Swap<T>
+    {
+        //Field
+        private List<T> list;
+
+        public Swap()
+        {
+            this.list = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            list.Add(element);
+        }
+
+        //Swap
+        public List<T> SwapString(int firstIndex, int secondIndex)
+        {
+            var first = list[firstIndex];
+            list[firstIndex] = list[secondIndex];
+            list[secondIndex] = first;
+
+            return list;
+        }
+
+
+        /// <summary>
+        /// If collection is empty
+        /// </summary>
+        private void ChekckIfEmpty()
+        {
+            if (this.list.Count == null)
+            {
+                throw new InvalidOperationException("Collection is empty!");
+            }
+        }
+
+        //Print 
+        public void Print()
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.GetType().FullName}: {item}");
+            }
+        }
+    }
+}
