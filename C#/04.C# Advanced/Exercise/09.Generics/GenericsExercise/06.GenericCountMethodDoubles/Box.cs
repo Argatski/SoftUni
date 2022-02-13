d7dd56301ae08fa945318d63bda001844379a154
Denis Argatski <denis.argatski@gmail.com>
2022-02-13 18:01:46 +0200

    Box.cs

---
+++ C#/04.C# Advanced/Exercise/09.Generics/GenericsExercise/06.GenericCountMethodDoubles/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCountMethodDoubles
+{
+    public class Box<T> : IComparable<T>
+        where T : IComparable<T>
+    {
+        private T element;
+
+        //Constructor
+        public Box(T value)
+        {
+            element = value;
+        }
+
+        //CompareTo elements
+        public int CompareTo(T value)
+        {
+            return element.CompareTo(value);
+        }
+    }
+}
