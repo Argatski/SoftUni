d430fbad54b8d79c08ff931eb63b7360485939eb
Denis Argatski <denis.argatski@gmail.com>
2022-02-13 18:00:28 +0200

    Box.cs

---
+++ C#/04.C# Advanced/Exercise/09.Generics/GenericsExercise/05.GenericCountMethodStrings/Box.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCountMethodStrings
+{
+    public class Box<T>
+        where T : IComparable<T>
+    {
+        //Field
+        private List<T> list;
+
+        //Constructor
+        public Box()
+        {
+            this.list = new List<T>();
+        }
+
+        //Add
+        public void Add(T element)
+        {
+            this.list.Add(element);
+        }
+
+        //The method should return the count of elements that are greater than the value of the given element.
+        public int GetCount(T value)
+        {
+            int count = 0;
+
+            foreach (var item in this.list)
+            {
+                if (item.CompareTo(value) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+    }
+}
