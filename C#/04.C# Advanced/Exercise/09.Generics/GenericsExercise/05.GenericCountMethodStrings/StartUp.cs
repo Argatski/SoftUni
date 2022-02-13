f67b82948d713839590dacdca1446e279e914631
Denis Argatski <denis.argatski@gmail.com>
2022-02-13 18:00:46 +0200

    StartUp.cs

---
+++ C#/04.C# Advanced/Exercise/09.Generics/GenericsExercise/05.GenericCountMethodStrings/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GenericCountMethodStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //On the first line, you will receive n - the number of elements to add to the list.
+            int n = int.Parse(Console.ReadLine());
+
+            //Instance
+            Box<string> box = new Box<string>();
+
+            //On the next n lines, you will receive the actual elements.
+            for (int i = 0; i < n; i++)
+            {
+                string elements = Console.ReadLine();
+
+                box.Add(elements);
+            }
+
+            //On the last line, you will get the value of the element for comparison.
+            string value = Console.ReadLine();
+
+            //Print count of elements that are larger than the value.
+
+            Console.WriteLine(box.GetCount(value));
+        }
+    }
+}
