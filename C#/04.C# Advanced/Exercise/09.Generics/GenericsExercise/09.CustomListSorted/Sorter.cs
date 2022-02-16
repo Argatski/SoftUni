eca02a1f240132bdbcd1c1c06b6d80af8e5935e5
Denis Argatski <denis.argatski@gmail.com>
2022-02-16 22:59:19 +0200

    Sorter.cs

---
+++ C#/04.C# Advanced/Exercise/09.Generics/GenericsExercise/09.CustomListSorted/Sorter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CustomListSorted
+{
+    public class Sorter
+    {
+        public static void Sort<T>(ref CustomList<T> list)
+        where T : IComparable<T>
+        {
+            List<T> sorted = list.OrderBy(e => e).ToList();
+
+            list = new CustomList<T>(sorted);
+        }
+    }
+}
