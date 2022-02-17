883b969b1ee41a5425027c22f48ccc447c064e54
Denis Argatski <denis.argatski@gmail.com>
2022-02-17 22:05:43 +0200

    Threeuple.cs

---
+++ C#/04.C# Advanced/Exercise/09.Generics/GenericsExercise/10.Threeuple/Threeuple.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threeuple
+{
+    public class Threeuple<T1, T2, T3>
+    {
+        //Properties
+        public T1 first { get; set; }
+        public T2 second { get; set; }
+        public T3 third { get; set; }
+
+
+        //Constructor
+        public Threeuple(T1 first, T2 second, T3 third)
+        {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+        }
+
+        //Print result
+        public override string ToString()
+        {
+            return $"{first} -> {second} -> {third}"; 
+        }
+    }
+}
