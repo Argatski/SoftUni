50ba9d65d7fd59adeede86a04615bb2a88f4eff4
Denis Argatski <denis.argatski@gmail.com>
2022-02-21 22:29:02 +0200

    Update Airfield.cs

--- C#/04.C# Advanced/Exams/Exam-16-Dec-2021/03.DronesProblemDescription/Airfield.cs
+++ C#/04.C# Advanced/Exams/Exam-16-Dec-2021/03.DronesProblemDescription/Airfield.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace Drones
 {
@@ -101,6 +102,62 @@ namespace Drones
             }
 
             return dronesRemoved;
+        }
+
+        /// <summary>
+        /// Method – fly (set its available property to false without removing it from the collection) the drone with the given name if exists. As a result return the drone, or null if does not exist.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public Drone FlyDrone(string name)
+        {
+            Drone flyDrone = null;
+            foreach (var drone in Drones)
+            {
+                if (drone.Name == name)
+                {
+                    drone.Available = false;
+                    flyDrone = drone;
+                }
+            }
+
+            return flyDrone;
+        }
+
+        /// <summary>
+        /// FlyDrones method - fly and returns all drones which have a range equal or bigger to the given. Return a list of all drones which have been flown. The range will always be valid.
+        /// </summary>
+        /// <param name="range"></param>
+        /// <returns></returns>
+        public List<Drone> FlyDronesByRange(int range)
+        {
+            List<Drone> flyDrone = new List<Drone>();
+
+            foreach (var drone in Drones)
+            {
+                if (drone.Range == range)
+                {
+                    flyDrone.Add(drone);
+                    drone.Available = false;
+                }
+            }
+
+            return flyDrone;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Drones available at {Name}:");
+            foreach (var drone in Drones)
+            {
+                if (drone.Available == true)
+                {
+                    sb.AppendLine(drone.ToString());
+                }
+            }
+
+            return sb.ToString();
         }
     }
 }
