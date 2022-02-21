26a0875a6186ceda5ce2dc9226ba4762d5f71892
Denis Argatski <denis.argatski@gmail.com>
2022-02-21 22:29:26 +0200

    Drone.cs

---
+++ C#/04.C# Advanced/Exams/Exam-16-Dec-2021/03.DronesProblemDescription/Drone.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace Drones
+{
+    public class Drone
+    {
+        //Field
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public int Range { get; set; }
+        public bool Available { get; set; }
+
+        //Constructor
+        public Drone(string name, string brand, int range)
+        {
+            Name = name;
+            Brand = brand;
+            Range = range;
+            Available = true;
+        }
+
+        //Print
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Drone: {Name}");
+            sb.AppendLine($"Manufactured by: {Brand}");
+            sb.AppendLine($"Range: {Range} kilometers");
+
+            return sb.ToString();
+        }
+    }
+}
