88aaef08139918a4d7289b42a6f24c427039dac6
Denis Argatski <denis.argatski@gmail.com>
2022-03-03 20:39:20 +0200

    Fish.cs

---
+++ C#/04.C# Advanced/Exams/Exam-20 February2022/03.Fishing/Fish.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace FishingNet
+{
+    public class Fish
+    {
+        //Properties
+        public string FishType { get; set; }
+        public double Length { get; set; }
+        public double Weight { get; set; }
+
+        //Constructor
+        public Fish(string fishType, double length, double weight)
+        {
+            FishType = fishType;
+            Length = length;
+            Weight = weight;
+        }
+
+        /// <summary>
+        /// Print the information about fish.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"There is a {FishType}, {Length} cm. long, and {Weight} gr. in weight.";
+        }
+
+    }
+}
