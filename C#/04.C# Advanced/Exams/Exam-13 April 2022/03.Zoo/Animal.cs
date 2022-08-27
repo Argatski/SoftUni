5b2e8f092dd913cfa4c7bcf1f2ae1a3080078b0f
Denis Argatski <denis.argatski@gmail.com>
2022-08-27 22:10:17 +0300

    Animal.cs

---
+++ C#/04.C# Advanced/Exams/Exam-13 April 2022/03.Zoo/Animal.cs
@@ -0,0 +1,34 @@
+﻿namespace Zoo
+{
+    public class Animal
+    {
+        public string Species { get; set; }
+        public string Diet { get; set; }
+        public double Weight { get; set; }
+        public double Lenght { get; set; }
+
+        public Animal()
+        {
+
+        }
+        /// <summary>
+        /// Constructor should receive (species,diet,weight and length)
+        /// </summary>
+        /// <param name="species"></param>
+        /// <param name="diet"></param>
+        /// <param name="weight"></param>
+        /// <param name="lenght"></param>
+        public Animal(string species, string diet, double weight, double lenght)
+        {
+            Species = species;
+            Diet = diet;
+            Weight = weight;
+            Lenght = lenght;
+        }
+
+        public override string ToString()
+        {
+            return $"The {Species} is a {Diet} and weighs {Weight} kg.";
+        }
+    }
+}
