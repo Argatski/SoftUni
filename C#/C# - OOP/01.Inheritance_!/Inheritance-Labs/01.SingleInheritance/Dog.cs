fa993b62a0f6a0c53e498036af783b63932579eb
Denis Argatski <denis.argatski@gmail.com>
2022-08-31 22:30:40 +0300

    Dog.cs

---
+++ C#/C# - OOP/01.Inheritance_!/Inheritance-Labs/01.SingleInheritance/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+
+{
+    public class Dog : Animal //Inheritance
+    {
+        public void Bark()
+        {
+            Console.WriteLine("barking…");
+        }
+
+    }
+}
