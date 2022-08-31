89431c37a13421baf8af24cf0bdaf196f4d2a8d0
Denis Argatski <denis.argatski@gmail.com>
2022-08-31 22:35:45 +0300

    Puppy.cs

---
+++ C#/C# - OOP/01.Inheritance_!/Inheritance-Labs/02.MultipleInheritance/Puppy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Puppy : Dog //Inheritance Dog -> Dog ->Animal
+    {
+        public void Weep()
+        {
+            Console.WriteLine("weeping…");
+        }
+    }
+}
