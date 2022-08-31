d5cfe07704117a7448ee766edbc3343d54209cc3
Denis Argatski <denis.argatski@gmail.com>
2022-08-31 22:44:25 +0300

    Cat.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Labs/03.HierarchicalInheritance/Cat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Cat: Animal //Child class, Derived class.Inheritance Cat->Animal
+    {
+        public void Meow()
+        {
+            Console.WriteLine("meowing…");
+        }
+    }
+}
