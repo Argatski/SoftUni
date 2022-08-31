e57d0f2fa007e1b5ab1251b63c638aefcb21f136
Denis Argatski <denis.argatski@gmail.com>
2022-08-31 22:45:05 +0300

    Animal.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Labs/03.HierarchicalInheritance/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Animal //Base Class (Parent Class)
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating…");
+        }
+    }
+}
