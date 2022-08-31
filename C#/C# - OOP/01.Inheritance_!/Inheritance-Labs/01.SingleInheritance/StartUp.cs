f1976022bd2ec2da923aa6235da9f59aaab8060b
Denis Argatski <denis.argatski@gmail.com>
2022-08-31 22:32:49 +0300

    StartUp.cs

---
+++ C#/C# - OOP/01.Inheritance_!/Inheritance-Labs/01.SingleInheritance/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Instance of public class dog
+            Dog dog = new Dog();
+           
+            //Method from public class dog
+            dog.Bark();
+
+            //Method from public class Animal.The class dog inheritance method "Eat".
+            dog.Eat(); 
+        }
+    }
+}
