7941cfbe1e7dd6ba5715e026422f571c0d217662
Denis Argatski <denis.argatski@gmail.com>
2022-08-31 22:44:11 +0300

    Dog.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Labs/03.HierarchicalInheritance/Dog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Dog : Animal //Child class, Derived class.Inheritance Dog->Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("barking…");
+        }
+    }
+}
