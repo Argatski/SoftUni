8d96707d23feec769f526e32762832352e4beced
Denis Argatski <denis.argatski@gmail.com>
2022-09-01 23:27:48 +0300

    StackOfStrings.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Labs/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        /// <summary>
+        /// Is empty stack.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+
+        /// <summary>
+        /// Add randge
+        /// </summary>
+        /// <param name="collection"></param>
+        public void AddRange(IEnumerable<string> collection)
+        {
+            foreach (var elemet in collection)
+            {
+                this.Push(elemet);
+            }
+        }
+
+    }
+}
