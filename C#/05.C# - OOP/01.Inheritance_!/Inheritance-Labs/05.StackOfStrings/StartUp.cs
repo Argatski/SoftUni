518128f7bd0ebfaf1844ce9d1d51128478cf18f5
Denis Argatski <denis.argatski@gmail.com>
2022-09-01 23:28:05 +0300

    StartUp.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Labs/05.StackOfStrings/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stackOfStrings = new StackOfStrings();
+
+            Console.WriteLine(stackOfStrings.IsEmpty());// Return "True"
+
+            stackOfStrings.AddRange(new string[] { "Pesho", "Gosho", "Stamat" });
+
+            Console.WriteLine(stackOfStrings.IsEmpty()); //Return "False"
+        }
+    }
+}
