4ebad218d8248aff46530b45a3a2c4bba66a429e
Denis Argatski <denis.argatski@gmail.com>
2022-09-01 23:24:47 +0300

    StartUp.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Labs/04.RandomList/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList randomList = new RandomList{ "zdrasti", "stamat", "Chao", "Pesho", "Gosho" };
+
+            Console.WriteLine(randomList.RanodomString());
+
+        }
+    }
+}
