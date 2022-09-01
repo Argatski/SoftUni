638fdeaadcdf79318350a4cd9fb8ec7a757c405a
Denis Argatski <denis.argatski@gmail.com>
2022-09-01 23:24:33 +0300

    RandomList.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Labs/04.RandomList/RandomList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string> //Inheritance RandomList-> List<string>
+    {
+        //Field
+        private Random rnd;
+
+        //Constructor
+        public RandomList()
+        {
+            rnd = new Random();
+        }
+
+        /// <summary>
+        /// Function that returns and removes a random element from the list
+        /// </summary>
+        /// <returns></returns>
+        public string RanodomString()
+        {
+
+            int index = rnd.Next(0, this.Count);
+
+            string result = string.Empty;
+            result = this[index];
+            this.RemoveAt(index);
+
+            return result;
+        }
+    }
+}
