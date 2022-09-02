59fae7ce193ad387899a44b3619a62cc3eae7034
Denis Argatski <denis.argatski@gmail.com>
2022-09-02 23:08:18 +0300

    Lizard.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/02.Zoo/Lizard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Lizard : Reptile
+    {
+        //Constructor
+        public Lizard(string name)
+            : base(name)
+        {
+        }
+    }
+}
