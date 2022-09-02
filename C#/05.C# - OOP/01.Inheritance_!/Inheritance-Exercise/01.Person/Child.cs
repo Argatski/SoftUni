e16247de24b73c80228e35de70de3b0cf03764a3
Denis Argatski <denis.argatski@gmail.com>
2022-09-02 23:06:00 +0300

    Child.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/01.Person/Child.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.Person
+{
+    public class Child : Person
+    {
+
+        //Constructor
+        public Child(string name, int age)
+            : base(name, age)
+        {
+
+        }
+
+
+        /// <summary>
+        /// Method override Base Age.
+        /// </summary>
+        public override int Age
+        {
+            get
+            {
+                return base.Age;
+            }
+            set
+            {
+                if (value > 15)
+                {
+                    throw new ArgumentException("Children should not be able to have an age greater than 15");
+                }
+                base.Age = value;
+            }
+        }
+    }
+}
