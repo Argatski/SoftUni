017fdc3c82dc9b4a7732d25e9afd0b0df3fd3866
Denis Argatski <denis.argatski@gmail.com>
2022-09-02 23:08:58 +0300

    Reptile.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/02.Zoo/Reptile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Reptile : Animal
+    {
+        //Constructor
+        public Reptile(string name)
+            : base(name)
+        {
+        }
+
+    }
+}
