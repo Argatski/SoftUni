f0bc96c80c178c58c69290664a0a2de86375675a
Denis Argatski <denis.argatski@gmail.com>
2022-09-02 23:06:31 +0300

    StartUp.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/01.Person/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01.Person
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Person person;
+            if (age <= 15)
+            {
+                person = new Child(name, age);
+            }
+            else
+            {
+                person = new Person(name, age);
+            }
+
+            Console.WriteLine(person);
+        }
+    }
+}
