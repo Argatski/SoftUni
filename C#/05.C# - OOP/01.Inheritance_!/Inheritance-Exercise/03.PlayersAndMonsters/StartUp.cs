4f491c983cd86a829428fa1e3fa068b096b1ae7a
Denis Argatski <denis.argatski@gmail.com>
2022-09-04 00:34:35 +0300

    StartUp.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/03.PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Hero hero = new Elf("Elf", 15);
+
+            Console.WriteLine(hero);
+
+            hero = new MuseElf("Pesho", 0);
+
+            Console.WriteLine(hero);
+
+            hero = new Knight("Gosho", 25);
+
+            Console.WriteLine(hero);
+
+            hero = new DarkKnight("Tosho", -5);
+
+            Console.WriteLine(hero);
+
+            hero = new BladeKnight("Shosho", 25);
+
+            Console.WriteLine(hero);
+
+        }
+    }
+}
