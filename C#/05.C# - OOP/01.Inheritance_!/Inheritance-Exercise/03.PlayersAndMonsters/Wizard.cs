9c7f14ca5f61594f05a43071b56d9b5ac7be9051
Denis Argatski <denis.argatski@gmail.com>
2022-09-04 00:34:54 +0300

    Wizard.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/03.PlayersAndMonsters/Wizard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Wizard : Hero
+    {
+        //Constructor
+        public Wizard(string username, int level)
+            : base(username, level)
+        {
+        }
+    }
+}
