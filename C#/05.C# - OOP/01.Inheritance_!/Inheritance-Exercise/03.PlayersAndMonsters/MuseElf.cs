a938e10b7472f15dff178d489ce5d304025f3a09
Denis Argatski <denis.argatski@gmail.com>
2022-09-04 00:33:57 +0300

    MuseElf.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/03.PlayersAndMonsters/MuseElf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class MuseElf : Elf
+    {
+        //Constructor
+
+        public MuseElf(string username, int level)
+            : base(username, level)
+        {
+        }
+    }
+}
