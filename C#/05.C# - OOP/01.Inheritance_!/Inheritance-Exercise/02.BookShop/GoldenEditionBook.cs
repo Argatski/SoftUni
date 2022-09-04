1a7792282de93ac840fb23b7477f6cff0973c07a
Denis Argatski <denis.argatski@gmail.com>
2022-09-04 23:05:41 +0300

    GoldenEditionBook.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/02.BookShop/GoldenEditionBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop
+{
+    public class GoldenEditionBook : Book
+    {
+        public GoldenEditionBook(string title, string author, decimal price)
+            : base(title, author, price)
+        {
+        }
+
+        public override decimal Price
+        {
+            get => base.Price * 1.3m;
+        }
+    }
+}
