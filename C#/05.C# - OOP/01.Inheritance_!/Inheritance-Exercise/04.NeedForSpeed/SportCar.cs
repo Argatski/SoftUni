74e19a8ed45e66ea64b951c407e0885090b1dad6
Denis Argatski <denis.argatski@gmail.com>
2022-09-05 23:22:00 +0300

    SportCar.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/04.NeedForSpeed/SportCar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class SportCar : Car
+    {
+        private const double DefaultFuelConsumption = 10;
+
+        public SportCar(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+        }
+        /*Another way to override
+        public override double FuelConsumption => DefaultFuelConsumption;
+        */
+        public override void Drive(double kilometers)
+        {
+            base.Fuel -= kilometers * DefaultFuelConsumption;
+        }
+    }
+}
