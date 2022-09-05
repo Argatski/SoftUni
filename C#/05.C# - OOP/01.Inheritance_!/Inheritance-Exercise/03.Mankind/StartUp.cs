8fc87668fdff1635b73ad3cbf3615b18b4c02881
Denis Argatski <denis.argatski@gmail.com>
2022-09-05 23:15:08 +0300

    StartUp.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/03.Mankind/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Mankind
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] tokens = input.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+            string firstName = tokens[0];
+            string lastName = tokens[1];
+            string facultyNumber = tokens[2];
+            Student student;
+            try
+            {
+                student = new Student(firstName, lastName, facultyNumber);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            input = Console.ReadLine();
+            tokens = input.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+            firstName = tokens[0];
+            lastName = tokens[1];
+            double weekSalary = double.Parse(tokens[2]);
+            double hoursPerDay = double.Parse(tokens[3]);
+
+            Worker worker;
+            try
+            {
+                worker = new Worker(firstName, lastName, weekSalary, hoursPerDay);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            Console.WriteLine(student);
+            Console.WriteLine();
+            Console.WriteLine(worker);
+            Console.WriteLine();
+        }
+    }
+}
