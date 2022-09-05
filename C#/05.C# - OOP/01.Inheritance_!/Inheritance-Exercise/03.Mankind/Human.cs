a1b4f5a821541fd380cdf4927f93d4a739610629
Denis Argatski <denis.argatski@gmail.com>
2022-09-05 23:15:28 +0300

    Human.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/03.Mankind/Human.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mankind
+{
+    public class Human
+    {
+        //Fields
+        private string firstName;
+        private string lastName;
+
+        //Constructor
+        public Human(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        //Properties
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                if (char.IsLower(value[0]))
+                {
+                    throw new ArgumentException($"Expected upper case letter! Argument: firstName");
+                }
+                else if (value.Length  < 4)
+                {
+                    throw new ArgumentException($"Expected length at least 4 symbols! Argument: firstName");
+                }
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                if (char.IsLower(value[0]))
+                {
+                    throw new ArgumentException($"Expected upper case letter! Argument: lastName");
+                }
+                else if (value.Length < 3)
+                {
+                    throw new ArgumentException($"Expected length at least 3 symbols! Argument: lastName");
+                }
+                this.lastName = value;
+            }
+        }
+
+    }
+}
