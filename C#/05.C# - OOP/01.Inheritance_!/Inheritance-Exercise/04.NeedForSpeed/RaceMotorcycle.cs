2a06f1aeb0623f67b7fae33b5aed808f87fbe18e
Denis Argatski <denis.argatski@gmail.com>
2022-09-05 23:21:45 +0300

    RaceMotorcycle.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/04.NeedForSpeed/RaceMotorcycle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class RaceMotorcycle : Motorcycle
+    {
+        //Field
+        private const double DefaultFuelConsumption = 8;
+
+        //Constructor
+        public RaceMotorcycle(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+
+        }
+
+        /*Another way to override
+        public override double FuelConsumption => DefaultFuelConsumption;
+        */
+
+        //Use virtual method Vehicle=>Motorcycle=>RaseMotorCycle
+        public override void Drive(double kilometers)
+        {
+            base.Fuel -= kilometers * DefaultFuelConsumption;
+        }
+
+    }
+}
