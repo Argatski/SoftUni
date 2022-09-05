545a985012f631b1624332663a0197ece0328ac0
Denis Argatski <denis.argatski@gmail.com>
2022-09-05 23:18:12 +0300

    Car.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/04.NeedForSpeed/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Car : Vehicle
+    {
+        private const double DefaultFuelConsumption = 3;
+        //Constructor
+        public Car(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+
+        }
+
+        //Method
+        /*Another way to override
+        public override double FuelConsumption => DefaultFuelConsumption;
+        */
+        public override void Drive(double kilometers)
+        {
+            base.Fuel -= kilometers * DefaultFuelConsumption;
+        }
+        
+    }
+}
