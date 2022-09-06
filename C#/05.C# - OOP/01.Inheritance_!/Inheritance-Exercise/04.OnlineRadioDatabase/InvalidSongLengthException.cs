53a45616d36c5a391e184a154184683b5809d9f4
Denis Argatski <denis.argatski@gmail.com>
2022-09-06 22:58:21 +0300

    InvalidSongLengthException.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/04.OnlineRadioDatabase/InvalidSongLengthException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineRadioDatabase
+{
+    public class InvalidSongLengthException : InvalidSongExeption
+    {
+        public override string Message => "Invalid song length.";
+    }
+}
