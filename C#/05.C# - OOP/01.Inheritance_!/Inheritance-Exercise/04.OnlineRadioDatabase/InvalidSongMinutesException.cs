3a6aa3e730e24a802dcec06e9c00f2fe567944c8
Denis Argatski <denis.argatski@gmail.com>
2022-09-06 22:58:48 +0300

    InvalidSongMinutesException.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/04.OnlineRadioDatabase/InvalidSongMinutesException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineRadioDatabase
+{
+    public class InvalidSongMinutesException : InvalidSongLengthException
+    {
+        public override string Message => "Song minutes should be between 0 and 14.";
+    }
+}
