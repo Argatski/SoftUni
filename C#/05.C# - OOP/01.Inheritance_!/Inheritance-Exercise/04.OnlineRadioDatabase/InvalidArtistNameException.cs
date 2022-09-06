2d3a0e6b259293e01e3cc5db3f327c69bc1644ed
Denis Argatski <denis.argatski@gmail.com>
2022-09-06 22:55:42 +0300

    InvalidArtistNameException.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/04.OnlineRadioDatabase/InvalidArtistNameException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineRadioDatabase
+{
+    public class InvalidArtistNameException : InvalidSongExeption
+    {
+        public override string Message
+        {
+            get { return "Artist name should be between 3 and 20 symbols."; }
+        }
+    }
+}
