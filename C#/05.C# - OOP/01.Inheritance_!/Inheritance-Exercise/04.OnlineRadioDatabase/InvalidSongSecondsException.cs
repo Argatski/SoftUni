261a89a608458a727a0e6a82aacc74b6bd6a322d
Denis Argatski <denis.argatski@gmail.com>
2022-09-06 22:59:06 +0300

    InvalidSongSecondsException.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/04.OnlineRadioDatabase/InvalidSongSecondsException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineRadioDatabase
+{
+    public class InvalidSongSecondsException : InvalidSongLengthException
+    {
+        public override string Message => "Song seconds should be between 0 and 59.";
+    }
+}
