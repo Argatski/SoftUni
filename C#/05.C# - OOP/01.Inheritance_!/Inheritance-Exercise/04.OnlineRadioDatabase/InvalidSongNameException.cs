4ab3f63a9f05aaf41949d8d0398df1d752e19a9d
Denis Argatski <denis.argatski@gmail.com>
2022-09-06 22:56:14 +0300

    InvalidSongNameException.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/04.OnlineRadioDatabase/InvalidSongNameException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineRadioDatabase
+{
+    public class InvalidSongNameException : InvalidSongExeption
+    {
+        public override string Message => "Song name should be between 3 and 30 symbols.";
+    }
+}
