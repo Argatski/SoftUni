e6fc86cc1ec10ca5471632d2f7667819903662cf
Denis Argatski <denis.argatski@gmail.com>
2022-09-06 22:57:36 +0300

    InvalidSongExeption.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/04.OnlineRadioDatabase/InvalidSongExeption.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineRadioDatabase
+{
+    public class InvalidSongExeption : Exception
+    {
+        public override string Message
+        {
+            get { return "Invalid song."; }
+        }
+    }
+}
