d5d7ead20e6709f93d871827604ee03097d1a965
Denis Argatski <denis.argatski@gmail.com>
2022-09-08 22:46:57 +0300

    05.Restaurant.ip

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/05.Restaurant/Beverage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Beverage : Product
+    {
+        //Constructor
+        public Beverage(string name, decimal price, double milliliers)
+            : base(name, price)
+        {
+            this.Milliliers = milliliers;
+        }
+        //Properties
+        public double Milliliers { get; set; }
+    }
+}
