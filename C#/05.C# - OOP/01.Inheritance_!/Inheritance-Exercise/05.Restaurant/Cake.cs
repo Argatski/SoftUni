835f06a95139994d3e26e6c52d1c2cb43092e762
Denis Argatski <denis.argatski@gmail.com>
2022-09-08 22:37:48 +0300

    Cake.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/05.Restaurant/Cake.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        public const double grams = 250;
+        public const double calories = 1000;
+        public const decimal Cakeprices = 5M;
+
+        public Cake(string name)
+            : base(name, Cakeprices, grams, calories)
+        {
+
+        }
+    }
+}
