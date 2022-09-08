f971b3f142827400ad6e74294ebcdf9a5ae4060a
Denis Argatski <denis.argatski@gmail.com>
2022-09-08 22:40:44 +0300

    Food.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/05.Restaurant/Food.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Food : Product
+    {
+        //Constructor
+        public Food(string name, decimal price, double grams)
+            : base(name, price)
+        {
+            this.Grams = grams;
+        }
+
+        //Properties
+        public double Grams { get; set; }
+    }
+}
