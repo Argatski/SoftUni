cfe0c960b55b26f6967bc594574a3ce89a925222
Denis Argatski <denis.argatski@gmail.com>
2022-09-08 22:39:23 +0300

    Product.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/05.Restaurant/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Product
+    {
+        public Product(string name, decimal price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        /*
+        public override string ToString()
+        {
+            return $"Type:{GetType().Name} Name:{Name} Price{Price}";
+        }
+        */
+    }
+}
