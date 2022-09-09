0637794e1f77d4986418a37ca41a4c333495f959
Denis Argatski <denis.argatski@gmail.com>
2022-09-09 22:16:56 +0300

    Cat.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/06.Animals/Cat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Cat : Animal
+    {
+        public Cat(string name, int age, string gender)
+            : base(name, age, gender)
+        {
+        }
+
+        public override string ProduceSound()
+        {
+            return "Meow meow";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine(this.GetType().Name)
+                .AppendLine(base.ToString());
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
