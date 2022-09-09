86dbd172d2adae157d5a8a689e343530dd0f18a5
Denis Argatski <denis.argatski@gmail.com>
2022-09-09 22:21:08 +0300

    Tomcat.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/06.Animals/Tomcat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Tomcat : Cat
+    {
+        public const string genderTomcat = "male";
+        public Tomcat(string name, int age)
+            : base(name, age, genderTomcat)
+        {
+        }
+
+        public override string ProduceSound()
+        {
+            return "MEOW";
+        }
+    }
+}
