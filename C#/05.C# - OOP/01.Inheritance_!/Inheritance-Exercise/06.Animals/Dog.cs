2c1b2a161e5d447e58fd07dd73690b3b4e26c800
Denis Argatski <denis.argatski@gmail.com>
2022-09-09 22:17:25 +0300

    Dog.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/06.Animals/Dog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Dog : Animal
+    {
+        public Dog(string name, int age, string gender)
+            : base(name, age, gender)
+        {
+        }
+
+        public override string ProduceSound()
+        {
+            return $"Woof!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine(this.GetType().Name)
+                .AppendLine(base.ToString());
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
