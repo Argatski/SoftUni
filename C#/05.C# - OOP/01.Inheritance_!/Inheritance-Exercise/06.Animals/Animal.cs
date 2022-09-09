873f8cf55e5f3fd43a8de2494294f24c4296b071
Denis Argatski <denis.argatski@gmail.com>
2022-09-09 22:16:33 +0300

    Animal.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/06.Animals/Animal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        //Fields
+        private int age;
+
+        //Constructor
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public string Name { get; set; }
+        public int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+                this.age = value;
+            }
+        }
+        public string Gender { get; set; }
+
+        public virtual string ProduceSound()
+        {
+            return null;
+        }
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"{this.Name} {this.Age} {this.Gender}")
+            .AppendLine(this.ProduceSound());
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
