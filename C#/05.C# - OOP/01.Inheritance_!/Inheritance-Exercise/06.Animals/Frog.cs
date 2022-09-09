0928460136b5038d120c8c2299236d6e7faf6f00
Denis Argatski <denis.argatski@gmail.com>
2022-09-09 22:18:18 +0300

    Frog.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/06.Animals/Frog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Frog : Animal
+    {
+        public Frog(string name, int age, string gender)
+            : base(name, age, gender)
+        {
+        }
+
+        public override string ProduceSound()
+        {
+            return $"Ribbit";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(this.GetType().Name)
+                .AppendLine(base.ToString());
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
