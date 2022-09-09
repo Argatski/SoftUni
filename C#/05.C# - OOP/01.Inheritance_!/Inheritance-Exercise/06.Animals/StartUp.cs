555d043543897e8f90c2415bb6d0648df1d340c1
Denis Argatski <denis.argatski@gmail.com>
2022-09-09 22:20:36 +0300

    StartUp.cs

---
+++ C#/05.C# - OOP/01.Inheritance_!/Inheritance-Exercise/06.Animals/StartUp.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            while (true)
+            {
+                try
+                {
+                    var command = Console.ReadLine();
+
+                    if (command == "Beast!")
+                    {
+                        break;
+                    }
+                    var animalInfo = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                    string name = animalInfo[0];
+                    int age = int.Parse(animalInfo[1]);
+                    string gender = animalInfo[2];
+
+                    switch (command)
+                    {
+                        case "Dog":
+                            var dog = new Dog(name, age, gender);
+                            animals.Add(dog);
+                            break;
+                        case "Cat":
+                            var cat = new Cat(name, age, gender);
+                            animals.Add(cat);
+                            break;
+                        case "Frog":
+                            var frog = new Frog(name, age, gender);
+                            animals.Add(frog);
+                            break;
+                        case "Kitten":
+                            var kitten = new Kitten(name, age);
+                            animals.Add(kitten);
+                            break;
+                        case "Tomcat":
+                            var tomcat = new Tomcat(name, age);
+                            animals.Add(tomcat);
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+
+                
+            }
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal.ToString());
+            }
+        }
+    }
+}
