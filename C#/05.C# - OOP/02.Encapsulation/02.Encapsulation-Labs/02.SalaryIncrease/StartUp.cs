fd9e13c37fc64dadaa4b209c5afdc4c090b3f8ec
Denis Argatski <denis.argatski@gmail.com>
2022-09-12 22:41:02 +0300

    StartUp.cs

---
+++ C#/05.C# - OOP/02.Encapsulation/02.Encapsulation-Labs/02.SalaryIncrease/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new Person(input[0], input[1], int.Parse(input[2]), decimal.Parse(input[3]));
+                people.Add(person);
+            }
+
+            decimal percsent = decimal.Parse(Console.ReadLine());
+
+            people.ForEach(p => p.IncreaseSalary(percsent));
+            people.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
