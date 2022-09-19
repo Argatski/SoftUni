4fa31a38536df3d3d983758ce62a6cb8c3933a49
Denis Argatski <denis.argatski@gmail.com>
2022-09-19 23:48:52 +0300

    StartUp.cs

---
+++ C#/05.C# - OOP/02.Encapsulation/Encapsulation-Exercise/03.ShoppingSpree/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingSpree
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+            engine.Run();
+
+        }
+    }
+}
