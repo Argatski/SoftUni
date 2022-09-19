febdfa567737e6cf3726a28b14f5fc35ad271c60
Denis Argatski <denis.argatski@gmail.com>
2022-09-19 23:45:59 +0300

    Product.cs

---
+++ C#/05.C# - OOP/02.Encapsulation/Encapsulation-Exercise/03.ShoppingSpree/Models/Product.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ShoppingSpree
+{
+    public class Product
+    {
+        private string name;
+        private decimal cost;
+
+        public Product(string name, decimal cost)
+        {
+            this.Name = name;
+            this.Cost = cost;
+
+        }
+        public decimal Cost
+        {
+            get { return this.cost; }
+            private set
+            {
+                ValidateCost(value);
+                this.cost = value;
+            }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            private set
+            {
+                ValidateName(value);
+                this.name = value;
+            }
+
+        }
+
+        private void ValidateName(string value)
+        {
+            if (string.IsNullOrEmpty(value)||string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException(GlobalConstants.EmptyNameExcMsg);
+            };
+        }
+        private void ValidateCost(decimal value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException(GlobalConstants.EmptyMoneyExcMsg);
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
