ada46e8d8dea33304c283e1b76c7df5c61787ef9
Denis Argatski <denis.argatski@gmail.com>
2022-09-27 22:10:26 +0300

    Engine.cs

---
+++ C#/05.C# - OOP/02.Encapsulation/Encapsulation-Exercise/04.PizzaCalories/Core/Engine.cs
@@ -0,0 +1,57 @@
+﻿namespace PizzaCalories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class Engine
+    {
+
+        public void Run()
+        {
+            try
+            {
+                var pizza = MakePizza();
+
+                var input = Console.ReadLine();
+                while (input != "END")
+                {
+                    var inputParts = input
+                        .Split();
+
+                    var command = inputParts[0];
+
+                    var toppingType = inputParts[1];
+                    var weight = double.Parse(inputParts[2]);
+
+                    var topping = new Topping(toppingType, weight);
+
+                    pizza.AddTopping(topping);
+
+                    input = Console.ReadLine();
+                }
+                Console.WriteLine(pizza);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+        }
+
+        private static Pizza MakePizza()
+        {
+            var pizzaName = Console.ReadLine()
+                .Split()[1];
+
+            var doughtInfo = Console.ReadLine()
+                .Split();
+
+            var fourType = doughtInfo[1];
+            var bakingTechnique = doughtInfo[2];
+            var doughWeight = double.Parse(doughtInfo[3]);
+
+            var dough = new Dough(fourType, bakingTechnique, doughWeight);
+
+            return new Pizza(pizzaName, dough);
+        }
+    }
+}
