28503f96396331911a9cf0f254e8f7584730f721
Denis Argatski <denis.argatski@gmail.com>
2022-09-27 22:06:09 +0300

    Pizza.cs

---
+++ C#/05.C# - OOP/02.Encapsulation/Encapsulation-Exercise/04.PizzaCalories/Models/Pizza.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PizzaCalories
+{
+    public class Pizza
+    {
+        private const int maxLongerName = 15;
+        private const int maxToppingNumber = 10;
+
+        private string name;
+        private List<Topping> toppings;
+
+        public Pizza(string name, Dough dough)
+        {
+            this.Name = name;
+            this.Dough = dough;
+            this.toppings = new List<Topping>();
+        }
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length > maxLongerName)
+                {
+                    throw new ArgumentException(ExceptionMessage.invalidtLenghtName);
+                }
+                this.name = value;
+            }
+        }
+        public Dough Dough { get; private set; }
+
+        public double TotalCalories => this.Dough.CaloriesPerGram + this.toppings.Sum(t => t.CaloriesPerGram);
+        public int ToppingCount => this.toppings.Count();
+
+        /// <summary>
+        /// Add topping in List<Topping>
+        /// </summary>
+        /// <param name="topping"></param>
+        public void AddTopping(Topping topping)
+        {
+            if (this.ToppingCount > maxToppingNumber)
+            {
+                throw new ArgumentException(ExceptionMessage.invalidToppingsRange);
+            }
+            this.toppings.Add(topping);
+        }
+
+        /// <summary>
+        /// Return string result.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{this.Name} - {this.TotalCalories:f2} Calories.");
+            return sb.ToString();
+        }
+
+    }
+}
