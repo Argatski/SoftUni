ed723887b4384f3b8daf1b70b21df8a6d393641b
Denis Argatski <denis.argatski@gmail.com>
2022-09-29 23:19:02 +0300

    Player.cs

---
+++ C#/05.C# - OOP/02.Encapsulation/Encapsulation-Exercise/05.FootballTeamGenerator/Model/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballTeamGenerator
+{
+    public class Player
+    {
+        private string name;
+
+        public Player(string name, Stat stat)
+        {
+            this.Name = name;
+            this.Stat = stat;
+
+        }
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                isValidNamePlayer(value);
+                this.name = value;
+            }
+        }
+        public Stat Stat { get; set; }
+        public double OveralSkill => this.Stat.OverallStats;
+
+        private void isValidNamePlayer(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException(ExceptionMessage.invalidName);
+            }
+        }
+    }
+}
