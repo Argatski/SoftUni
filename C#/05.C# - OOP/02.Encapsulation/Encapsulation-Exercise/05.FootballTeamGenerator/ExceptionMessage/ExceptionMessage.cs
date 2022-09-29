4c4bdc572a2e760723ebc73e4b0acae40d9718e1
Denis Argatski <denis.argatski@gmail.com>
2022-09-29 23:21:27 +0300

    ExceptionMessage.cs

---
+++ C#/05.C# - OOP/02.Encapsulation/Encapsulation-Exercise/05.FootballTeamGenerator/ExceptionMessage/ExceptionMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballTeamGenerator
+{
+    public class ExceptionMessage
+    {
+        public static string invalidName = "A name should not be empty.";
+        public static string invalidStats = "{0} should be between 0 and 100.";
+
+        public static string missingPlayer = "Player {0} is not in {1} team.";
+    }
+}
