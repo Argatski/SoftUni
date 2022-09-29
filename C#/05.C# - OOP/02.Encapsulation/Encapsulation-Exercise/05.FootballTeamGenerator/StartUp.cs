ee8535c654acd01bb9b995e447bf4374e8c43c67
Denis Argatski <denis.argatski@gmail.com>
2022-09-29 23:17:25 +0300

    StartUp.cs

---
+++ C#/05.C# - OOP/02.Encapsulation/Encapsulation-Exercise/05.FootballTeamGenerator/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FootballTeamGenerator
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
