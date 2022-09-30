e1404a8a4056f859e73470b40e463f0919bc9ba7
Denis Argatski <denis.argatski@gmail.com>
2022-09-30 22:21:50 +0300

    Command.cs

---
+++ C#/05.C# - OOP/02.Encapsulation/Encapsulation-Exercise/05.FootballTeamGenerator/Command/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballTeamGenerator
+{
+    public class Command
+    {
+        public Command(string name, string[] arguments)
+        {
+            this.Name = name;
+            this.Argumetns = arguments;
+        }
+
+        public string Name { get; private set; }
+        public string[] Argumetns { get; private set; }
+    }
+}
