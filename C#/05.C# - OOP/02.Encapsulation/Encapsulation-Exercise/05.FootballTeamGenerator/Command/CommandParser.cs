f1b8f42bff117efc5debb0a2510bd42503995f3e
Denis Argatski <denis.argatski@gmail.com>
2022-09-30 22:22:03 +0300

    CommandParser.cs

---
+++ C#/05.C# - OOP/02.Encapsulation/Encapsulation-Exercise/05.FootballTeamGenerator/Command/CommandParser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FootballTeamGenerator
+{
+    public class CommandParser
+    {
+        public Command Parse(string input)
+        {
+            var inputParts = input.Split(";");
+            var name = inputParts[0];
+            var arguments = inputParts.Skip(1).ToArray();
+
+            return new Command(name, arguments);
+        }
+
+    }
+}
