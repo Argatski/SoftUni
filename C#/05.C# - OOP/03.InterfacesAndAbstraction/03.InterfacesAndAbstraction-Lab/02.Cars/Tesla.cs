51665c4fa5ae6cd6d2ce455e20c2df1eb8c9c7a8
Denis Argatski <denis.argatski@gmail.com>
2022-10-02 14:21:43 +0300

    Tesla.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Lab/02.Cars/Tesla.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = battery;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get; set; }
+
+
+        public void Start()
+        {
+            Console.WriteLine("Engine start");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Engine Breaaak!");
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} Tesla {this.Model} with {this.Battery} Batteries";
+        }
+    }
+}
