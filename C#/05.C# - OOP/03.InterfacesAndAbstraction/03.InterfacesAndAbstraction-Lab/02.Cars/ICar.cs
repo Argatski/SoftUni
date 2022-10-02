b1d3514d45c009738fe0eb7fa40fc98780a6d302
Denis Argatski <denis.argatski@gmail.com>
2022-10-02 14:20:00 +0300

    ICar.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Lab/02.Cars/ICar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    interface ICar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public void Start();
+        public void Stop();
+
+    }
+}
