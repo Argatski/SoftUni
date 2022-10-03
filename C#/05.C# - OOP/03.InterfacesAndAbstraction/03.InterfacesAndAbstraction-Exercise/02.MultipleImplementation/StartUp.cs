5abfec7e1734d3ed1ecf5dac44be02cafc47c47d
Denis Argatski <denis.argatski@gmail.com>
2022-10-03 22:45:30 +0300

    StartUp.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/02.MultipleImplementation/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PersonInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string id = Console.ReadLine();
+            string birthdate = Console.ReadLine();
+
+           // Citizen citizen = new Citizen(name, age, id, birthdate);
+
+            IIdentifiable identifiable = new Citizen(name, age, id, birthdate);
+            IBirthable birthable = new Citizen(name, age, id, birthdate);
+
+            //Console.WriteLine(citizen.Id);
+            //Console.WriteLine(citizen.Birthdate);
+
+            Console.WriteLine(identifiable.Id);
+            Console.WriteLine(birthable.Birthdate);
+        }
+    }
+}
