11ed22e1fd75eca2bce2531234e2a8173707ea61
Denis Argatski <denis.argatski@gmail.com>
2022-10-05 22:45:22 +0300

    Smartphone.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/03.Telephony/Models/Smartphone.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Telephony
+{
+    public class Smartphone : ICallable, IBrowsable
+    {
+
+        public string Browsing(string site)
+        {
+            if (site.Any(x => char.IsDigit(x)))
+            {
+                throw new ArgumentException(ExceptionMassage.invalidURL);
+            }
+
+            return $"Browsing: {site}!";
+        }
+
+        public string Call(string number)
+        {
+            if (number.Any(n => char.IsLetter(n)))
+            {
+                throw new ArgumentException(ExceptionMassage.invalidNumber);
+            }
+
+            string resutl = $"Calling... {number}";
+            return resutl;
+        }
+
+    }
+}
