8e739f9e39811c197864e51dc9b0ec3c428560e0
Denis Argatski <denis.argatski@gmail.com>
2022-10-05 22:46:44 +0300

    StartUp.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/03.Telephony/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Telephony
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ICallable caller = new Smartphone();
+            IBrowsable browser = new Smartphone();
+
+            Engine engine = new Engine(caller, browser);
+            engine.Run();
+
+        }
+    }
+}
