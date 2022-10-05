f04b1762f2257749cfe8a7ab0494da4a48306133
Denis Argatski <denis.argatski@gmail.com>
2022-10-05 22:45:40 +0300

    StationaryPhone.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/03.Telephony/Models/StationaryPhone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Telephony
+{
+    public class StationaryPhone : ICallable
+    {
+        /// <summary>
+        /// If the number is 7 digits long, you are making a call from your stationary phone and you print:
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns>Dialing... {number}</returns>
+        public string Call(string number)
+        {
+            if (number.Any(n => char.IsLetter(n)))
+            {
+                Console.WriteLine(ExceptionMassage.invalidNumber);
+            }
+
+            string resutl = $"Dialing... {number}";
+            return resutl;
+        }
+    }
+}
