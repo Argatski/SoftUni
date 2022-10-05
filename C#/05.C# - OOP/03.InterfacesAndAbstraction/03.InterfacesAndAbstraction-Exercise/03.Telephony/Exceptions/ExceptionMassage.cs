3448e13cbe03c115634a0fc2c31a066b62fb7a01
Denis Argatski <denis.argatski@gmail.com>
2022-10-05 22:44:54 +0300

    ExceptionMassage.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/03.Telephony/Exceptions/ExceptionMassage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telephony
+{
+    public class ExceptionMassage
+    {
+        public static string invalidNumber = "Invalid number!";
+        public static string invalidURL = "Invalid URL!";
+    }
+}
