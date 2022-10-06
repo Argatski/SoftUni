44c33d70367a15e71cbb6ff097f289b6e35a6e11
Denis Argatski <denis.argatski@gmail.com>
2022-10-06 23:10:38 +0300

    StartUp.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/04.BorderControl/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BorderControl
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+
+            engine.Run();
+
+        }
+    }
+}
