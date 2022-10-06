716a9d2601683c5847494ef775909e7dda91d124
Denis Argatski <denis.argatski@gmail.com>
2022-10-06 23:14:10 +0300

    Robot.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/04.BorderControl/Models/Robot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public class Robot : IIdentifiable
+    {
+        public Robot(string name, string id)
+        {
+            this.Name = name;
+            this.Id = id;
+        }
+        public string Name { get; private set; }
+
+        public string Id { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{this.Id}";
+        }
+    }
+}
