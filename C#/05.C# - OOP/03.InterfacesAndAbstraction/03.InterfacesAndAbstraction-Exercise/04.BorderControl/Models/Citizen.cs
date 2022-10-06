e2948401a75f4a49c233c52a651eab4fdc96f5ab
Denis Argatski <denis.argatski@gmail.com>
2022-10-06 23:13:56 +0300

    Citizen.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/04.BorderControl/Models/Citizen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public class Citizen : IIdentifiable
+    {
+        public Citizen(string name, int age, string id)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+        }
+        public int Age { get; private set; }
+        public string Name { get; private set; }
+        public string Id { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{this.Id}";
+        }
+
+    }
+}
