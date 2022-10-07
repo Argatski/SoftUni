bb95c9fd4872f3bd756d1e934aa8b17eac0c5659
Denis Argatski <denis.argatski@gmail.com>
2022-10-07 11:44:17 +0300

    Mammal.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/05.BirthdayCelebrations/Models/Mammal.cs
@@ -0,0 +1,19 @@
+﻿namespace BirthdayCelebrations
+{
+    public class Mammal : IMammal
+    {
+        public Mammal(string name, string birthdate)
+        {
+            this.Name = name;
+            this.Birthdate = birthdate;
+        }
+        public string Name { get; private set; }
+
+        public string Birthdate { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{this.Birthdate}";
+        }
+    }
+}
