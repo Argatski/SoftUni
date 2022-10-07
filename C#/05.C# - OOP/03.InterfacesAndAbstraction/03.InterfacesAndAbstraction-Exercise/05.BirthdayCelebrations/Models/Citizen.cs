7b769606e2cae5c01bf0b7745b067ca3b430ca3c
Denis Argatski <denis.argatski@gmail.com>
2022-10-07 11:43:57 +0300

    Citizen.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/05.BirthdayCelebrations/Models/Citizen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace BirthdayCelebrations
+{
+    public class Citizen : Mammal, IIdentifiable
+    {
+        public Citizen(string name, int age, string id, string birthdate)
+            : base(name, birthdate)
+        {
+            this.Age = age;
+            this.Id = id;
+        }
+
+        public string Id { get; private set; }
+        public int Age { get; private set; }
+    }
+}
