3a599ba16613a416761e87cb61954b96d63d7b0b
Denis Argatski <denis.argatski@gmail.com>
2022-10-07 11:44:44 +0300

    Robot.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/05.BirthdayCelebrations/Models/Robot.cs
@@ -0,0 +1,19 @@
+﻿namespace BirthdayCelebrations
+{
+    public class Robot : IIdentifiable
+    {
+        public Robot(string model, string id)
+        {
+            this.Model = model;
+            this.Id = id;
+        }
+        public string Model { get; private set; }
+
+        public string Id { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{this.Id}";
+        }
+    }
+}
