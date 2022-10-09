4bef2cda2a025047357b74cbfb318d1f97350cf3
Denis Argatski <denis.argatski@gmail.com>
2022-10-09 23:20:09 +0300

    Engine.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/06.FoodShortage/Core/Engine.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FoodShortage
+{
+    public class Engine
+    {
+        public readonly List<IBuyer> buyers;
+        public Engine()
+        {
+            this.buyers = new List<IBuyer>();
+        }
+        public void Run()
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split();
+                AddPerson(input, buyers);
+            }
+
+            string argument;
+            while ((argument = Console.ReadLine()) != "End")
+            {
+                var person = buyers.FirstOrDefault(b => b.Name == argument);
+                if (person != null)
+                {
+                    person.BuyFood();
+                }
+
+            }
+
+            int totalFoodAmount = this.buyers.Sum(x => x.Food);
+
+            Console.WriteLine(totalFoodAmount);
+        }
+
+        private void AddPerson(string[] input, List<IBuyer> buyers)
+        {
+            if (input.Length == 4)
+            {
+                string name, id, birthdate;
+                int age;
+
+                name = input[0];
+                age = int.Parse(input[1]);
+                id = input[2];
+                birthdate = input[3];
+
+
+                IBuyer buyer = new Citizen(name, age, id, birthdate);
+
+                buyers.Add(buyer);
+            }
+            else
+            {
+                string name, group;
+                int age;
+
+                name = input[0];
+                age = int.Parse(input[1]);
+                group = input[2];
+
+
+                IBuyer buyer = new Rebel(name, age, group);
+
+                buyers.Add(buyer);
+            }
+        }
+    }
+}
