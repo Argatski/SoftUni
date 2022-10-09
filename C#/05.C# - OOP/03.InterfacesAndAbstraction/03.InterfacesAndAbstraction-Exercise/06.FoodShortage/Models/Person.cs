4d4224279b479d9178986254780107adc1c09df0
Denis Argatski <denis.argatski@gmail.com>
2022-10-09 23:21:01 +0300

    Person.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/06.FoodShortage/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace FoodShortage
+{
+    public abstract class Person : IBuyer
+    {
+        protected Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        protected Person(string name, int age, string birthdate)
+            : this(name, age)
+        {
+            this.Birthdate = birthdate;
+        }
+
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public int Food { get; protected set; }
+        public string Birthdate { get; private set; }
+
+        public abstract void BuyFood();
+
+    }
+}
