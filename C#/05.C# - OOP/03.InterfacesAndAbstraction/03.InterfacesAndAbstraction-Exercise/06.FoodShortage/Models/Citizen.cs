e27d1d447af8aa5971b6ae73d0085084726c170d
Denis Argatski <denis.argatski@gmail.com>
2022-10-09 23:20:44 +0300

    Citizen.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/06.FoodShortage/Models/Citizen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace FoodShortage
+{
+    public class Citizen : Person, IIdentifiable
+    {
+        private const int food = 10;
+        public Citizen(string name, int age, string id, string birthdate)
+            : base(name, age, birthdate)
+        {
+            this.Id = id;
+        }
+
+        public string Id { get; private set; }
+
+        public override void BuyFood()
+        {
+            this.Food += food;
+        }
+    }
+}
