53d7dd7c8376a4c629220464f6fc570851c1bf68
Denis Argatski <denis.argatski@gmail.com>
2022-10-09 23:19:03 +0300

    IBuyer.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/06.FoodShortage/Contracts/IBuyer.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodShortage
+{
+    public interface IBuyer
+    {
+        int Food { get; }
+        string Name { get; }
+        int Age { get; }
+        void BuyFood();
+
+    }
+}
