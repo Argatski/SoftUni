770da860a46974d64cb089acd3e1d616b0909c7d
Denis Argatski <denis.argatski@gmail.com>
2022-10-09 23:21:16 +0300

    Rebel.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/06.FoodShortage/Models/Rebel.cs
@@ -0,0 +1,18 @@
+﻿namespace FoodShortage
+{
+    public class Rebel : Person
+    {
+        public const int food = 5;
+        public Rebel(string name, int age, string group)
+            : base(name, age)
+        {
+            this.Group = group;
+        }
+        public string Group
+        { get; private set; }
+        public override void BuyFood()
+        {
+            this.Food += food;
+        }
+    }
+}
