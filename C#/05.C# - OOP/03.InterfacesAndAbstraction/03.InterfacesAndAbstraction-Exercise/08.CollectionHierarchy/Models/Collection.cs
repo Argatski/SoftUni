97288c966d4a81e4dc22f5c6a9842edcb81ee832
Denis Argatski <denis.argatski@gmail.com>
2022-10-18 22:21:45 +0300

    Collection.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/08.CollectionHierarchy/Models/Collection.cs
@@ -0,0 +1,23 @@
+﻿using CollectionHierarchy.Contracts;
+using System.Collections.Generic;
+
+namespace CollectionHierarchy.Models
+{
+    public abstract class Collection : IAddCollections
+    {
+        public Collection()
+        {
+            this.Data = new List<string>();
+        }
+
+        protected List<string> Data { get; }
+        public virtual int Add(string item)
+        {
+
+            var index = 0;
+            this.Data.Insert(index, item);
+            return index;
+        }
+
+    }
+}
