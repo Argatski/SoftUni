bca4b587bc2da08302cd1566e70200cd18fe0c09
Denis Argatski <denis.argatski@gmail.com>
2022-10-18 22:21:24 +0300

    AddRemoveCollection.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/08.CollectionHierarchy/Models/AddRemoveCollection.cs
@@ -0,0 +1,15 @@
+﻿using CollectionHierarchy.Contracts;
+
+namespace CollectionHierarchy.Models
+{
+    public class AddRemoveCollection : Collection, IAddRemoveCollection
+    {
+        public virtual string Remove()
+        {
+            string item = this.Data[this.Data.Count - 1];
+            this.Data.RemoveAt(this.Data.Count - 1);
+
+            return item;
+        }
+    }
+}
