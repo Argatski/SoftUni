e08c6cd534795d68436326e25f8c629f356c3194
Denis Argatski <denis.argatski@gmail.com>
2022-10-18 22:22:02 +0300

    MyList.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/08.CollectionHierarchy/Models/MyList.cs
@@ -0,0 +1,16 @@
+﻿namespace CollectionHierarchy.Models
+{
+    using CollectionHierarchy.Contracts;
+    public class MyList : AddRemoveCollection, IMyList
+    {
+        public int Used => this.Data.Count;
+
+        public override string Remove()
+        {
+            string removeItem = this.Data[0];
+            this.Data.RemoveAt(0);
+
+            return removeItem;
+        }
+    }
+}
