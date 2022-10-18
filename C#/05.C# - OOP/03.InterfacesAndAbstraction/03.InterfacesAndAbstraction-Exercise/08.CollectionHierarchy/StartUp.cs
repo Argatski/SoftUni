f93430890ee25b30c0dcf580fc6cd9f28e621ea0
Denis Argatski <denis.argatski@gmail.com>
2022-10-18 22:24:03 +0300

    StartUp.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/08.CollectionHierarchy/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CollectionHierarchy.Engine;
+
+namespace CollectionHierarchy
+{
+
+    using CollectionHierarchy.Models;
+    using Engine;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var addCollection = new AddCollection();
+            var addRemoveCollection = new AddRemoveCollection();
+            var myList = new MyList();
+
+            Engine.Engine engine = new Engine.Engine(addCollection, addRemoveCollection, myList);
+            engine.Run();
+        }
+    }
+}
