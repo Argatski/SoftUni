acf4211960a1bed33b3ac5556caf2757749c2bb9
Denis Argatski <denis.argatski@gmail.com>
2022-10-19 22:11:37 +0300

    Engine.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/09.ExplicitInterfaces/Engine/Engine.cs
@@ -0,0 +1,29 @@
+﻿using ExplicitInterfaces.Contracts;
+using ExplicitInterfaces.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExplicitInterfaces.Engine
+{
+    public class Engine
+    {
+        public void Run()
+        {
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] input = command.Split();
+                string name = input[0];
+                string country = input[1];
+                int age = int.Parse(input[2]);
+
+                IPerson person = new Citizen(name, age, country);
+                Console.WriteLine(person.GetName());
+
+                IResident resident = new Citizen(name, age, country);
+                Console.WriteLine(resident.GetName());
+            }
+        }
+    }
+}
