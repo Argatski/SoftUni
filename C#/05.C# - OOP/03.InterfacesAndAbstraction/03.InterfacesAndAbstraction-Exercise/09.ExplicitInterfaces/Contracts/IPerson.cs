61592e98b48170b281f0d72036fbc73463529ea2
Denis Argatski <denis.argatski@gmail.com>
2022-10-19 22:09:20 +0300

    IPerson.cs

---
+++ C#/05.C# - OOP/03.InterfacesAndAbstraction/03.InterfacesAndAbstraction-Exercise/09.ExplicitInterfaces/Contracts/IPerson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExplicitInterfaces.Contracts
+{
+    public interface IPerson
+    {
+        public string Name { get; }
+        public int Age { get; }
+        string GetName();
+    }
+}
