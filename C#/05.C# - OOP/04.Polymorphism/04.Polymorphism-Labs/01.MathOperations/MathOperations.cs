55e14b1607057aafcd9f34e98b3ead52acefb442
Denis Argatski <denis.argatski@gmail.com>
2022-10-22 23:24:27 +0300

    MathOperations.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Labs/01.MathOperations/MathOperations.cs
@@ -0,0 +1,19 @@
+﻿namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int num1, int num2)
+        {
+            int num = num1 + num2;
+            return num;
+        }
+        public double Add(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+        public decimal Add(decimal d1, decimal d2, decimal d3)
+        {
+            return d1 + d2 + d3;
+        }
+    }
+}
