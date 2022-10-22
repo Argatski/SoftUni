9e7388e93fdf10f74783ad575f60f60677b4bf28
Denis Argatski <denis.argatski@gmail.com>
2022-10-22 23:24:46 +0300

    StartUp.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Labs/01.MathOperations/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Operations
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            MathOperations mathOperation = new MathOperations();
+            Console.WriteLine(mathOperation.Add(2, 3));
+            Console.WriteLine(mathOperation.Add(2.2, 3.3, 5.5));
+            Console.WriteLine(mathOperation.Add(2.2m, 3.3m, 4.4m));
+
+        }
+    }
+}
