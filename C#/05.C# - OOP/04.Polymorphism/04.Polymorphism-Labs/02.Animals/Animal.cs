133c355b63b13341e3a57672aad746d3c67c7306
Denis Argatski <denis.argatski@gmail.com>
2022-10-22 23:26:52 +0300

    Animal.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Labs/02.Animals/Animal.cs
@@ -0,0 +1,22 @@
+﻿namespace Animals
+{
+    public abstract class Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        public Animal(string name, string favouriteFood)
+        {
+
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+        public string Name { get; private set; }
+        public string FavouriteFood { get; private set; }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my fovourite food is {this.FavouriteFood}";
+        }
+    }
+}
