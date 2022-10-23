c6bc3ba92ccc094b9a02b81630fc410dc9284598
Denis Argatski <denis.argatski@gmail.com>
2022-10-23 23:23:25 +0300

    StartUp

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Labs/03.Shapes/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape rectangle = new Rectangle(3, 3);
+            Console.WriteLine(rectangle.CalculatePerimeter());
+            Console.WriteLine(rectangle.CalculateArea());
+            Console.WriteLine(rectangle.Draw());
+            Shape circle = new Circle(3);
+            Console.WriteLine(circle.CalculateArea());
+            Console.WriteLine(circle.CalculatePerimeter());
+            Console.WriteLine(circle.Draw());
+        }
+    }
+}
