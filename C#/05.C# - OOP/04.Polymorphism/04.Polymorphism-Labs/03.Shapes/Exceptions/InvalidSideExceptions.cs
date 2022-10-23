edabab49a2ee25d1235c8228e1080a8ed5b4778d
Denis Argatski <denis.argatski@gmail.com>
2022-10-23 23:24:49 +0300

    InvalidSideExceptions.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Labs/03.Shapes/Exceptions/InvalidSideExceptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class InvalidSideExceptions : Exception
+    {
+        public InvalidSideExceptions(string message)
+            : base(message)
+        {
+        }
+    }
+}
