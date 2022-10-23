dbef6594327728f9894aa06b3d20ec853c5d9a7c
Denis Argatski <denis.argatski@gmail.com>
2022-10-23 23:25:33 +0300

    Rectangle.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Labs/03.Shapes/Models/Rectangle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    class Rectangle : Shape
+    {
+        private const string InvalidSideExceptionsMessage = "{0} must be a positive number!";
+
+        private double height;
+        private double width;
+        public Rectangle(double height, double width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public double Height
+        {
+            get { return this.height; }
+            private set
+            {
+                this.ValideteSide(value, nameof(this.Height));
+                this.height = value;
+            }
+        }
+
+        public double Width
+        {
+            get { return this.width; }
+            private set
+            {
+                this.ValideteSide(value, nameof(this.Width));
+                this.width = value;
+            }
+        }
+
+        private void ValideteSide(double value, string sideName)
+        {
+            if (value <= 0)
+            {
+                throw new InvalidSideExceptions(string.Format(InvalidSideExceptionsMessage, sideName));
+            }
+        }
+
+        public override double CalculateArea()
+        {
+            return height * width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * height + 2 * width;
+        }
+        public override string Draw()
+        {
+            return base.Draw() + $" {this.GetType().Name}";
+        }
+    }
+}
