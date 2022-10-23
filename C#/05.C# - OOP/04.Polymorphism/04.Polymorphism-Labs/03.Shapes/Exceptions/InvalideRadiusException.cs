a478e52c2b0b0a92d0a3940367679b1a28d6ff2f
Denis Argatski <denis.argatski@gmail.com>
2022-10-23 23:24:03 +0300

    InvalideRadiusException.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Labs/03.Shapes/Exceptions/InvalideRadiusException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class InvalideRadiusException : Exception
+    {
+        private const string ExceptionMessage = "Radius must be a positive number!";
+
+        public InvalideRadiusException()
+            : base(ExceptionMessage)
+        {
+        }
+    }
+}
