44a818818b6edb04fd1c2d326334d6b29d1c4b9a
Denis Argatski <denis.argatski@gmail.com>
2022-10-24 23:23:11 +0300

    Truck.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/01.Vehicles/Models/Truck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles.Models
+{
+    public class Truck : Vehicle
+    {
+        private const double airConditionerConsumption = 1.6;
+        private const double fuelWastage = 0.95;
+        public Truck(double fuelQuantity, double litersPerKm)
+            : base(fuelQuantity, litersPerKm + airConditionerConsumption)
+        {
+        }
+
+        public override void AmountOfFuel(double fuel)
+        {
+            this.FuelQuantity += fuel * fuelWastage;
+        }
+    }
+}
