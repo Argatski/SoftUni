6fec8365bc60b2fc891bdd4ec0b43af5300d54cf
Denis Argatski <denis.argatski@gmail.com>
2022-10-24 23:23:35 +0300

    Vehicle.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/01.Vehicles/Models/Vehicle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Vehicles.Contracts;
+using Vehicles.Exceptions;
+
+namespace Vehicles.Models
+{
+    public abstract class Vehicle : IVehicle
+    {
+        public Vehicle(double fuelQuantity, double litersPerKm)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.LitersPerKm = litersPerKm;
+        }
+        public double FuelQuantity { get; protected set; }
+
+        public double LitersPerKm { get; protected set; }
+
+        public virtual void AmountOfFuel(double fuel)
+        {
+            this.FuelQuantity += fuel;
+        }
+        public string Drive(double distance)
+        {
+            bool carDrive = this.FuelQuantity >= this.LitersPerKm * distance;
+
+            if (!carDrive)
+            {
+                throw new LowFuelException($"{this.GetType().Name} needs refueling");
+            }
+            this.FuelQuantity -= this.LitersPerKm * distance;
+            return $"{this.GetType().Name} travelled {distance} km";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.GetType().Name}: {this.FuelQuantity:f2}";
+        }
+
+    }
+}
+ 
