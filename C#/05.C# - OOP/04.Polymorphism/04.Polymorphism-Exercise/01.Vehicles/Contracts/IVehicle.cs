a3b0a3855f46a0559a693d6a778b1e713eb7bb12
Denis Argatski <denis.argatski@gmail.com>
2022-10-24 23:19:22 +0300

    IVehicle.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/01.Vehicles/Contracts/IVehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles.Contracts
+{
+    public interface IVehicle
+    {
+        public double FuelQuantity { get; }
+        public double LitersPerKm { get; }
+
+        public abstract string Drive(double distance);
+        public abstract void AmountOfFuel(double fuel);
+    }
+}
