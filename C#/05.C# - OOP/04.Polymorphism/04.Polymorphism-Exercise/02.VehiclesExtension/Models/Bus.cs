8ae7b9c353f89cc4ccea5d98eed3e926df902505
Denis Argatski <denis.argatski@gmail.com>
2022-10-28 23:07:09 +0300

    Bus.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/02.VehiclesExtension/Models/Bus.cs
@@ -0,0 +1,21 @@
+﻿namespace VehiclesExtension.Models
+{
+    using VehiclesExtension.Contract;
+    using VehiclesExtension.Exceptions;
+
+    public class Bus : Vehicle, IBus
+    {
+        private const double AirConditionConsumption = 1.4;
+
+        public Bus(double fuelQuantity, double fuelConsumptionPerKm, double tankCapacity)
+            : base(fuelQuantity, fuelConsumptionPerKm + AirConditionConsumption, tankCapacity)
+        {
+        }
+
+        public string DriveEmpty(double distance)
+        {
+            this.FuelConsumptionPerKm -= AirConditionConsumption;
+            return base.Drive(distance);
+        }
+    }
+}
