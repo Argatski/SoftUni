8bf1b74a5297a3e672561c66dd0c7c685354ac0d
Denis Argatski <denis.argatski@gmail.com>
2022-10-28 22:48:43 +0300

    IVehicle.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/02.VehiclesExtension/Contract/IVehicle.cs
@@ -0,0 +1,15 @@
+﻿namespace VehiclesExtension.Contract
+{
+    public interface IVehicle
+    {
+        double FuelQuantity { get; }
+
+        double FuelConsumptionPerKm { get; }
+
+        double TankCapacity { get; }
+
+        string Drive(double distance);
+
+        void Refuel(double fuel);
+    }
+}
