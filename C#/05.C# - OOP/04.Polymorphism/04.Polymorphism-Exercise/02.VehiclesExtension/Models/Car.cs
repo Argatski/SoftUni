cf7ee6022e227941cefee7a3d22fc2e720606663
Denis Argatski <denis.argatski@gmail.com>
2022-10-28 23:08:48 +0300

    Car.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/02.VehiclesExtension/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using VehiclesExtension.Exceptions;
+
+namespace VehiclesExtension.Models
+{
+    public class Car : Vehicle
+    {
+        private const double AirConditionerConsumption = 0.9;
+
+        public Car(
+            double fuelQuantity,
+            double fuelConsumptionPerKm,
+            double tankCapacity)
+            : base(fuelQuantity, fuelConsumptionPerKm + AirConditionerConsumption, tankCapacity)
+        {
+        }
+    }
+}
