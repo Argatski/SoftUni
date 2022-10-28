040ecb0e0a51992f1b4ed01152591896ffc83ee8
Denis Argatski <denis.argatski@gmail.com>
2022-10-28 22:54:23 +0300

    LowFuelException.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/02.VehiclesExtension/Exceptions/LowFuelException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehiclesExtension.Exceptions
+{
+    public class LowFuelException : Exception
+    {
+        public LowFuelException(string message)
+            : base(message)
+        {
+        }
+    }
+}
