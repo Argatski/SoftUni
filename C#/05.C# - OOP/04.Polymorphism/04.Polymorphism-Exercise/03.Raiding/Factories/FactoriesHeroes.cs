da656cbf23d8469e1f9ded80630a7c9d4330574c
Denis Argatski <denis.argatski@gmail.com>
2022-10-29 23:29:13 +0300

    FactoriesHeroes.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/03.Raiding/Factories/FactoriesHeroes.cs
@@ -0,0 +1,33 @@
+﻿using Raiding.Exceptions;
+using Raiding.Models;
+using System;
+
+namespace Raiding.Factories
+{
+    public class FactoriesHeroes
+    {
+        public static BaseHero CeateHero(string type, string name)
+        {
+            BaseHero baseHero;
+            switch (type)
+            {
+                case "Druid":
+                    baseHero = new Druid(name);
+                    break;
+                case "Paladin":
+                    baseHero = new Paladin(name);
+                    break;
+                case "Rogue":
+                    baseHero = new Rogue(name);
+                    break;
+                case "Warrior":
+                    baseHero = new Warrior(name);
+                    break;
+                default:
+                    throw new ArgumentException(InvalidInput.InvalidHero);
+            }
+            return baseHero;
+        }
+    }
+
+}
