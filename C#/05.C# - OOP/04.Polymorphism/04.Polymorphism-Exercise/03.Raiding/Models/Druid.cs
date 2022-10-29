735773cc98b8c9f5e45294426a206da743d8c73e
Denis Argatski <denis.argatski@gmail.com>
2022-10-29 23:26:52 +0300

    Druid.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/03.Raiding/Models/Druid.cs
@@ -0,0 +1,11 @@
+﻿namespace Raiding.Models
+{
+    public class Druid : BaseHero
+    {
+        private const int DRUIDPOWER = 80;
+        public Druid(string name)
+            : base(name, DRUIDPOWER)
+        {
+        }
+    }
+}
