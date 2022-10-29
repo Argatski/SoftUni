b4136d91e8d115f9a3df9dc67d29d421a128802e
Denis Argatski <denis.argatski@gmail.com>
2022-10-29 23:26:21 +0300

    BaseHero.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/03.Raiding/Models/BaseHero.cs
@@ -0,0 +1,29 @@
+﻿using Raiding.Contracts;
+
+namespace Raiding.Models
+{
+    public abstract class BaseHero : IHeroes
+    {
+        protected BaseHero(string name, int power)
+        {
+            this.Name = name;
+            this.Power = power;
+        }
+        public string Name
+        {
+            get;
+            private set;
+        }
+
+        public int Power
+        {
+            get;
+            private set;
+        }
+
+        public virtual string CastAbility()
+        {
+            return $"{this.GetType().Name} - {this.Name} healed for {this.Power}";
+        }
+    }
+}
