2aa0384be0bed49d6e6ee8314c68b1e0cbd51aea
Denis Argatski <denis.argatski@gmail.com>
2022-10-29 23:27:32 +0300

    Paladin.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/03.Raiding/Models/Paladin.cs
@@ -0,0 +1,12 @@
+﻿namespace Raiding.Models
+{
+    public class Paladin : BaseHero
+    {
+        private const int PALADINPOWER = 100;
+        public Paladin(string name)
+            : base(name, PALADINPOWER)
+        {
+        }
+        
+    }
+}
