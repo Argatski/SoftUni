30016c9d2c1436112218bb2130b1878f68f4ff6e
Denis Argatski <denis.argatski@gmail.com>
2022-10-29 23:27:57 +0300

    Rogue.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/03.Raiding/Models/Rogue.cs
@@ -0,0 +1,15 @@
+﻿namespace Raiding.Models
+{
+    public class Rogue : BaseHero
+    {
+        private const int ROGUEPOWER = 100;
+        public Rogue(string name)
+            : base(name, ROGUEPOWER)
+        {
+        }
+        public override string CastAbility()
+        {
+            return $"{this.GetType().Name} - {base.Name} hit for {base.Power} damage";
+        }
+    }
+}
