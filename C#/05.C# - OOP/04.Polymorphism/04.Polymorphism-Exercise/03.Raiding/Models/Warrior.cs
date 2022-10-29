7dc491a811ec476049bb1e81710c9b3d6612717f
Denis Argatski <denis.argatski@gmail.com>
2022-10-29 23:28:26 +0300

    Warrior.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/03.Raiding/Models/Warrior.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace Raiding.Models
+{
+    public class Warrior : BaseHero
+    {
+        private const int WARRIONPOWER = 100;
+        public Warrior(string name)
+            : base(name, WARRIONPOWER)
+        {
+        }
+
+        public override string CastAbility()
+        {
+            return $"{this.GetType().Name} - {base.Name} hit for {base.Power} damage";
+        }
+    }
+}
