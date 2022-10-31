d88d2d3ee6bdb9311021fcf054845784cb9709a8
Denis Argatski <denis.argatski@gmail.com>
2022-10-31 23:16:45 +0200

    Mammal.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/04.WildFarm/Models/Animal/Mammal.cs
@@ -0,0 +1,18 @@
+﻿namespace WildFarm.Core
+{
+    public abstract class Mammal : Animal
+    {
+        protected Mammal(string name, double weight, string livingRegion)
+            : base(name, weight)
+        {
+            this.LivingRegion = livingRegion;
+        }
+
+        public string LivingRegion { get; }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"{this.Weight}, {this.LivingRegion}, {this.FoodEaten}]";
+        }
+    }
+}
