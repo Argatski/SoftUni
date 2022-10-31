73443e7784768f20bbfb3747580b0319b907b484
Denis Argatski <denis.argatski@gmail.com>
2022-10-31 23:15:42 +0200

    Cat.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/04.WildFarm/Models/Animal/Cat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using WildFarm.Models.Foods;
+
+namespace WildFarm.Core
+{
+    public class Cat : Feline
+    {
+        private const double weightConst = 0.3;
+
+        public Cat(string name, double weight, string livingRegion, string breed)
+            : base(name, weight, livingRegion, breed)
+        {
+        }
+
+        public override double WeightMultiplier => weightConst;
+
+        public override ICollection<Type> PreferredFoods => new List<Type>() { typeof(Vegetable), typeof(Meat) };
+
+        public override string Sound()
+        {
+            return "Meow";
+        }
+    }
+}
