84a14cf49a34d1c032cf5f9153170abe54d544c6
Denis Argatski <denis.argatski@gmail.com>
2022-10-31 23:11:40 +0200

    FoodFactory.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/04.WildFarm/Factories/FoodFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using WildFarm.Common;
+using WildFarm.Core;
+
+namespace WildFarm.Factories
+{
+    public class FoodFactory
+    {
+        public Food Create(string strType, int quantity)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            Type type = assembly
+                .GetTypes()
+                .FirstOrDefault(t => t.Name == strType);
+
+            if (type == null)
+            {
+                throw new InvalidOperationException(ExceptionMessages.InvalidType);
+            }
+
+            object[] ctorParams = new object[] { quantity };
+            Food food = (Food)Activator.CreateInstance(type, ctorParams);
+
+            return food;
+        }
+    }
+}
