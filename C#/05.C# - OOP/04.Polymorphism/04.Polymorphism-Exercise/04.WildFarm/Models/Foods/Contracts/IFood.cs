356103caf79467ed86243a8c858a243c8b82c029
Denis Argatski <denis.argatski@gmail.com>
2022-10-31 23:18:44 +0200

    IFood.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/04.WildFarm/Models/Foods/Contracts/IFood.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm.Models.Foods.Contracts
+{
+    public interface IFood
+    {
+        int Quantity { get; }
+
+    }
+}
