4c62377105ef0b3c061e9b48fe3c7b597468a815
Denis Argatski <denis.argatski@gmail.com>
2022-10-31 23:17:17 +0200

    Owl.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/04.WildFarm/Models/Animal/Mouse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using WildFarm.Models.Foods;
+
+namespace WildFarm.Core
+{
+    public class Mouse : Mammal
+    {
+        private const double weightConst = 0.10;
+
+        public Mouse(string name, double weight, string livingRegion)
+            : base(name, weight, livingRegion)
+        {
+        }
+
+        public override double WeightMultiplier => weightConst;
+
+        public override ICollection<Type> PreferredFoods => new List<Type>() { typeof(Vegetable), typeof(Fruit) };
+
+        public override string Sound()
+        {
+            return "Squeak";
+        }
+    }
+}
---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/04.WildFarm/Models/Animal/Owl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using WildFarm.Models.Foods;
+
+namespace WildFarm.Core
+{
+    public class Owl : Bird
+    {
+        private const double weightConst = 0.25;
+
+        public Owl(string name, double weight, double wingSize)
+             : base(name, weight, wingSize)
+        {
+        }
+
+        public override double WeightMultiplier => weightConst;
+
+        public override ICollection<Type> PreferredFoods => new List<Type>() { typeof(Meat) };
+
+        public override string Sound()
+        {
+            return "Hoot Hoot";
+        }
+    }
+}
