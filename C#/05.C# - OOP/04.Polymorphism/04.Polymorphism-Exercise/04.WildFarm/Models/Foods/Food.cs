a6e9a34ffea04dbe58643be413ccc9983523796e
Denis Argatski <denis.argatski@gmail.com>
2022-10-31 23:19:27 +0200

    Food.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/04.WildFarm/Models/Foods/Food.cs
@@ -0,0 +1,13 @@
+﻿using WildFarm.Models.Foods.Contracts;
+
+namespace WildFarm.Core
+{
+    public abstract class Food : IFood
+    {
+        protected Food(int quantity)
+        {
+            this.Quantity = quantity;
+        }
+        public int Quantity { get; }
+    }
+}
