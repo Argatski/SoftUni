da5bc44cc4c8c9ae4bdec0da5d67d7510373f031
Denis Argatski <denis.argatski@gmail.com>
2022-10-31 23:13:47 +0200

    IFeedable.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/04.WildFarm/Models/Animal/Contracts/IFeedable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using WildFarm.Models.Foods.Contracts;
+
+namespace WildFarm.Models.Animal.Contracts
+{
+    public interface IFeedable
+    {
+        void Feed(IFood food);
+        int FoodEaten { get; }
+        double WeightMultiplier { get; }
+
+        ICollection<Type> PreferredFoods { get; }
+    }
+}
