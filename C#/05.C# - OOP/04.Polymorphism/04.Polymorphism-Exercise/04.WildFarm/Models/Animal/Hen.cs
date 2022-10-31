1fdc2514a57b8dd7f1b4039bd2ca9be8bead62bb
Denis Argatski <denis.argatski@gmail.com>
2022-10-31 23:16:24 +0200

    Hen.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/04.WildFarm/Models/Animal/Hen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using WildFarm.Models.Foods;
+
+namespace WildFarm.Core
+{
+    public class Hen : Bird
+    {
+        private const double weightConst = 0.35;
+
+        public Hen(string name, double weight, double wingSize)
+            : base(name, weight, wingSize)
+        {
+        }
+
+        public override double WeightMultiplier => weightConst;
+
+        public override ICollection<Type> PreferredFoods =>
+           new List<Type>() { typeof(Fruit), typeof(Seeds), typeof(Meat), typeof(Vegetable) };
+
+        public override string Sound()
+        {
+            return "Cluck";
+        }
+    }
+}
