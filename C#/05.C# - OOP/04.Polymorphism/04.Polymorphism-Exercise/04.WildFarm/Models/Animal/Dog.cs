f050ddaf7d38588a903074f62d0745bbf753bc75
Denis Argatski <denis.argatski@gmail.com>
2022-10-31 23:15:54 +0200

    Dog.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/04.WildFarm/Models/Animal/Dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using WildFarm.Models.Foods;
+
+namespace WildFarm.Core
+{
+    public class Dog : Mammal
+    {
+        private const double weightConst = 0.4;
+        public Dog(string name, double weight, string livingRegion)
+            : base(name, weight, livingRegion)
+        {
+        }
+
+        public override double WeightMultiplier => weightConst;
+
+        public override ICollection<Type> PreferredFoods => new List<Type>() { typeof(Meat) };
+        public override string Sound()
+        {
+            return "Woof!";
+        }
+    }
+}
