d342c9c6f5d61d73259434d488b030312444abc1
Denis Argatski <denis.argatski@gmail.com>
2022-10-31 23:16:10 +0200

    Feline.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/04.WildFarm/Models/Animal/Feline.cs
@@ -0,0 +1,19 @@
+﻿namespace WildFarm.Core
+{
+    public abstract class Feline : Mammal
+    {
+        private string breed;
+
+        protected Feline(string name, double weight, string livingRegion, string breed)
+            : base(name, weight, livingRegion)
+        {
+            this.Breed = breed;
+        }
+
+        public string Breed { get; }
+        public override string ToString()
+        {
+            return $"{this.GetType().Name} [{this.Name}, {this.Breed}, {this.Weight}, {this.LivingRegion}, {this.FoodEaten}]";
+        }
+    }
+}
