23d51cd4147dbb412d8a6264b15f1af563c880da
Denis Argatski <denis.argatski@gmail.com>
2022-10-31 23:17:32 +0200

    Tiger.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/04.WildFarm/Models/Animal/Tiger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using WildFarm.Models.Foods;
+
+namespace WildFarm.Core
+{
+    public class Tiger : Feline
+    {
+        private const double weightConst = 1.0;
+
+        public Tiger(string name, double weight, string livingRegion, string breed)
+            : base(name, weight, livingRegion, breed)
+        {
+        }
+
+        public override double WeightMultiplier => weightConst;
+
+        public override ICollection<Type> PreferredFoods => new List<Type>() { typeof(Meat) };
+
+        public override string Sound()
+        {
+            return "ROAR!!!";
+        }
+    }
+}
