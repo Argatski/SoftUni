0f1a7b73699f1d9d46e89f3ba679aab87a982987
Denis Argatski <denis.argatski@gmail.com>
2022-10-31 23:15:30 +0200

    Bird.cs

---
+++ C#/05.C# - OOP/04.Polymorphism/04.Polymorphism-Exercise/04.WildFarm/Models/Animal/Bird.cs
@@ -0,0 +1,17 @@
+﻿namespace WildFarm.Core
+{
+    public abstract class Bird : Animal
+    {
+        protected Bird(string name, double weight, double wingSize)
+            : base(name, weight)
+        {
+            this.WingSize = wingSize;
+        }
+
+        public double WingSize { get; }
+        public override string ToString()
+        {
+            return base.ToString() + $"{this.WingSize}, {this.Weight}, {this.FoodEaten}]";
+        }
+    }
+}
