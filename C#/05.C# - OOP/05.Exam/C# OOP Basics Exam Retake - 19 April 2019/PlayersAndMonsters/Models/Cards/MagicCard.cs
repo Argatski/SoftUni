de14755209cffb5e7b836319fc0e6355c31006d5
Denis Argatski <denis.argatski@gmail.com>
2022-11-01 22:29:52 +0200

    MagicCard.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 19 April 2019/PlayersAndMonsters/Models/Cards/MagicCard.cs
@@ -0,0 +1,12 @@
+﻿namespace PlayersAndMonsters.Models.Cards
+{
+    public class MagicCard : Card
+    {
+        private const int damagePoints = 5;
+        private const int healthPoints = 80;
+        public MagicCard(string name)
+            : base(name, damagePoints, healthPoints)
+        {
+        }
+    }
+}
