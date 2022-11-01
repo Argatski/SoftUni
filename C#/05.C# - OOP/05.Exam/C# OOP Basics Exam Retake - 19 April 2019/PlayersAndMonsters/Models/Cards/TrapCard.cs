db648f82aebcfeb6f96ae14304f188f3859f2b70
Denis Argatski <denis.argatski@gmail.com>
2022-11-01 22:30:55 +0200

    TrapCard.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 19 April 2019/PlayersAndMonsters/Models/Cards/TrapCard.cs
@@ -0,0 +1,12 @@
+﻿namespace PlayersAndMonsters.Models.Cards
+{
+    public class TrapCad : Card
+    {
+        private const int damagePoints = 120;
+        private const int healthPoints = 5;
+        public TrapCad(string name)
+            : base(name, damagePoints, healthPoints)
+        {
+        }
+    }
+}
