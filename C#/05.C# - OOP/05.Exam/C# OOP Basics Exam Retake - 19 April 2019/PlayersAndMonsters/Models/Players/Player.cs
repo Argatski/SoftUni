482beb0b66a02488445a8d4cfd358e36bee0007c
Denis Argatski <denis.argatski@gmail.com>
2022-11-01 22:27:19 +0200

    Player.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 19 April 2019/PlayersAndMonsters/Models/Players/Player.cs
@@ -0,0 +1,71 @@
+﻿using PlayersAndMonsters.Models.Players.Contracts;
+using PlayersAndMonsters.Repositories.Contracts;
+using System;
+
+namespace PlayersAndMonsters.Models.Players
+{
+    public abstract class Player : IPlayer
+    {
+        private const string INVALID_USER_NAME = "Player's username cannot be null or an empty string.";
+        private const string HEALTH_LESS_ZERO = "Player's health bonus cannot be less than zero.";
+        private const string DAMAGE_LESS_ZERO = "Damage points cannot be less than zero.";
+        private string name;
+        private int health;
+
+        protected Player(ICardRepository cardRepository, string username, int health)
+        {
+            this.CardRepository = cardRepository;
+            this.Username = username;
+            this.Health = health;
+        }
+
+        public ICardRepository CardRepository
+        {
+            get;
+        }
+
+        public string Username
+        {
+            get { return this.name; }
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(INVALID_USER_NAME);
+                }
+                this.name = value;
+            }
+        }
+
+        public int Health
+        {
+            get
+            {
+                return this.health;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(HEALTH_LESS_ZERO);
+                }
+                this.health = value;
+            }
+        }
+
+        public bool IsDead => this.Health >= 0;
+
+        public void TakeDamage(int damagePoints)
+        {
+            if (damagePoints < 0)
+            {
+                throw new ArgumentException(DAMAGE_LESS_ZERO);
+            }
+            if (this.IsDead == true)
+            {
+                this.Health -= damagePoints;
+
+            }
+        }
+    }
+}
