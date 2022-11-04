9c3a6ba343c6412eb4d9ec97e4e4429de71b4d72
Denis Argatski <denis.argatski@gmail.com>
2022-11-04 22:53:54 +0200

    PlayerRepository.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 19 April 2019/PlayersAndMonsters/Repositories/PlayerRepository.cs
@@ -0,0 +1,54 @@
+﻿using PlayersAndMonsters.Models.Players.Contracts;
+using PlayersAndMonsters.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlayersAndMonsters.Repositories
+{
+    public class PlayerRepository : IPlayerRepository
+    {
+        private const string PlayerCannotNull = "Player cannot be null";
+
+        private List<IPlayer> players;
+
+        public PlayerRepository()
+        {
+            this.players = new List<IPlayer>();
+        }
+
+        public int Count => this.players.Count();
+
+        public IReadOnlyCollection<IPlayer> Players => this.players.AsReadOnly();
+
+        public void Add(IPlayer player)
+        {
+            this.ChekIfPlayerIsNull(player);
+
+            if (this.players.Any(x => x.Username == player.Username))
+            {
+                throw new ArgumentException($"Player {player.Username} already exists!");
+            }
+            this.players.Add(player);
+        }
+
+        public IPlayer Find(string username)
+        {
+            IPlayer player = this.players.FirstOrDefault(x => x.Username == username);
+            return player;
+        }
+
+        public bool Remove(IPlayer player)
+        {
+            this.ChekIfPlayerIsNull(player);
+            return this.players.Remove(player);
+        }
+        private void ChekIfPlayerIsNull(IPlayer player)
+        {
+            if (player == null)
+            {
+                throw new ArgumentException(PlayerCannotNull);
+            }
+        }
+    }
+}
