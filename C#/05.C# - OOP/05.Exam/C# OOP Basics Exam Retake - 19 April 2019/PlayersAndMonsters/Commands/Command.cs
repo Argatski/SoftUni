b176d9a3fec959675d2739b54d37709a6ac3eab4
Denis Argatski <denis.argatski@gmail.com>
2022-11-06 22:17:54 +0200

    Command.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 19 April 2019/PlayersAndMonsters/Commands/Command.cs
@@ -0,0 +1,19 @@
+﻿using PlayersAndMonsters.Commands.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters.Commands
+{
+    public class Command : ICommand
+    {
+        public Command(string name, string[] arguments)
+        {
+            this.Name = name;
+            this.Arguments = arguments;
+        }
+        public string Name { get; private set; }
+
+        public string[] Arguments { get; private set; }
+    }
+}
