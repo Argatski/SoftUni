6045049420489f195bf6ab8e2a2355e8dfba356e
Denis Argatski <denis.argatski@gmail.com>
2022-11-06 22:18:10 +0200

    CommandParser.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 19 April 2019/PlayersAndMonsters/Commands/CommandParser.cs
@@ -0,0 +1,17 @@
+﻿using PlayersAndMonsters.Commands.Contracts;
+using System.Linq;
+
+namespace PlayersAndMonsters.Commands
+{
+    public class CommandParser : ICommandParser
+    {
+        public ICommand Parse(string input)
+        {
+            var inputParts = input.Split();
+            var name = inputParts[0];
+            var argument = inputParts.Skip(1).ToArray();
+
+            return new Command(name, argument);
+        }
+    }
+}
