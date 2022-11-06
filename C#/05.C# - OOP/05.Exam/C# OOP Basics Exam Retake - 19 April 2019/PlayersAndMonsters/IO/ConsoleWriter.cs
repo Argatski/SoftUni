1b35c9e15e4b7ad0665d4230274cbb6e2959f217
Denis Argatski <denis.argatski@gmail.com>
2022-11-06 22:27:28 +0200

    ConsoleWriter.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 19 April 2019/PlayersAndMonsters/IO/ConsoleWriter.cs
@@ -0,0 +1,12 @@
+﻿using PlayersAndMonsters.IO.Contracts;
+using System;
+
+namespace PlayersAndMonsters.IO
+{
+    public class ConsoleWriter : IWriter
+    {
+        public void Write(object message) => Console.Write(message);
+
+        public void WriteLine(object message) => Console.WriteLine(message);
+    }
+}
