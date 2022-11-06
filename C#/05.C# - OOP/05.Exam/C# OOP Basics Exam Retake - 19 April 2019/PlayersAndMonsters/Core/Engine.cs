4b431dd602c0bf9d655e4af26bdfcfa2d08882ea
Denis Argatski <denis.argatski@gmail.com>
2022-11-06 22:24:45 +0200

    Engine.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 19 April 2019/PlayersAndMonsters/Core/Engine.cs
@@ -0,0 +1,89 @@
+﻿using PlayersAndMonsters.Commands.Contracts;
+using PlayersAndMonsters.Core.Contracts;
+using PlayersAndMonsters.IO.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters.Core
+{
+    public class Engine : IEngine
+    {
+        private const string EndCommand = "Exit";
+
+        private readonly ICommandParser commandParser;
+        private readonly IManagerController managerController;
+
+        private readonly IReader dataReader;
+        private readonly IWriter dataWriter;
+
+        public Engine
+            (ICommandParser commandParser, IManagerController managerController,
+            IReader reader, IWriter writer)
+        {
+            this.commandParser = commandParser;
+            this.managerController = managerController;
+            this.dataReader = reader;
+            this.dataWriter = writer;
+        }
+
+        public void Run()
+        {
+            var input = this.dataReader.ReadLine();
+
+            while (input != EndCommand)
+            {
+                try
+                {
+                    this.ExecuteCommands(input);
+                }
+                catch (ArgumentException ae)
+                {
+                    this.dataWriter.WriteLine(ae.Message);
+                }
+                input = this.dataReader.ReadLine();
+            }
+        }
+
+        private void ExecuteCommands(string input)
+        {
+            var command = this.commandParser.Parse(input);
+
+            var message = string.Empty;
+
+            if (command.Name == "AddPlayer")
+            {
+                var playerType = command.Arguments[0];
+                var playerUsername = command.Arguments[1];
+
+                message = this.managerController.AddPlayer(playerType, playerUsername);
+            }
+            else if (command.Name == "AddCard")
+            {
+                var cardType = command.Arguments[0];
+                var cardName = command.Arguments[1];
+
+                message = this.managerController.AddCard(cardType, cardName);
+            }
+            else if (command.Name == "AddPlayerCard")
+            {
+                var playerUsername = command.Arguments[0];
+                var cardName = command.Arguments[1];
+
+                message = this.managerController.AddPlayerCard(playerUsername, cardName);
+            }
+            else if (command.Name == "Fight")
+            {
+                var attackUser = command.Arguments[0];
+                var enemyUser = command.Arguments[1];
+
+                message = this.managerController.Fight(attackUser, enemyUser);
+            }
+            else if (command.Name == "Report")
+            {
+                message = this.managerController.Report();
+            }
+            this.dataWriter.WriteLine(message);
+        }
+    }
+}
