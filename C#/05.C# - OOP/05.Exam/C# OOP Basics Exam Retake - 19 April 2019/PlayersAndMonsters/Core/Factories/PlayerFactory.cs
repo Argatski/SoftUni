5758c077a9ce35d76a0813787f87963a5e210c5e
Denis Argatski <denis.argatski@gmail.com>
2022-11-06 22:23:42 +0200

    PlayerFactory.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 19 April 2019/PlayersAndMonsters/Core/Factories/PlayerFactory.cs
@@ -0,0 +1,38 @@
+﻿using PlayersAndMonsters.Core.Factories.Contracts;
+using PlayersAndMonsters.Models.Players.Contracts;
+using PlayersAndMonsters.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace PlayersAndMonsters.Core.Factories
+{
+    public class PlayerFactory : IPlayerFactory
+    {
+        public IPlayer CreatePlayer(string type, string username)
+        {
+            var playerType = Assembly
+                 .GetCallingAssembly()
+                 .GetTypes()
+                 .Where(p => p.Name == type && !p.IsAbstract)
+                 .FirstOrDefault();
+
+            if (playerType == null)
+            {
+                throw new ArgumentException("Invalid player type!");
+            }
+
+            var parameters = new object[]
+            {
+                new CardRepository(),
+                username
+            };
+
+            var player = (IPlayer)Activator.CreateInstance(playerType, parameters);
+
+            return player;
+        }
+    }
+}
