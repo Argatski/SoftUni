b9070b3ab1757c3e45500f5617cd2d96cfe40779
Denis Argatski <denis.argatski@gmail.com>
2022-11-06 22:13:04 +0200

    StartUp.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 19 April 2019/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace PlayersAndMonsters
+{
+    using Core;
+    using Core.Factories;
+    using IO;
+    using Models.BattleFields;
+    using PlayersAndMonsters.Commands;
+    using Repositories;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var commandParser = new CommandParser();
+
+            var playerRepository = new PlayerRepository();
+            var cardRepository = new CardRepository();
+            var battleField = new BattleField();
+            var playerFactory = new PlayerFactory();
+            var cardFactory = new CardFactory();
+
+            var managerController = new ManagerController
+                (playerRepository, cardRepository, battleField, playerFactory, cardFactory);
+
+            var dataReader = new ConsoleReader();
+            var dataWriter = new ConsoleWriter();
+
+            var engine = new Engine
+                (
+                commandParser, managerController, dataReader, dataWriter
+                );
+
+            engine.Run();
+        }
+    }
+}
