bfa15250743300f1ccd1182b0cadcc99a544ff67
Denis Argatski <denis.argatski@gmail.com>
2022-11-06 22:27:13 +0200

    ConsoleReader.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 19 April 2019/PlayersAndMonsters/IO/ConsoleReader.cs
@@ -0,0 +1,12 @@
+﻿using PlayersAndMonsters.IO.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters.IO
+{
+    public class ConsoleReader : IReader
+    {
+        public string ReadLine() => Console.ReadLine();
+    }
+}
