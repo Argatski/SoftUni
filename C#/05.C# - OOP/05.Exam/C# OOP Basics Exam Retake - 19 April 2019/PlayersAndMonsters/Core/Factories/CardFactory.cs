e2d80c42945472828e2eb7ffb505d55e696fb191
Denis Argatski <denis.argatski@gmail.com>
2022-11-06 22:23:18 +0200

    CardFactory.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 19 April 2019/PlayersAndMonsters/Core/Factories/CardFactory.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace PlayersAndMonsters.Core.Factories
+{
+    using Contracts;
+    using Models.Cards.Contracts;
+    using System;
+    using System.Linq;
+
+    public class CardFactory : ICardFactory
+    {
+        public ICard CreateCard(string type, string name)
+        {
+            var cardTyper = Assembly
+                .GetCallingAssembly()
+                .GetTypes()
+                .Where(n => n.Name == $"{type}Card" && !n.IsAbstract)
+                .FirstOrDefault();
+
+            if (cardTyper == null)
+            {
+                throw new ArgumentException("Invalid card typer!");
+            }
+
+            var card = (ICard)Activator.CreateInstance(cardTyper, name);
+
+            return card;
+        }
+    }
+}
