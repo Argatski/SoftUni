5f0d851fb45c05355351ef55cea23f0bc33ebf15
Denis Argatski <denis.argatski@gmail.com>
2022-11-06 22:16:34 +0200

    ICommand.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 19 April 2019/PlayersAndMonsters/Commands/Contracts/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters.Commands.Contracts
+{
+    public interface ICommand
+    {
+        string Name { get; }
+        string[] Arguments { get; }
+    }
+}
