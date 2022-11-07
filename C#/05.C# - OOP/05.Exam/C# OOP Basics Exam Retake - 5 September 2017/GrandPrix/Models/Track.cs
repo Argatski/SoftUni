a15393ebe7a78022ab9284870995874bc517d842
Denis Argatski <denis.argatski@gmail.com>
2022-11-07 23:01:21 +0200

    Track.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 5 September 2017/GrandPrix/Models/Track.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrandPrix.Models
+{
+    public class Track
+    {
+        public Track(int totalLaps, int length)
+        {
+            this.TotalLaps = totalLaps;
+            this.Length = length;
+        }
+        public int TotalLaps { get; }
+        public int Length { get; }
+        public int CurrentLap { get; set; }
+        public string Weather { get; set; }
+    }
+}
