5b26b0e5f3355bbc4179054bf345a2b8e92df361
Denis Argatski <denis.argatski@gmail.com>
2022-11-07 22:58:50 +0200

    AggressiveDriver.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 5 September 2017/GrandPrix/Models/Drivers/AggressiveDriver.cs
@@ -0,0 +1,18 @@
+﻿using GrandPrix.Models.Cars;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrandPrix.Models.Drivers
+{
+    public class AggressiveDriver : Driver
+    {
+        private const double aggressiveFuelConsumpton = 2.7;
+        private const double multipliedSpeed = 1.3;
+        public AggressiveDriver(string name, Car car)
+            : base(name, car, aggressiveFuelConsumpton)
+        {
+        }
+        public override double Speed => base.Speed * multipliedSpeed;
+    }
+}
