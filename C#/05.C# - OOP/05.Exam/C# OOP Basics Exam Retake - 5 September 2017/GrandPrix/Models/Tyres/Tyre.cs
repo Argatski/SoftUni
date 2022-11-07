4b813b09d717bdf16c18bedbbefe287cecebb6f3
Denis Argatski <denis.argatski@gmail.com>
2022-11-07 23:00:30 +0200

    Tyre.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 5 September 2017/GrandPrix/Models/Tyres/Tyre.cs
@@ -0,0 +1,42 @@
+﻿using GrandPrix.Exception;
+using GrandPrix.Models.Tyres.Contract;
+using System;
+
+namespace GrandPrix.Models.Tyres
+{
+    public abstract class Tyre : ITyre
+    {
+        private double startPointDegradation = 100;
+        private string name;
+        private double hardness;
+        private double degradation;
+
+        public Tyre(string name, double hardness)
+        {
+            this.Name = name;
+            this.Hardness = hardness;
+            this.Degradation = degradation;
+        }
+        public string Name { get { return this.name; } private set { this.name = value; } }
+
+        public double Hardness { get {return this.hardness; } private set { this.hardness = value; } }
+
+        public virtual double Degradation
+        {
+            get { return this.startPointDegradation; }
+            protected set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ErroMessage.TyreDegradation);
+                }
+                this.degradation = value;
+            }
+        }
+        public virtual void ReduceDegradation()
+        {
+            this.Degradation -= this.Hardness;
+        }
+
+    }
+}
