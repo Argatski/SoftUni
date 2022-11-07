7dfbc6d03bdbcfa5cfe16b06f73f0552a7b1cf4e
Denis Argatski <denis.argatski@gmail.com>
2022-11-07 23:00:16 +0200

    HardTyre.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 5 September 2017/GrandPrix/Models/Tyres/HardTyre.cs
@@ -0,0 +1,10 @@
+﻿namespace GrandPrix.Models.Tyres
+{
+    public class HardTyre : Tyre
+    {
+        public HardTyre(string name, double hardness)
+            : base("Hard", hardness)
+        {
+        }
+    }
+}
