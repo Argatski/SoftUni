9b4803c8648e46029529f871f6e598d37f9838f9
Denis Argatski <denis.argatski@gmail.com>
2022-11-07 23:00:47 +0200

    UltrasoftTyre.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 5 September 2017/GrandPrix/Models/Tyres/UltrasoftTyre.cs
@@ -0,0 +1,46 @@
+﻿using GrandPrix.Exception;
+using System;
+
+namespace GrandPrix.Models.Tyres
+{
+    public class UltrasoftTyre : Tyre
+    {
+        private const double MinDegradation = 30;
+        private double degradation;
+        private double grip;
+        public UltrasoftTyre(string name, double hardness)
+            : base(name, hardness)
+        {
+            this.Grip = this.grip;
+        }
+        public double Grip
+        {
+            get { return this.grip; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Invalid input");
+                }
+                this.grip = value;
+            }
+        }
+
+        public override double Degradation
+        {
+            get { return this.degradation; }
+            protected set
+            {
+                if (value < MinDegradation)
+                {
+                    throw new ArgumentException(ErroMessage.TyreDegradation);
+                }
+                this.degradation = value;
+            }
+        }
+        public override void ReduceDegradation()
+        {
+            this.Degradation -= this.Grip + this.Hardness;
+        }
+    }
+}
