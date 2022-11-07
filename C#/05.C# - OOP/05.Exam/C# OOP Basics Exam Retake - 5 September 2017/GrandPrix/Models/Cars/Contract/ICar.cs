c315b5b1960e2ff295c1ebf277f306456da48d97
Denis Argatski <denis.argatski@gmail.com>
2022-11-07 22:57:07 +0200

    Contract

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 5 September 2017/GrandPrix/Models/Cars/Contract/ICar.cs
@@ -0,0 +1,13 @@
+﻿using GrandPrix.Models.Tyres;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrandPrix.Models.Cars.Contract
+{
+    public interface ICar
+    {
+        int Hp { get; }
+        double FuelAmount { get; }
+    }
+}
