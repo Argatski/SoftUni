9ed87ad00bea089ac4b9abd8790cb6850d563035
Denis Argatski <denis.argatski@gmail.com>
2022-11-07 23:00:01 +0200

    Contract

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 5 September 2017/GrandPrix/Models/Tyres/Contract/ITyre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrandPrix.Models.Tyres.Contract
+{
+    public interface ITyre
+    {
+        string Name { get; }
+        double Hardness { get; }
+        double Degradation { get; }
+    }
+}
