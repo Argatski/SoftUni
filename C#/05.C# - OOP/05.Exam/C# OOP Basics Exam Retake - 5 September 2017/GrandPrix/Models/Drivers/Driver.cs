340994c16991036ace39cf0a262bd9af0a8df0d8
Denis Argatski <denis.argatski@gmail.com>
2022-11-10 23:16:53 +0200

    Driver.cs

--- C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 5 September 2017/GrandPrix/Models/Drivers/Driver.cs
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 5 September 2017/GrandPrix/Models/Drivers/Driver.cs
@@ -1,50 +1,45 @@
-﻿using GrandPrix.Models.Cars;
-using GrandPrix.Models.Drivers.Contracts;
-
-namespace GrandPrix.Models.Drivers
+﻿public abstract class Driver
 {
-    public abstract class Driver : IDriver
+    private string name;
+    private double totalTime;
+    private Car car;
+    private double fuelConsumptionPerKm;
+
+    protected Driver(string name, Car car, double fuelConsumptionPerKm)
     {
-        private string name;
-        private double totalTime;
-        private Car car;
-        private double consumptionPerKm;
+        this.Name = name;
+        this.Car = car;
+        this.FuelConsumptionPerKm = fuelConsumptionPerKm;
+    }
 
-        protected Driver(string name, Car car, double fuelConsumptionPerKm)
-        {
-            this.Name = name;
-            this.Car = car;
-            this.FuelConsumptionPerKm = fuelConsumptionPerKm;
-        }
+    public virtual double Speed => (this.Car.Hp + this.Car.Tyre.Degradation) / this.Car.FuelAmount;
 
-        public Car Car
-        {
-            get { return this.car; }
-            private set { this.car = value; }
-        }
+    public double FuelConsumptionPerKm
+    {
+        get { return this.fuelConsumptionPerKm; }
+        private set { this.fuelConsumptionPerKm = value; }
+    }
 
-        public string Name
-        {
-            get { return this.name; }
-            private set { this.name = value; }
-        }
+    public Car Car
+    {
+        get { return this.car; }
+        private set { this.car = value; }
+    }
 
-        public double TotalTime
-        {
-            get { return this.totalTime; }
-            private set { this.totalTime = value; }
-        }
+    public double TotalTime
+    {
+        get { return this.totalTime; }
+        set { this.totalTime = value; }
+    }
 
-        public virtual double Speed => (this.car.Hp + this.car.Tyre.Degradation) / this.car.FuelAmount;
+    public string Name
+    {
+        get { return this.name; }
+        private set { this.name = value; }
+    }
 
-        public double FuelConsumptionPerKm
-        {
-            get { return this.consumptionPerKm; }
-            private set { this.consumptionPerKm = value; }
-        }
-        public void ReduceFuelAmount(int lenght)
-        {
-            this.Car.ReduceFuel(lenght, this.FuelConsumptionPerKm);
-        }
+    public void ReduceFuelAmount(int length)
+    {
+        this.Car.ReduceFuel(length, this.FuelConsumptionPerKm);
     }
 }
