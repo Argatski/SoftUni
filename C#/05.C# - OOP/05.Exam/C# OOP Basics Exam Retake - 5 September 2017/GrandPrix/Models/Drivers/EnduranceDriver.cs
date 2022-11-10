f94b5349b4e224530887f6cc73fa90f10165dea8
Denis Argatski <denis.argatski@gmail.com>
2022-11-10 23:17:10 +0200

    EnduranceDriver.cs

--- C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 5 September 2017/GrandPrix/Models/Drivers/EnduranceDriver.cs
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam Retake - 5 September 2017/GrandPrix/Models/Drivers/EnduranceDriver.cs
@@ -1,13 +1,9 @@
-﻿using GrandPrix.Models.Cars;
-
-namespace GrandPrix.Models.Drivers
+﻿public class EnduranceDriver : Driver
 {
-    public class EnduranceDriver : Driver
+    private const double EnduranceDriverFuelConsumption = 1.5;
+
+    public EnduranceDriver(string name, Car car)
+        : base(name, car, EnduranceDriverFuelConsumption)
     {
-        private const double enduranceDriverConsumption = 1.5;
-        public EnduranceDriver(string name, Car car)
-            : base(name, car, enduranceDriverConsumption)
-        {
-        }
     }
 }
