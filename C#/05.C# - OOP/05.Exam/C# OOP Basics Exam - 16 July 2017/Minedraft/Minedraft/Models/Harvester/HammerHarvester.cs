bb40d16bdb26b48ea61c088aeac47e596d5ee6a8
Denis Argatski <denis.argatski@gmail.com>
2022-11-16 23:16:23 +0200

    HammerHarvester.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam - 16 July 2017/Minedraft/Minedraft/Models/Harvester/HammerHarvester.cs
@@ -0,0 +1,15 @@
+﻿namespace Minedraft.Models.Harvester
+{
+    public class HammerHarvester : Harverster
+    {
+        private const int IncreaseOreOutput = 200;
+        private const int IncreaseEnergyRequirement = 100;
+        public HammerHarvester(string id, double oreOutput, double energyrequirement)
+            : base(id, oreOutput, energyrequirement)
+        {
+            this.OreOutput = oreOutput + (oreOutput * IncreaseOreOutput / 100);
+            this.EnergyRequirement = energyrequirement + (energyrequirement * IncreaseEnergyRequirement / 100);
+        }
+
+    }
+}
