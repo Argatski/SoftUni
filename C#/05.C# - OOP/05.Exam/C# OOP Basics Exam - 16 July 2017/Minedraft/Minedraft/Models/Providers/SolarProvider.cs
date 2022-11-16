83941723fa094d3002979f84f0994c504242b70f
Denis Argatski <denis.argatski@gmail.com>
2022-11-16 23:20:19 +0200

    SolarProvider.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam - 16 July 2017/Minedraft/Minedraft/Models/Providers/SolarProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace Minedraft.Models.Providers
+{
+    public class SolarProvider : Providers
+    {
+        public SolarProvider(string id, double energyOutput)
+            : base(id, energyOutput)
+        {
+        }
+    }
+}
