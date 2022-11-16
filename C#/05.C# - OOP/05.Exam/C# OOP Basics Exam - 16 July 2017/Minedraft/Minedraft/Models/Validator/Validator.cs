662532a35175bcc74183fd1559d4f80c2d7644ca
Denis Argatski <denis.argatski@gmail.com>
2022-11-16 23:20:54 +0200

    Validator.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam - 16 July 2017/Minedraft/Minedraft/Models/Validator/Validator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Minedraft.Models.Validator
+{
+    public class Validator
+    {
+        public static void CheckHarvesterOreOuput(string property, double value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException($"Harvester is not registered, because of it's {property}");
+            }
+        }
+
+        public static void CheckHarvesterEnergyRequirement(string property, double value)
+        {
+            if (value < 0 || value > 20000)
+            {
+                throw new ArgumentException($"Harvester is not registered, because of it's {property}");
+            }
+        }
+
+        public static void CheckProviderEnerygOutput(string property, double value)
+        {
+            if (value < 0 || value > 10000)
+            {
+                throw new ArgumentException($"Provider is not registered, because of it's {property}");
+            }
+        }
+    }
+}
