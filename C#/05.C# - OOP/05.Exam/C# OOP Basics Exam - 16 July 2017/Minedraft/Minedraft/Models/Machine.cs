70c1c644129260a4d690590b4f22bb39a76ed3a7
Denis Argatski <denis.argatski@gmail.com>
2022-11-16 23:21:21 +0200

    Machine.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam - 16 July 2017/Minedraft/Minedraft/Models/Machine.cs
@@ -0,0 +1,17 @@
+﻿namespace Minedraft.Models
+{
+    public abstract class Machine
+    {
+        private string id;
+        protected Machine(string id)
+        {
+            this.Id = id;
+        }
+        public string Id
+        {
+            get { return this.id; }
+            protected set { this.id = value; }
+        }
+
+    }
+}
