4656c5922dfd0b245fdc25d671f997d59996ca40
Denis Argatski <denis.argatski@gmail.com>
2022-11-20 17:40:00 +0200

    PressureProvider.cs

--- C#/05.C# - OOP/05.Exam/C# OOP Basics Exam - 16 July 2017/Minedraft/Minedraft/Models/Providers/PressureProvider.cs
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam - 16 July 2017/Minedraft/Minedraft/Models/Providers/PressureProvider.cs
@@ -1,12 +1,21 @@
-﻿namespace Minedraft.Models.Providers
+﻿using System.Text;
+
+namespace Minedraft.Models.Providers
 {
-    public class PressureProvider : Providers
+    public class PressureProvider : Provider
     {
         private const double IncreaseEnergyOutput = 50;
         public PressureProvider(string id, double energyOutput)
             : base(id, energyOutput)
         {
             this.EnergyOutput = energyOutput + (energyOutput * IncreaseEnergyOutput / 100);
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Pressure Provider - {this.Id}");
+            sb.Append(base.ToString());
+            return sb.ToString();
         }
     }
 }
