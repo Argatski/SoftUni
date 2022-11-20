a65ccd07e0e4ae0132a48cf2fb677d9664375b23
Denis Argatski <denis.argatski@gmail.com>
2022-11-20 17:30:33 +0200

    StartUp.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam - 16 July 2017/Minedraft/Minedraft/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using Minedraft.Controller;
+using System;
+
+namespace Minedraft
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
