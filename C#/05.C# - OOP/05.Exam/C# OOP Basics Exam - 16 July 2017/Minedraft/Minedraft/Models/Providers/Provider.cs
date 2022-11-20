7c48c585ac1c8821f67eb04fb22427ecbde103a8
Denis Argatski <denis.argatski@gmail.com>
2022-11-20 17:40:17 +0200

    Provider.cs

---
+++ C#/05.C# - OOP/05.Exam/C# OOP Basics Exam - 16 July 2017/Minedraft/Minedraft/Models/Providers/Provider.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Minedraft.Models.Providers
+{
+    public abstract class Provider : Machine
+    {
+        private double energyOutput;
+        protected Provider(string id, double energyOutput)
+            : base(id)
+        {
+            this.EnergyOutput = energyOutput;
+        }
+        public double EnergyOutput
+        {
+            get { return this.energyOutput; }
+            protected set
+            {
+                Validator.Validator.CheckProviderEnerygOutput(nameof(EnergyOutput), value);
+                this.energyOutput = value;
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Energy Output: {this.EnergyOutput}");
+            return sb.ToString();
+        }
+    }
+}
