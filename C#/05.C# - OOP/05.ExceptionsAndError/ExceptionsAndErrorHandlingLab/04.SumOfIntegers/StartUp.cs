68ef874535450f78c271cb90780144ca160f6460
Denis Argatski <denis.argatski@gmail.com>
2022-11-21 22:06:45 +0200

    StartUp.cs

---
+++ C#/05.C# - OOP/05.ExceptionsAndError/ExceptionsAndErrorHandlingLab/04.SumOfIntegers/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _04.SumOfIntegers
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] elements = Console.ReadLine()
+                 .Split(" ");
+
+            int sum = 0;
+            for (int i = 0; i < elements.Length; i++)
+            {
+                try
+                {
+                    int num = int.Parse(elements[i]);
+
+                    sum += num;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{elements[i]}' is in wrong format!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{elements[i]}' is out of range!");
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{elements[i]}' processed - current sum: {sum}");
+                }
+            }
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+        }
+    }
+}
