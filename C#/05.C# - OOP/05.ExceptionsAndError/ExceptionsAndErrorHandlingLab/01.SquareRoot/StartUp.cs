8bfc5439e658db8d9d226c74a9cb11e5d1ab7d9e
Denis Argatski <denis.argatski@gmail.com>
2022-11-21 22:02:44 +0200

    StartUp.cs

---
+++ C#/05.C# - OOP/05.ExceptionsAndError/ExceptionsAndErrorHandlingLab/01.SquareRoot/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01.SquareRoot
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            try
+            {
+
+                if (num < 0)
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+                Console.WriteLine(Math.Sqrt(num));
+
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
