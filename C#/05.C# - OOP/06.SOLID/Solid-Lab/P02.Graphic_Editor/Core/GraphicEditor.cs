caf8136592d3387095c38eb117139b3c9d32ed3c
Denis Argatski <denis.argatski@gmail.com>
2022-11-27 23:02:20 +0200

    Core

---
+++ C#/05.C# - OOP/06.SOLID/Solid-Lab/P02.Graphic_Editor/Core/GraphicEditor.cs
@@ -0,0 +1,13 @@
+﻿using P02.Graphic_Editor.Models;
+using P02.Graphic_Editor.Models.Interfaces;
+
+namespace P02.Graphic_Editor.Core
+{
+    public class GraphicEditor
+    {
+        public void DrawShape(IShape shape)
+        {
+            shape.Draw();
+        }
+    }
+}
