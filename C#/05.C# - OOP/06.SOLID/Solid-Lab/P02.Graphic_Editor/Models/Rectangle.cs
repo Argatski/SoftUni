314b83d9c2133abc0cc74e47c9d576b4a2e5f610
Denis Argatski <denis.argatski@gmail.com>
2022-11-27 23:03:20 +0200

    Rectangle.cs

---
+++ C#/05.C# - OOP/06.SOLID/Solid-Lab/P02.Graphic_Editor/Models/Rectangle.cs
@@ -0,0 +1,13 @@
+﻿using P02.Graphic_Editor.Models.Interfaces;
+using System;
+
+namespace P02.Graphic_Editor.Models
+{
+    public class Rectangle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("I'm Recangle");
+        }
+    }
+}
