91cadfe23c3ad6d33b1b81752bf1ab0efddf8599
Denis Argatski <denis.argatski@gmail.com>
2022-11-27 22:59:38 +0200

    StreamProgressInfo.cs

---
+++ C#/05.C# - OOP/06.SOLID/Solid-Lab/P01.Stream_Progress/Core/StreamProgressInfo.cs
@@ -0,0 +1,20 @@
+﻿
+namespace StreamProgressInfo.Core
+{
+    using Models.Interfaces;
+    public class StreamProgressInfo
+    {
+        private readonly IStreamable stremable;
+
+        public StreamProgressInfo(IStreamable stremable)
+        {
+            this.stremable = stremable;
+        }
+
+        public int CalculateCurrentPercent()
+        {
+            return (this.stremable.BytesSent * 100) / this.stremable.Length;
+        }
+
+    }
+}
