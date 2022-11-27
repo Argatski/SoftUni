140fa200166d9e22f5935b24e2d61fca3aa6c50c
Denis Argatski <denis.argatski@gmail.com>
2022-11-27 23:01:47 +0200

    StartUp.cs

---
+++ C#/05.C# - OOP/06.SOLID/Solid-Lab/P02.Graphic_Editor/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using P02.Graphic_Editor.Core;
+using P02.Graphic_Editor.Models;
+
+namespace P02.Graphic_Editor
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            GraphicEditor graphicEditor = new GraphicEditor();
+
+            graphicEditor.DrawShape(new Circle());
+            graphicEditor.DrawShape(new Rectangle());
+            graphicEditor.DrawShape(new Square());
+        }
+    }
+}
