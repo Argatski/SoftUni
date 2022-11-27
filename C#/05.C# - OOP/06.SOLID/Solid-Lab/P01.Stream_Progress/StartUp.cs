af39970b71eee4e31f4e357a4792e350b729b96b
Denis Argatski <denis.argatski@gmail.com>
2022-11-27 22:58:20 +0200

    StartUp.cs

---
+++ C#/05.C# - OOP/06.SOLID/Solid-Lab/P01.Stream_Progress/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace P01.Stream_Progress
+{
+    using StreamProgressInfo.Models;
+    using StreamProgressInfo.Core;
+    public class StartUp
+    {
+        static void Main()
+        {
+            File file = new File("Abba", 125, 10);
+            Music music = new Music("Queen", "The game", 1000, 220);
+
+            StreamProgressInfo first = new StreamProgressInfo(file);
+            StreamProgressInfo second = new StreamProgressInfo(music);
+
+            Console.WriteLine(first.CalculateCurrentPercent());
+            Console.WriteLine(second.CalculateCurrentPercent());
+        }
+    }
+}
