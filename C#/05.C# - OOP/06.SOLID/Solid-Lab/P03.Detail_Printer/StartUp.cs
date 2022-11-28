ff4cfb74792080731c23727ed1bf5535d62aff48
Denis Argatski <denis.argatski@gmail.com>
2022-11-28 20:47:50 +0200

    StartUp.cs

---
+++ C#/05.C# - OOP/06.SOLID/Solid-Lab/P03.Detail_Printer/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace P03.DetailPrinter
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Employee first = new Employee("Pesho");
+            Employee second = new Employee("Ivan");
+
+            Manager manager = new Manager("Gosho", new List<string>() { "data.txt", "previev.pptx", "sallaries.xsl" });
+            IList<Employee> employees = new List<Employee>()
+            { first,second,manager};
+
+            DetailsPrinter printer = new DetailsPrinter(employees);
+            printer.PrintDetails();
+        }
+    }
+}
