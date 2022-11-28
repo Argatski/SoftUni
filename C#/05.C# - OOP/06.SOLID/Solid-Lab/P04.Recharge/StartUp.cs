38797c42615c0f0ba34be9ad60dcc3f3940bca4b
Denis Argatski <denis.argatski@gmail.com>
2022-11-28 20:58:20 +0200

    StartUp.cs

---
+++ C#/05.C# - OOP/06.SOLID/Solid-Lab/P04.Recharge/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace P04.Recharge
+{
+    using P04.Recharge.Models;
+    using System;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            Employee employee = new Employee("Ivan Ivanov");
+            Robot robot = new Robot("T1000", 100);
+            RechargeStation rechargeStation = new RechargeStation();
+            rechargeStation.Recharge(robot);
+
+            employee.Work(8);
+            robot.Work(24);
+            Console.WriteLine(robot.CurrentPower);
+        }
+    }
+}
