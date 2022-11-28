ab28cafdfc2cc04ff326edc13385b1ac076ef0c4
Denis Argatski <denis.argatski@gmail.com>
2022-11-28 20:54:37 +0200

    IRechargeable.cs

---
+++ C#/05.C# - OOP/06.SOLID/Solid-Lab/P04.Recharge/Models/Interfaces/IRechargeable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04.Recharge.Models.Interfaces
+{
+    public interface IRechargeable
+    {
+        void Recharge();
+    }
+}
