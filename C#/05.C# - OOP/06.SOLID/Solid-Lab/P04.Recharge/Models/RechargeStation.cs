40ea7e5e1b0cc4f5f15929f0ad6c02c1921b0590
Denis Argatski <denis.argatski@gmail.com>
2022-11-28 20:56:22 +0200

    RechargeStation.cs

---
+++ C#/05.C# - OOP/06.SOLID/Solid-Lab/P04.Recharge/Models/RechargeStation.cs
@@ -0,0 +1,12 @@
+﻿using P04.Recharge.Models.Interfaces;
+
+namespace P04.Recharge.Models
+{
+    public class RechargeStation
+    {
+        public void Recharge(IRechargeable rechargeable)
+        {
+            rechargeable.Recharge();
+        }
+    }
+}
