dfb7015df9362d83f0afd917090b0ac7482f1b56
Denis Argatski <denis.argatski@gmail.com>
2022-11-28 20:56:00 +0200

    Employee.cs

---
+++ C#/05.C# - OOP/06.SOLID/Solid-Lab/P04.Recharge/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace P04.Recharge
+{
+    using P04.Recharge.Models.Interfaces;
+    using System;
+
+    public class Employee : Worker, ISleeper
+    {
+        public Employee(string id)
+            : base(id)
+        {
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("Employee is sleeping!");
+        }
+
+
+    }
+}
