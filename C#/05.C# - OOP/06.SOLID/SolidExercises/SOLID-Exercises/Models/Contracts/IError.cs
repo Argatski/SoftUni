3f73c82029e580326cefbee770d73bbba6741792
Denis Argatski <denis.argatski@gmail.com>
2022-12-03 22:55:51 +0200

    IError.cs

---
+++ C#/05.C# - OOP/06.SOLID/SolidExercises/SOLID-Exercises/Models/Contracts/IError.cs
@@ -0,0 +1,14 @@
+﻿using SOLID_Exercises.Models.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Exercises.Models.Contracts
+{
+    public interface IError
+    {
+        DateTime DateTime { get; }
+        string Message { get; }
+        Level Level { get; }
+    }
+}
