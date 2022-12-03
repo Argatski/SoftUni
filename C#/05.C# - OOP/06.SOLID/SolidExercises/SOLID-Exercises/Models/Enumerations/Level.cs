726a1148c560cc11a9c57b9aa6ac3660b63d0593
Denis Argatski <denis.argatski@gmail.com>
2022-12-03 22:58:13 +0200

    Enumerations

---
+++ C#/05.C# - OOP/06.SOLID/SolidExercises/SOLID-Exercises/Models/Enumerations/Level.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Exercises.Models.Enumerations
+{
+    public enum Level
+    {
+        INFO,
+        WARNING,
+        ERROR,
+        CRITTICAL,
+        FATAL
+    }
+}
