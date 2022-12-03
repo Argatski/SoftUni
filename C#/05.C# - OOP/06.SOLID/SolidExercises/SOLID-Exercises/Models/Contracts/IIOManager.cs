1d1ca8451efc60b61e172c7f82cafe519727a0ed
Denis Argatski <denis.argatski@gmail.com>
2022-12-03 22:56:56 +0200

    IIOManager.cs

---
+++ C#/05.C# - OOP/06.SOLID/SolidExercises/SOLID-Exercises/Models/Contracts/IIOManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Exercises.Models.Contracts
+{
+    public interface IIOManager
+    {
+        string CurrentDirectoryPath { get; }
+        string CurrentFilePath { get; }
+
+        void EnsureDirectoryAndFileExist();
+        string GetCurrentPath();
+    }
+}
