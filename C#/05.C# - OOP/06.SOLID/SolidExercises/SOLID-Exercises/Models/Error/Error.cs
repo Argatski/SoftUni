46dfc86e28bf60cf63070faa84e9402fe6677e19
Denis Argatski <denis.argatski@gmail.com>
2022-12-03 22:58:27 +0200

    Error

---
+++ C#/05.C# - OOP/06.SOLID/SolidExercises/SOLID-Exercises/Models/Error/Error.cs
@@ -0,0 +1,23 @@
+﻿using SOLID_Exercises.Models.Contracts;
+using SOLID_Exercises.Models.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Exercises.Models.Error
+{
+    public class Error : IError
+    {
+        public Error(DateTime dateTime, string message, Level level = Level.INFO)
+        {
+            this.DateTime = dateTime;
+            this.Message = message;
+            this.Level = level;
+        }
+        public DateTime DateTime { get; }
+
+        public string Message { get; }
+
+        public Level Level { get; }
+    }
+}
