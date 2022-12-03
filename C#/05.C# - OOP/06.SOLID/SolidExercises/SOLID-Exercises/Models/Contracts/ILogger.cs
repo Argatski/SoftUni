f8a7413b2e7d8303f5966a48c0e81c90504e0f70
Denis Argatski <denis.argatski@gmail.com>
2022-12-03 22:57:31 +0200

    ILogger.cs

---
+++ C#/05.C# - OOP/06.SOLID/SolidExercises/SOLID-Exercises/Models/Contracts/ILogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Exercises.Models.Contracts
+{
+    public interface ILogger
+    {
+        IReadOnlyCollection<IAppender> Appenders { get; }
+        void Log(IError error);
+    }
+}
