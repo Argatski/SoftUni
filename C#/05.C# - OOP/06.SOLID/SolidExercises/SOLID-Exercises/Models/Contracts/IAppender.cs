289a579e334d711d8b496679be088f45f13f5a67
Denis Argatski <denis.argatski@gmail.com>
2022-12-03 22:55:34 +0200

    IAppender.cs

---
+++ C#/05.C# - OOP/06.SOLID/SolidExercises/SOLID-Exercises/Models/Contracts/IAppender.cs
@@ -0,0 +1,12 @@
+﻿using SOLID_Exercises.Models.Enumerations;
+
+namespace SOLID_Exercises.Models.Contracts
+{
+    public interface IAppender
+    {
+        ILayout Layout { get; }
+
+        Level Level { get; }
+        void Append(IError error);
+    }
+}
