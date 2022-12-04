6a475ecd753c6e77fdf65fd3f6049d44e7837c74
Denis Argatski <denis.argatski@gmail.com>
2022-12-04 22:20:34 +0200

    XmlLayout.cs

---
+++ C#/05.C# - OOP/06.SOLID/SolidExercises/SOLID-Exercises/Models/Layouts/XmlLayout.cs
@@ -0,0 +1,23 @@
+﻿using SOLID_Exercises.Models.Contracts;
+using System;
+using System.Text;
+
+namespace SOLID_Exercises.Models.Layouts
+{
+    public class XmlLayout : ILayout
+    {
+        public string Format => this.GetFormat();
+
+        private string GetFormat()
+        {
+            var result = new StringBuilder();
+            result.AppendLine("<log>");
+            result.AppendLine("\t<date>{0}</date>");
+            result.AppendLine("\t<level>{1}</level>");
+            result.AppendLine("\t<message>{2}</message>");
+            result.AppendLine("<log>");
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
