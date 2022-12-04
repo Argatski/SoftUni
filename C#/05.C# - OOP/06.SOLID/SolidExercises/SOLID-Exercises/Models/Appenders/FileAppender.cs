40d000ec6a500710e887a5d5b39e756ccc6832f3
Denis Argatski <denis.argatski@gmail.com>
2022-12-04 22:19:27 +0200

    FileAppender.cs

---
+++ C#/05.C# - OOP/06.SOLID/SolidExercises/SOLID-Exercises/Models/Appenders/FileAppender.cs
@@ -0,0 +1,34 @@
+﻿using SOLID_Exercises.Models.Contracts;
+using SOLID_Exercises.Models.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Exercises.Models.Appenders
+{
+    public class FileAppender : IAppender
+    {
+        private int messagesAppended;
+
+        public FileAppender(ILayout layout, Level level, IFile file)
+        {
+            this.Layout = layout;
+            this.Level = level;
+            this.File = file;
+        }
+        public ILayout Layout { get; private set; }
+        public Level Level { get; private set; }
+        public IFile File { get; private set; }
+        public void Append(IError error)
+        {
+            string formattedMessage = this.File.Write(this.Layout, error) + Environment.NewLine;
+
+            System.IO.File.AppendAllText(this.File.Path, formattedMessage);
+            this.messagesAppended++;
+        }
+        public override string ToString()
+        {
+            return $"Appender type: {this.GetType().Name}, Layout type: {this.Layout.GetType().Name}, Report level: {this.Level.ToString()}, Messages appended: {this.messagesAppended}, File size: {this.File.Size}";
+        }
+    }
+}
