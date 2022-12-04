5e2507cb25f5a0b8b6f80a3c4d6bec3eb9702667
Denis Argatski <denis.argatski@gmail.com>
2022-12-04 22:27:45 +0200

    InvalidLayoutTypeException.cs

---
+++ C#/05.C# - OOP/06.SOLID/SolidExercises/SOLID-Exercises/Exceptions/InvalidLayoutTypeException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Exercises.Exceptions
+{
+    public class InvalidLayoutTypeException : Exception
+    {
+        private const string ExceptionMessage = "Invalid Layout Type!";
+
+        public InvalidLayoutTypeException() : base(ExceptionMessage)
+        {
+        }
+        public InvalidLayoutTypeException(string message) : base(message)
+        {
+
+        }
+    }
+}
