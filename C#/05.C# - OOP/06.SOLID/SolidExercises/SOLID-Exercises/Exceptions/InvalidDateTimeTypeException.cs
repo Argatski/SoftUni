e5df1be1f0ecd8e14d96e8fd204e7e9f41390787
Denis Argatski <denis.argatski@gmail.com>
2022-12-04 22:27:27 +0200

    InvalidDateTimeTypeException.cs

---
+++ C#/05.C# - OOP/06.SOLID/SolidExercises/SOLID-Exercises/Exceptions/InvalidDateTimeTypeException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Exercises.Exceptions
+{
+    public class InvalidDateTimeTypeException : Exception
+    {
+        private const string ExceptionMessage = "Invalid Date Type!";
+        public InvalidDateTimeTypeException() : base(ExceptionMessage)
+        {
+        }
+        public InvalidDateTimeTypeException(string message) : base(message)
+        {
+        }
+        public InvalidDateTimeTypeException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
