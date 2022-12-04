6f43a60f55a3b5c04261b7667b91917e1c125f45
Denis Argatski <denis.argatski@gmail.com>
2022-12-04 22:28:53 +0200

    Engine.cs

---
+++ C#/05.C# - OOP/06.SOLID/SolidExercises/SOLID-Exercises/Core/Engine.cs
@@ -0,0 +1,48 @@
+﻿using SOLID_Exercises.Core.Contracts;
+using SOLID_Exercises.Factories.Contracts;
+using SOLID_Exercises.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SOLID_Exercises.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly ILogger logger;
+        private readonly IErrorFactory errorFactory;
+        public Engine(ILogger logger, IErrorFactory errorFactory)
+        {
+            this.logger = logger;
+            this.errorFactory = errorFactory;
+        }
+        public void Run()
+        {
+            var command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                var errorArguments = command
+                    .Split("|")
+                    .ToArray();
+
+                var level = errorArguments[0];
+                var date = errorArguments[1];
+                var messege = errorArguments[2];
+
+                try
+                {
+                    var error = this.errorFactory.GetError(date, level, messege);
+                    this.logger.Log(error);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(this.logger);
+        }
+    }
+}
