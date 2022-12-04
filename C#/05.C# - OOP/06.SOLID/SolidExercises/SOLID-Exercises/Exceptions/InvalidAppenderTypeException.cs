67b30a6fba7e074635eb33ad34c29d39f6686fc4
Denis Argatski <denis.argatski@gmail.com>
2022-12-04 22:27:08 +0200

    InvalidAppenderTypeException.cs

---
+++ C#/05.C# - OOP/06.SOLID/SolidExercises/SOLID-Exercises/Exceptions/InvalidAppenderTypeException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Exercises.Exceptions
+{
+    public class InvalidAppenderTypeException : Exception
+    {
+        private const string ExceptionMessage = "Invalid Appender Type!";
+
+        public InvalidAppenderTypeException()
+            : base(ExceptionMessage)
+        {
+        }
+        public InvalidAppenderTypeException(string message) : base(message)
+        {
+
+        }
+    }
+}
