378556222062c6fb67409bc20faa2933632ee7dd
Denis Argatski <denis.argatski@gmail.com>
2022-12-04 22:28:36 +0200

    Contracts

---
+++ C#/05.C# - OOP/06.SOLID/SolidExercises/SOLID-Exercises/Core/Contracts/IEngine.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Exercises.Core.Contracts
+{
+    public interface IEngine
+    {
+        void Run();
+    }
+}
