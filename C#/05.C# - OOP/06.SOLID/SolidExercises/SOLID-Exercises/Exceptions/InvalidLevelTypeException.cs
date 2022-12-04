9e281eaea3b78b3cbc5c74ffe38f79d92686af19
Denis Argatski <denis.argatski@gmail.com>
2022-12-04 22:28:06 +0200

    InvalidLevelTypeException.cs

---
+++ C#/05.C# - OOP/06.SOLID/SolidExercises/SOLID-Exercises/Exceptions/InvalidLevelTypeException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Exercises.Exceptions
+{
+    public class InvalidLevelTypeException : Exception
+    {
+        private const string ExceptionMessage = "Invalid Level Type!";
+
+        public InvalidLevelTypeException() : base(ExceptionMessage)
+        {
+        }
+        public InvalidLevelTypeException(string message) : base(message)
+        {
+        }
+    }
+}
