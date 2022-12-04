6e747020e5dc112bba9fd1e395604ffb32634ce1
Denis Argatski <denis.argatski@gmail.com>
2022-12-04 22:20:57 +0200

    SimpleLayout.cs

---
+++ C#/05.C# - OOP/06.SOLID/SolidExercises/SOLID-Exercises/Models/Layouts/SimpleLayout.cs
@@ -0,0 +1,9 @@
+﻿using SOLID_Exercises.Models.Contracts;
+
+namespace SOLID_Exercises.Models.Layouts
+{
+    public class SimpleLayout : ILayout
+    {
+        public string Format => $"{0} - {1} - {2}";
+    }
+}
