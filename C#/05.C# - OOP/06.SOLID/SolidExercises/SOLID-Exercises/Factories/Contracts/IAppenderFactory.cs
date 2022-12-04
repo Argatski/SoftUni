0dc69a4388a72d06db8ce9fc1e16be447fda21d5
Denis Argatski <denis.argatski@gmail.com>
2022-12-04 22:21:33 +0200

    IAppenderFactory.cs

---
+++ C#/05.C# - OOP/06.SOLID/SolidExercises/SOLID-Exercises/Factories/Contracts/IAppenderFactory.cs
@@ -0,0 +1,12 @@
+﻿using SOLID_Exercises.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Exercises.Factories.Contracts
+{
+    public interface IAppenderFactory
+    {
+        IAppender GetAppender(string appenderType, string layoutType, string levelString);
+    }
+}
