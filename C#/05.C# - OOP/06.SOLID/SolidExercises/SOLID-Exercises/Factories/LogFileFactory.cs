55b78684989c3e366153bdad32fba7d2e920cca2
Denis Argatski <denis.argatski@gmail.com>
2022-12-04 22:26:31 +0200

    LogFileFactory.cs

---
+++ C#/05.C# - OOP/06.SOLID/SolidExercises/SOLID-Exercises/Factories/LogFileFactory.cs
@@ -0,0 +1,14 @@
+﻿using SOLID_Exercises.Factories.Contracts;
+using SOLID_Exercises.Models.Contracts;
+using SOLID_Exercises.Models.Files;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Exercises.Factories
+{
+    public class LogFileFactory : ILogFileFactory
+    {
+        public IFile GetLogFile() => new LogFile();
+    }
+}
