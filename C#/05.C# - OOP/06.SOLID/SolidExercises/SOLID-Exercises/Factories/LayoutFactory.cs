6e2b880efda8c4ec19644e6884e4f2bed2731957
Denis Argatski <denis.argatski@gmail.com>
2022-12-04 22:26:13 +0200

    LayoutFactory.cs

---
+++ C#/05.C# - OOP/06.SOLID/SolidExercises/SOLID-Exercises/Factories/LayoutFactory.cs
@@ -0,0 +1,29 @@
+﻿using SOLID_Exercises.Exceptions;
+using SOLID_Exercises.Factories.Contracts;
+using SOLID_Exercises.Models.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace SOLID_Exercises.Factories
+{
+    public class LayoutFactory : ILayoutFactory
+    {
+        public ILayout GetLayout(string type)
+        {
+            var typeToCreate = Assembly
+                .GetExecutingAssembly()
+                .GetTypes()
+                .FirstOrDefault(x => x.Name == type);
+
+            if (typeToCreate == null)
+            {
+                throw new InvalidLayoutTypeException();
+            }
+
+            ILayout layout = (ILayout)Activator.CreateInstance(typeToCreate);
+
+            return layout;
+        }
+    }
+}
