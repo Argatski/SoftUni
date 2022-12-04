efb7001dd5001ded3e120711afdfb5a4f3859cdf
Denis Argatski <denis.argatski@gmail.com>
2022-12-04 22:19:07 +0200

    ConsoleAppender.cs

---
+++ C#/05.C# - OOP/06.SOLID/SolidExercises/SOLID-Exercises/Models/Appenders/ConsoleAppender.cs
@@ -0,0 +1,47 @@
+﻿using SOLID_Exercises.Models.Contracts;
+using SOLID_Exercises.Models.Enumerations;
+using System;
+using System.Globalization;
+
+namespace SOLID_Exercises.Models.Appenders
+{
+    public class ConsoleAppender : IAppender
+    {
+        private const string DateFormant = "M/dd/yyyy h:mm:ss tt";
+        private int messagesAppended;
+
+        public ConsoleAppender(ILayout layout, Level level)
+        {
+            this.Layout = layout;
+            this.Level = level;
+        }
+        public ILayout Layout { get; }
+
+        public Level Level { get; }
+
+        public void Append(IError error)
+        {
+            var format = this.Layout.Format;
+
+            var dateTime = error.DateTime;
+            var level = error.Level;
+            var message = error.Message;
+
+            string formattedMessage = string.Format(
+                format,
+                dateTime.ToString(DateFormant, CultureInfo.InvariantCulture),
+                level.ToString(),
+                message
+                );
+
+            Console.WriteLine(formattedMessage);
+            this.messagesAppended++;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Appender type: {this.GetType().Name}, Layout type: {this.Layout.GetType().Name}, Report level: {this.Level.ToString()}, Messages appended: {this.messagesAppended}";
+        }
+    }
+}
