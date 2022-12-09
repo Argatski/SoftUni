69f75344d04e156a681818059c9366275d057330
Denis Argatski <denis.argatski@gmail.com>
2022-12-09 23:12:58 +0200

    Spy.cs

---
+++ C#/05.C# - OOP/07.Reflection/ReflectionAndAttributes-Lab/01. Stealer/Spy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string investigatedClass, params string[] requestedFields)
+        {
+            Type classType = Type.GetType(investigatedClass);
+            FieldInfo[] classFields = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+
+            StringBuilder stb = new StringBuilder();
+
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+            stb.AppendLine($"Class under investigation: {investigatedClass}");
+
+            foreach (var field in classFields.Where(f => requestedFields.Contains(f.Name)))
+            {
+                stb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return stb.ToString().Trim();
+        }
+    }
+}
