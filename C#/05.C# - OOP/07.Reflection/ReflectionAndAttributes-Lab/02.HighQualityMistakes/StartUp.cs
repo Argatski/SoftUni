e63f98a17f6da5fa232a1912d397b49d5141a091
Denis Argatski <denis.argatski@gmail.com>
2022-12-10 23:10:41 +0200

    StartUp.cs

---
+++ C#/05.C# - OOP/07.Reflection/ReflectionAndAttributes-Lab/02.HighQualityMistakes/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Spy spy = new Spy();
+        string result = spy.AnalyzeAcessModifiers("Hacker");
+        Console.WriteLine(result);
+    }
+}
+
