8d5ce382f80cb234b01987ccde8c742dd2256db0
Denis Argatski <denis.argatski@gmail.com>
2022-12-11 23:31:15 +0200

    StartUp.cs

---
+++ C#/05.C# - OOP/07.Reflection/ReflectionAndAttributes-Lab/05.CreateAttributes/StartUp.cs
@@ -0,0 +1,13 @@
+﻿
+namespace AuthorProblem
+{
+    [Author("Ventsi")]
+    public class StartUp
+    {
+        [Author("Gosho")]
+        public static void Main()
+        {
+        }
+    }
+}
+
