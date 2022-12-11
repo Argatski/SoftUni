d9cd152fb9e9afe0a4e46f1e0d122b407c6d6f3d
Denis Argatski <denis.argatski@gmail.com>
2022-12-11 23:25:39 +0200

    "Methods/Tracker.cs"

---
+++ C#/05.C# - OOP/07.Reflection/ReflectionAndAttributes-Lab/06.CodeTracker/Methods/Tracker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+
+public class Tracker
+{
+    public void PringMethodsByAutor()
+    {
+        Type type = typeof(StartUp);
+        MethodInfo[] methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static);
+
+        foreach (var method in methods)
+        {
+            if (method.CustomAttributes.Any(n => n.AttributeType == typeof(AuthorAttribute)))
+            {
+                var attributes = method.GetCustomAttributes(false);
+
+                foreach (AuthorAttribute attr in attributes)
+                {
+                    Console.WriteLine($"{method.Name} is written by {attr.Name}");
+                }
+            }
+
+        }
+    }
+}
