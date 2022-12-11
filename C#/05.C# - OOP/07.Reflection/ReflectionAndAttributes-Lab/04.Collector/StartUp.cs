57ac8bba4699160cfd1c413311ad705b4bbe85c8
Denis Argatski <denis.argatski@gmail.com>
2022-12-11 23:29:35 +0200

    StartUp.cs

---
+++ C#/05.C# - OOP/07.Reflection/ReflectionAndAttributes-Lab/04.Collector/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.CollectGettersAndSetters("Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
