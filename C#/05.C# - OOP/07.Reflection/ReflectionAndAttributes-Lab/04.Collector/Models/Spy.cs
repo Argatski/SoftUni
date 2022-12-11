9be7724d11df834887bfc26dfc0c6a51dcd94b5d
Denis Argatski <denis.argatski@gmail.com>
2022-12-11 23:28:14 +0200

    Models

---
+++ C#/05.C# - OOP/07.Reflection/ReflectionAndAttributes-Lab/04.Collector/Models/Spy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+
+public class Spy
+{
+    public string CollectGettersAndSetters(string investigateClass)
+    {
+        Type clasType = Type.GetType(investigateClass);
+
+        MethodInfo[] classMethods = clasType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+        StringBuilder stb = new StringBuilder();
+
+        foreach (var method in classMethods.Where(m => m.Name.StartsWith("get")))
+        {
+            stb.AppendLine($"{method.Name} will return {method.ReturnType}");
+        }
+        foreach (var method in classMethods.Where(m => m.Name.StartsWith("set")))
+        {
+            stb.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+        }
+
+        return stb.ToString().Trim();
+    }
+}
