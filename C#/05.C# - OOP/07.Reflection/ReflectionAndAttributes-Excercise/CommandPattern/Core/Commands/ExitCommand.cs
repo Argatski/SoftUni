0c846b8b947d8938c939768f3dd6be4207362f00
Denis Argatski <denis.argatski@gmail.com>
2022-12-12 22:03:17 +0200

    ExitCommand.cs

---
+++ C#/05.C# - OOP/07.Reflection/ReflectionAndAttributes-Excercise/CommandPattern/Core/Commands/ExitCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommandPattern.Core.Contracts.Commands
+{
+    public class ExitCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            Environment.Exit(0);
+            return null;
+        }
+    }
+}
