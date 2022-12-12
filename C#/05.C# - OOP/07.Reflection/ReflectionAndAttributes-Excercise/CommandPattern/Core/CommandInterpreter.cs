49c1bd6e9eded4c19b07b6934cd95e62ef9f196d
Denis Argatski <denis.argatski@gmail.com>
2022-12-12 22:07:05 +0200

    "CommandInterpreter.cs"

---
+++ C#/05.C# - OOP/07.Reflection/ReflectionAndAttributes-Excercise/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,33 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            var argumentParts = args
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            var commandName = argumentParts[0];
+            var commandArguments = argumentParts.Skip(1).ToArray();
+
+            var commandType = Assembly
+                .GetCallingAssembly()
+                .GetTypes()
+                .Where(n => n.Name == $"{commandName}Command")
+                .FirstOrDefault();
+
+            var commandInstance = Activator.CreateInstance(commandType) as ICommand;
+
+            var result = commandInstance.Execute(commandArguments);
+            return result;
+        }
+    }
+}
