c2236df9a3dd338ccaec6df7d754c077acb06fab
Denis Argatski <denis.argatski@gmail.com>
2022-12-12 22:03:38 +0200

    HelloCommand.cs

---
+++ C#/05.C# - OOP/07.Reflection/ReflectionAndAttributes-Excercise/CommandPattern/Core/Commands/HelloCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace CommandPattern.Core.Contracts.Commands
+{
+    public class HelloCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            return $"Hello, {args[0]}";
+        }
+    }
+}
