2c2cb4fbed65fae46e934484831c2ac64315f64a
Denis Argatski <denis.argatski@gmail.com>
2022-12-15 22:49:54 +0200

    Validator.cs

---
+++ C#/05.C# - OOP/07.Reflection/ReflectionAndAttributes-Excercise/ValidationAttributes/Validator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    using System.Linq;
+    using ValidationAttributes.Attributes;
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            var propertyInfo = obj.GetType()
+                .GetProperties();
+
+            foreach (var property in propertyInfo)
+            {
+                var attributes = property
+                    .GetCustomAttributes(true)
+                    .Where(a => a is MyValidationAttribute)
+                    .Cast<MyValidationAttribute>()
+                    .ToArray();
+
+                foreach (var att in attributes)
+                {
+                    if (att.IsValid(property.GetValue(obj)) == false)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
