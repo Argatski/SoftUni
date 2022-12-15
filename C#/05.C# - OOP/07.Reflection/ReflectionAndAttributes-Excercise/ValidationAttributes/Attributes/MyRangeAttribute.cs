958d5b059b6d631e84bf4ae0b19d5678dc34fdc2
Denis Argatski <denis.argatski@gmail.com>
2022-12-15 22:53:09 +0200

    MyRangeAttribute.cs

---
+++ C#/05.C# - OOP/07.Reflection/ReflectionAndAttributes-Excercise/ValidationAttributes/Attributes/MyRangeAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ValidationAttributes.Attributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private readonly int minValue;
+        private readonly int maxValue;
+
+        public MyRangeAttribute(int min, int max)
+        {
+            this.minValue = min;
+            this.maxValue = max;
+        }
+        public override bool IsValid(object obj)
+        {
+            var currentAge = Convert.ToInt32(obj);
+
+            if (currentAge < this.minValue || currentAge > this.maxValue)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
