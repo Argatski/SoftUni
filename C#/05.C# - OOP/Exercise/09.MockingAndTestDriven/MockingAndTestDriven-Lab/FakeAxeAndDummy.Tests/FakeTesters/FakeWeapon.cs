71d32cd14718f9db7da611f1cc51d18081586aad
Denis Argatski <denis.argatski@gmail.com>
2023-02-09 23:14:48 +0200

    FakeWeapon.cs

---
+++ C#/05.C# - OOP/Exercise/09.MockingAndTestDriven/MockingAndTestDriven-Lab/FakeAxeAndDummy.Tests/FakeTesters/FakeWeapon.cs
@@ -0,0 +1,19 @@
+﻿using FakeAxeAndDummy.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FakeAxeAndDummy.Tests.FakeTesters
+{
+    public class FakeWeapon : IWeapon
+    {
+        public int AttackPoints => 10;
+
+        public int DurabilityPoints => 10;
+
+        public void Attack(ITarget target)
+        {
+        }
+
+    }
+}
