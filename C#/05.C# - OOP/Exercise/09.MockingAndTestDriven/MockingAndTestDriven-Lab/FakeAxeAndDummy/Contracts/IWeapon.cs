3a5dfc8ddde8d08143cdd30c2c91d780be6a9306
Denis Argatski <denis.argatski@gmail.com>
2023-02-09 23:14:03 +0200

    IWeapon.cs

---
+++ C#/05.C# - OOP/Exercise/09.MockingAndTestDriven/MockingAndTestDriven-Lab/FakeAxeAndDummy/Contracts/IWeapon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FakeAxeAndDummy.Contracts
+{
+    public interface IWeapon
+    {
+        int AttackPoints { get; }
+        int DurabilityPoints { get; }
+
+        void Attack(ITarget target);
+    }
+}
