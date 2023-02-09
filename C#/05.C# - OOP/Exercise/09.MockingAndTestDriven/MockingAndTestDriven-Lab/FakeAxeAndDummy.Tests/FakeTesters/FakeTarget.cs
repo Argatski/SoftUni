56ad52df6b2ed5cdf54bcdb3fa01fc7a0de5e696
Denis Argatski <denis.argatski@gmail.com>
2023-02-09 23:14:35 +0200

    FakeTarget.cs

---
+++ C#/05.C# - OOP/Exercise/09.MockingAndTestDriven/MockingAndTestDriven-Lab/FakeAxeAndDummy.Tests/FakeTesters/FakeTarget.cs
@@ -0,0 +1,24 @@
+﻿using FakeAxeAndDummy.Contracts;
+
+
+namespace FakeAxeAndDummy.Tests.FakeTesters
+{
+    public class FakeTarget : ITarget
+    {
+        public int Health => 0;
+
+        public int GiveExperience()
+        {
+            return 20;
+        }
+
+        public bool IsDead()
+        {
+            return true;
+        }
+
+        public void TakeAttack(int attackPoints)
+        {
+        }
+    }
+}
