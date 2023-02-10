37703cdfe5389d805994e2a38aaea29d5ee024bc
Denis Argatski <denis.argatski@gmail.com>
2023-02-10 22:46:18 +0200

    Program.cs

---
+++ C#/05.C# - OOP/Exercise/09.MockingAndTestDriven/MockingAndTestDriven-Lab/FizzBuzz.Tests/FizzBuzz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
