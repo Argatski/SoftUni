48b035f8e488b3f27947e951f2b60b591d9b03c8
Denis Argatski <denis.argatski@gmail.com>
2023-02-10 22:47:00 +0200

    IWriter.cs

---
+++ C#/05.C# - OOP/Exercise/09.MockingAndTestDriven/MockingAndTestDriven-Lab/FizzBuzz.Tests/FizzBuzz/Contracts/IWriter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz.Contracts
+{
+    public interface IWriter
+    {
+        void WriteLine(string input);
+    }
+}
+
