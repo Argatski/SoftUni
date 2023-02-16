148011b1d79de9602d719074379953751aaec464
Denis Argatski <denis.argatski@gmail.com>
2023-02-16 21:53:52 +0200

    Program.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/01.SingletonPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using _01.SingletonPattern;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var logger = LoggerSingleton.Instance;
+        logger.LogToFile();
+
+        LoggerSingleton.Instance.LogToFile();
+    }
+}
