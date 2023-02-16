bb058b77c47f41c6ca79e921e4bd08a62b1c0502
Denis Argatski <denis.argatski@gmail.com>
2023-02-16 23:05:12 +0200

    Bear.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/03.FactoryMethodPattern/Bear.cs
@@ -0,0 +1,14 @@
+﻿using _03.FactoryMethodPattern.Contracts;
+
+namespace _03.FactoryMethodPattern
+{
+    internal class Bear : ICarnivore
+    {
+        private string eat = "Eat sheep.";
+        public Bear()
+        {
+
+        }
+        public string AnimalsThatEat { get => this.eat; set => eat = value; }
+    }
+}
