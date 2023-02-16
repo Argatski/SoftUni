f27ac93d25d84a6d3fde0519772ac03c3c14ea99
Denis Argatski <denis.argatski@gmail.com>
2023-02-16 22:07:17 +0200

    Program.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/02.SimpleFactoryPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using _02.SimpleFactoryPattern;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        IAnimal lion = AnimalFactory.CreateAnima("Lion");
+        Console.WriteLine(lion.Name);
+
+        IAnimal cat = AnimalFactory.CreateAnima("Cat");
+        Console.WriteLine(cat.Name);
+    }
+}
