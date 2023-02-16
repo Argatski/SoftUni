b1924f8f95f2fb743b97a7913a2a39ca3cc61793
Denis Argatski <denis.argatski@gmail.com>
2023-02-16 23:05:26 +0200

    Lion.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/03.FactoryMethodPattern/Lion.cs
@@ -0,0 +1,14 @@
+﻿using _03.FactoryMethodPattern.Contracts;
+
+namespace _03.FactoryMethodPattern
+{
+    public class Lion : ICarnivore
+    {
+        private string eat = "Eat gazelle.";
+        public Lion()
+        {
+
+        }
+        public string AnimalsThatEat { get => this.eat; set => this.eat = value; }
+    }
+}
