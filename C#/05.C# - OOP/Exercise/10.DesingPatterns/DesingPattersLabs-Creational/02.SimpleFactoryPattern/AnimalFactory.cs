874679bda50b97d9660d541b4d1fe11b707dc4ad
Denis Argatski <denis.argatski@gmail.com>
2023-02-16 22:04:57 +0200

    AnimalFactory.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/02.SimpleFactoryPattern/AnimalFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.SimpleFactoryPattern
+{
+    public class AnimalFactory
+    {
+        public static IAnimal CreateAnima(string animal)
+        {
+            if (animal == "Lion")
+            {
+                return new Lion();
+            }
+            else if (animal == "Cat")
+            {
+                return new Cat();
+            }
+            return null;
+        }
+    }
+}
