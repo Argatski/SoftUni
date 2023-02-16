68dba361f994cec275345ef0d5f9edb5540f3541
Denis Argatski <denis.argatski@gmail.com>
2023-02-16 23:03:02 +0200

    IAnimal.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/03.FactoryMethodPattern/Contracts/IAnimal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.FactoryMethodPattern.Contracts
+{
+    public interface IAnimal
+    {
+        string Name { get; }
+    }
+}
