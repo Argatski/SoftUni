72a7f690af1fbcc78de163a720c99926517ad2a5
Denis Argatski <denis.argatski@gmail.com>
2023-02-16 22:05:19 +0200

    Cat.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/02.SimpleFactoryPattern/Cat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.SimpleFactoryPattern
+{
+    internal class Cat : IAnimal
+    {
+        public string Name => "Cat";
+    }
+}
