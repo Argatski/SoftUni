01cf3878372a9a85732bcc84ca0462a0a4803b25
Denis Argatski <denis.argatski@gmail.com>
2023-02-16 23:04:21 +0200

    AfricanFactory.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/03.FactoryMethodPattern/Factories/AfricanFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _03.FactoryMethodPattern.Contracts;
+
+namespace _03.FactoryMethodPattern.Factories
+{
+    class AfricanFactory : IAnimalFactory
+    {
+        public ICarnivore GetCarnivore()
+        {
+            return new Lion();
+        }
+
+
+    }
+}
