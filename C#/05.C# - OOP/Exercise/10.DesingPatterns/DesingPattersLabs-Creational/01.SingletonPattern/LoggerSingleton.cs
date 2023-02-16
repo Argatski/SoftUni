fa74e2241f0be79115fcc18265ec07bd8fb58492
Denis Argatski <denis.argatski@gmail.com>
2023-02-16 21:54:13 +0200

    LoggerSingleton.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/01.SingletonPattern/LoggerSingleton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.SingletonPattern
+{
+    public sealed class LoggerSingleton
+    {
+        private static LoggerSingleton instance;
+        private static object _lock = new object();
+        private LoggerSingleton() { }
+
+        public void LogToFile()
+        {
+            Console.WriteLine("Logged to file");
+        }
+        public static LoggerSingleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (instance == null)
+                        {
+                            Console.WriteLine("Created instance only once.");
+                            instance = new LoggerSingleton();
+
+                        }
+
+                    }
+                    
+                }
+                return instance;
+            }
+        }
+    }
+}
