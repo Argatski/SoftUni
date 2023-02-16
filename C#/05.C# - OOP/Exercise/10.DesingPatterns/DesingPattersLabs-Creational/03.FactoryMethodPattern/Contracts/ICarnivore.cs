bd5056bc4f22dfb22b645a8857663b97372a69eb
Denis Argatski <denis.argatski@gmail.com>
2023-02-16 23:03:34 +0200

    ICarnivore.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/03.FactoryMethodPattern/Contracts/ICarnivore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.FactoryMethodPattern.Contracts
+{
+    public interface ICarnivore
+    {
+        public string AnimalsThatEat { get; set; }
+    }
+}
