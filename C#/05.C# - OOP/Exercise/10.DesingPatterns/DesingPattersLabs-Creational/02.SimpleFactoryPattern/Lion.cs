fa84dea670214fc30085b93f48de193ff14ea6c1
Denis Argatski <denis.argatski@gmail.com>
2023-02-16 22:07:04 +0200

    Lion.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/02.SimpleFactoryPattern/Lion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.SimpleFactoryPattern
+{
+    internal class Lion : IAnimal
+    {
+        public string Name => "Lion";
+    }
+}
