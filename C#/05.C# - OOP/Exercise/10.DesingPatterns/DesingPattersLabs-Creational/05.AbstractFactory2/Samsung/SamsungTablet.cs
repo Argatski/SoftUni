613962e6d447791f64a12dbb8ff9409b27cb1ee7
Denis Argatski <denis.argatski@gmail.com>
2023-02-17 23:17:14 +0200

    SamsungTablet.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/05.AbstractFactory2/Samsung/SamsungTablet.cs
@@ -0,0 +1,15 @@
+﻿using _05.AbstractFactory2.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.AbstractFactory2.Samsung
+{
+    internal class SamsungTablet : ITablet
+    {
+        public string OS { get; set; }
+    }
+}
