2dd202bf02d3dc3b998592583a9403a8d22608e7
Denis Argatski <denis.argatski@gmail.com>
2023-02-17 23:15:29 +0200

    ITechnologyAbstractFactory.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/05.AbstractFactory2/Contracts/ITechnologyAbstractFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.AbstractFactory2.Contracts
+{
+    public interface ITechnologyAbstractFactory
+    {
+        IMobilePhone CreatePhone();
+        ITablet CreateTable();
+    }
+}
