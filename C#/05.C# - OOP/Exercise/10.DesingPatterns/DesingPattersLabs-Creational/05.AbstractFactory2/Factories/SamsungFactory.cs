eb65dd2e8dc7682788d32c8a70cb0ce8ca852858
Denis Argatski <denis.argatski@gmail.com>
2023-02-17 23:16:38 +0200

    SamsungFactory.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/05.AbstractFactory2/Factories/SamsungFactory.cs
@@ -0,0 +1,24 @@
+﻿using _05.AbstractFactory2.Contracts;
+using _05.AbstractFactory2.Samsung;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.AbstractFactory2.Factories
+{
+    public class SamsungFactory : ITechnologyAbstractFactory
+    {
+        public IMobilePhone CreatePhone()
+        {
+            return new SamsungPhone();
+        }
+
+        public ITablet CreateTable()
+        {
+            return new SamsungTablet();
+
+        }
+    }
+}
