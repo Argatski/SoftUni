1021776da62e31fffa5e2de7d62f92175a843ab2
Denis Argatski <denis.argatski@gmail.com>
2023-02-17 23:10:37 +0200

    IAnimalFactory.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/04.AbstractFactoryPattern/Contracts/IAnimalFactory.cs
@@ -0,0 +1,11 @@
+﻿
+namespace _04.AbstractFactoryPattern.Contracts
+{
+    public interface IAnimalFactory
+    {
+        public ICarnivore GetCarnivore();
+        public IVegan GetVegan();
+        public IInsect GetInsect();
+
+    }
+}
