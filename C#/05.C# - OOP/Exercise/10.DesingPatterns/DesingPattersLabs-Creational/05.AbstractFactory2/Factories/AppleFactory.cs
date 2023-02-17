cd3191c09e7888800d354081d924651cf9537c5c
Denis Argatski <denis.argatski@gmail.com>
2023-02-17 23:16:18 +0200

    AppleFactory.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/05.AbstractFactory2/Factories/AppleFactory.cs
@@ -0,0 +1,24 @@
+﻿using _05.AbstractFactory2.Apple;
+using _05.AbstractFactory2.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.AbstractFactory2.Factories
+{
+    internal class AppleFactory : ITechnologyAbstractFactory
+    {
+        public IMobilePhone CreatePhone()
+        {
+            return new ApplePhone();
+        }
+
+        public ITablet CreateTable()
+        {
+            return new AppleTablet();
+
+        }
+    }
+}
