f9085a882704faefb4ee00cdce3350444819c260
Denis Argatski <denis.argatski@gmail.com>
2023-02-17 23:14:14 +0200

    AppleTablet.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/05.AbstractFactory2/Apple/AppleTablet.cs
@@ -0,0 +1,14 @@
+﻿using _05.AbstractFactory2.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.AbstractFactory2.Apple
+{
+    public class AppleTablet : ITablet
+    {
+        public string OS { get; set; }
+    }
+}
