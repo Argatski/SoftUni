c1f126add86c0f16cf4b54fc8b62d9bf54037ba8
Denis Argatski <denis.argatski@gmail.com>
2023-02-17 23:15:07 +0200

    ITablet.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/05.AbstractFactory2/Contracts/ITablet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.AbstractFactory2.Contracts
+{
+    public interface ITablet
+    {
+        public string OS { get; set; }
+    }
+}
