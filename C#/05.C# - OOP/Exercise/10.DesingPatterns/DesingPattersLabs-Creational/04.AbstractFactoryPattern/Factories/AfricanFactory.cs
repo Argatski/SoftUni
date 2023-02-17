d0fff17b8d6e236dc0ebba1ba46b8f8a9e963152
Denis Argatski <denis.argatski@gmail.com>
2023-02-17 23:11:57 +0200

    AfricanFactory.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/04.AbstractFactoryPattern/Factories/AfricanFactory.cs
@@ -0,0 +1,22 @@
+﻿using _04.AbstractFactoryPattern.Contracts;
+
+namespace _04.AbstractFactoryPattern.Factories
+{
+    class AfricanFactory : IAnimalFactory
+    {
+        public ICarnivore GetCarnivore()
+        {
+            return new Lion();
+        }
+
+        public IInsect GetInsect()
+        {
+            return new Idolomantis();
+        }
+
+        public IVegan GetVegan()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
