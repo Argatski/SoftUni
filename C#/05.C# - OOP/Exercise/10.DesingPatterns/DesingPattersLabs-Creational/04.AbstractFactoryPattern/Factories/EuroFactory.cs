7f928e2460833cedc3f3d96d30e876dca47705b2
Denis Argatski <denis.argatski@gmail.com>
2023-02-17 23:12:11 +0200

    EuroFactory.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/04.AbstractFactoryPattern/Factories/EuroFactory.cs
@@ -0,0 +1,22 @@
+﻿using _04.AbstractFactoryPattern.Contracts;
+
+namespace _04.AbstractFactoryPattern.Factories
+{
+    class EuroFactory : IAnimalFactory
+    {
+        public ICarnivore GetCarnivore()
+        {
+            return new Bear();
+        }
+
+        public IInsect GetInsect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IVegan GetVegan()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
