fb644702d731b45cd3e5e8dfd248c4bf76638a28
Denis Argatski <denis.argatski@gmail.com>
2023-02-17 23:12:37 +0200

    Bear.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/04.AbstractFactoryPattern/Bear.cs
@@ -0,0 +1,14 @@
+﻿using _04.AbstractFactoryPattern.Contracts;
+
+namespace _04.AbstractFactoryPattern
+{
+    internal class Bear : ICarnivore
+    {
+        private string eat = "Eat sheep.";
+        public Bear()
+        {
+
+        }
+        public string AnimalsThatEat { get => eat; set => eat = value; }
+    }
+}
