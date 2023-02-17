d1102fe69bac635338a51a03cd313e2c252a9a53
Denis Argatski <denis.argatski@gmail.com>
2023-02-17 23:17:03 +0200

    SamsungPhone.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/05.AbstractFactory2/Samsung/SamsungPhone.cs
@@ -0,0 +1,14 @@
+﻿using _05.AbstractFactory2.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.AbstractFactory2.Samsung
+{
+    public class SamsungPhone : IMobilePhone
+    {
+        public int Number { get; set; }
+    }
+}
