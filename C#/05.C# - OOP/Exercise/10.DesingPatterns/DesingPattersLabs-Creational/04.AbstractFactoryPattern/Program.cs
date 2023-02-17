60fd1e197542f4d5da2981b594dc86b791177a0c
Denis Argatski <denis.argatski@gmail.com>
2023-02-17 23:13:30 +0200

    Program.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/04.AbstractFactoryPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using _04.AbstractFactoryPattern.Contracts;
+using _04.AbstractFactoryPattern.Factories;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Which continet do you wanna  play?");
+        
+        string continent =  Console.ReadLine();
+
+        IAnimalFactory factory = new EuroFactory(); 
+
+        if (continent == "Africa")
+        {
+            factory= new AfricanFactory();
+        }
+        
+
+        ICarnivore animal =  factory.GetCarnivore();
+
+        Console.WriteLine(animal.AnimalsThatEat);
+    }
+}
