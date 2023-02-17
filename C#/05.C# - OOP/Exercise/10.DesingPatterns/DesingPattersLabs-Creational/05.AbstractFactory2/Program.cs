7ad618f7cc545734f65d0b3bed58ab710206bb60
Denis Argatski <denis.argatski@gmail.com>
2023-02-17 23:18:27 +0200

    Program.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/05.AbstractFactory2/Program.cs
@@ -0,0 +1,30 @@
+﻿using _05.AbstractFactory2.Contracts;
+using _05.AbstractFactory2.Factories;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Are you an apple Fangirl.");
+        //yes
+
+        var isFangirl = Console.ReadLine() == "yes" ? true : false;
+
+        ITechnologyAbstractFactory technologyAbstractFactory = null;
+
+        if (isFangirl)
+        {
+            technologyAbstractFactory = new AppleFactory();
+        }
+        else
+        {
+            technologyAbstractFactory = new SamsungFactory();
+        }
+
+        IMobilePhone myPhone = technologyAbstractFactory.CreatePhone();
+        ITablet myTablet = technologyAbstractFactory.CreateTable();
+
+        Console.WriteLine(myPhone.GetType().Name);
+        Console.WriteLine(myTablet.GetType().Name);
+    }
+}
