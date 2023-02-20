485d07f3714aaa3232c87260da162b9830cb8e15
Denis Argatski <denis.argatski@gmail.com>
2023-02-20 22:03:36 +0200

    ICarBuilder.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/06.BilderPattern/ICarBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.BuilderPattern
+{
+    public interface ICarBuilder
+    {
+        void BuildTyres(Car car);
+
+        void BuildEngine(Car car);
+
+        void BuildTransmission(Car car);
+    }
+}
