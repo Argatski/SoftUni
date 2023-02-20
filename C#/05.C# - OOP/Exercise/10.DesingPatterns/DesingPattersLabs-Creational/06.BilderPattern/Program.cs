d837c08559959af97273816c3762851d7e091d8f
Denis Argatski <denis.argatski@gmail.com>
2023-02-20 22:03:51 +0200

    Program.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/06.BilderPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using _06.BilderPattern;
+using _06.BuilderPattern;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Car car = new Car();
+
+        CarBuilder carBuilder = new CarBuilder();
+
+        carBuilder.BuildEngine(car);
+        carBuilder.BuildTransmission(car);
+        carBuilder.BuildTyres(car);
+
+        Console.WriteLine(car.Engine + " " + car.Transmission + " " + car.Tyres);
+    }
+}
