f7d4720dd1780e62efc44d57de35515cc12b6179
Denis Argatski <denis.argatski@gmail.com>
2023-02-20 22:03:17 +0200

    CarBuilder.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/06.BilderPattern/CarBuilder.cs
@@ -0,0 +1,27 @@
+﻿using _06.BuilderPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.BilderPattern
+{
+    internal class CarBuilder : ICarBuilder
+    {
+        public void BuildEngine(Car car)
+        {
+            car.Engine = "Mercedes";
+        }
+
+        public void BuildTransmission(Car car)
+        {
+            car.Transmission = "DCT";
+        }
+
+        public void BuildTyres(Car car)
+        {
+            car.Tyres = "Michelin";
+        }
+    }
+}
