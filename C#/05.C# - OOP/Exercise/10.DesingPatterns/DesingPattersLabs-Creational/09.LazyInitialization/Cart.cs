7c722cfac6590aff7b1bf140f192b5e7ee345818
Denis Argatski <denis.argatski@gmail.com>
2023-02-20 23:14:37 +0200

    "09.LazyInitialization"

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/09.LazyInitialization/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.LazyInitialization
+{
+    public class Cart
+    {
+        public Cart(string param)
+        {
+            Console.WriteLine("Initializaed" + param);
+        }
+        public int Balance { get; set; }
+    }
+}
---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/09.LazyInitialization/Program.cs
@@ -0,0 +1,14 @@
+﻿using _09.LazyInitialization;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Lazy<Cart> cart = new Lazy<Cart>(() =>  new Cart(" Startting yea"));
+
+        Console.WriteLine("In main");
+
+        cart.Value.Balance = 50;
+        Console.WriteLine(cart.Value.Balance);
+    }
+}
