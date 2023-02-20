75198a70b4e0c27a9f528c209b3cfd9585d3dbdc
Denis Argatski <denis.argatski@gmail.com>
2023-02-20 23:13:52 +0200

    Program.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/08.Prototype/Program.cs
@@ -0,0 +1,25 @@
+﻿using _08.Prototype;
+using System.Runtime.Loader;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Country country = new Country() { Name = "Bulgaria" };
+        City city = new City() { Name = "Sofiq" };
+
+        Address address = new Address() { Street = "Simeonovsko shose" };
+
+        address.City = city;
+        address.Country = country;
+
+        Address prototypeAddres = (Address)address.Clone();
+        prototypeAddres.Street = "bul.Bulgaria";
+
+        prototypeAddres.City.Name = "Smolqn";
+
+
+        Console.WriteLine(address);
+        Console.WriteLine(prototypeAddres);
+    }
+}
