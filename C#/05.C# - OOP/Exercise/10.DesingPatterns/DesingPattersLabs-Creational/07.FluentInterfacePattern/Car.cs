f698c48b58b3b126f2d617858b54d23a27d5456c
Denis Argatski <denis.argatski@gmail.com>
2023-02-20 23:11:39 +0200

    Car.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/07.FluentInterfacePattern/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace _06.BuilderPattern
+{
+    public class Car
+    {
+        public string Engine { get; set; }
+        public string Tyres { get; set; }
+        public string Transmission { get; set; }
+    }
+}
