1adcd3c725f099411eedd26b1d798f3dea16ec59
Denis Argatski <denis.argatski@gmail.com>
2023-02-20 23:12:00 +0200

    ICarBuilder.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/07.FluentInterfacePattern/ICarBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.BuilderPattern
+{
+    public interface ICarBuilder
+    {
+        ICarBuilder BuildTyres(Car car);
+        //here is a difference in builder pattern is (void or diff) in fluent interface (ICarbuilder or another interface)
+
+        ICarBuilder BuildEngine(Car car);
+
+        ICarBuilder BuildTransmission(Car car);
+    }
+}
