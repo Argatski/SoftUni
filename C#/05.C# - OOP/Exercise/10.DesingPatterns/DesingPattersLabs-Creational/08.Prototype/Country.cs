1ef013758515266280273a88bf20aa5d9f7ea1fc
Denis Argatski <denis.argatski@gmail.com>
2023-02-20 23:13:32 +0200

    Country.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLabs-Creational/08.Prototype/Country.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace _08.Prototype
+{
+    [Serializable]
+    public class Country : ICloneable
+    {
+        public string Name { get; set; }
+
+        public object Clone() 
+        {
+            return this.MemberwiseClone();
+        }
+
+        public static T DeepClone<T>(T obj)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(ms, obj);
+                ms.Position = 0;
+
+                return (T)formatter.Deserialize(ms);
+            }
+        }
+    }
+}
