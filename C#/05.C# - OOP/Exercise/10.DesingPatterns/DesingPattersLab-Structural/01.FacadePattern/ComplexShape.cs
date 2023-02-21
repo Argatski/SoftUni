5916d1309e886412c9b4465ca0d1fcae6b944087
Denis Argatski <denis.argatski@gmail.com>
2023-02-21 23:09:48 +0200

    ComplexShape.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLab-Structural/01.FacadePattern/ComplexShape.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.FacadePattern
+{
+    internal class ComplexShape : IDrawable
+    {
+        private List<IDrawable> _shapes = new List<IDrawable>();
+        public string Name { get; set; }
+
+        public ComplexShape(string name)
+        {
+            this.Name = name;
+        }
+
+        public void AddChild(IDrawable child)
+        {
+            _shapes.Add(child);
+        }
+
+        public void Draw(int level)
+        {
+            Console.Write(new string(' ', level));
+            Console.WriteLine(Name);
+
+            foreach (var shape in _shapes)
+            {
+                shape.Draw(level + 3);
+            }
+        }
+
+
+    }
+}
