d4ee9c158e1237f9600e18707e562d834fdfa0e2
Denis Argatski <denis.argatski@gmail.com>
2023-02-21 23:10:07 +0200

    IDrawable.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLab-Structural/01.FacadePattern/IDrawable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.FacadePattern
+{
+    internal interface IDrawable
+    {
+        public string Name { get; set; }
+        public void Draw(int level);
+        public void AddChild(IDrawable child);
+    }
+}
