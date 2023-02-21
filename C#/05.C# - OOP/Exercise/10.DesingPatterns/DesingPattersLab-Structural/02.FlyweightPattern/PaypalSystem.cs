03187227da6290e8e7bce3b147d5afd8e53ea0f4
Denis Argatski <denis.argatski@gmail.com>
2023-02-21 23:12:55 +0200

    PaypalSystem.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLab-Structural/02.FlyweightPattern/PaypalSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.FlyweightPattern
+{
+    internal class PaypalSystem : IPaymentSystem
+    {
+        public void LoanMoney(string from, string to, int amount)
+        {
+            Console.WriteLine($"Loaned {amount} momney from {from} to {to}");
+        }
+
+        public void PayMoney(string from, string to, int amount)
+        {
+            Console.WriteLine($"Paid {amount} momney from {from} to {to}");
+
+        }
+    }
+}
