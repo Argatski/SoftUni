a4d4f654fa5c0e417d0ecd85d864747c08c26a2b
Denis Argatski <denis.argatski@gmail.com>
2023-02-21 23:12:15 +0200

    PaymentSystemDecorator.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLab-Structural/02.FlyweightPattern/PaymentSystemDecorator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.FlyweightPattern
+{
+    internal class PaymentSystemDecorator : IPaymentSystem
+    {
+        private IPaymentSystem paymentSystem;
+        public PaymentSystemDecorator(IPaymentSystem paymentSystem)
+        {
+            this.paymentSystem = paymentSystem;
+        }
+
+        public void LoanMoney(string from, string to, int amount)
+        {
+            Console.WriteLine($"Decorated payment system and loggin in our system loans");
+
+            paymentSystem.LoanMoney(from, to, amount);
+        }
+
+        public void PayMoney(string from, string to, int amount)
+        {
+            Console.WriteLine($"Decorated payment system and loggin in our system payments");
+
+            paymentSystem.PayMoney(from, to, amount);
+        }
+    }
+}
