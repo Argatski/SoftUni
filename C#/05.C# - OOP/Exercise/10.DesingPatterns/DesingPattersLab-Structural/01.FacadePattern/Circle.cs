bf5cfb33edddc93d8c906bc7287703664eb1cad4
Denis Argatski <denis.argatski@gmail.com>
2023-02-21 23:09:28 +0200

    Circle.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLab-Structural/01.FacadePattern/Circle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.FacadePattern
+{
+    internal class Circle : SimpleShape
+    {
+        public Circle() : base("Cicle")
+        {
+        }
+    }
+}
