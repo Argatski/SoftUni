8dcbe630f6a6b74594bc5a87a01cd7b8bc05e54c
Denis Argatski <denis.argatski@gmail.com>
2023-02-21 23:10:39 +0200

    SimpleShape.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLab-Structural/01.FacadePattern/SimpleShape.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.FacadePattern
+{
+    internal class SimpleShape : IDrawable
+    {
+        public string Name { get; set; }
+        public SimpleShape(string name)
+        {
+            this.Name = name;
+        }
+
+        public void AddChild(IDrawable child)
+        {
+            throw new ArgumentException("Simple Shapes connot have children");
+        }
+
+        public void Draw(int level)
+        {
+            Console.Write(new string(' ', level));
+            Console.WriteLine(this.Name);
+        }
+    }
+}
