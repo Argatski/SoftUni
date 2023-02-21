be0734c06d65e9cb647a7253b1364d2bcb96a2d0
Denis Argatski <denis.argatski@gmail.com>
2023-02-21 23:11:48 +0200

    IPaymentSystem.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLab-Structural/02.FlyweightPattern/IPaymentSystem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.FlyweightPattern
+{
+    internal interface IPaymentSystem
+    {
+        void PayMoney(string from, string to, int amount);
+        void LoanMoney(string from, string to, int amount);
+    }
+}
