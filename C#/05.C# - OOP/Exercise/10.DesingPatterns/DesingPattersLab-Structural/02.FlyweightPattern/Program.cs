7301e6db4be8061a1cbc4f538aab526c3b7d45d1
Denis Argatski <denis.argatski@gmail.com>
2023-02-21 23:12:29 +0200

    Program.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattersLab-Structural/02.FlyweightPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿using _02.FlyweightPattern;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        IPaymentSystem paymentSystem = new PaymentSystemDecorator(new PaypalSystem()); //->this is decorator
+
+        paymentSystem.LoanMoney("Pesho", "Gosho", 22);
+        paymentSystem.PayMoney("Pesho", "Gosho", 22555);
+        paymentSystem.LoanMoney("Pesho", "Gosho", 56);
+
+    }
+}
