f3cd512b8a9a6c96232f025fe04ae471435d64a1
Denis Argatski <denis.argatski@gmail.com>
2023-02-23 21:01:36 +0200

    BogoSort.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/BehavioralPatterns/03.StrategyPattern/Strategies/BogoSort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.StrategyPattern.Strategies
+{
+    internal class BogoSort : ISortingStrategy
+    {
+        public void Sort(List<int> collection)
+        {
+            Console.WriteLine("Bogo bogo");
+        }
+    }
+}
