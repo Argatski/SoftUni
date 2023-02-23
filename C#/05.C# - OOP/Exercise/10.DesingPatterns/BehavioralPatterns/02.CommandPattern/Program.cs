ea652ce9531b688c32360e99202fd9d771c6ee0b
Denis Argatski <denis.argatski@gmail.com>
2023-02-23 21:00:15 +0200

    Program.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/BehavioralPatterns/02.CommandPattern/Program.cs
@@ -0,0 +1,47 @@
+﻿using _02.CommandPattern;
+using _02.CommandPattern.Commands;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();
+
+        while (true)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            int value = int.Parse(input[1]);
+
+            ICommand command = null;
+
+            switch (input[0])
+            {
+                case "+":
+                    command = new PlusCommand(value);
+                    break;
+                case "*":
+                    command = new MultiplyCommand(value);
+                    break;
+                case "-":
+                    command = new MinusCommand(value);
+                    break;
+                case "undo":
+                    calculator.Undo(value);
+                    break;
+                case "Redo":
+                    calculator.Redo(value);
+                    break;
+
+            }
+            if (command != null)
+            {
+                calculator.AddCommand(command);
+            }
+
+            Console.WriteLine(calculator.Result);
+        }
+
+
+    }
+}
