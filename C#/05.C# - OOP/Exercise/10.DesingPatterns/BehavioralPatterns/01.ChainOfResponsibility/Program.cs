d7385a399bfd6cfbbdd2146b05781b86d2c18d71
Denis Argatski <denis.argatski@gmail.com>
2023-02-23 20:57:05 +0200

    Proogram.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/BehavioralPatterns/01.ChainOfResponsibility/Program.cs
@@ -0,0 +1,16 @@
+﻿using _01.ChainOfResponsibility;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Approver  teamLead =  new TeamLead();
+        Approver cto =  new CTO();
+
+        teamLead.SetNext(cto);
+
+        Console.WriteLine(teamLead.HandleRequest(3));
+        Console.WriteLine(teamLead.HandleRequest(50));
+        Console.WriteLine(teamLead.HandleRequest(5000));
+    }
+}
