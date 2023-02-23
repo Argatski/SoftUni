b389b96b74abb2896ccbfa7062a0a73ff6765b85
Denis Argatski <denis.argatski@gmail.com>
2023-02-23 21:03:57 +0200

    Program.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/BehavioralPatterns/03.StrategyPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿using _03.StrategyPattern;
+using _03.StrategyPattern.Strategies;
+using System.Reflection;
+
+string sortType = Console.ReadLine();
+Console.WriteLine();
+
+var strategyType = Assembly.GetEntryAssembly()
+    .GetTypes()
+    .Where(t => typeof(ISortingStrategy).IsAssignableFrom(t) && t.Name.StartsWith(sortType))
+    .FirstOrDefault();
+
+ISortingStrategy strategy = (ISortingStrategy)Activator.CreateInstance(strategyType);
+
+Sorter sorter = new Sorter(strategy);
+sorter.Sort(new List<int> { 1, 5, 2, 3, 7, 8, 10 });
