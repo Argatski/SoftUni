c678d2831389532f1f0f2a262eb4b2b5b317b3ad
Denis Argatski <denis.argatski@gmail.com>
2023-02-23 21:02:04 +0200

    QuickSort.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/BehavioralPatterns/03.StrategyPattern/Strategies/QuickSort.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.StrategyPattern.Strategies
+{
+    internal class QuickSort : ISortingStrategy
+    {
+        public void Sort(List<int> collection)
+        {
+            Console.WriteLine("Quick sort");
+        }
+    }
+}
+
