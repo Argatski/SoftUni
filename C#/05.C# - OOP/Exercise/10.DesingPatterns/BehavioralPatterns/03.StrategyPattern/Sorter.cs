b0026ea756a2a930079b718da27bb063130cf44a
Denis Argatski <denis.argatski@gmail.com>
2023-02-23 21:04:13 +0200

    Sorter.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/BehavioralPatterns/03.StrategyPattern/Sorter.cs
@@ -0,0 +1,22 @@
+﻿using _03.StrategyPattern.Strategies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.StrategyPattern
+{
+    internal class Sorter
+    {
+        private ISortingStrategy sortingStrategy;
+        public Sorter(ISortingStrategy sortingStrategy)
+        {
+            this.sortingStrategy = sortingStrategy;
+        }
+        public void Sort(List<int> collection)
+        {
+            sortingStrategy.Sort(collection);
+        }
+    }
+}
