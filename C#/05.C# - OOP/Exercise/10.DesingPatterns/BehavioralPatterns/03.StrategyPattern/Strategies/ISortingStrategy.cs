0e8e0288436c5f4860c7923dacf90aaf4b3560a8
Denis Argatski <denis.argatski@gmail.com>
2023-02-23 21:02:48 +0200

    ISortingStrategy.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/BehavioralPatterns/03.StrategyPattern/Strategies/ISortingStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.StrategyPattern.Strategies
+{
+    public interface ISortingStrategy
+    {
+        void Sort(List<int> collection);
+    }
+}
