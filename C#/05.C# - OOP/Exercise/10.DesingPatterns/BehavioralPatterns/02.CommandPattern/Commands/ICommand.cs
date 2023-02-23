e210fb3333715201bc58c08e7214b945b28ee9de
Denis Argatski <denis.argatski@gmail.com>
2023-02-23 20:58:18 +0200

    ICommand.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/BehavioralPatterns/02.CommandPattern/Commands/ICommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.CommandPattern.Commands
+{
+    public abstract class ICommand
+    {
+
+        protected int operand;
+
+        public ICommand(int operand)
+        {
+            this.operand = operand;
+        }
+
+        public abstract int Calculate(int currentValue);
+
+        public abstract int UndoCalculation(int currentValue);
+    }
+}
