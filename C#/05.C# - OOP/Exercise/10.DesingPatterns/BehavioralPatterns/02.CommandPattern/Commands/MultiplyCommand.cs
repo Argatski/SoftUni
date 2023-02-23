d3194067b0a82135c471072db8ff8612e55bf7c8
Denis Argatski <denis.argatski@gmail.com>
2023-02-23 20:59:00 +0200

    PlusCommand.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/BehavioralPatterns/02.CommandPattern/Commands/MultiplyCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.CommandPattern.Commands
+{
+    internal class MultiplyCommand : ICommand
+    {
+        public MultiplyCommand(int operand) : base(operand)
+        {
+        }
+
+        public override int Calculate(int currentValue)
+        {
+            return currentValue * operand;
+        }
+
+        public override  int UndoCalculation(int currentValue)
+        {
+            return currentValue / operand;
+        }
+    }
+}
---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/BehavioralPatterns/02.CommandPattern/Commands/PlusCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.CommandPattern.Commands
+{
+    internal class PlusCommand : ICommand
+    {
+        public PlusCommand(int operand) : base(operand)
+        {
+        }
+
+        public override int Calculate(int currentValue)
+        {
+            return currentValue + operand;
+        }
+
+        public override int UndoCalculation(int currentValue)
+        {
+            return currentValue - operand;
+        }
+    }
+}
