b1595c6587fe31dbaee9b2a7360fb2a81cefe978
Denis Argatski <denis.argatski@gmail.com>
2023-02-23 20:56:07 +0200

    CTO.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/BehavioralPatterns/01.ChainOfResponsibility/CTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ChainOfResponsibility
+{
+    internal class CTO : Approver
+    {
+        public override bool HandleRequest(int amount)
+        {
+            if (amount < 500)
+            {
+                Console.WriteLine("Get this money.");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
