13cf5b83a6483d05d28dbf50340012d1f7f7e82b
Denis Argatski <denis.argatski@gmail.com>
2023-02-23 21:01:51 +0200

    MergeSort.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/BehavioralPatterns/03.StrategyPattern/Strategies/MergeSort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.StrategyPattern.Strategies
+{
+    internal class MergeSort : ISortingStrategy
+    {
+        public void Sort(List<int> collection)
+        {
+            Console.WriteLine("Merge sort");
+        }
+    }
+}
