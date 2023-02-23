ec22eaaf506f4284d7525cb70b35728b299b26a3
Denis Argatski <denis.argatski@gmail.com>
2023-02-23 20:55:41 +0200

    Approver.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/BehavioralPatterns/01.ChainOfResponsibility/Approver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ChainOfResponsibility
+{
+    abstract class Approver
+    {
+        protected Approver Next { get; set; }
+        public abstract bool HandleRequest(int amount);
+
+        public virtual void SetNext(Approver approver)
+        {
+            Next = approver;
+        }
+    }
+}
