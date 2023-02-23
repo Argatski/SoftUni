b7fbfbaa1ab72fc3648f65d740b65da0a82ea150
Denis Argatski <denis.argatski@gmail.com>
2023-02-23 20:58:35 +0200

    MinusCommand.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/BehavioralPatterns/02.CommandPattern/Commands/MinusCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.CommandPattern.Commands
+{
+    internal class MinusCommand : ICommand
+    {
+        public MinusCommand(int operand) : base(operand)
+        {
+        }
+
+        public override int Calculate(int currentValue)
+        {
+            return currentValue - operand;
+        }
+
+        public override int UndoCalculation(int currentValue)
+        {
+            return currentValue + operand;
+        }
+    }
+}
