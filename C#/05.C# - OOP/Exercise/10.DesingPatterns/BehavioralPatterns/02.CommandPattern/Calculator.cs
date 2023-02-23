813e50a3b5fffa184eb6d661814606536718e789
Denis Argatski <denis.argatski@gmail.com>
2023-02-23 21:00:01 +0200

    Calculator.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/BehavioralPatterns/02.CommandPattern/Calculator.cs
@@ -0,0 +1,50 @@
+﻿using _02.CommandPattern.Commands;
+
+
+namespace _02.CommandPattern
+{
+    public class Calculator
+    {
+        private List<ICommand> commands = new List<ICommand>();
+        private int index = -1;
+
+        public int Result { get; set; }
+
+        public void AddCommand(ICommand command)
+        {
+            commands.Insert(++index,command);
+            Result = commands[index].Calculate(Result);
+
+        }
+
+        public void Undo(int level)
+        {
+            for (int i = 0; i < level; i++)
+            {
+                if (index >= 0)
+                {
+                    Result = commands[i].UndoCalculation(Result);
+
+                    index--;
+                }
+
+               
+            }
+        }
+
+        public void Redo(int level)
+        {
+            for (int i = 0; i < level; i++)
+            {
+                if (index < commands.Count)
+                {
+                    Result = commands[i].Calculate(Result);
+
+                    index++;
+                }
+
+
+            }
+        }
+    }
+}
