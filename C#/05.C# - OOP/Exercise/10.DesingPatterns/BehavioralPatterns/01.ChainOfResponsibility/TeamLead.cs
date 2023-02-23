3ae54e725e55d284b5d9d074a258c41d79d77e56
Denis Argatski <denis.argatski@gmail.com>
2023-02-23 20:57:27 +0200

    TeamLead.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/BehavioralPatterns/01.ChainOfResponsibility/TeamLead.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ChainOfResponsibility
+{
+    internal class TeamLead : Approver
+    {
+
+        public override bool HandleRequest(int amount)
+        {
+            if (amount < 5)
+            {
+                Console.WriteLine("Get my money");
+                return true;
+            }
+            else
+            {
+               return Next.HandleRequest(amount);
+            }
+        }
+    }
+}
