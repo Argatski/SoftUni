e9069ea9a55ef01f8649cecda3d908b98552b7ef
Denis Argatski <denis.argatski@gmail.com>
2023-02-25 21:46:57 +0200

    ISingletonContainer.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattern-Labs/01.Singleton/Contracts/ISingletonContainer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Singleton.Contracts
+{
+    public interface ISingletonContainer
+    {
+        int GetPopulation(string name);
+    }
+}
