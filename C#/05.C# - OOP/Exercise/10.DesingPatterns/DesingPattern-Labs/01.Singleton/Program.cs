be238f41bfe55bc3eb76dd6b05a5cd80158217b7
Denis Argatski <denis.argatski@gmail.com>
2023-02-25 21:48:04 +0200

    01.Singleton.csproj

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPattern-Labs/01.Singleton/Program.cs
@@ -0,0 +1,13 @@
+﻿using _01.Singleton;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var db = SingletonDataContainer.Instance;
+        Console.WriteLine(db.GetPopulation("Washington,D.C."));
+        var db2 = SingletonDataContainer.Instance;
+        Console.WriteLine(db2.GetPopulation("London"));
+
+    }
+}
