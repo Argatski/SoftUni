4cc0368dd02afcb729ba6f6e93c246d2dd309f70
Denis Argatski <denis.argatski@gmail.com>
2023-02-26 23:01:17 +0200

    SingleGift.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPatterns-Exercise/02.CompositePattern/SingleGift.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.CompositePattern
+{
+    internal class SingleGift : GiftBaseClass
+    {
+        public SingleGift(string name, int price)
+            : base(name, price)
+        {
+        }
+
+        public override int CalculateTotalPrice()
+        {
+            return this.Price;
+        }
+    }
+}
