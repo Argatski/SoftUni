8689290666023c2c1cebb754aaba48d3f62a4eb1
Denis Argatski <denis.argatski@gmail.com>
2023-02-26 23:01:36 +0200

    Composite.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPatterns-Exercise/02.CompositePattern/Composite.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.CompositePattern
+{
+    internal class Composite : GiftBaseClass, IGiftOperation
+    {
+        private ICollection<GiftBaseClass> giftBaseClasses;
+
+        public Composite(string name, int price)
+            : base(name, price)
+        {
+            this.giftBaseClasses = new List<GiftBaseClass>();
+
+        }
+
+        public void Add(GiftBaseClass giftBaseClass)
+        {
+            this.giftBaseClasses.Add(giftBaseClass);
+
+           
+        }
+
+        public bool Remove(GiftBaseClass giftBaseClass)
+        {
+            return this.giftBaseClasses.Remove(giftBaseClass);
+        }
+        public override int CalculateTotalPrice()
+        {
+            var totalPrice = 0;
+
+            foreach (var item in giftBaseClasses)
+            {
+                totalPrice += item.CalculateTotalPrice();
+            }
+            return totalPrice;
+        }
+    }
+}
