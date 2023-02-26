f9c8dfa02806ae091f39c9f11987b8ab644fd2b9
Denis Argatski <denis.argatski@gmail.com>
2023-02-26 23:03:25 +0200

    IGiftOperation.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPatterns-Exercise/02.CompositePattern/IGiftOperation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.CompositePattern
+{
+    public interface IGiftOperation
+    {
+        void Add(GiftBaseClass giftBaseClass);
+        bool Remove(GiftBaseClass giftBaseClass);
+    }
+}
