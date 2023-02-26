c60efebd274fa142cb76a98015080e694b026b61
Denis Argatski <denis.argatski@gmail.com>
2023-02-26 23:01:03 +0200

    Program.cs

--- C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPatterns-Exercise/02.CompositePattern/Program.cs
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPatterns-Exercise/02.CompositePattern/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _02.CompositePattern;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var phone = new SingleGift("Test", 10);
+
+        Console.WriteLine(phone.CalculateTotalPrice());
+
+        var compositeGift = new Composite("Rootbox", 0);
+        var singleGift = new SingleGift("qwow", 50);
+        var singleGift2 = new SingleGift("qwow", 70);
+
+        compositeGift.Add(singleGift);
+        compositeGift.Add(singleGift2);
+        Console.WriteLine(compositeGift.CalculateTotalPrice());
+
+    }
+}
