b0d39837b6f498d6cf16dd04d9350eaa3eeab1ba
Denis Argatski <denis.argatski@gmail.com>
2023-02-26 22:32:58 +0200

    SandwichPrototype.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPatterns-Exercise/01.Prototype/SandwichPrototype.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Prototype
+{
+    public abstract class SandwichPrototype<T>
+    {
+        public abstract T ShalloClone();
+        public abstract T DeepClone();
+    }
+}
