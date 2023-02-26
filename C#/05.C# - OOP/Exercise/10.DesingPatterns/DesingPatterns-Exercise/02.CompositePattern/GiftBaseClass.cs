4005f28a288bdd1669c62b6351157bfb3f21537c
Denis Argatski <denis.argatski@gmail.com>
2023-02-26 23:02:39 +0200

    GiftBaseClass.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPatterns-Exercise/02.CompositePattern/GiftBaseClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.CompositePattern
+{
+   public abstract class GiftBaseClass
+    {
+        public GiftBaseClass(string name,int price)
+        {
+            Name = name;
+            this.Price = price;   
+        }
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public abstract int CalculateTotalPrice();
+    }
+}
