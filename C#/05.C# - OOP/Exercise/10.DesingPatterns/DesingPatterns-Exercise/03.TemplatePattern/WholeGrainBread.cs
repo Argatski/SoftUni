9bd4fc8ceb893c0876caf2402b5831e4d650e8e0
Denis Argatski <denis.argatski@gmail.com>
2023-03-01 22:20:54 +0200

    WholeGrainBread.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPatterns-Exercise/03.TemplatePattern/WholeGrainBread.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.TemplatePattern
+{
+    public class WholeGrainBread : Bread
+    {
+        public override void Bake()
+        {
+            Console.WriteLine("10 min.");
+        }
+
+        public override void MixIngridients()
+        {
+            Console.WriteLine("a lot of seeds");
+        }
+
+        public override void Slice()
+        {
+            Console.WriteLine("Sliced 2");
+        }
+    }
+}
