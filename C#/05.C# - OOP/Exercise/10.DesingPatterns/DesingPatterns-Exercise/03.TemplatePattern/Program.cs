e55c105dc5ddff82ea2446279fd30d4f0b8d74cc
Denis Argatski <denis.argatski@gmail.com>
2023-03-01 22:19:31 +0200

    Program.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPatterns-Exercise/03.TemplatePattern/Program.cs
@@ -0,0 +1,11 @@
+﻿using _03.TemplatePattern;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        WholeGrainBread wholeGrainBread = new WholeGrainBread();
+
+        wholeGrainBread.Make();
+    }
+}
