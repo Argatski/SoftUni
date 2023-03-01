23186690bcaf31f074c38964d671cda111dd000c
Denis Argatski <denis.argatski@gmail.com>
2023-03-01 22:20:35 +0200

    Bread.cs

---
+++ C#/05.C# - OOP/Exercise/10.DesingPatterns/DesingPatterns-Exercise/03.TemplatePattern/Bread.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.TemplatePattern
+{
+    public abstract class Bread
+    {
+        public abstract void Bake();
+        public abstract void MixIngridients();
+        public abstract void Slice();
+        public void Make()
+        {
+            this.MixIngridients();
+            this.Bake();
+            this.Slice();
+        }
+
+    }
+}
