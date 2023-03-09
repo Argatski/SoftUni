ac388a864301359cc9f41ce6556e95c74091ce3d
Denis Argatski <denis.argatski@gmail.com>
2023-03-09 23:23:44 +0200

    CentralProcessingUnit.cs

---
+++ C#/05.C# - OOP/Exam/C#OOPExam -16August2020/OnlineShop-Skeleton/OnlineShop/Models/Products/Components/CentralProcessingUnit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop.Models.Products.Components
+{
+    public class CentralProcessingUnit : Component
+    {
+        private const double CentralProcessingMultiplier = 1.25;
+        public CentralProcessingUnit(int id, string manufacturer, string model, decimal price, double overallPerformence, int generation) : base(id, manufacturer, model, price, overallPerformence, generation)
+        {
+            this.OverallPerformance *= CentralProcessingMultiplier;
+        }
+    }
+}
