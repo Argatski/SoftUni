a03c37e4517da6e425fc347d2ce4070a886b6cb0
Denis Argatski <denis.argatski@gmail.com>
2023-03-09 23:25:16 +0200

    Headset.cs

---
+++ C#/05.C# - OOP/Exam/C#OOPExam -16August2020/OnlineShop-Skeleton/OnlineShop/Models/Products/Peripherals/Headset.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop.Models.Products.Peripherals
+{
+    public class Headset : Peripheral
+    {
+        public Headset(int id, string manufacturer, string model, decimal price, double overallPerformence, string connectionType) : base(id, manufacturer, model, price, overallPerformence, connectionType)
+        {
+        }
+    }
+}
