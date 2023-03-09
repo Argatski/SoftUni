5b5a67fc677a006e2ea71c70db94013fad7dc19e
Denis Argatski <denis.argatski@gmail.com>
2023-03-09 23:22:41 +0200

    PowerSupply.cs

---
+++ C#/05.C# - OOP/Exam/C#OOPExam -16August2020/OnlineShop-Skeleton/OnlineShop/Models/Products/Components/PowerSupply.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop.Models.Products.Components
+{
+    public class PowerSupply : Component
+    {
+        private const double PowerSupplyMultiplier = 1.05;
+        public PowerSupply(int id, string manufacturer, string model, decimal price, double overallPerformence, int generation) : base(id, manufacturer, model, price, overallPerformence, generation)
+        {
+            this.OverallPerformance *= PowerSupplyMultiplier;
+        }
+    }
+}
