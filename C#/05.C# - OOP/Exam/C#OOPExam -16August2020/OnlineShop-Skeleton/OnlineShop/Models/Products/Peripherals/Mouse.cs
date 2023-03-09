b452fd49502dd17b5c479c260ea4aac4d04c4be8
Denis Argatski <denis.argatski@gmail.com>
2023-03-09 23:26:46 +0200

    Mouse.cs

---
+++ C#/05.C# - OOP/Exam/C#OOPExam -16August2020/OnlineShop-Skeleton/OnlineShop/Models/Products/Peripherals/Mouse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop.Models.Products.Peripherals
+{
+    public class Mouse : Peripheral
+    {
+        public Mouse(int id, string manufacturer, string model, decimal price, double overallPerformence, string connectionType) : base(id, manufacturer, model, price, overallPerformence, connectionType)
+        {
+        }
+    }
+}
