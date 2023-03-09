d18c11d465d44504f11c9447e9e2797011460f47
Denis Argatski <denis.argatski@gmail.com>
2023-03-09 23:22:57 +0200

    Motherboard.cs

---
+++ C#/05.C# - OOP/Exam/C#OOPExam -16August2020/OnlineShop-Skeleton/OnlineShop/Models/Products/Components/Motherboard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop.Models.Products.Components
+{
+    internal class Motherboard : Component
+    {
+        private const double MotherBoardMultiplier = 1.25;
+        public Motherboard(int id, string manufacturer, string model, decimal price, double overallPerformence, int generation) : base(id, manufacturer, model, price, overallPerformence, generation)
+        {
+            this.OverallPerformance *= MotherBoardMultiplier;
+        }
+    }
+}
