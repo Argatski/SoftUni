8e9647d68a31e5e2a3db8ed8d368e1387df74437
Denis Argatski <denis.argatski@gmail.com>
2023-03-09 23:23:26 +0200

    Component.cs

---
+++ C#/05.C# - OOP/Exam/C#OOPExam -16August2020/OnlineShop-Skeleton/OnlineShop/Models/Products/Components/Component.cs
@@ -0,0 +1,22 @@
+﻿using OnlineShop.Common.Constants;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop.Models.Products.Components
+{
+    public abstract class Component : Product, IComponent
+    {
+        protected Component(int id, string manufacturer, string model, decimal price, double overallPerformence, int generation) : base(id, manufacturer, model, price, overallPerformence)
+        {
+            this.Generation = generation;
+        }
+
+        public int Generation { get; }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " +string.Format(SuccessMessages.ComponentToString, this.Generation);
+        }
+    }
+}
