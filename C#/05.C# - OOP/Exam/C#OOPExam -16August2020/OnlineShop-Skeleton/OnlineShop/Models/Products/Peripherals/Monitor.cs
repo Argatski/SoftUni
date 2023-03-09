f6a9a2173ceeb87a2a7f2ff02866e42c3a06f3e2
Denis Argatski <denis.argatski@gmail.com>
2023-03-09 23:26:28 +0200

    Monitor.cs

---
+++ C#/05.C# - OOP/Exam/C#OOPExam -16August2020/OnlineShop-Skeleton/OnlineShop/Models/Products/Peripherals/Monitor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop.Models.Products.Peripherals
+{
+    public class Monitor : Peripheral
+    {
+        public Monitor(int id, string manufacturer, string model, decimal price, double overallPerformence, string connectionType) : base(id, manufacturer, model, price, overallPerformence, connectionType)
+        {
+        }
+    }
+}
