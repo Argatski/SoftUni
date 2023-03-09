86a5b5ce4da8a3ac3f9c98637b72e47e0aa64145
Denis Argatski <denis.argatski@gmail.com>
2023-03-09 23:24:27 +0200

    Laptop.cs

---
+++ C#/05.C# - OOP/Exam/C#OOPExam -16August2020/OnlineShop-Skeleton/OnlineShop/Models/Products/Computers/Laptop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop.Models.Products.Computers
+{
+    public class Laptop : Computer
+    {
+        private const double laptopPerformance = 10;
+        public Laptop(int id, string manufacturer, string model, decimal price) : base(id, manufacturer, model, price, laptopPerformance)
+        {
+        }
+    }
+}
