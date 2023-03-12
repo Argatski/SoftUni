548dfd2490a026d6fb0b6478b135b1b107082ee2
Denis Argatski <denis.argatski@gmail.com>
2023-03-12 16:50:04 +0200

    Product.cs

---
+++ C#/05.C# - OOP/Exam/C#OOPExam -16August2020/OnlineShop-Skeleton/OnlineShop/Models/Products/Product.cs
@@ -0,0 +1,111 @@
+﻿using OnlineShop.Common.Constants;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop.Models.Products
+{
+    public abstract class Product : IProduct
+    {
+        private int id;
+        private string manufacturer;
+        private string model;
+        private decimal price;
+        private double overallPerformence;
+
+        public Product(int id, string manufacturer, string model, decimal price, double overallPerformence)
+        {
+            this.Id = id;
+            this.Manufacturer = manufacturer;
+            this.Model = model;
+            this.Price = price;
+            this.OverallPerformance = overallPerformence;
+
+        }
+
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidProductId);
+                }
+                id = value;
+            }
+        }
+
+        public string Manufacturer
+        {
+            get
+            {
+                return this.manufacturer;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidManufacturer);
+                }
+                this.manufacturer = value;
+            }
+        }
+
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidModel);
+                }
+                model = value;
+            }
+        }
+
+        public virtual decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidPrice);
+                }
+                price = value;
+            }
+        }
+
+        public virtual double OverallPerformance
+        {
+            get
+            {
+                return this.overallPerformence;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidOverallPerformance);
+                }
+                overallPerformence = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format(SuccessMessages.ProductToString, this.OverallPerformance.ToString("F2"), this.Price.ToString("F2"), this.GetType().Name, this.Manufacturer, this.Model, this.Id);
+        }
+    }
+}
