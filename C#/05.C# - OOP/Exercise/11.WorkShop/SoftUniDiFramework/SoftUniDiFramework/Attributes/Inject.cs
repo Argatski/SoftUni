eb2d171cd064655c9b364cbd58e10982dedd040d
Denis Argatski <denis.argatski@gmail.com>
2023-03-19 21:37:33 +0200

    Inject.ccs

---
+++ C#/05.C# - OOP/Exercise/11.WorkShop/SoftUniDiFramework/SoftUniDiFramework/Attributes/Inject.cs
@@ -0,0 +1,17 @@
+﻿namespace SoftUniDiFramework.Attributes
+{
+    [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Field)]
+    internal class Inject : Attribute
+    {
+
+    }
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Field)]
+    public class Named : Attribute
+    {
+        public Named(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; }
+    }
+}
