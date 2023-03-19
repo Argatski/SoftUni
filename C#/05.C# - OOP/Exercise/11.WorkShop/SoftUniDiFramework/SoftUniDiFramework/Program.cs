b4e0278c9c613cc3b2fc3ef01bf1251aa410def1
Denis Argatski <denis.argatski@gmail.com>
2023-03-19 21:39:14 +0200

    Program.cs

---
+++ C#/05.C# - OOP/Exercise/11.WorkShop/SoftUniDiFramework/SoftUniDiFramework/Program.cs
@@ -0,0 +1,15 @@
+﻿using SoftUniDiFramework.Injectors;
+using SoftUniDiFramework.Modules;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       
+    }
+    public static Injector CreateInjector(IModule module)
+    {
+        module.Configure();
+        return new Injector(module);
+    }
+}
