e6c6df5b119e31119c76cf755a277994c531eea0
Denis Argatski <denis.argatski@gmail.com>
2023-03-19 21:38:30 +0200

    IModule.cs

---
+++ C#/05.C# - OOP/Exercise/11.WorkShop/SoftUniDiFramework/SoftUniDiFramework/Modules/IModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniDiFramework.Modules
+{
+    public   interface IModule
+    {
+        void Configure();
+        Type GetMapping(Type currentInterface, object attribute);
+
+        object GetInstance(Type type);
+        void SetInstance(Type implementation, object instance);
+    }
+}
