b0bab6c666b848da46bf31dfa6e67ad52bcea578
Denis Argatski <denis.argatski@gmail.com>
2023-03-22 18:04:05 +0200

    Food.cs

---
+++ C#/05.C# - OOP/Exercise/11.WorkShop/SnakeWorkshop/SimpleSnake/GameObjects/Food.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleSnake.GameObjects
+{
+    public abstract class Food : Point
+    {
+        private char foodSymbol;
+        private readonly Random random;
+        private readonly Wall wall;
+
+        protected Food(Wall wall, char foodSymbol, int points)
+                     : base(wall.LeftX, wall.TopY)
+        {
+            this.wall = wall;
+            this.FoodPoints = points;
+            this.random = new Random();
+            this.foodSymbol = foodSymbol;
+        }
+
+        public int FoodPoints { get; private set; }
+
+        public void SetRandomPosition(Queue<Point> snakeElemets)
+        {
+            this.LeftX = random.Next(2, wall.LeftX - 2);
+            this.TopY = random.Next(2, wall.TopY - 2);
+
+            bool isPointOfSnake = snakeElemets.Any(x => x.LeftX == this.LeftX && x.TopY == this.TopY);
+
+            while (isPointOfSnake)
+            {
+                this.LeftX = random.Next(2, wall.LeftX - 2);
+                this.TopY = random.Next(2, wall.TopY - 2);
+
+                isPointOfSnake = snakeElemets.Any(x => x.LeftX == this.LeftX && x.TopY == this.TopY);
+            }
+            Console.BackgroundColor = ConsoleColor.Red;
+            this.Draw(foodSymbol);
+            Console.BackgroundColor = ConsoleColor.White;
+        }
+
+        public bool IsFoodPoint(Point snake)
+        {
+            return snake.TopY == this.TopY && snake.LeftX == this.LeftX;
+        }
+    }
+}
