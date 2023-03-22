b9020cdf8f62d841d7043f8ff1a765895c43d5b8
Denis Argatski <denis.argatski@gmail.com>
2023-03-22 18:05:12 +0200

    FoodHash.cs

---
+++ C#/05.C# - OOP/Exercise/11.WorkShop/SnakeWorkshop/SimpleSnake/GameObjects/FoodDollar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleSnake.GameObjects
+{
+    public class FoodDollar : Food
+    {
+        private const char foodSymbol = '$';
+        private const int points = 2;
+        public FoodDollar(Wall wall)
+            : base(wall, foodSymbol, points)
+        {
+        }
+    }
+}
