24dff3024196cebf3e613e1606b8a81a4e3dab4f
Denis Argatski <denis.argatski@gmail.com>
2023-03-25 22:15:32 +0200

    Pistol.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 12 April 2020/CounterStrike/Models/Guns/Pistol.cs
@@ -0,0 +1,24 @@
+﻿namespace CounterStrike.Models.Guns
+{
+    public class Pistol : Gun
+    {
+        private const int strikeBullets = 1;
+        public Pistol(string name, int bulletsCount)
+            : base(name, bulletsCount)
+        {
+        }
+
+        public override int Fire()
+        {
+            if (BulletsCount - strikeBullets >= 0)
+            {
+                BulletsCount -= strikeBullets;
+                return strikeBullets;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
