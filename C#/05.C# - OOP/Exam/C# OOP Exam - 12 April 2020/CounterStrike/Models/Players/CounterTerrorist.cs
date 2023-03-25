b86622ed0cbf0d212f052995eacfaffd73088453
Denis Argatski <denis.argatski@gmail.com>
2023-03-25 22:17:20 +0200

    CounterTerrorist.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 12 April 2020/CounterStrike/Models/Players/CounterTerrorist.cs
@@ -0,0 +1,14 @@
+﻿using CounterStrike.Models.Guns.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CounterStrike.Models.Players
+{
+    public class CounterTerrorist : Player
+    {
+        public CounterTerrorist(string name, int helath, int armor, IGun gun) : base(name, helath, armor, gun)
+        {
+        }
+    }
+}
