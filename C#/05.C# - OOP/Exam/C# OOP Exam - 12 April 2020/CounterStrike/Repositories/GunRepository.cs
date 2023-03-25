2873558709d9245dadd1dd1337ff1dbe1cc1201a
Denis Argatski <denis.argatski@gmail.com>
2023-03-25 22:19:18 +0200

    GunRepository.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 12 April 2020/CounterStrike/Repositories/GunRepository.cs
@@ -0,0 +1,43 @@
+﻿using CounterStrike.Models.Guns.Contracts;
+using CounterStrike.Repositories.Contracts;
+using CounterStrike.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CounterStrike.Repositories
+{
+    internal class GunRepository : IRepository<IGun>
+    {
+        public readonly List<IGun> guns;
+        public GunRepository()
+        {
+            this.guns = new List<IGun>();
+        }
+
+        public IReadOnlyCollection<IGun> Models => this.guns.AsReadOnly();
+
+        public void Add(IGun model)
+        {
+            if (model == null)
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidGunRepository);
+            }
+            this.guns.Add(model);
+        }
+
+        public IGun FindByName(string name)
+        {
+            IGun gun = this.guns.FirstOrDefault(x => x.Name == name);
+
+            return gun;
+        }
+
+        public bool Remove(IGun model)
+        {
+            bool resutl = this.guns.Remove(model);
+
+            return resutl;
+        }
+    }
+}
