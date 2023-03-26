00185d87636f2964969fbf14cb4ece262de7a3b5
Denis Argatski <denis.argatski@gmail.com>
2023-03-26 23:54:11 +0300

    StartUp.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 12 April 2020/CounterStrike/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace CounterStrike
+{
+    using CounterStrike.Core;
+    using CounterStrike.Core.Contracts;
+    using CounterStrike.Models.Guns;
+    using CounterStrike.Models.Players;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            IEngine engine = new Engine();
+            engine.Run();
+
+          
+        }
+    }
+}
