34dd5b96e88f3816960e86ec4e3a018c1fe3c343
Denis Argatski <denis.argatski@gmail.com>
2023-04-01 17:33:06 +0300

    "RobotsTests.cs"

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 12 April 2020/CounterStrikeUnitTests/Robots.Tests/RobotsTests.cs
@@ -0,0 +1,42 @@
+﻿namespace Robots.Tests
+{
+    using NUnit.Framework;
+    using System;
+
+    public class RobotsTests
+    {
+        private Robot robot;
+        private RobotManager manager;
+        [SetUp]
+        public void SetUp()
+        {
+            robot = new Robot("Test", 10);
+            manager = new RobotManager(10);
+        }
+        [Test]
+        public void WhenRobotsIsCreated_PropertiesShouldBeSet()
+        {
+            Assert.AreEqual("Test", robot.Name);
+            Assert.AreEqual(10, robot.Battery);
+            Assert.AreEqual(10, robot.MaximumBattery);
+        }
+
+        [Test]
+        public void WhenRobotManagerIsCreated_CapacityBeSet()
+        {
+            Assert.AreEqual(10, manager.Capacity);
+        }
+        [Test]
+        public void WhenRobotManagerIsNegative_ExceptionShouldBeThrown()
+        {
+
+            Assert.Throws<ArgumentException>(() => { RobotManager robotManager = new RobotManager(-5); });
+        }
+
+        [Test]
+        public void WhenRobotManagerIsZero_CountShouldBeZero()
+        {
+            Assert.AreEqual(0, manager.Count);
+        }
+    }
+}
