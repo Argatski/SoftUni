eff4843960419a78785b1f75a6cca5af0f1f9afe
Denis Argatski <denis.argatski@gmail.com>
2023-04-05 18:07:05 +0300

    SpaceMissiles.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 14 Aug 2022/BusinessLogic/Models/Weapons/Entities/SpaceMissiles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.Weapons.Entities
+{
+    public class SpaceMissiles : Weapon
+    {
+        private const double spacePrice = 8.75;
+        public SpaceMissiles(int destructionLevel) : base(destructionLevel, spacePrice)
+        {
+        }
+    }
+}
