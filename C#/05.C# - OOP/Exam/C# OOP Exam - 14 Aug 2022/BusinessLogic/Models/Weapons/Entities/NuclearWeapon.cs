9e2e21a00856cbbdbc8eeb0555d4f5f9796d4e89
Denis Argatski <denis.argatski@gmail.com>
2023-04-05 18:06:53 +0300

    Nuc

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 14 Aug 2022/BusinessLogic/Models/Weapons/Entities/NuclearWeapon.cs
@@ -0,0 +1,11 @@
+﻿namespace PlanetWars.Models.Weapons.Entities
+{
+    public class NuclearWeapon : Weapon
+    {
+        private const double nuclearPrice = 15.0;
+
+        public NuclearWeapon(int destructionLevel) : base(destructionLevel, nuclearPrice)
+        {
+        }
+    }
+}
