c76a226fb15840d57a056e732493ed977ce67a36
Denis Argatski <denis.argatski@gmail.com>
2023-04-05 18:08:33 +0300

    WeaponRepository.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 14 Aug 2022/BusinessLogic/Repositories/Entities/WeaponRepository.cs
@@ -0,0 +1,34 @@
+﻿using PlanetWars.Models.Weapons.Contracts;
+using PlanetWars.Repositories.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlanetWars.Repositories.Entities
+{
+    public class WeaponRepository : IRepository<IWeapon>
+    {
+        private List<IWeapon> weapons;
+
+        public WeaponRepository()
+        {
+            this.weapons = new List<IWeapon>();
+        }
+
+        public IReadOnlyCollection<IWeapon> Models => this.weapons;
+
+        public void AddItem(IWeapon model)
+        {
+            this.weapons.Add(model);
+        }
+
+        public IWeapon FindByName(string name)
+        {
+            return this.weapons.FirstOrDefault(w => w.GetType().Name == name);
+        }
+
+        public bool RemoveItem(string name)
+        {
+            return this.weapons.Remove(weapons.FirstOrDefault(n => n.GetType().Name == name));
+        }
+    }
+}
