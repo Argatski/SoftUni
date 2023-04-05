b22778486d3b63c8764fa952254826ab5742f1a5
Denis Argatski <denis.argatski@gmail.com>
2023-04-05 18:08:16 +0300

    UnitRepository.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 14 Aug 2022/BusinessLogic/Repositories/Entities/UnitRepository.cs
@@ -0,0 +1,34 @@
+﻿using PlanetWars.Models.MilitaryUnits.Contracts;
+using PlanetWars.Repositories.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlanetWars.Repositories.Entities
+{
+    public class UnitRepository : IRepository<IMilitaryUnit>
+    {
+        private List<IMilitaryUnit> _units;
+        public UnitRepository()
+        {
+            this._units = new List<IMilitaryUnit>();
+        }
+
+        public IReadOnlyCollection<IMilitaryUnit> Models => this._units;
+
+        public void AddItem(IMilitaryUnit model)
+        {
+            this._units.Add(model);
+        }
+
+        public IMilitaryUnit FindByName(string name)
+        {
+            return this._units.FirstOrDefault(n => n.GetType().Name == name);
+        }
+
+        //TODO .......
+        public bool RemoveItem(string name)
+        {
+            return this._units.Remove(FindByName(name));
+        }
+    }
+}
