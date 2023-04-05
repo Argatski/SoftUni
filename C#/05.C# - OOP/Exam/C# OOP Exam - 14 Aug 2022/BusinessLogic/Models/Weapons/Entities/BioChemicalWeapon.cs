0013fb501aa19eb35687cfebd950b1a932b5b446
Denis Argatski <denis.argatski@gmail.com>
2023-04-05 18:06:38 +0300

    BioChemicalWeapon.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 14 Aug 2022/BusinessLogic/Models/Weapons/Entities/BioChemicalWeapon.cs
@@ -0,0 +1,12 @@
+﻿namespace PlanetWars.Models.Weapons.Entities
+{
+    public class BioChemicalWeapon : Weapon
+    {
+        private const double bioChemicalWeaponPrice = 3.2;
+
+        public BioChemicalWeapon(int destructionLevel) : base(destructionLevel, bioChemicalWeaponPrice)
+        {
+        }
+    }
+}
+
