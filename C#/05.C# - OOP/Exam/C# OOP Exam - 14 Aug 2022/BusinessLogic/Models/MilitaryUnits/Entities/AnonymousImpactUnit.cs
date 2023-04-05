c49826875d23c36619126aca2b7e28af282a5cc6
Denis Argatski <denis.argatski@gmail.com>
2023-04-05 18:04:40 +0300

    AnonymousImpactUnit.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 14 Aug 2022/BusinessLogic/Models/MilitaryUnits/Entities/AnonymousImpactUnit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.MilitaryUnits.Entities
+{
+    public class AnonymousImpactUnit : MilitaryUnit
+    {
+        private const double costAnonymous = 30;
+        public AnonymousImpactUnit() : base(costAnonymous)
+        {
+        }
+    }
+}
+ 
