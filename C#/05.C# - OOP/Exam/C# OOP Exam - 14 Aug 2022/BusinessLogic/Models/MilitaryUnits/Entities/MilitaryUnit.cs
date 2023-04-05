9c3c8a3cf319b58041f476a6bc4d7132ae44c3fc
Denis Argatski <denis.argatski@gmail.com>
2023-04-05 18:04:53 +0300

    MilitaryUnit.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 14 Aug 2022/BusinessLogic/Models/MilitaryUnits/Entities/MilitaryUnit.cs
@@ -0,0 +1,36 @@
+﻿using PlanetWars.Models.MilitaryUnits.Contracts;
+using PlanetWars.Utilities.Messages;
+using System;
+
+namespace PlanetWars.Models.MilitaryUnits.Entities
+{
+    public abstract class MilitaryUnit : IMilitaryUnit
+    {
+
+        private double cost;
+        private int enduranceLevelPoint;
+
+        protected MilitaryUnit(double cost)
+        {
+            this.Cost = cost;
+            this.enduranceLevelPoint = 1;
+        }
+
+        public double Cost
+        {
+            get => this.cost;
+            private set => this.cost = value;
+        }
+
+        public int EnduranceLevel => this.enduranceLevelPoint;
+
+        public void IncreaseEndurance()
+        {
+            if (this.EnduranceLevel == 20)
+            {
+                throw new ArgumentException(ExceptionMessages.EnduranceLevelExceeded);
+            }
+            this.enduranceLevelPoint++;
+        }
+    }
+}
