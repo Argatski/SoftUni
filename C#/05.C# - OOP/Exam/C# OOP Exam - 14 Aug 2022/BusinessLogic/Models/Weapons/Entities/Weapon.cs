5895f39443d8f8c6c45edb8ebc5aa0aeac81dd97
Denis Argatski <denis.argatski@gmail.com>
2023-04-05 18:07:20 +0300

    Wepon.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 14 Aug 2022/BusinessLogic/Models/Weapons/Entities/Weapon.cs
@@ -0,0 +1,44 @@
+﻿using PlanetWars.Models.Weapons.Contracts;
+using PlanetWars.Utilities.Messages;
+using System;
+
+namespace PlanetWars.Models.Weapons.Entities
+{
+    public abstract class Weapon : IWeapon
+    {
+        private const int minDestructionLevel = 1;
+        private const int maxDestructionLevel = 10;
+
+        private int destructionLevel;
+        private double price;
+
+        protected Weapon(int destructionLevel, double price)
+        {
+            Price = price;
+            DestructionLevel = destructionLevel;
+        }
+
+        public double Price
+        {
+            get => this.price;
+            private set => this.price = value;
+        }
+
+        public int DestructionLevel
+        {
+            get { return destructionLevel; }
+            private set
+            {
+                if (value < minDestructionLevel)
+                {
+                    throw new ArgumentException(ExceptionMessages.TooLowDestructionLevel);
+                }
+                else if (value > maxDestructionLevel)
+                {
+                    throw new ArgumentException(ExceptionMessages.TooHighDestructionLevel);
+                }
+                destructionLevel = value;
+            }
+        }
+    }
+}
