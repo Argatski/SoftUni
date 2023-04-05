847f828b8858995d39debe5866a050810422b917
Denis Argatski <denis.argatski@gmail.com>
2023-04-05 18:08:00 +0300

    PlanetRepository.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 14 Aug 2022/BusinessLogic/Repositories/Entities/PlanetRepository.cs
@@ -0,0 +1,36 @@
+﻿using PlanetWars.Models.Planets.Contracts;
+using PlanetWars.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlanetWars.Repositories.Entities
+{
+    internal class PlanetRepository : IRepository<IPlanet>
+    {
+        private List<IPlanet> _planets;
+
+        public PlanetRepository()
+        {
+            this._planets = new List<IPlanet>();
+        }
+
+        public IReadOnlyCollection<IPlanet> Models => throw new NotImplementedException();
+
+        public void AddItem(IPlanet model)
+        {
+            this._planets.Add(model);
+        }
+
+        public IPlanet FindByName(string name)
+        {
+            return this._planets.FirstOrDefault(p => p.Name == name);
+        }
+
+        public bool RemoveItem(string name)
+        {
+            return this._planets.Remove(FindByName(name));
+        }
+    }
+}
