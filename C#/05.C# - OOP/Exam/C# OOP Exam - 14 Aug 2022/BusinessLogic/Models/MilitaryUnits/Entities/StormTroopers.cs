00a7f48f38014368b696a4097d6c0f62046e07ba
Denis Argatski <denis.argatski@gmail.com>
2023-04-05 18:05:24 +0300

    Stor

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 14 Aug 2022/BusinessLogic/Models/MilitaryUnits/Entities/StormTroopers.cs
@@ -0,0 +1,10 @@
+﻿namespace PlanetWars.Models.MilitaryUnits.Entities
+{
+    public class StormTroopers : MilitaryUnit
+    {
+        private const double costStorm = 2.5;
+        public StormTroopers() : base(costStorm)
+        {
+        }
+    }
+}
