e855f87123683899c2b0d0543ee74ac464bb072d
Denis Argatski <denis.argatski@gmail.com>
2023-04-15 17:45:16 +0300

    "Delicacy.cs"

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 10 December 2022/Solution/Models/Delicacies/Entity/Delicacy.cs
@@ -0,0 +1,49 @@
+﻿using ChristmasPastryShop.Models.Delicacies.Contracts;
+using ChristmasPastryShop.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Models.Delicacies.Entity
+{
+    public abstract class Delicacy : IDelicacy
+    {
+        private string name;
+        private double price;
+
+        public Delicacy(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            protected set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.NameNullOrWhitespace);
+                }
+                name = value;
+            }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            protected set
+            {
+                price = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder strb = new StringBuilder();
+            strb.AppendLine($"{Name} - {Price:f2} lv");
+            return strb.ToString();
+        }
+    }
+}
