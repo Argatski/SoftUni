3539001e5297ee14fc11d54be8e58919d741deb9
Denis Argatski <denis.argatski@gmail.com>
2023-04-15 17:45:49 +0300

    Gingerbread.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 10 December 2022/Solution/Models/Delicacies/Entity/Gingerbread.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Models.Delicacies.Entity
+{
+    public class Gingerbread : Delicacy
+    {
+        private const double ConstGingerBread = 4.00;
+        public Gingerbread(string name) : base(name, ConstGingerBread)
+        {
+        }
+    }
+}
