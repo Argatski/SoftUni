d95fb0e323c95f1926909ab2157a7eb6c485b7ea
Denis Argatski <denis.argatski@gmail.com>
2023-04-15 17:46:17 +0300

    Stolen.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 10 December 2022/Solution/Models/Delicacies/Entity/Stolen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Models.Delicacies.Entity
+{
+    public class Stolen : Delicacy
+    {
+        private const double ConstStolen = 3.50;
+        public Stolen(string name) : base(name, ConstStolen)
+        {
+        }
+    }
+}
