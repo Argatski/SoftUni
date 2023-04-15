66ca6a62bb4722fd8508cecc73ca735e4fec2718
Denis Argatski <denis.argatski@gmail.com>
2023-04-15 17:42:41 +0300

    Hibernation.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 10 December 2022/Solution/Models/Cocktails/Entity/Hibernation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Models.Cocktails.Entity
+{
+    public class Hibernation : Cocktail
+    {
+        private const double LargeHibernation = 10.50;
+
+        public Hibernation(string cocktailName, string size) : base(cocktailName, size, LargeHibernation)
+        {
+        }
+    }
+}
