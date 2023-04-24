9ddcb6dd9b3b072e28fa03487c028755da2091a0
Denis Argatski <denis.argatski@gmail.com>
2023-04-24 18:31:28 +0300

    DelicacyRepository.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 10 December 2022/Solution/Repositories/Entity/DelicacyRepository.cs
@@ -0,0 +1,25 @@
+﻿using ChristmasPastryShop.Models.Delicacies.Contracts;
+using ChristmasPastryShop.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Repositories.Entity
+{
+    public class DelicacyRepository : IRepository<IDelicacy>
+    {
+        private readonly List<IDelicacy> _delicacies;
+
+        public DelicacyRepository()
+        {
+            this._delicacies = new List<IDelicacy>();
+        }
+
+        public IReadOnlyCollection<IDelicacy> Models => this._delicacies;
+
+        public void AddModel(IDelicacy model)
+        {
+            this._delicacies.Add(model);
+        }
+    }
+}
