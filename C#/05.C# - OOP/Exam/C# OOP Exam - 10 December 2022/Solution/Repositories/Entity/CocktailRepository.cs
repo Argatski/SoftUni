b70a46aaaa15c1fee86a9e236d744148098361cb
Denis Argatski <denis.argatski@gmail.com>
2023-04-24 18:31:09 +0300

    CocktailRepository.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 10 December 2022/Solution/Repositories/Entity/CocktailRepository.cs
@@ -0,0 +1,19 @@
+﻿using ChristmasPastryShop.Models.Cocktails.Contracts;
+using ChristmasPastryShop.Models.Cocktails.Entity;
+using ChristmasPastryShop.Repositories.Contracts;
+using System.Collections.Generic;
+
+namespace ChristmasPastryShop.Repositories.Entity
+{
+    public class CocktailRepository : IRepository<ICocktail>
+    {
+        private List<ICocktail> _cocktails = new List<ICocktail>();
+
+        public IReadOnlyCollection<ICocktail> Models => this._cocktails;
+
+        public void AddModel(ICocktail model)
+        {
+            this._cocktails.Add(model);
+        }
+    }
+}
