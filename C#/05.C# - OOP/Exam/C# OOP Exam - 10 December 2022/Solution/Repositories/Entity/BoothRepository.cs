a37e6d100321e6b5cb2a49902a90239c2d0055c0
Denis Argatski <denis.argatski@gmail.com>
2023-04-24 18:30:33 +0300

    BoothRepository.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam - 10 December 2022/Solution/Repositories/Entity/BoothRepository.cs
@@ -0,0 +1,25 @@
+﻿using ChristmasPastryShop.Models.Booths.Contracts;
+using ChristmasPastryShop.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Repositories.Entity
+{
+    public class BoothRepository : IRepository<IBooth>
+    {
+        private readonly List<IBooth> _booths;
+
+        public BoothRepository()
+        {
+            this._booths = new List<IBooth>();
+        }
+
+        public IReadOnlyCollection<IBooth> Models => this._booths;
+
+        public void AddModel(IBooth model)
+        {
+            this._booths.Add(model);
+        }
+    }
+}
