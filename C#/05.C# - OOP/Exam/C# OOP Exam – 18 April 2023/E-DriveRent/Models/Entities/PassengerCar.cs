a7bf2c0d257c9855b4b6e9f78d723064a8e045f8
Denis Argatski <denis.argatski@gmail.com>
2023-04-30 23:28:17 +0300

    PassengerCar.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam – 18 April 2023/E-DriveRent/Models/Entities/PassengerCar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDriveRent.Models.Entities
+{
+    public class PassengerCar : Vehicle
+    {
+        private const double _maxMileage = 450;
+        public PassengerCar(string brand, string model, string licensePlateNumber)
+            : base(brand, model, _maxMileage, licensePlateNumber)
+        {
+        }
+    }
+}
