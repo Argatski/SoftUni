b177b8ead4b1472db43d9f1c69c6122e4fde8d60
Denis Argatski <denis.argatski@gmail.com>
2023-04-30 23:30:07 +0300

    RouteRepository.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam – 18 April 2023/E-DriveRent/Repositories/Entities/RouteRepository.cs
@@ -0,0 +1,30 @@
+﻿using EDriveRent.Models.Contracts;
+using EDriveRent.Repositories.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EDriveRent.Repositories.Entities
+{
+    public class RouteRepository : IRepository<IRoute>
+    {
+        private List<IRoute> routes;
+        public RouteRepository()
+        {
+            routes = new List<IRoute>();
+        }
+
+        public void AddModel(IRoute model)
+        {
+            routes.Add(model);
+        }
+
+        public IRoute FindById(string identifier) => routes.FirstOrDefault(r => r.RouteId == int.Parse(identifier));
+
+
+        public IReadOnlyCollection<IRoute> GetAll() => routes;
+
+
+        public bool RemoveById(string identifier) => routes.Remove(FindById(identifier));
+
+    }
+}
