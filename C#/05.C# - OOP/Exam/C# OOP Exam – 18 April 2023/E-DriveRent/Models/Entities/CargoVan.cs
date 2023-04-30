0497aa1d3a8df62fc8917733713a87df5c64fa91
Denis Argatski <denis.argatski@gmail.com>
2023-04-30 23:28:00 +0300

    CargoVan.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam – 18 April 2023/E-DriveRent/Models/Entities/CargoVan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDriveRent.Models.Entities
+{
+    public class CargoVan : Vehicle
+    {
+        private const int _maxMileage = 180;
+        public CargoVan(string brand, string model, string licensePlateNumber)
+            : base(brand, model, _maxMileage, licensePlateNumber)
+        {
+        }
+    }
+}
