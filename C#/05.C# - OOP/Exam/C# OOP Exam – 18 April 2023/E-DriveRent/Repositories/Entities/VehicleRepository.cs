d4ea04b38b02099a51817a42ecd615f5f23a6c72
Denis Argatski <denis.argatski@gmail.com>
2023-04-30 23:30:53 +0300

    VehicleRepository.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam – 18 April 2023/E-DriveRent/Repositories/Entities/VehicleRepository.cs
@@ -0,0 +1,27 @@
+﻿using EDriveRent.Models.Contracts;
+using EDriveRent.Repositories.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EDriveRent.Repositories.Entities
+{
+    public class VehicleRepository : IRepository<IVehicle>
+    {
+        private List<IVehicle> vehicles;
+
+        public VehicleRepository()
+        {
+            vehicles = new List<IVehicle>();
+        }
+
+        public void AddModel(IVehicle model)
+        {
+            vehicles.Add(model);
+        }
+
+        public IVehicle FindById(string identifier) => vehicles.FirstOrDefault(u => u.LicensePlateNumber == identifier);
+        public IReadOnlyCollection<IVehicle> GetAll() => vehicles;
+        public bool RemoveById(string identifier) => vehicles.Remove(FindById(identifier));
+
+    }
+}
