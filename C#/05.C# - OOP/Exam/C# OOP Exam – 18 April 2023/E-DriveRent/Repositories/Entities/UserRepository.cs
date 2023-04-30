a8dffb3840a4ffe092c86c5a139d9ae5b9321fd3
Denis Argatski <denis.argatski@gmail.com>
2023-04-30 23:30:30 +0300

    UserRepository.cs

---
+++ C#/05.C# - OOP/Exam/C# OOP Exam – 18 April 2023/E-DriveRent/Repositories/Entities/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using EDriveRent.Models.Contracts;
+using EDriveRent.Repositories.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EDriveRent.Repositories.Entities
+{
+    public class UserRepository : IRepository<IUser>
+    {
+        private List<IUser> users;
+
+        public UserRepository()
+        {
+            users = new List<IUser>();
+        }
+
+        public void AddModel(IUser model)
+        {
+            users.Add(model);
+        }
+
+        public IUser FindById(string identifier) => users.FirstOrDefault(u => u.DrivingLicenseNumber == identifier);
+
+
+        public IReadOnlyCollection<IUser> GetAll() => users;
+
+        public bool RemoveById(string identifier) => users.Remove(FindById(identifier));
+
+
+    }
+}
