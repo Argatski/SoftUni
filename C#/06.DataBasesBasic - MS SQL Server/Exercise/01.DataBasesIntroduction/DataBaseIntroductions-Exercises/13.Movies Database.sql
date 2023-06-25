940d7d99c518a17569853b3c1a35a44303cb1e03
Denis Argatski <denis.argatski@gmail.com>
2023-06-25 23:03:14 +0300

    "Update"

--- C#/06.DataBasesBasic - MS SQL Server/Exercise/01.DataBasesIntroduction/DataBaseIntroductions-Exercises/13.Movies Database.sql
+++ C#/06.DataBasesBasic - MS SQL Server/Exercise/01.DataBasesIntroduction/DataBaseIntroductions-Exercises/13.Movies Database.sql
@@ -1,151 +1,68 @@
 CREATE DATABASE Movies
 GO
 
-USE Movies
+USE Movies 
 
-
---•	Directors (Id, DirectorName, Notes)
-CREATE TABLE Directors
+CREATE TABLE Directors 
 (
-	Id INT NOT NULL PRIMARY KEY,
-	DirectorName NVARCHAR(50) NOT NULL,
+	Id INT PRIMARY KEY IDENTITY,
+	DirectorName NVARCHAR(30) UNIQUE NOT NULL,
 	Notes NVARCHAR(MAX)
-);
+)
 
-INSERT INTO Directors(Id,DirectorName)
-VALUES
-(
-	1,
-	'Director One'
-),
-(
-	2,
-	'Director Two'
-),
-(
-	3,
-	'Director Three'
-),
-(
-	4,
-	'Director Four'
-),
-(
-	5,
-	'Director Five'
-);
-
---•	Genres (Id, GenreName, Notes)
 CREATE TABLE Genres
 (
-	Id INT NOT NULL PRIMARY KEY,
-	GenreName NVARCHAR(50) NOT NULL,
-	Notes NVARCHAR (MAX)
+	Id INT PRIMARY KEY IDENTITY,
+	GenreName NVARCHAR(30) UNIQUE NOT NULL,
+	Notes NVARCHAR(MAX)
 )
 
-INSERT INTO Genres(Id,GenreName)
-VALUES
-(
-	1,
-	'Genre One'
-),
-(
-	2,
-	'Genre Two'
-),
-(
-	3,
-	'Genre Three'
-),
-(
-	4,
-	'Genre Four'
-),
-(
-	5,
-	'Genre Five'
-);
-
---•	Categories (Id, CategoryName, Notes)
 CREATE TABLE Categories
 (
-	Id INT NOT NULL  PRIMARY KEY,
-	CategoryName NVARCHAR (50) NOT NULL,
-	Notes NVARCHAR (MAX)
+	Id INT PRIMARY KEY IDENTITY,
+	CategoryName NVARCHAR(30) UNIQUE NOT NULL,
+	Notes NVARCHAR(MAX)
 )
 
-INSERT INTO Categories(Id,CategoryName)
-VALUES
-(
-       1,
-       'Category One'
-),
-(
-       2,
-       'Category Two'
-),
-(
-       3,
-       'Category Three'
-),
-(
-       4,
-       'Category Four'
-),
-(
-       5,
-       'Category Five'
-);
-
---•	Movies (Id, Title, DirectorId, CopyrightYear, Length, GenreId, CategoryId, Rating, Notes)
 CREATE TABLE Movies
 (
-	Id INT NOT NULL PRIMARY KEY,
-	Title NVARCHAR(255) NOT NULL,
+	Id INT PRIMARY KEY IDENTITY,
+	Title NVARCHAR(30) UNIQUE NOT NULL,
 	DirectorId INT FOREIGN KEY REFERENCES Directors(Id),
-	CopyrightYear INT,
-	Length  NVARCHAR(50),
+	CopyrightYear DATE,
+	[Length] TIME NOT NULL,
 	GenreId INT FOREIGN KEY REFERENCES Genres(Id),
 	CategoryId INT FOREIGN KEY REFERENCES Categories(Id),
-	Rating INT,
-	Notes NVARCHAR (MAX)
-);
+	Rating DECIMAL(2, 1) NOT NULL,
+	Notes NVARCHAR(MAX)
+)
 
-INSERT INTO Movies(Id,Title,DirectorId,GenreId,CategoryId)
+INSERT INTO Directors(DirectorName, Notes)
+VALUES		('Pesho', NULL),
+		('Gosho', NULL),
+		('Kiro', NULL),
+		('Ivan', 'Hi!!!'),
+		('Test', 'How are you?')
+
+INSERT INTO Genres(GenreName, Notes)
+VALUES		('Romantic', NULL),
+		('Drama', NULL),
+		('Action', NULL),
+		('Thriller', NULL),
+		('Comedy', NULL)
+
+INSERT INTO Categories(CategoryName, Notes)
+VALUES		('Category1', NULL),
+		('Category2', NULL),
+		('Category3', NULL),
+		('Category4', NULL),
+		('Category5', NULL)
+
+INSERT INTO Movies
+			(Title, DirectorId, CopyrightYear, [Length], GenreId, CategoryId, Rating, Notes)
 VALUES
-(
-       1,
-       'Title One',
-       2,
-       3,
-       4
-),
-(
-       2,
-       'Title Two',
-       3,
-       4,
-       5
-),
-(
-       3,
-       'Title Three',
-       1,
-       2,
-       3
-),
-(
-       4,
-       'Title Four',
-       5,
-       1,
-       3
-),
-(
-       5,
-       'Title Five',
-       3,
-       5,
-       2
-);
-
+			('The Godfather', 1, '1972', '02:30:00', 1, 1, 9.5, NULL),
+			('Garfield', 2, '2010', '02:30:00', 2, 2, 6.8, NULL),
+			('Geostorm', 3, '2012', '02:30:00', 3, 3, 5.2, NULL),
+			('Guardians of the Galaxy', 4, '2001', '02:30:00', 4, 4, 7.8, NULL),
+			('Ghostbusters', 5, '1984', '02:30:00', 5, 5, 8.4, NULL)
