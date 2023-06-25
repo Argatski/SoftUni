4686210fa0a00ef525632a8132535c3b052911da
Denis Argatski <denis.argatski@gmail.com>
2023-06-25 22:51:43 +0300

    "Update"

--- C#/06.DataBasesBasic - MS SQL Server/Exercise/01.DataBasesIntroduction/DataBaseIntroductions-Exercises/08.Create Table Users.sql
+++ C#/06.DataBasesBasic - MS SQL Server/Exercise/01.DataBasesIntroduction/DataBaseIntroductions-Exercises/08.Create Table Users.sql
@@ -21,3 +21,33 @@ GO
 	
 
 SELECT * FROM Users
+USE Minions
+GO
+
+ALTER TABLE Users
+DROP CONSTRAINT PK_Users;
+
+ALTER TABLE Users
+ADD CONSTRAINT PK_Users PRIMARY KEY (Id,Username);
+GO
+
+--10.Add Check Constraint
+ALTER TABLE Users ADD CONSTRAINT CHK_PasswordLength CHECK (LEN(Password) >=5);
+
+--11.Set Default Value of a Field
+ALTER TABLE Users
+ADD CONSTRAINT DF_LastLoginTime
+DEFAULT GETDATE() FOR LastLoginTime
+
+--12.Set Unique Field
+ALTER TABLE Users
+DROP CONSTRAINT PK_Users
+
+ALTER TABLE Users
+ADD CONSTRAINT PK_Users PRIMARY KEY(Id);
+
+ALTER TABLE Users
+ADD CONSTRAINT UQ_Username UNIQUE (Username);
+
+ALTER TABLE UserS
+ADD CONSTRAINT CHK_User CHECK(LEN(Password)>=3);
