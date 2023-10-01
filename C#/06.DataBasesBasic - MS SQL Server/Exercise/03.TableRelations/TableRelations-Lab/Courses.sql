ade83f78cf51290e0c67d2d4499230f08974442d
Denis Argatski <denis.argatski@gmail.com>
2023-10-01 19:27:19 +0300

    Courses.sql

---
+++ C#/06.DataBasesBasic - MS SQL Server/Exercise/03.TableRelations/TableRelations-Lab/Courses.sql
@@ -0,0 +1,29 @@
+USE [Schooll]
+GO
+
+/****** Object:  Table [dbo].[Courses]    Script Date: 10/1/2023 5:33:39 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Courses](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[CourseName] [nvarchar](50) NOT NULL,
+	[TownId] [int] NOT NULL,
+ CONSTRAINT [PK_Courses] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Courses]  WITH CHECK ADD  CONSTRAINT [FK_Courses_Towns] FOREIGN KEY([TownId])
+REFERENCES [dbo].[Towns] ([Id])
+GO
+
+ALTER TABLE [dbo].[Courses] CHECK CONSTRAINT [FK_Courses_Towns]
+GO
+
+
