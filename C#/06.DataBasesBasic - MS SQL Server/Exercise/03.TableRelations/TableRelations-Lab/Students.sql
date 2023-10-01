fc36800622c989f04ba21f4fe116adfae29a8d71
Denis Argatski <denis.argatski@gmail.com>
2023-10-01 19:28:19 +0300

    Students.sql

---
+++ C#/06.DataBasesBasic - MS SQL Server/Exercise/03.TableRelations/TableRelations-Lab/Students.sql
@@ -0,0 +1,30 @@
+USE [Schooll]
+GO
+
+/****** Object:  Table [dbo].[Students]    Script Date: 10/1/2023 5:35:17 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Students](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[FacultyNumber] [char](6) NOT NULL,
+	[Photo] [varbinary](max) NULL,
+	[DateOfEnlistment] [date] NOT NULL,
+ CONSTRAINT [PK_Students] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Students', @level2type=N'COLUMN',@level2name=N'Id'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Format 012344' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Students', @level2type=N'COLUMN',@level2name=N'FacultyNumber'
+GO
+
+
