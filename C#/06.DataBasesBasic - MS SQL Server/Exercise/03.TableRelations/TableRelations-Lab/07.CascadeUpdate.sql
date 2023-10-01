de8ab63d4c3218233f3b31bf829040b0ff4f3d13
Denis Argatski <denis.argatski@gmail.com>
2023-10-01 19:26:38 +0300

    07.CascadeUpdate.sql

---
+++ C#/06.DataBasesBasic - MS SQL Server/Exercise/03.TableRelations/TableRelations-Lab/07.CascadeUpdate.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Products
+(
+	BarcodeId INT PRIMARY KEY,
+	[Name] VARCHAR(50)
+	
+)
+
+CREATE TABLE Stock
+(
+	Id INT PRIMARY KEY,
+	BarcodeId INT,
+	CONSTRAINT FK_Stock_Products FOREIGN KEY(BarcodeId)
+	REFERENCES Products(BarcodeId) ON UPDATE CASCADE
+)
