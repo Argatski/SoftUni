b57225ad94b49f061b3e7847e150ea169d6928e4
Denis Argatski <denis.argatski@gmail.com>
2023-10-02 22:07:30 +0300

    06.UniversityDatabase.sql

---
+++ C#/06.DataBasesBasic - MS SQL Server/Exercise/03.TableRelations/TableRelations-Exercises/06.UniversityDatabase.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE University 
+
+CREATE TABLE Majors
+(
+	MajorID INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR (50) NOT NULL,
+)
+
+CREATE TABLE Students
+(
+	StudentID INT PRIMARY KEY IDENTITY,
+	StudentNumber INT NOT NULL,
+	StudentName NVARCHAR(50) NOT NULL,
+	MajorID  INT NOT NULL,
+
+	CONSTRAINT FK_Students_MajorID
+	FOREIGN KEY (MajorID) REFERENCES Majors(MajorID)
+)
+
+CREATE TABLE Payments
+(
+	PaymentID INT PRIMARY KEY IDENTITY,
+	PaymentDate DATE NOT NULL,
+	PaymentAnount DECIMAL(15,2) NOT NULL,
+	StudentID INT NOT NULL,
+
+	CONSTRAINT FK_Payments_StudentID
+	FOREIGN KEY (StudentID) REFERENCES Students(StudentID)
+)
+
+CREATE TABLE Subjects
+(
+	SubjectID INT PRIMARY KEY IDENTITY,
+	SubjectName NVARCHAR(50) NOT NULL,
+)
+
+CREATE TABLE Agenda
+(
+	StudentID INT NOT NULL,
+	SubjectID INT NOT NULL,
+
+	CONSTRAINT PK_StudentsSubjects
+	PRIMARY KEY(StudentID,SubjectID),
+
+	CONSTRAINT FK_Agenda_StudentID 
+	FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
+
+	CONSTRAINT FK_Agenda_SubjectID
+	FOREIGN KEY (SubjectID) REFERENCES SubjectS(SubjectID)
+)
