37b74e09e0dbe4654d2105db1fef6fe5cf926ac9
Denis Argatski <denis.argatski@gmail.com>
2023-10-09 22:03:43 +0300

    Part III.sql

--- C#/06.DataBasesBasic - MS SQL Server/Exercise/04.BUILT-IN FUNCTIONS/04.BUILT-IN FUNCTIONS-Exercise/Part III.sql
+++ C#/06.DataBasesBasic - MS SQL Server/Exercise/04.BUILT-IN FUNCTIONS/04.BUILT-IN FUNCTIONS-Exercise/Part III.sql
@@ -7,3 +7,16 @@ SELECT TOP(50) [Name],
   ORDER BY [Start],[Name]
 
 /*Problem 15.User Email Providers*/
+SELECT Username,
+		SUBSTRING(Email,CHARINDEX('@',Email)+1,LEN(Email))AS [Email Provider]
+	FROM Users
+	ORDER BY [Email Provider], Username
+
+/*Problem 16.Get Users with IPAdress Like Pattern*/
+SELECT  Username,
+	IpAddress AS [IP Address]
+	FROM Users
+	WHERE IpAddress LIKE '___.1%.%.___'
+	ORDER BY Username
+
+/*Problem 17.Show All Games with Duration and Part of the Day*/
