2d97aa540d512fb527153e3e37e79a2e35175a72
Denis Argatski <denis.argatski@gmail.com>
2023-10-10 22:05:15 +0300

    Part IV.sql

---
+++ C#/06.DataBasesBasic - MS SQL Server/Exercise/04.BUILT-IN FUNCTIONS/04.BUILT-IN FUNCTIONS-Exercise/Part IV.sql
@@ -0,0 +1,31 @@
+/*Problem 18.Orders Table*/
+USE Orders
+SELECT ProductName,
+	   OrderDate,
+	   DATEADD(DAY,3,OrderDate)
+	   AS [Pay Due],
+	   DATEADD(MONTH,1,OrderDate)
+	   AS [Deliver Due]
+	FROM Orders
+
+/*Problem 19.People Table*/
+CREATE TABLE People
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(30) NOT NULL,
+	Birthdate DATETIME NOT NULL
+)
+
+INSERT INTO People([Name],Birthdate)
+VALUES      ('Victor', '2000-12-07'),
+            ('Steven', '1992-09-10'),
+			 ('Stephen', '1910-09-19'),
+			('John', '2010-01-06')
+
+SELECT [Name],
+	DATEDIFF(YEAR,Birthdate,GETDATE()) AS [Age in Years],
+	DATEDIFF(MONTH,Birthdate,GETDATE()) AS [Age in Months],
+	DATEDIFF(DAY,Birthdate,GETDATE()) AS [Age in Days],
+	DATEDIFF(MINUTE,Birthdate,GETDATE()) AS [Age in Minutes]
+	FROM People
+	
