ddf4ae50a1943213d7fd87fa753ace0219be0bef
Denis Argatski <denis.argatski@gmail.com>
2023-10-15 17:48:19 +0300

    07.EmployeesWithProject.sql

---
+++ C#/06.DataBasesBasic - MS SQL Server/Exercise/05.SubqeruesAndJoins/05.SubqeruesAndJoins-Exercise/07.EmployeesWithProject.sql
@@ -0,0 +1,13 @@
+USE SoftUni
+/*Problem 7.Employees with Project*/
+SELECT TOP(5) e.EmployeeID,
+	   e.FirstName,
+	   p.[Name] AS [ProjectName]
+	FROM Employees AS e
+	JOIN EmployeesProjects AS ep
+	ON e.EmployeeID = ep.EmployeeID
+	JOIN Projects AS p
+	ON P.ProjectID = ep.ProjectID
+	WHERE P.StartDate> '2002-08-13'
+	AND p.EndDate IS NULL
+	ORDER BY e.EmployeeID
