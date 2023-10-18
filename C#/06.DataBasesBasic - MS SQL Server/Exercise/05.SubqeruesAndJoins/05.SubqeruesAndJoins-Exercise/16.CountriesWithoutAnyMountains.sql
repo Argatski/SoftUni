6ff77c00230eb2ba6f0edd88233d38afabb73c41
Denis Argatski <denis.argatski@gmail.com>
2023-10-18 22:04:26 +0300

    16.CountriesWithoutAnyMountains.sql

---
+++ C#/06.DataBasesBasic - MS SQL Server/Exercise/05.SubqeruesAndJoins/05.SubqeruesAndJoins-Exercise/16.CountriesWithoutAnyMountains.sql
@@ -0,0 +1,11 @@
+USE Geography
+/*Problem 16.Countries without any Mountains*/
+SELECT COUNT(c.CountryCode) AS [CountryCode]
+  FROM (   SELECT c.CountryCode
+             FROM Countries AS c 
+        LEFT JOIN MountainsCountries AS mc
+               ON mc.CountryCode = c.CountryCode
+        LEFT JOIN Mountains AS m
+               ON m.Id = mc.MountainId
+            WHERE m.Id IS NULL
+         GROUP BY c.CountryCode) AS c
