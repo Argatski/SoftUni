1b29accc559b52899567324aa730daba0605841a
Denis Argatski <denis.argatski@gmail.com>
2023-10-23 21:41:31 +0300

    11.AverageInterest.sql

---
+++ C#/06.DataBasesBasic - MS SQL Server/Exercise/06.IndicesAndDataAggregation/11.AverageInterest.sql
@@ -0,0 +1,12 @@
+USE Gringotts
+/*11.Average Interest*/
+
+SELECT DepositGroup
+		,IsDepositExpired
+		,AVG(DepositInterest) AS [AverageInterest]
+	FROM WizzardDeposits
+	WHERE DepositStartDate > '1985-01-01'
+	GROUP BY DepositGroup,
+			IsDepositExpired
+	ORDER BY DepositGroup DESC,
+			IsDepositExpired ASC
