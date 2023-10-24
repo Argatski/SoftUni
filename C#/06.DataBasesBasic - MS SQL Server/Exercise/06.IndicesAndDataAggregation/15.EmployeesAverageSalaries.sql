b6f46dc119d7a7479428eb4552b5efd30b013374
Denis Argatski <denis.argatski@gmail.com>
2023-10-24 20:29:23 +0300

    15.EmployeesAverageSalaries.sql

---
+++ C#/06.DataBasesBasic - MS SQL Server/Exercise/06.IndicesAndDataAggregation/15.EmployeesAverageSalaries.sql
@@ -0,0 +1,18 @@
+USE SoftUni
+/*15. Employees Average Salaries*/
+SELECT *
+	INTO EmployeesAverageSalaries
+	FROM Employees
+	WHERE Salary > 30000
+
+DELETE FROM EmployeesAverageSalaries
+WHERE ManagerID =  42
+
+UPDATE EmployeesAverageSalaries
+	SET Salary +=5000
+	WHERE DepartmentID = 1
+
+SELECT DepartmentID,
+		AVG(Salary) AS [AverageSalary]
+	FROM EmployeesAverageSalaries
+	GROUP BY DepartmentID
