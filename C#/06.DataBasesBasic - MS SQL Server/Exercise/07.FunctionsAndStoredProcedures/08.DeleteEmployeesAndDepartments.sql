0d527723d65b39eb53feb094307cc6f22eb5b45a
Denis Argatski <denis.argatski@gmail.com>
2023-11-05 19:09:03 +0200

    08.DeleteEmployeesAndDepartments.sql

---
+++ C#/06.DataBasesBasic - MS SQL Server/Exercise/07.FunctionsAndStoredProcedures/08.DeleteEmployeesAndDepartments.sql
@@ -0,0 +1,42 @@
+USE SoftUni
+/*Problem 8. * Delete Employees and Departments*/
+CREATE PROC usp_DeleteEmployeesFromDepartment (@departmentId INT) 
+AS
+	BEGIN
+		DELETE FROM EmployeesProjects
+		WHERE EmployeeID IN 
+			(
+			SELECT EmployeeID 
+				FROM Employees
+				WHERE DepartmentID = @departmentId
+			)
+		
+		UPDATE Employees
+			SET ManagerID = NULL
+			WHERE ManagerID IN 
+					(
+						SELECT EmployeeID
+						FROM Employees
+						WHERE DepartmentID=  @departmentId
+					)
+
+		ALTER TABLE Departments
+		ALTER COLUMN ManagerId INT
+
+		UPDATE Departments
+		SET ManagerID =  NULL
+		WHERE DepartmentID =  @departmentId
+
+		DELETE FROM Employees
+		WHERE DepartmentID =  @departmentId
+
+		DELETE FROM Departments
+		WHERE DepartmentID = @departmentId
+
+		SELECT COUNT(*)
+		FROM Employees
+		WHERE DepartmentID = @departmentId
+	END
+
+SELECT *
+	FROM Departments
