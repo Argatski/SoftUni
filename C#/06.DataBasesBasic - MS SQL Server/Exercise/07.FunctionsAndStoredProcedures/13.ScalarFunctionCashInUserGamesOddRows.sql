229a23aab81a1c60c255bb99d8c3a9ee4d1c2b25
Denis Argatski <denis.argatski@gmail.com>
2023-11-13 20:50:14 +0200

    13.ScalarFunctionCashInUserGamesOddRows.sql

---
+++ C#/06.DataBasesBasic - MS SQL Server/Exercise/07.FunctionsAndStoredProcedures/13.ScalarFunctionCashInUserGamesOddRows.sql
@@ -0,0 +1,26 @@
+USE Diablo
+
+/*Problem 13. *Scalar Function: Cash in User Games Odd Rows*/
+
+CREATE FUNCTION ufn_CashInUsersGames(@gameName NVARCHAR(MAX))
+RETURNS TABLE
+AS
+RETURN
+		SELECT SUM(r.Cash) AS [SumCash]
+			FROM
+				(
+				SELECT g.[Name],
+						us.Cash,
+						ROW_NUMBER() OVER (PARTITION BY @gameName ORDER BY us.Cash DESC) AS [Row Number]
+						FROM Games AS g
+						JOIN UsersGames AS us
+						ON us.GameId = g.Id
+						WHERE g.[Name] = @gameName 
+				)AS r
+			WHERE r.[Row Number]%2 !=0
+
+
+SELECT SumCash
+	FROM dbo.ufn_CashInUsersGames('Love in a mist')
+
+
