456625366890356ee4609ccc8ae08ba36843ffcd
Denis Argatski <denis.argatski@gmail.com>
2023-11-15 23:12:07 +0200

    01.CreateTableLogs.sql

---
+++ C#/06.DataBasesBasic - MS SQL Server/Exercise/08.TriggersAndTranslations/01.CreateTableLogs.sql
@@ -0,0 +1,24 @@
+USE Bank
+
+/*Problem 14. Create Table Logs*/
+CREATE OR ALTER TRIGGER tr_AccountChanges ON Accounts FOR UPDATE
+AS
+BEGIN
+	DECLARE @accountId INT;
+	DECLARE @oldSum DECIMAL(15, 2);
+	DECLARE @newSum DECIMAL(15, 2);
+
+	SET @accountId = (SELECT i.Id
+		            FROM inserted AS i)
+
+	SET @oldSum = (SELECT d.Balance
+		         FROM deleted AS d)
+
+	SET @newSum = (SELECT i.Balance
+		         FROM inserted AS i)
+
+	INSERT INTO Logs(AccountId, OldSum, NewSum)
+	VALUES		(@accountId, @oldSum, @newSum)
+END
+
+
