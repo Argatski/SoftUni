20dbdd8a687bd82cd64ecea9d42c0c25a35cc87a
Denis Argatski <denis.argatski@gmail.com>
2023-12-02 15:29:08 +0200

    04.Delete.sql

---
+++ C#/06.DataBasesBasic - MS SQL Server/Exam/Databases MSSQL Server Exam - 19 June 2022/04.Delete.sql
@@ -0,0 +1,10 @@
+USE Zoo
+/*04.Delete*/
+DELETE FROM Volunteers 
+WHERE DepartmentId =2
+
+DELETE FROM VolunteersDepartments
+WHERE Id=2
+
+SELECT * FROM VolunteersDepartments
+SELECT * FROM Volunteers
