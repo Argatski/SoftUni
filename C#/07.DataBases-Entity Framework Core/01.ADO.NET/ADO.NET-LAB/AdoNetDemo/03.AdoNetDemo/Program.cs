50f5420aab4d2a00b0974cb0f469cb18ef07cc1e
Denis Argatski <denis.argatski@gmail.com>
2023-12-13 20:04:01 +0200

    Program.cs

---
+++ C#/07.DataBases-Entity Framework Core/01.ADO.NET/ADO.NET-LAB/AdoNetDemo/03.AdoNetDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Data.SqlClient;
+
+internal class Program
+{
+    private const string connection = @"Server=DESKTOP-P22IBOP\MSSQLSERVER_2023;Database=SoftUni;Integrated Security=True;TrustServerCertificate=true";
+
+    private static void Main(string[] args)
+    {
+        using (SqlConnection  sqlConnection = new SqlConnection(connection))
+        {
+            sqlConnection.Open();
+
+            //Command
+            string command = @"SELECT [FirstName],[LastName],[Salary] FROM Employees WHERE FirstName LIKE 'N%'";
+            SqlCommand cmd = new SqlCommand(command, sqlConnection);
+
+            //Read
+            
+
+            ReaderSQL(cmd);
+
+
+            //Update salary
+            SqlCommand updCmd = new SqlCommand("UPDATE Employees SET Salary = Salary * 1.10",sqlConnection);
+
+            int updateRows = updCmd.ExecuteNonQuery();
+
+            Console.WriteLine(updateRows);
+
+            ReaderSQL(cmd);
+          
+
+        }
+        
+    }
+
+    private static void ReaderSQL(SqlCommand cmd)
+    {
+        using (SqlDataReader reader = cmd.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                string firstName = (string)reader[0];
+                string lastName = (string)reader[1];
+                decimal salary = (decimal)reader[2];
+
+                Console.WriteLine(firstName + " " + lastName + " " + salary);
+            }
+
+        }
+    }
+}
