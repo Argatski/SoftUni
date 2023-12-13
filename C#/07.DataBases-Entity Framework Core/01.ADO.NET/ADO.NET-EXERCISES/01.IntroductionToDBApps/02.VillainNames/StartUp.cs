d19f419c19229b6cae57ce10e572a4913e8072a3
Denis Argatski <denis.argatski@gmail.com>
2023-12-13 21:47:27 +0200

    StartUp.cs

---
+++ C#/07.DataBases-Entity Framework Core/01.ADO.NET/ADO.NET-EXERCISES/01.IntroductionToDBApps/02.VillainNames/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using _02.VillainNames;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data.SqlClient.Server;
+
+internal class StartUp
+{
+    private static void Main(string[] args)
+    {
+        using (var connection =  new SqlConnection(Configuration.ConnectionString))
+        {
+            connection.Open();
+
+            string selectQuery= "SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount " +
+                    "FROM Villains AS v JOIN MinionsVillains AS mv ON v.Id = mv.VillainId " +
+                    "GROUP BY v.Id, v.Name " +
+                    "HAVING COUNT(mv.VillainId) > 3" +
+                    "ORDER BY COUNT(mv.VillainId)";
+
+            using (var command =  new SqlCommand(selectQuery,connection))
+            {
+                using (var dataRead = command.ExecuteReader())
+                {
+                    while (dataRead.Read())
+                    {
+                        Console.WriteLine($"{dataRead[0]} - {dataRead[1]}");
+                    }
+                }
+            }
+
+        }
+    }
+}
