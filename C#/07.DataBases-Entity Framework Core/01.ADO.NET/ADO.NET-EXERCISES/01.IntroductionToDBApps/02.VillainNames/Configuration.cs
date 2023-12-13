62057230d4164c55dd71e8c04f321ca3de89b97e
Denis Argatski <denis.argatski@gmail.com>
2023-12-13 21:47:17 +0200

    Configuration.cs

---
+++ C#/07.DataBases-Entity Framework Core/01.ADO.NET/ADO.NET-EXERCISES/01.IntroductionToDBApps/02.VillainNames/Configuration.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace _02.VillainNames
+{
+    public  static class Configuration
+    {
+        public const string ConnectionString = @"Server=DESKTOP-P22IBOP\MSSQLSERVER_2023;Database=MinionsDB;Integrated Security=True;TrustServerCertificate=True";
+    }
+}
