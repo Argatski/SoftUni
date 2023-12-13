8614de7806cef778f118c3075d7057c2da127446
Denis Argatski <denis.argatski@gmail.com>
2023-12-13 22:30:18 +0200

    Queries.cs

---
+++ C#/07.DataBases-Entity Framework Core/01.ADO.NET/ADO.NET-EXERCISES/01.IntroductionToDBApps/03.MinionNames/Queries.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.MinionNames
+{
+   public class Queries
+    {
+        public const string VillainName = @"SELECT [Name] FROM Villains WHERE Id=@Id";
+
+        public const string MinionsNames =
+            @"SELECT ROW_NUMBER() OVER (ORDER BY m.Name) as RowNum,
+                       m.Name, 
+                       m.Age FROM MinionsVIllains AS mv
+            JOIN Minions AS m ON mv.MinionId = m.id WHERE mv.VillainId=@Id
+            ORDER BY m.Name";
+    }
+}
