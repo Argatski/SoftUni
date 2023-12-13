5cc134b6fa5a004ed3f03aefccd87640cba0bb89
Denis Argatski <denis.argatski@gmail.com>
2023-12-13 20:03:24 +0200

    Demo-ExecuteReader.cs

---
+++ C#/07.DataBases-Entity Framework Core/01.ADO.NET/ADO.NET-LAB/AdoNetDemo/02.AdoNetDemo/Demo-ExecuteReader.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Reflection.PortableExecutable;
+
+internal class Program
+{
+    private const string connectionString = @"Server=DESKTOP-P22IBOP\MSSQLSERVER_2023;Database=SoftUni;Integrated Security=True;TrustServerCertificate=true";
+    private static void Main(string[] args)
+    {
+        using (SqlConnection connnect= new SqlConnection(connectionString))
+        {
+            connnect.Open();
+
+            //Command
+            string command = @"SELECT [FirstName], [LastName] FROM Employees
+            WHERE FirstName LIKE 'N%'";
+
+            SqlCommand cmd = new SqlCommand(command, connnect);
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    string firstName = (string)reader[0];
+                    string lastName = (string)reader[1];
+
+                    Console.WriteLine(firstName + " " + lastName);
+
+                }
+            } 
+
+        }
+    }
+}
