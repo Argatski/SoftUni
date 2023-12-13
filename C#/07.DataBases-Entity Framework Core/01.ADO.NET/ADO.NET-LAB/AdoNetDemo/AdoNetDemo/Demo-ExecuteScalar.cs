3db0ca3dc7edc4e9cd8abbc591c3e8a467c11b73
Denis Argatski <denis.argatski@gmail.com>
2023-12-13 20:01:55 +0200

    Demo-ExecuteScalar.cs

---
+++ C#/07.DataBases-Entity Framework Core/01.ADO.NET/ADO.NET-LAB/AdoNetDemo/AdoNetDemo/Demo-ExecuteScalar.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+
+internal class Demo
+{
+    // .
+    // localhost 127.0.0.
+    // DESKTOP-P22IBOP
+    //Connect with server
+    private const string connectionString = @"Server=DESKTOP-P22IBOP\MSSQLSERVER_2023;Database=SoftUni;Integrated Security=True;TrustServerCertificate=true";
+
+   
+    private static void Main(string[] args)
+    {
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            //Open connection
+            connection.Open();
+            
+            //Command to database
+            string command = @"SELECT COUNT(*) FROM Employees";
+
+            SqlCommand sqlCommand = new SqlCommand(command, connection);
+
+            int result = (int)sqlCommand.ExecuteScalar();//USE WHEN WE HAVE ONE CELL
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
