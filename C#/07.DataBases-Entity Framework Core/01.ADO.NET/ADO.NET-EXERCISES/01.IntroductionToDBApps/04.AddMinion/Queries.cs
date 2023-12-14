cc4e68cbb85f62d54c192728c3d182e53ebd93e2
Denis Argatski <denis.argatski@gmail.com>
2023-12-14 21:27:27 +0200

    Queries.cs

---
+++ C#/07.DataBases-Entity Framework Core/01.ADO.NET/ADO.NET-EXERCISES/01.IntroductionToDBApps/04.AddMinion/Queries.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.AddMinion
+{
+    public class Queries
+    {
+        public const string TownId = @"SELECT Id FROM Towns WHERE [Name]=@townName";
+
+        public const string VillainId = @"SELECT Id FROM Villains WHERE [Name]=@Name";
+
+        public const string MinionId = @"SELECT Id FROM Minions WHERE [Name]=@Name";
+
+        public const string InsertMinionsVillains = @"INSERT INTO MinionsVillains (MinionId, VillainId) VALUES (@villainId,@minionId)";
+
+        public const string InsertVillain = @"INSERT INTO Villains([NAME], EvilnessFactorId) VALUES (@villaiName, 4)";
+
+        public const string InsertMinion = @"INSERT INTO Minions ([Name],Age, TownId) VALUES (@name,@age,@townId)";
+
+        public const string InsertTown = @"INSERT INTO Towns ([Name]) VALUES (@townName)";
+    }
+}
