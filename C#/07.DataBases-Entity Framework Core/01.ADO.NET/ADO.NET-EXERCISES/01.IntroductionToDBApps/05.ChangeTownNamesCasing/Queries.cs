368c100d354dafc81c87c2841b0114696f3916a7
Denis Argatski <denis.argatski@gmail.com>
2023-12-14 22:26:13 +0200

    Queries.cs

---
+++ C#/07.DataBases-Entity Framework Core/01.ADO.NET/ADO.NET-EXERCISES/01.IntroductionToDBApps/05.ChangeTownNamesCasing/Queries.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.ChangeTownNamesCasing
+{
+    public class Queries
+    {
+        public const string namesQuery = @"SELECT t.[Name] FROM Towns AS t JOIN Countries AS c ON t.CountryCode=c.Id WHERE c.Name =@countryName";
+
+        public const string updateQuery = @"UPDATE Towns SET Name = UPPER(Name) WHERE CountryCode = (SELECT c.Id FROM Countries AS c WHERE c.Name = @countryName)";
+    }
+}
