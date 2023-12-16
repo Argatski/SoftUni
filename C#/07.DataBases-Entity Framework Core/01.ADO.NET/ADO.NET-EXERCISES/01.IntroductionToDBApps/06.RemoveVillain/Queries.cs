94a4b5943a67bd0ca07e3d9da31e45949aaea70c
Denis Argatski <denis.argatski@gmail.com>
2023-12-16 17:28:06 +0200

    Queries.cs

---
+++ C#/07.DataBases-Entity Framework Core/01.ADO.NET/ADO.NET-EXERCISES/01.IntroductionToDBApps/06.RemoveVillain/Queries.cs
@@ -0,0 +1,15 @@
+﻿
+namespace _06.RemoveVillain
+{
+    public static class Queries
+    {
+        public const string SelectVillainNameById
+            = @"SELECT Name FROM Villains WHERE Id = @villainId";
+
+        public const string DeleteFromMappingTable
+            = @"DELETE FROM MinionsVillains WHERE VillainId = @villainId";
+
+        public const string DeleteFromVillain
+            = @"DELETE FROM Villains WHERE Id = @villainId";
+    }
+}
