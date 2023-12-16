d0bff88227400044ab4a32ce3307f155650720ac
Denis Argatski <denis.argatski@gmail.com>
2023-12-16 21:17:19 +0200

    Queries.cs

---
+++ C#/07.DataBases-Entity Framework Core/01.ADO.NET/ADO.NET-EXERCISES/01.IntroductionToDBApps/09IncreaseAgeStoredProcedure/Queries.cs
@@ -0,0 +1,19 @@
+﻿namespace _09IncreaseAgeStoredProcedure
+{
+    internal class Queries
+    {
+        public const string CreateGetOlderProcedure
+           = @"CREATE OR ALTER PROC usp_GetOlder @id INT
+                AS
+                    UPDATE Minions
+                       SET Age += 1
+                     WHERE Id = @id";
+
+        public const string ExecuteProcedure
+            = @"EXEC usp_GetOlder @id";
+
+        public const string SelectMinionById
+            = @"SELECT Name, Age FROM Minions WHERE Id = @Id";
+
+    }
+}
