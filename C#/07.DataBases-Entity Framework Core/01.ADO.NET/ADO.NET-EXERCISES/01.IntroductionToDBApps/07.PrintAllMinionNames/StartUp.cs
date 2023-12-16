3da58c4cde5b401320a56813afe2de281d32e776
Denis Argatski <denis.argatski@gmail.com>
2023-12-16 17:44:51 +0200

    StartUp.cs

---
+++ C#/07.DataBases-Entity Framework Core/01.ADO.NET/ADO.NET-EXERCISES/01.IntroductionToDBApps/07.PrintAllMinionNames/StartUp.cs
@@ -0,0 +1,60 @@
+﻿using _07.PrintAllMinionNames;
+using Microsoft.Data.SqlClient;
+using System.Security.Cryptography.X509Certificates;
+
+internal class StartUp
+{
+    private static void Main(string[] args)
+    {
+        using (SqlConnection connection =  new SqlConnection(Configurate.ConnectionString))
+        {
+            connection.Open();
+
+            //Get all minion names in list
+            var minionNames = GetAllMinionNames(connection);
+
+
+            //Console.WriteLine(string.Join(", ",minionNames));
+
+            PrintAllMinnionNames(minionNames);
+
+        }
+    }
+
+    private static void PrintAllMinnionNames(List<string> minionNames)
+    {
+       if (minionNames.Count >0)
+        {
+            for (int i = 0; i < minionNames.Count/2; i++)
+            {
+                Console.WriteLine(minionNames[i]);
+                Console.WriteLine(minionNames[minionNames.Count - 1 - i]);
+            }
+            if (minionNames.Count % 2 != 0)
+            {
+                Console.WriteLine(minionNames[minionNames.Count / 2]);
+            }
+        }
+    }
+
+    private static List<string> GetAllMinionNames(SqlConnection connection)
+    {
+        var list =  new List<string>();
+
+
+        using(SqlCommand command = new SqlCommand(Query.selectMinonNames, connection)) 
+        {
+            using (var dataReader =  command.ExecuteReader())
+            {
+                while (dataReader.Read())
+                {
+                    var name = (string)dataReader[0];
+
+                    list.Add(name);
+                }
+            }
+        }
+
+        return list;
+    }
+}
