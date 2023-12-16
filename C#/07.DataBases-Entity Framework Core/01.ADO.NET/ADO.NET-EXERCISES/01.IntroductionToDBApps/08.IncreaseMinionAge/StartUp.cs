521907cd34467ee6b110b5b6a3cceaaef6301ecf
Denis Argatski <denis.argatski@gmail.com>
2023-12-16 21:15:45 +0200

    StartUp.cs

---
+++ C#/07.DataBases-Entity Framework Core/01.ADO.NET/ADO.NET-EXERCISES/01.IntroductionToDBApps/08.IncreaseMinionAge/StartUp.cs
@@ -0,0 +1,53 @@
+﻿using _08.IncreaseMinionAge;
+using Microsoft.Data.SqlClient;
+
+internal class StartUp
+{
+    private static void Main(string[] args)
+    {
+        int[] ids = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+
+        using (SqlConnection connection = new SqlConnection(Configurate.ConnectionString))
+        {
+            connection.Open();
+
+            foreach(int id in ids)
+            {
+                UpdateMinionsById(id, connection);
+            }
+
+            PrintAllMinions(connection);
+        }
+    }
+
+    private static void PrintAllMinions(SqlConnection connection)
+    {
+        using (SqlCommand command =  new SqlCommand(Queries.SelectNameAge,connection))
+        {
+            using (var dataReader =  command.ExecuteReader())
+            {
+                while (dataReader.Read())
+                {
+                    var minonName = (string)dataReader[0];
+                    var minonAge = (int)dataReader[1];
+
+                    Console.WriteLine($"{minonName} {minonAge}");
+                }
+            }
+        }
+    }
+
+    private static void UpdateMinionsById(int id, SqlConnection connection)
+    {
+        using (SqlCommand command =  new SqlCommand(Queries.UpdateMinionById,connection))
+        {
+            command.Parameters.AddWithValue("@Id", id);
+
+            command.ExecuteNonQuery();
+        }
+    }
+}
