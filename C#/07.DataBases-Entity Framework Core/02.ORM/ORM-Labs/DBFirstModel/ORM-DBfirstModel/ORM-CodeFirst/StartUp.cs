ebf1dcf6608d21fb86943578778de4a48ba808df
Denis Argatski <denis.argatski@gmail.com>
2023-12-19 17:37:48 +0200

    StartUp.cs

---
+++ C#/07.DataBases-Entity Framework Core/02.ORM/ORM-Labs/DBFirstModel/ORM-DBfirstModel/ORM-CodeFirst/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using ORM_CodeFirst.Models;
+
+internal class StartUp
+{
+    private static void Main(string[] args)
+    {
+        var dbContext = new StudentDBContext();
+
+        dbContext.Database.EnsureCreated();
+        dbContext.Courses.Add(new Course { Name = "EntityFrameworkCore" });
+        dbContext.Courses.Add(new Course { Name = "SqlServer" });
+       
+        //dbContext.Courses.Remove(new Course { Id = 1 });
+        
+        dbContext.SaveChanges();
+    }
+}
