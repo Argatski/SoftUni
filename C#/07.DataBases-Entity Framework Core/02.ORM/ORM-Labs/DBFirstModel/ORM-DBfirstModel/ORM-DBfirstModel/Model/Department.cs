896fc458a502b13a84bc67c74c1b479f0dcb77be
Denis Argatski <denis.argatski@gmail.com>
2023-12-19 17:34:26 +0200

    Department.cs

---
+++ C#/07.DataBases-Entity Framework Core/02.ORM/ORM-Labs/DBFirstModel/ORM-DBfirstModel/ORM-DBfirstModel/Model/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ORM_DBfirstModel.Model;
+
+public partial class Department
+{
+    public int DepartmentId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int ManagerId { get; set; }
+
+    public virtual ICollection<Employee> Employees { get; set; } = new List<Employee>();
+
+    public virtual Employee Manager { get; set; } = null!;
+}
