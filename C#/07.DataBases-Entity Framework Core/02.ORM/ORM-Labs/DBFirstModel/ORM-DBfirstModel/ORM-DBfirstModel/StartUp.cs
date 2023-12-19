d1aef7c272c5dcc3a71fcc78da023ae81eec7e3a
Denis Argatski <denis.argatski@gmail.com>
2023-12-19 17:37:09 +0200

    StartUp.cs

---
+++ C#/07.DataBases-Entity Framework Core/02.ORM/ORM-Labs/DBFirstModel/ORM-DBfirstModel/ORM-DBfirstModel/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using ORM_DBfirstModel.Model;
+using System.Data.SqlTypes;
+
+internal class StartUp
+{
+    private static void Main(string[] args)
+    {
+        var dbContext = new SoftuniContext();
+        /*
+        var employees =  dbContext.Employees.Where(x => x.Department.Manager.Department.Name == "Sales")
+            .Select(x => new
+            {
+                Name = x.FirstName + ' ' + x.LastName,
+                Department =  x.Department.Name,
+                Manger =  x.Manager.FirstName+ ' ' + x.Manager.LastName
+            });
+
+        foreach (var emp in employees)
+        {
+            Console.WriteLine(emp.Name+ ' - '+emp.Department + ' - ' + emp.Manger);
+
+        }
+        */
+
+        var allEmp = dbContext.Employees.ToList();
+       
+        foreach (var emp in allEmp) 
+        {
+            Console.WriteLine(emp.FirstName + ' ' + emp.LastName + " - " + emp.Salary);
+        }
+    }
+}
