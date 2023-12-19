1a97c1ef76dfffd32a376373c850e202905bcff0
Denis Argatski <denis.argatski@gmail.com>
2023-12-19 17:35:11 +0200

    Town.cs

---
+++ C#/07.DataBases-Entity Framework Core/02.ORM/ORM-Labs/DBFirstModel/ORM-DBfirstModel/ORM-DBfirstModel/Model/Town.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ORM_DBfirstModel.Model;
+
+public partial class Town
+{
+    public int TownId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Address> Addresses { get; set; } = new List<Address>();
+}
