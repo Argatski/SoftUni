30ec906937c6ad6932e63bab00edde68cd3eabf3
Denis Argatski <denis.argatski@gmail.com>
2023-12-19 17:38:43 +0200

    StudentDBContext.cs

---
+++ C#/07.DataBases-Entity Framework Core/02.ORM/ORM-Labs/DBFirstModel/ORM-DBfirstModel/ORM-CodeFirst/Models/StudentDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_CodeFirst.Models
+{
+    internal class StudentDBContext : DbContext
+    {
+        public StudentDBContext() { }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Grade> Grades { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-P22IBOP\MSSQLSERVER_2023;Database=Grades;Integrated Security=true;TrustServerCertificate=true");
+        }
+
+    }
+}
