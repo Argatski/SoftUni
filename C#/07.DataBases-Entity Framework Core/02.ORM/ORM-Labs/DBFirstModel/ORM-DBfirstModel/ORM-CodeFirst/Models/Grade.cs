d3db89e0bc45ec2cf00063afb5536d7103d69d75
Denis Argatski <denis.argatski@gmail.com>
2023-12-19 17:38:18 +0200

    Grade.cs

---
+++ C#/07.DataBases-Entity Framework Core/02.ORM/ORM-Labs/DBFirstModel/ORM-DBfirstModel/ORM-CodeFirst/Models/Grade.cs
@@ -0,0 +1,13 @@
+﻿namespace ORM_CodeFirst.Models
+{
+    internal class Grade
+    {
+        public int Id { get; set; }
+
+        public decimal Value { get; set; }
+
+        public Student? Student { get; set; }
+
+        public Course? Course { get; set; }
+    }
+}
