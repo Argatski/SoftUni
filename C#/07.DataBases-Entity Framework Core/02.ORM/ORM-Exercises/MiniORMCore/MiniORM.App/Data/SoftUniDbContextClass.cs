a74d393b8c554c00a0145b55a9c5b8d37ae2e3f7
Denis Argatski <denis.argatski@gmail.com>
2023-12-25 11:36:14 +0200

    SoftUniDbContextClass.cs

---
+++ C#/07.DataBases-Entity Framework Core/02.ORM/ORM-Exercises/MiniORMCore/MiniORM.App/Data/SoftUniDbContextClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniORM.App.Data
+{
+    using Entities;
+    internal class SoftUniDbContextClass:DbContext
+    {
+        public SoftUniDbContextClass(string connectionString):base(connectionString) { }
+        
+        public DbSet<Employee> Employees { get; }
+
+        public DbSet<Departments> Departments { get; }
+
+        public DbSet<Project> Projects { get; }
+
+        public DbSet<EmployeesProject> EmployeesProjects { get; }
+    }
+}
