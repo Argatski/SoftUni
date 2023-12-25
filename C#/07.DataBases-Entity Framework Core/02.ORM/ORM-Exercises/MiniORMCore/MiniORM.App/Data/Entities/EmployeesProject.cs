bafd26bf5fb07538c2ce16747b9abb33a8778b67
Denis Argatski <denis.argatski@gmail.com>
2023-12-25 11:35:40 +0200

    EmployeesProject.cs

---
+++ C#/07.DataBases-Entity Framework Core/02.ORM/ORM-Exercises/MiniORMCore/MiniORM.App/Data/Entities/EmployeesProject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniORM.App.Data.Entities
+{
+    internal class EmployeesProject
+    {
+        [Key]
+        [ForeignKey(nameof(Employee))]
+        public int EmployeeId { get; set; }
+
+        [Key]
+        [ForeignKey(nameof(Departments))]
+        public int ProjectId { get; set; }
+
+        public Employee Employee { get; set; }  
+
+        public Project Project { get; set; }
+    }
+}
