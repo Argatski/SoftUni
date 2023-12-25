f7a3aa005ae02214d125b16dd6d7a10f08649e0e
Denis Argatski <denis.argatski@gmail.com>
2023-12-25 11:35:34 +0200

    Employee.cs

---
+++ C#/07.DataBases-Entity Framework Core/02.ORM/ORM-Exercises/MiniORMCore/MiniORM.App/Data/Entities/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniORM.App.Data.Entities
+{
+    internal class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public bool IsEmployed { get; set; }
+
+        [ForeignKey(nameof(Departments))]
+        public int DepartmentId { get; set; }
+
+        public Departments Departments { get; set; }
+        public ICollection<EmployeesProject> EmployeesProject { get; }
+
+    }
+}
