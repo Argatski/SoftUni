21835ddaa2d8a497d6ee50a0535dfd83e9b146f0
Denis Argatski <denis.argatski@gmail.com>
2023-12-25 11:35:48 +0200

    Project.cs

---
+++ C#/07.DataBases-Entity Framework Core/02.ORM/ORM-Exercises/MiniORMCore/MiniORM.App/Data/Entities/Project.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniORM.App.Data.Entities
+{
+    internal class Project
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        ICollection<EmployeesProject> EmployeesProject { get; }
+
+    }
+}
