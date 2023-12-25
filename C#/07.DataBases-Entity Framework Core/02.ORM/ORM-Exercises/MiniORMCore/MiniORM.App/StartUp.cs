4aa2f4bbd8e1ca937c6b0c42f4e432dbc7c01874
Denis Argatski <denis.argatski@gmail.com>
2023-12-25 11:36:33 +0200

    StartUp.cs

---
+++ C#/07.DataBases-Entity Framework Core/02.ORM/ORM-Exercises/MiniORMCore/MiniORM.App/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+
+internal class StartUp
+{
+    private static void Main(string[] args)
+    {
+        var connectionString = "Server=DESKTOP-P22IBOP\\MSSQLSERVER_2023;Database=MiniORM;Integrated Security=True;TrustServerCertificate=True";
+
+        var context =  new SoftUniDbContextClass(connectionString);
+
+        context.Employees.Add(new Employee
+        {
+            FirstName = "Gosho",
+            LastName = "Inserted",
+            DepartmentId = context.Departments.First().Id,
+            IsEmployed = true
+
+        });
+
+        var employee = context.Employees.Last();
+        employee.FirstName = "Modified";
+
+        context.SaveChanges();
+            
+         
+    }
+}
