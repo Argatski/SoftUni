d24111ed2200abeea6db05d6eff2e70e48556eb7
Denis Argatski <denis.argatski@gmail.com>
2023-12-25 11:35:19 +0200

    Departments.cs

---
+++ C#/07.DataBases-Entity Framework Core/02.ORM/ORM-Exercises/MiniORMCore/MiniORM.App/Data/Entities/Departments.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniORM.App.Data.Entities
+{
+    internal class Departments
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        ICollection<Employee> Employees { get; }
+    }
+}
