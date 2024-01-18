df60abeb80d038961679b1769c6fbef851285b34
Denis Argatski <denis.argatski@gmail.com>
2024-01-18 10:17:31 +0200

    StartUp.cs

---
+++ C#/07.DataBases-Entity Framework Core/03.EntityFrameworkCoreIntroduction/03.EntityFrameworkCoreIntroduction-Exercises/02.DatabaseFirst/StartUp.cs
@@ -0,0 +1,15 @@
+﻿
+namespace SoftUni    
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+
+/*PowerShell command:
+ dotnet ef dbcontext scaffold "Server=DESKTOP-P22IBOP\MSSQLSERVER_2023;Database=SoftUni;Integrated Security=True;TrustServerCertificate=True" Microsoft.EntityFrameworkCore.SqlServer -o Models
+ */
