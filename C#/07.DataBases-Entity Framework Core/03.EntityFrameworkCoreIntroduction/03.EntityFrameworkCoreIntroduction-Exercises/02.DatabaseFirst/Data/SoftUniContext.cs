e1315628b799c494df6a14137377647f6f639cce
Denis Argatski <denis.argatski@gmail.com>
2024-01-18 10:15:37 +0200

    SoftUniContext.cs

--- C#/07.DataBases-Entity Framework Core/03.EntityFrameworkCoreIntroduction/03.EntityFrameworkCoreIntroduction-Exercises/02.DatabaseFirst/Data/SoftUniContext.cs
+++ C#/07.DataBases-Entity Framework Core/03.EntityFrameworkCoreIntroduction/03.EntityFrameworkCoreIntroduction-Exercises/02.DatabaseFirst/Data/SoftUniContext.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using _02.DatabaseFirst.Models;
+﻿using SoftUni.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
 
-// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
-// If you have enabled NRTs for your project, then un-comment the following line:
-// #nullable disable
-
-namespace _02.DatabaseFirst.Data
+namespace SoftUni.Data
 {
     public partial class SoftUniContext : DbContext
     {
