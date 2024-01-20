8be61830e134fdeb7053d4048926b3e3b29274dd
Denis Argatski <denis.argatski@gmail.com>
2024-01-20 22:53:47 +0200

    .\05.EmployeesFromResearchAndDevelopment.csproj

---
+++ C#/07.DataBases-Entity Framework Core/03.EntityFrameworkCoreIntroduction/05.EmployeesFromResearchAndDevelopment/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System.Text;
+
+namespace SoftUni
+{
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (SoftUniContext context = new SoftUniContext())
+            {
+                string result = GetEmployeesFromResearchAndDevelopment(context);
+
+                Console.WriteLine(result);
+            }
+
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var str = new StringBuilder();
+
+            var employees = context.Employees
+                .Select(
+                e => new 
+                { 
+                    e.FirstName,
+                    e.LastName,
+                    DepartmentName = e.Department.Name,
+                    e.Salary }
+                )
+                .Where(e => e.DepartmentName == "Research and Development")
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .ToList();
+
+            foreach ( var employee in employees )
+            {
+                str.AppendLine($"{employee.FirstName} {employee.LastName} from {employee.DepartmentName} - {employee.Salary:f2}");
+            }
+
+            return str.ToString().TrimEnd();
+        }
+    }
+}
