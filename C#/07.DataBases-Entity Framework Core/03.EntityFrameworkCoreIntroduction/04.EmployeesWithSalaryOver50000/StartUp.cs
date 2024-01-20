1da42e4872c2dc5a51cdfbfa4f6ee894beead57a
Denis Argatski <denis.argatski@gmail.com>
2024-01-20 22:49:32 +0200

    .\04.EmployeesWithSalaryOver50000.csproj

---
+++ C#/07.DataBases-Entity Framework Core/03.EntityFrameworkCoreIntroduction/04.EmployeesWithSalaryOver50000/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace SoftUni
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    using SoftUni.Data;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var context = new SoftUniContext())
+            {
+                var employeesFullInfo = GetEmployeesWithSalaryOver50000(context);
+
+                Console.WriteLine(employeesFullInfo);
+            }
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var result = new StringBuilder();
+
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                }).Where(e=>e.Salary>50000)
+                .OrderBy(e => e.FirstName)
+                .ToList();
+
+            foreach (var employee in employees)
+            {
+                result.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
