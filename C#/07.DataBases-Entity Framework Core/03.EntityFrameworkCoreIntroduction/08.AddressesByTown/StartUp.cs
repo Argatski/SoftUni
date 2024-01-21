a3a5427b401aa85fc9bd680510acac9ad90e2392
Denis Argatski <denis.argatski@gmail.com>
2024-01-21 22:15:16 +0200

    .\StartUp.cs

---
+++ C#/07.DataBases-Entity Framework Core/03.EntityFrameworkCoreIntroduction/08.AddressesByTown/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace SoftUni
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    using SoftUni.Data;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var context = new SoftUniContext())
+            {
+                string result = GetAddressesByTown(context);
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+            var stb = new StringBuilder();
+
+            var addreses = context.Addresses
+                .Select
+                (
+                   a => new { a.AddressText, TownName = a.Town.Name, count = a.Employees.Count }
+                ).OrderByDescending(c=>c.count).ThenBy(c=>c.TownName).ThenBy(c=>c.AddressText);
+
+
+            foreach ( var address in addreses )
+            {
+                stb.AppendLine($"{address.AddressText}, {address.TownName} - {address.count} employees");
+            }
+
+            return stb.ToString().TrimEnd();
+        }
+    }
+}
