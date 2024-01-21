4820fb187c96a51df2e7f113c815e6904c6dcccb
Denis Argatski <denis.argatski@gmail.com>
2024-01-21 22:18:05 +0200

    .\StartUp.cs

---
+++ C#/07.DataBases-Entity Framework Core/03.EntityFrameworkCoreIntroduction/09.Employee147/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (SoftUniContext context = new SoftUniContext())
+            {
+                var result = GetEmployee147(context);
+
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetEmployee147(SoftUniContext context)
+        {
+
+            var employee = context.Employees
+                            .Where(e => e.EmployeeId == 147)
+                            .Select(e => new
+                            {
+                                e.FirstName,
+                                e.LastName,
+                                e.JobTitle,
+                                e.Projects
+                                    
+                            })
+                            .FirstOrDefault();
+
+           
+
+            StringBuilder sb = new StringBuilder();
+
+           sb.AppendLine($"{employee.FirstName} {employee.LastName} - {employee.JobTitle}");
+            foreach (var project in employee.Projects.OrderBy(p=>p.Name))
+            {
+                sb.AppendLine(project.Name);
+            }
+
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
