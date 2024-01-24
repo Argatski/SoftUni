6a2ca51582795c085490a1a8424a01548046d0ba
Denis Argatski <denis.argatski@gmail.com>
2024-01-24 21:56:30 +0200

    .\StartUp.cs

---
+++ C#/07.DataBases-Entity Framework Core/03.EntityFrameworkCoreIntroduction/11.FindLatest10Projects/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using System;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (SoftUniContext context = new SoftUniContext())
+            {
+                string result = GetLatestProjects(context);
+                Console.WriteLine(result);
+            }
+           
+        }
+
+        public static string GetLatestProjects(SoftUniContext context)
+        {
+            var projects =  context.Projects
+                .OrderByDescending(p=>p.StartDate)
+                .Select(p=> new { p.Name,p.Description,p.StartDate })
+                .Take(10)
+                .OrderBy(p=>p.Name)
+                .ToList();
+
+            var stb = new StringBuilder();
+
+            foreach (var project in projects) 
+            {
+                stb.AppendLine(project.Name);
+                stb.AppendLine(project.Description);
+                stb.AppendLine(project.StartDate.ToString("M/d/yyyy h:mm:ss tt",CultureInfo.InvariantCulture));
+            }
+
+            return stb.ToString().TrimEnd();
+        }
+    }
+}
