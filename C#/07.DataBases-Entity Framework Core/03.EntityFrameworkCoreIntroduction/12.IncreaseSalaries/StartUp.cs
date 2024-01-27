4db302da527099eaef4135a43f821c8992a111e9
Denis Argatski <denis.argatski@gmail.com>
2024-01-27 22:26:04 +0200

    .\StartUp.cs

---
+++ C#/07.DataBases-Entity Framework Core/03.EntityFrameworkCoreIntroduction/12.IncreaseSalaries/StartUp.cs
@@ -0,0 +1,54 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using System;
+    using System.Globalization;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (SoftUniContext context = new SoftUniContext())
+            {
+                string result = IncreaseSalaries(context);
+
+                Console.WriteLine(result);
+            }
+
+        }
+
+        public static string IncreaseSalaries(SoftUniContext context)
+        {
+            var deparments = context.Employees.Where(d => d.Department.Name == "Engineering" || d.Department.Name == "Tool Design"
+                    || d.Department.Name == "Marketing"
+                    || d.Department.Name == "Information Services").ToList();
+
+            foreach (var d in deparments)
+            {
+                d.Salary += d.Salary * 0.12m;
+            }
+
+
+            context.SaveChanges();
+
+
+            var updatedEmployyes = context.Employees
+                .Where(d => d.Department.Name == "Engineering" || d.Department.Name == "Tool Design"
+                    || d.Department.Name == "Marketing"
+                    || d.Department.Name == "Information Services")
+                .Select(s => new { s.FirstName, s.LastName, s.Salary })
+                .OrderBy(s => s.FirstName)
+                .ThenBy(s => s.LastName).ToList();
+
+            StringBuilder stb = new StringBuilder();
+
+            foreach (var emp in updatedEmployyes)
+            {
+                stb.AppendLine($"{emp.FirstName} {emp.LastName} (${emp.Salary:f2})");
+            }
+
+            return stb.ToString().TrimEnd();
+        }
+    }
+}
